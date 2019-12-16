29deddd97371c656f9b3f0ce70a38fa3d9c23f67
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2019-12-16 13:36:54 +0000

    US274 - Technical Story - To create a system framework for the Results and certification framework Setting up the domain - Added domain project with BaseEntity

---
+++ src/Sfa.Tl.ResultsAndCertification.Domain/Models/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sfa.Tl.ResultsAndCertification.Domain.Models
+{
+    public class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedOn { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public string ModifiedBy { get; set; }
+    }
+}
--- src/Sfa.Tl.ResultsAndCertification.Web/Startup.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -39,18 +34,15 @@ namespace Sfa.Tl.ResultsAndCertification.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
