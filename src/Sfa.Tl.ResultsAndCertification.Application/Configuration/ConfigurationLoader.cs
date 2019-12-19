736e2e1a60d0c8ce454098a22578fd9884789a1d
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2019-12-19 14:51:40 +0000

    US274 - Technical Story - To create a system framework for the Results and certification framework Setting up the application layer - Created application project and implemented configuration load to retrive configuration details form Azure

---
+++ src/Sfa.Tl.ResultsAndCertification.Application/Configuration/ConfigurationLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+
+namespace Sfa.Tl.ResultsAndCertification.Application.Configuration
+{
+    public static class ConfigurationLoader
+    {
+        public static object Load(string environment, string storageConnectionString,
+            string version, string serviceName)
+        {
+            try
+            {
+                var conn = CloudStorageAccount.Parse(storageConnectionString);
+                var tableClient = conn.CreateCloudTableClient();
+                var table = tableClient.GetTableReference("Configuration");
+
+                var operation = TableOperation.Retrieve(environment, $"{serviceName}_{version}");
+                var result = table.ExecuteAsync(operation).GetAwaiter().GetResult();
+
+                var dynResult = result.Result as DynamicTableEntity;
+                var data = dynResult?.Properties["Data"].StringValue;
+
+                return JsonConvert.DeserializeObject(data);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Configuration could not be loaded. Please check your configuration files or see the inner exception for details", ex);
+            }
+        }
+    }
+}
