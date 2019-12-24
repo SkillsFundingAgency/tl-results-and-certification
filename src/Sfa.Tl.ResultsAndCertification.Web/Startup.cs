4330771915cb9ebf5d68cb3956b84859b0c92111
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2019-12-24 13:50:02 +0000

    TLRC-148 - Implement the start page as per approved layout templated - Implemented StartPage content and layout

--- src/Sfa.Tl.ResultsAndCertification.Web/Startup.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,7 +19,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
