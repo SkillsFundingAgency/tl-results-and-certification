7ac720324bf541bcece78f725502e0df07c5d0f1
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2019-12-27 13:44:02 +0000

    Integrating DfE Sign-In with R&C Portal - Added Models project and added configuration models

---
+++ src/Sfa.Tl.ResultsAndCertification.Models/Configuration/DfeSignInSettings.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Sfa.Tl.ResultsAndCertification.Models.Configuration
+{
+    public class DfeSignInSettings
+    {
+        public string MetadataAddress { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string CallbackPath { get; set; }
+        public string SignedOutCallbackPath { get; set; }
+        public string LogoutPath { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string APISecret { get; set; }
+        public string APIUri { get; set; }
+        public string TokenEndpoint { get; set; }
+        public string Authority { get; set; }
+    }
+}
---
+++ src/Sfa.Tl.ResultsAndCertification.Models/Configuration/ResultsAndCertificationConfiguration.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Sfa.Tl.ResultsAndCertification.Models.Configuration
+{
+    public class ResultsAndCertificationConfiguration
+    {
+        public string BlobStorageConnectionString { get; set; }
+
+        public string SqlConnectionString { get; set; }
+
+        public DfeSignInSettings DfeSignInSettings { get; set; }
+
+        public string ResultsAndCertificationInternalApiUri { get; set; }
+    }
+}
