d0d49d0f2cb1ceec03316a37e7bce689ef50cc4d
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-01-13 15:18:49 +0000

    TLRC - 174 - Error Page - Generic Error Page   - TLRC - 207 - Dev - Implemented Generic Error Functionality

---
+++ src/Sfa.Tl.ResultsAndCertification.Web/Filters/CustomExceptionFilterAttribute.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Sfa.Tl.ResultsAndCertification.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.Filters
+{
+    public class CustomExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        private readonly ILogger _logger;
+
+        public CustomExceptionFilterAttribute(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<CustomExceptionFilterAttribute>();
+        }
+
+        public override void OnException(ExceptionContext context)
+        {
+            _logger.LogError(default(int), context.Exception, context.Exception.Message);
+
+            var result = new ViewResult { ViewName = "~/Views/Home/Error.cshtml" };
+            var modelMetadata = new EmptyModelMetadataProvider();
+            result.ViewData = new ViewDataDictionary(modelMetadata, context.ModelState)
+            {
+                {
+                    "Exception", context.Exception
+                }
+            };
+
+            result.ViewData.Model = new ErrorViewModel { RequestId = Activity.Current?.Id ?? context.HttpContext.TraceIdentifier };
+            result.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Result = result;
+            context.ExceptionHandled = true;
+        }
+    }
+}
