0990259bbab75613a70d43ecb49fa95067e1dfa6
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-01-15 14:47:21 +0000

    TLRC - 169 - Error Page - Access Denied   - TLRC - 188 - Dev - Implemented Access Denied Page

--- src/Sfa.Tl.ResultsAndCertification.Web/Controllers/HomeController.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Controllers/HomeController.cs
@@ -21,6 +21,11 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Controllers
             return View();
         }
 
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
