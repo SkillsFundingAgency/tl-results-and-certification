32ab3c5033611e62effcdae8afcfb5f8415dd102
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-01-17 12:02:23 +0000

    TLRC - 174 - Error Page - Generic Error Page - Moved logic to ErrorController

--- src/Sfa.Tl.ResultsAndCertification.Web/Startup.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Startup.cs
@@ -71,7 +71,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error/500");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
