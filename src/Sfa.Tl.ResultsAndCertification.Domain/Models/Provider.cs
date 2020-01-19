bd749f9ef6f04fdf302d6795b9cb9c1aee54cd03
Ravi Bobbili <ravi.bob@outlook.com>
2020-01-19 11:35:31 +0000

    Tech - Unit test framework - initial version.

---
+++ src/Sfa.Tl.ResultsAndCertification.Domain/Models/Provider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sfa.Tl.ResultsAndCertification.Domain.Models
+{
+    public partial class Provider : BaseEntity
+    {
+        public Provider()
+        {
+            TqProvider = new HashSet<TqProvider>();
+        }
+
+        public int Id { get; set; }
+        public long UkPrn { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public bool IsTlevelProvider { get; set; }
+
+        public virtual ICollection<TqProvider> TqProvider { get; set; }
+    }
+}
