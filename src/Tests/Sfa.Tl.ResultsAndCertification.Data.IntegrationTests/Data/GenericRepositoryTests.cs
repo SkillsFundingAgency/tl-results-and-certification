ac2978d6043c0c7a4174408dce85713bd3cd21f2
Ravi Bobbili <ravi.bob@outlook.com>
2020-01-28 09:20:42 +0000

    TLRC-216 - IntegrationTest project name renamed and Test has been added.

--- src/Tests/Sfa.Tl.ResultsAndCertification.Data.IntegrationTests/Data/GenericRepositoryTests.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Data.IntegrationTests/Data/GenericRepositoryTests.cs
@@ -21,7 +21,7 @@ namespace Sfa.Tl.ResultsAndCertification.Data.IntegrationTests.Data
         }
 
         [Fact]
-        public async Task GetTqProvidersOfGivenAwardingOrganisation()
+        public async Task GetTqProvidersByAwardingOrganisation()
         {
             // 1. Build Provider, Ao and TlProvider data
             using var context = CreateDbContext("GetTqProvidersOfGivenAwardingOrganisation");
@@ -49,6 +49,99 @@ namespace Sfa.Tl.ResultsAndCertification.Data.IntegrationTests.Data
             // 4. Assert results. 
             Assert.NotNull(result);
             Assert.True(result.Count() == 0);
+        }
+
+
+        [Fact(Skip = "This is not fully functional.")]
+        public async Task GetQualificationsByAwardingOrganisation()
+        {
+            // Input: "AOID"
+            // Output[i.e. Qualification]: "RouteName: PathwayName" 
+
+            // 1. Build Provider, Ao and TlProvider data
+            using var context = CreateDbContext("GetQualificationsByAwardingOrganisation");
+
+            // TODO: Write/Use Builders
+            await CreateRoutes(context);
+            await CreatePathways(context);
+            await CreateSpecialisms(context);
+            await CreateProviders(context);
+
+            var inputAoId = 1;
+
+            //var result = context.TqProvider.Where(x => x.AwardingOrganisationId == inputAoId)
+            //    .Select(x => x.Route.Name + ": " + x.Pathway.Name);
+
+            //Assert.Equal("Construction: Design, Surveying and Planning", result.FirstOrDefault());
+
+            var genericRepository = new GenericRepository<TqProvider>(mockLogger.Object, context);
+            
+            //IEnumerable<TqProvider> result = await genericRepository.GetManyAsync(x => x.AwardingOrganisation.Name.Equals(inputAoId));
+        }
+
+        private async Task CreateProviders(ResultsAndCertificationDbContext context)
+        {
+            await context.TqProvider.AddRangeAsync(new List<TqProvider>
+            {
+                new TqProvider { AwardingOrganisationId  = 1, ProviderId = 1, RouteId = 1, PathwayId = 1, SpecialismId = 1, CreatedBy = "System", CreatedOn = DateTime.UtcNow },
+            });
+
+            await context.SaveChangesAsync();
+        }
+
+        private async Task CreateSpecialisms(ResultsAndCertificationDbContext context)
+        {
+            await context.TlSpecialism.AddRangeAsync(new List<TlSpecialism>
+            {
+                new TlSpecialism {  Id = 1, Name ="Surveying and design for construction and the built environment", PathwayId = 1, CreatedBy = "Sys", CreatedOn = DateTime.UtcNow },
+                new TlSpecialism {  Id = 2, Name ="Civil Engineering", PathwayId = 1, CreatedBy = "Sys", CreatedOn = DateTime.UtcNow },
+                new TlSpecialism {  Id = 3, Name ="Building services design", PathwayId = 1, CreatedBy = "Sys", CreatedOn = DateTime.UtcNow },
+                new TlSpecialism {  Id = 4, Name ="Hazardous materials analysis and surveying", PathwayId = 1, CreatedBy = "Sys", CreatedOn = DateTime.UtcNow },
+                new TlSpecialism {  Id = 5, Name ="Early years education and childcare", PathwayId = 2, CreatedBy = "Sys", CreatedOn = DateTime.UtcNow },
+                new TlSpecialism {  Id = 6, Name ="Assisting teaching", PathwayId = 2, CreatedBy = "Sys", CreatedOn = DateTime.UtcNow },
+                new TlSpecialism {  Id = 7, Name ="Supporting and mentoring students in further and higher education", PathwayId = 2, CreatedBy = "Sys", CreatedOn = DateTime.UtcNow },
+                new TlSpecialism {  Id = 8, Name ="Digital Production, Design and Development", PathwayId = 3, CreatedBy = "Sys", CreatedOn = DateTime.UtcNow },
+            });
+
+            await context.SaveChangesAsync();
+        }
+
+        private async Task CreatePathways(ResultsAndCertificationDbContext context)
+        {
+            var data = new List<TlPathway>
+            {
+                new TlPathway {  Id = 1, RouteId = 1, LarId = "10123456", Name = "Design, Surveying and Planning", CreatedBy = "Sys", CreatedOn = DateTime.UtcNow },
+                new TlPathway {  Id = 2, RouteId = 2, LarId = "10223456", Name = "Education", CreatedBy = "Sys", CreatedOn = DateTime.UtcNow },
+                new TlPathway {  Id = 3, RouteId = 3, LarId = "10223456", Name = "Digital Production, Design and Development", CreatedBy = "Sys", CreatedOn = DateTime.UtcNow }
+            };
+
+            var mockLogger = new Mock<ILogger<GenericRepository<TlPathway>>>();
+            var genericRepository = new GenericRepository<TlPathway>(mockLogger.Object, context);
+            await genericRepository.CreateManyAsync(data);
+
+            //await context.TlPathway.AddRangeAsync(data);
+            //context.Entry<IEnumerable<TlPathway>>(data).State = EntityState.Detached;
+            //await context.SaveChangesAsync();
+        }
+
+        private async Task CreateRoutes(ResultsAndCertificationDbContext context)
+        {
+            var data = new List<TlRoute>
+            {
+                new TlRoute { Id = 1, Name = "Construction", CreatedBy = "Sys", CreatedOn = DateTime.UtcNow },
+                new TlRoute { Id = 2, Name = "Education and Childcare", CreatedBy = "Sys", CreatedOn = DateTime.UtcNow },
+                new TlRoute { Id = 3, Name = "Digital", CreatedBy = "Sys", CreatedOn = DateTime.UtcNow },
+            };
+
+            var mockLogger = new Mock<ILogger<GenericRepository<TlRoute>>>();
+            var genericRepository = new GenericRepository<TlRoute>(mockLogger.Object, context);
+            await genericRepository.CreateManyAsync(data);
+
+            //await context.TlRoute.AddRangeAsync(data);
+
+            //context.Entry<IEnumerable<TlRoute>>(data).State = EntityState.Detached;
+
+            //await context.SaveChangesAsync();
         }
 
         private async Task CreateOneTqProvider(ResultsAndCertificationDbContext context)
@@ -87,6 +180,7 @@ namespace Sfa.Tl.ResultsAndCertification.Data.IntegrationTests.Data
         {
             var options = new DbContextOptionsBuilder<ResultsAndCertificationDbContext>()
                 .UseInMemoryDatabase(dbName)
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
                 .Options;
 
             return new ResultsAndCertificationDbContext(options);
