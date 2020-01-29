5c9b5649fd57da90333d3db1c69dde5db76da695
Ravi Bobbili <ravi.bob@outlook.com>
2020-01-29 16:03:54 +0000

    TLRC-216 - Unit Tests are added for TlPathway entity

---
+++ src/Sfa.Tl.ResultsAndCertification.Tests.Common/DataBuilders/TlPathwayBuilder.cs
@@ -0,0 +1,58 @@
+﻿using Sfa.Tl.ResultsAndCertification.Tests.Common.Helpers;
+using System.Collections.Generic;
+
+namespace Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders
+{
+    public class TlPathwayBuilder
+    {
+        public Domain.Models.TlPathway Build() => new Domain.Models.TlPathway
+        {
+            Id = 1,
+            RouteId = 1,
+            Name = "Design, Surveying and Planning",
+            LarId = "10123456",
+            CreatedBy = Constants.CreatedByUser,
+            CreatedOn = Constants.CreatedOn,
+            ModifiedBy = Constants.ModifiedByUser,
+            ModifiedOn = Constants.ModifiedOn
+        };
+
+        public IList<Domain.Models.TlPathway> BuildList() => new List<Domain.Models.TlPathway>
+        {
+            new Domain.Models.TlPathway
+            {
+                Id = 1,
+                RouteId = 1,
+                Name = "Design, Surveying and Planning",
+                LarId = "10123456",
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            },
+
+            new Domain.Models.TlPathway
+            {
+                Id = 2,
+                RouteId = 2,
+                Name = "Education",
+                LarId = "10123456",
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            },
+            new Domain.Models.TlPathway
+            {
+                Id = 3,
+                RouteId = 3,
+                Name = "Digital Production, Design and Development",
+                LarId = "10123456",
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            }
+        };
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Data.UnitTests/Repositories/Pathway/When_PathRepository_CreateMany_Is_Called.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Data.UnitTests.Repositories.Pathway
+{
+    public class When_PathRepository_CreateMany_Is_Called : BaseTest<TlPathway>
+    {
+        private IList<TlPathway> _data;
+        private int _result;
+
+        public override void Given()
+        {
+            _data = new TlPathwayBuilder().BuildList();
+        }
+
+        public override void When()
+        {
+            _result = Repository.CreateManyAsync(_data).GetAwaiter().GetResult();
+        }
+
+        [Fact]
+        public void Then_Two_Record_Should_Have_Been_Created() =>
+            _result.Should().Be(3);
+    }
+}
