929aa0f49b3599631ff461f23f08c419477ee401
Ravi Bobbili <ravi.bob@outlook.com>
2020-01-30 10:27:29 +0000

    TLRC-216 - Technology Story to Create Unit Test Framework              TLRC-244 - Unit test for Specialism added.

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Data.UnitTests/Repositories/Specialism/When_SpecialismRepository_CreateMany_Is_Called.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Data.UnitTests.Repositories.Specialism
+{
+    public class When_SpecialismRepository_CreateMany_Is_Called : BaseTest<TlSpecialism>
+    {
+        private IList<TlSpecialism> _data;
+        private int _result;
+
+        public override void Given()
+        {
+            _data = new TlSpecialismBuilder().BuildList();
+        }
+
+        public override void When()
+        {
+            _result = Repository.CreateManyAsync(_data).GetAwaiter().GetResult();
+        }
+
+        [Fact]
+        public void Then_Two_Record_Should_Have_Been_Created() =>
+            _result.Should().Be(8);
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Data.UnitTests/Repositories/Specialism/When_SpecialismRepository_GetMany_Is_Called.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Data.UnitTests.Repositories.Specialism
+{
+    public class When_SpecialismRepository_GetMany_Is_Called : BaseTest<TlSpecialism>
+    {
+        private IEnumerable<TlSpecialism> _result;
+        private IEnumerable<TlSpecialism> _data;
+
+        public override void Given()
+        {
+            _data = new TlSpecialismBuilder().BuildList();
+            DbContext.AddRange(_data);
+            DbContext.SaveChanges();
+        }
+
+        public override void When()
+        {
+            _result = Repository.GetManyAsync().ToList();
+        }
+
+        [Fact]
+        public void Then_Results_Not_Null()
+        {
+            _result.Should().NotBeNull();
+        }
+
+
+        [Fact]
+        public void Then_The_Expected_Number_Of_Paths_Is_Returned()
+        {
+            _result.Count().Should().Be(8);
+        }
+
+        [Fact]
+        public void Then_EntityFields_Are_As_Expected()
+        {
+            var expectedResult = _data.FirstOrDefault();
+            var actualResult = _result.FirstOrDefault();
+            
+            expectedResult.Should().NotBeNull();
+            actualResult.Should().NotBeNull();
+            actualResult.Id.Should().Be(1);
+            actualResult.Name.Should().BeEquivalentTo(expectedResult.Name);
+            actualResult.LarId.Should().BeEquivalentTo(expectedResult.LarId);
+            actualResult.PathwayId.Should().Be(expectedResult.PathwayId);
+            actualResult.CreatedBy.Should().BeEquivalentTo(expectedResult.CreatedBy);
+            actualResult.CreatedOn.Should().Be(expectedResult.CreatedOn);
+            actualResult.ModifiedBy.Should().BeEquivalentTo(expectedResult.ModifiedBy);
+            actualResult.ModifiedOn.Should().Be(expectedResult.ModifiedOn);
+        }
+    }
+}
