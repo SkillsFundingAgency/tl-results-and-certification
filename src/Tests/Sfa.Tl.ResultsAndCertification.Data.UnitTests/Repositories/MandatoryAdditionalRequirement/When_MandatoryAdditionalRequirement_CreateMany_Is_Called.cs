ce0fe52325c26078ee775dc6283ca9a9c2c90666
Ravi Bobbili <ravi.bob@outlook.com>
2020-01-30 15:35:15 +0000

    TLRC-216 - Technology Story to Create Unit Test Framework              TLRC-247 - Unit test for TlMandatoryAdditionalRequirement are added.

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Data.UnitTests/Repositories/MandatoryAdditionalRequirement/When_MandatoryAdditionalRequirement_CreateMany_Is_Called.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Data.UnitTests.Repositories.Pathway
+{
+    public class When_MandatoryAdditionalRequirement_CreateMany_Is_Called : BaseTest<TlMandatoryAdditionalRequirement>
+    {
+        private IList<TlMandatoryAdditionalRequirement> _data;
+        private int _result;
+
+        public override void Given()
+        {
+            _data = new TlMandatoryAdditionalRequirementBuilder().BuildList();
+        }
+
+        public override void When()
+        {
+            _result = Repository.CreateManyAsync(_data).GetAwaiter().GetResult();
+        }
+
+        [Fact]
+        public void Then_Two_Record_Should_Have_Been_Created() =>
+            _result.Should().Be(8);
+    }
+}
