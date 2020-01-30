8402b63853b0a2a2b75578f60905ac3de4dad663
Ravi Bobbili <ravi.bob@outlook.com>
2020-01-30 15:35:15 +0000

    TLRC-216 - Technology Story to Create Unit Test Framework              TLRC-247 - Unit test for TlMandatoryAdditionalRequirement are added.

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Data.UnitTests/Repositories/MandatoryAdditionalRequirement/When_MandatoryAdditionalRequirement_SingleOrDefault_Is_Called.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Data.UnitTests.Repositories.Pathway
+{
+    public class When_MandatoryAdditionalRequirement_SingleOrDefault_Is_Called : BaseTest<TlMandatoryAdditionalRequirement>
+    {
+        private TlMandatoryAdditionalRequirement _result;
+        private TlMandatoryAdditionalRequirement _data;
+
+        public override void Given()
+        {
+            _data = new TlMandatoryAdditionalRequirementBuilder().Build();
+            DbContext.Add(_data);
+            DbContext.SaveChanges();
+        }
+
+        public override void When()
+        {
+            _result = Repository.GetSingleOrDefaultAsync(x => x.Id == 1).GetAwaiter().GetResult();
+        }
+
+        [Fact]
+        public void Then_Fields_Are_As_Expected()
+        {
+            _result.Should().NotBeNull();
+            _result.Id.Should().Be(1);
+            _result.Name.Should().BeEquivalentTo(_data.Name);
+            _result.LarId.Should().BeEquivalentTo(_data.LarId);
+            _result.IsRegulatedQualification.Should().Be(_data.IsRegulatedQualification);
+            _result.CreatedBy.Should().BeEquivalentTo(_data.CreatedBy);
+            _result.CreatedOn.Should().Be(_data.CreatedOn);
+            _result.ModifiedBy.Should().BeEquivalentTo(_data.ModifiedBy);
+            _result.ModifiedOn.Should().Be(_data.ModifiedOn);
+        }
+    }
+}
