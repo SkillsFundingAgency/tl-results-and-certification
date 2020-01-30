259cfb07bf0fcdda22ab1e568c4c7ae84b41ed8e
Ravi Bobbili <ravi.bob@outlook.com>
2020-01-30 14:51:03 +0000

    TLRC-216 - Technology Story to Create Unit Test Framework              TLRC-246 - Unit test for PathwaySpecialismCombination are added.

---
+++ src/Sfa.Tl.ResultsAndCertification.Tests.Common/DataBuilders/TlPathwaySpecialismCombinationBuilder.cs
@@ -0,0 +1,111 @@
+﻿using Sfa.Tl.ResultsAndCertification.Tests.Common.Helpers;
+using System.Collections.Generic;
+
+namespace Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders
+{
+    public class TlPathwaySpecialismCombinationBuilder
+    {
+        public Domain.Models.TlPathwaySpecialismCombination Build() => new Domain.Models.TlPathwaySpecialismCombination
+        {
+            Id = 1,
+            PathwayId = 1,
+            SpecialismId = 1,
+            Group = "G1",
+            CreatedBy = Constants.CreatedByUser,
+            CreatedOn = Constants.CreatedOn,
+            ModifiedBy = Constants.ModifiedByUser,
+            ModifiedOn = Constants.ModifiedOn
+        };
+        public IList<Domain.Models.TlPathwaySpecialismCombination> BuildList() => new List<Domain.Models.TlPathwaySpecialismCombination>
+        {
+            new Domain.Models.TlPathwaySpecialismCombination
+            {
+                Id = 1,
+                PathwayId = 1,
+                SpecialismId = 1,
+                Group = "G1",
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            },
+            new Domain.Models.TlPathwaySpecialismCombination
+            {
+                Id = 2,
+                PathwayId = 1,
+                SpecialismId = 2,
+                Group = "G1",
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            },
+            new Domain.Models.TlPathwaySpecialismCombination
+            {
+                Id = 3,
+                PathwayId = 1,
+                SpecialismId = 3,
+                Group = "G2",
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            },
+            new Domain.Models.TlPathwaySpecialismCombination
+            {
+                Id = 4,
+                PathwayId = 1,
+                SpecialismId = 4,
+                Group = "G2",
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            },
+            new Domain.Models.TlPathwaySpecialismCombination
+            {
+                Id = 5,
+                PathwayId = 2,
+                SpecialismId = 5,
+                Group = "G1",
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            },
+            new Domain.Models.TlPathwaySpecialismCombination
+            {
+                Id = 6,
+                PathwayId = 2,
+                SpecialismId = 6,
+                Group = "G1",
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            },
+            new Domain.Models.TlPathwaySpecialismCombination
+            {
+                Id = 7,
+                PathwayId = 2,
+                SpecialismId = 7,
+                Group = "G1",
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            },
+            new Domain.Models.TlPathwaySpecialismCombination
+            {
+                Id = 8,
+                PathwayId = 3,
+                SpecialismId = 8,
+                Group = "G1",
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            }
+        };
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Data.UnitTests/Repositories/PathwaySpecialismCombination/When_PathwaySpecialismCombinationRepository_CreateMany_Is_Called.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Data.UnitTests.Repositories.PathwaySpecialismCombination
+{
+    public class When_PathwaySpecialismCombinationRepository_CreateMany_Is_Called : BaseTest<TlPathwaySpecialismCombination>
+    {
+        private IList<TlPathwaySpecialismCombination> _data;
+        private int _result;
+
+        public override void Given()
+        {
+            _data = new TlPathwaySpecialismCombinationBuilder().BuildList();
+        }
+
+        public override void When()
+        {
+            _result = Repository.CreateManyAsync(_data).GetAwaiter().GetResult();
+        }
+
+        [Fact]
+        public void Then_Two_Record_Should_Have_Been_Created() =>
+            _result.Should().Be(8);
+    }
+}
