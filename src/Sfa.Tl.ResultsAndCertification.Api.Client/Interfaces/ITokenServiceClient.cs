e7eca0bb3de3b389931e72b30a7191b3e0b92965
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-02-07 10:20:30 +0000

    TLRC - 284- Create Your T Levels Page   - TLRC - 302 - Dev - Initial checkin for Api Client Implementation

---
+++ src/Sfa.Tl.ResultsAndCertification.Api.Client/Interfaces/ITokenServiceClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sfa.Tl.ResultsAndCertification.Api.Client.Interfaces
+{
+    public interface ITokenServiceClient
+    {
+        string GetToken();
+    }
+}
