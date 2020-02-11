a2a291ff22db954fe29e63bcffa7e84cd3416058
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-02-11 09:46:20 +0000

    TLRC - 284- Create Your T Levels Page   - TLRC - 302 - Dev - Implemented ExceptionHandlerMiddleware for internal api

---
+++ src/Sfa.Tl.ResultsAndCertification.Api/Infrastructure/ApiResponse.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace Sfa.Tl.ResultsAndCertification.InternalApi.Infrastructure
+{
+    public class ApiResponse
+    {
+        public int StatusCode { get; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Message { get; }
+
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        private static string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    return "Resource not found";
+                case 500:
+                    return "An unhandled error occurred";
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ src/Sfa.Tl.ResultsAndCertification.Api/Infrastructure/BadRequestResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Sfa.Tl.ResultsAndCertification.InternalApi.Infrastructure
+{
+    public class BadRequestResponse : ApiResponse
+    {
+
+        public BadRequestResponse() : base(400)
+        {
+        }
+
+        public BadRequestResponse(string message) : base(400, message)
+        {
+        }
+
+        public IEnumerable<string> Errors { get; }
+
+        public BadRequestResponse(ModelStateDictionary modelState) : base(400)
+        {
+            if (modelState.IsValid)
+            {
+                throw new ArgumentException("ModelState must be invalid", nameof(modelState));
+            }
+
+            Errors = modelState.SelectMany(x => x.Value.Errors)
+                .Select(x => x.ErrorMessage).ToArray();
+        }
+    }
+}
