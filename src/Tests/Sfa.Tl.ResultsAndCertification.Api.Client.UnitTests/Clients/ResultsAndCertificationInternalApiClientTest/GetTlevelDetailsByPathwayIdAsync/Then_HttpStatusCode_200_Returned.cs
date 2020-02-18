39610713038b3d90babece4ac44afb1eb353dc04
Ravi Bobbili <ravi.bob@outlook.com>
2020-02-18 08:47:30 +0000

    TLRC-285 Create 'T Level Details' page   - TLRC-336 - Dev - Unit Test - Api Client test for GetTLevelDetailsyPathwayIdAsync

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Api.Client.UnitTests/Clients/ResultsAndCertificationInternalApiClientTest/GetTlevelDetailsByPathwayIdAsync/Then_HttpStatusCode_200_Returned.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Api.Client.UnitTests.Clients.GetTlevelDetailsByPathwayIdAsync;
+using System.Linq;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Api.Client.UnitTests.Clients.ResultsAndCertificationInternalApiClientTest.GetTlevelDetailsByPathwayIdAsync
+{
+    public class Then_HttpStatusCode_200_Returned : When_GetTlevelDetailsByPathwayIdAsync_Is_Called
+    {
+        [Fact]
+        public void Then_Expected_Result_Returned()
+        {
+            var expectedResult = Result.Result;
+
+            expectedResult.Should().NotBeNull();
+            expectedResult.RouteName.Should().Be(RouteName);
+            expectedResult.PathwayName.Should().Be(PathwayName);
+            expectedResult.PathwayStatusId.Should().Be(Status);
+
+            expectedResult.Specialisms.Should().NotBeNullOrEmpty();
+            expectedResult.Specialisms.Count().Should().Be(2);
+            expectedResult.Specialisms.First().Should().Be(Specialisms.First());
+        }
+    }
+}
