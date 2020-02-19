936dca41923f797adea75395d15780ade2cc66b9
Ravi Bobbili <ravi.bob@outlook.com>
2020-02-19 10:57:28 +0000

    TLRC-285 Create 'T Level Details' page   - TLRC-336 - Dev - Unit Test -  Updated

--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/TlevelLoaderTests/GetAllTlevelsByUkprnAsync/Then_ApiClient_Mapper_Is_Called.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/TlevelLoaderTests/GetAllTlevelsByUkprnAsync/Then_ApiClient_Mapper_Is_Called.cs
@@ -4,20 +4,21 @@ using System.Collections.Generic;
 using Sfa.Tl.ResultsAndCertification.Models.Contracts;
 using Sfa.Tl.ResultsAndCertification.Web.Models;
 
-namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.TlevelLoaderTests
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.TlevelLoaderTests.GetAllTlevelsByUkprnAsync
 {
     public class Then_ApiClient_Mapper_Is_Called : When_Called_Method_GetTlevelsByUkprnAsync
     {
         [Fact]
         public void Then_ApiClient_Is_Called()
         {
-            InternalApiClient.Received().GetAllTlevelsByUkprnAsync(9);
+            InternalApiClient.Received().GetAllTlevelsByUkprnAsync(Ukprn);
         }
 
         [Fact]
         public void Then_Mapper_Is_Called()
         {
-            Mapper.Received().Map<IEnumerable<YourTlevelsViewModel>>(Arg.Any<List<AwardingOrganisationPathwayStatus>>());
+            Mapper.Received().Map<IEnumerable<YourTlevelsViewModel>>(ApiClientResponse);
         }
+
     }
 }
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/TlevelLoaderTests/GetTlevelDetailsByPathwayIdAsync/Then_ApiClient_Mapper_Is_Called.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Web.Models;
+using FluentAssertions;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.TlevelLoaderTests.GetTlevelDetailsByPathwayIdAsync
+{
+    public class Then_ApiClient_Mapper_Is_Called : When_Called_Method_GetTlevelDetailsByPathwayIdAsync
+    {
+        [Fact]
+        public void Then_ApiClient_Is_Called()
+        {
+            InternalApiClient.Received().GetTlevelDetailsByPathwayIdAsync(Ukprn, Id);
+        }
+
+        [Fact]
+        public void Then_Mapper_Is_Called()
+        {
+            Mapper.Received().Map<YourTLevelDetailsViewModel>(ApiClientResponse);
+        }
+
+        [Fact]
+        public void Then_Expected_Results_Are_Returned()
+        {
+            ActualResult.Should().NotBeNull();
+
+            ActualResult.PathwayId.Should().Be(ExpectedResult.PathwayId);
+            ActualResult.RouteName.Should().Be(ExpectedResult.RouteName);
+            ActualResult.PathwayName.Should().Be(ExpectedResult.PathwayName);
+            ActualResult.PathwayStatusId.Should().Be(ExpectedResult.PathwayStatusId);
+        }
+    }
+}
