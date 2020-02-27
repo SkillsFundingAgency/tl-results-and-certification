0a248f0b9b4edb9006bed7c941884a049118e8e3
Ravi Bobbili <ravi.bob@outlook.com>
2020-02-27 08:57:40 +0000

    TLRC-318 - Create 'Select a T Level to review' page -> Web - Unit Test added for Loader classes.

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/TlevelLoaderTests/GetTlevelsByStatusIdAsync/Then_Returns_Expected_Results.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using NSubstitute;
+using FluentAssertions;
+using System.Linq;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.TlevelLoaderTests.GetTlevelsByStatusIdAsync
+{
+    public class Then_Returns_Expected_Results : When_Called_Method_GetTlevelsByStatusIdAsync
+    {
+        [Fact]
+        public void Then_ApiClient_Is_Called()
+        {
+            InternalApiClient.Received().GetTlevelsByStatusIdAsync(Ukprn, statusId);
+        }
+
+        [Fact]
+        public void Then_Expected_Results_Are_Returned()
+        {
+            ActualResult.Should().NotBeNull();
+
+            var actualResult = ActualResult.First();
+
+            actualResult.PathwayId.Should().Be(ExpectedResult.PathwayId);
+            actualResult.TlevelTitle.Should().Be(ExpectedTLevelTitle);
+            actualResult.PageTitle.Should().Be(ExpectedPageTitle);
+            actualResult.StatusId.Should().Be(ExpectedResult.StatusId);
+        }
+    }
+}
