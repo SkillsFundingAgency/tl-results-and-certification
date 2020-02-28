a4b6abe9023d32002cc54e6858908a32a3507f87
Ravi Bobbili <ravi.bob@outlook.com>
2020-02-28 17:18:40 +0000

    TLRC-322 - Create 'T Level confirmed' confirmation page -> Unit Test added for Controller Actions.

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/TlevelControllerTests/ConfirmTlevel/Then_ModelState_Invalid_Redirected_To_PageNotFound.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.TlevelControllerTests.ConfirmTlevel
+{
+    public class Then_ModelState_Invalid_Redirected_To_PageNotFound : When_ConfirmTlevel_Action_Is_Called
+    {
+        public override void Given()
+        {
+
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/TlevelControllerTests/ConfirmTlevel/Then_ViewModel_Has_NoAwaitingStatus_Reredirected_To_PageNotFound.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.TlevelControllerTests.ConfirmTlevel
+{
+    public class Then_ViewModel_Has_NoAwaitingStatus_Reredirected_To_PageNotFound : When_ConfirmTlevel_Action_Is_Called
+    {
+        public override void Given()
+        {
+            InputModel = new VerifyTlevelViewModel { PathwayStatusId = (int)TlevelReviewStatus.Confirmed };
+        }
+
+        [Fact]
+        public void Then_Null_ViewModel_Redirected_To_Route_PageNotFound()
+        {
+            var routeName = (Result.Result as RedirectToRouteResult).RouteName;
+            routeName.Should().Be(RouteConstants.PageNotFound);
+        }
+    }
+}
