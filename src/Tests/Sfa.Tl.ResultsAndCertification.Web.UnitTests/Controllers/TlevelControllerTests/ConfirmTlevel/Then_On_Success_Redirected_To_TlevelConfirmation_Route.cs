1d407a74fb72fb36354699367689cbe9b1f818d9
Ravi Bobbili <ravi.bob@outlook.com>
2020-03-02 10:13:29 +0000

    TLRC-322 - Create 'T Level confirmed' confirmation page ->Controller Unit Tests are completed.

--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/TlevelControllerTests/ConfirmTlevel/Then_On_Success_Redirected_To_TlevelConfirmation_Route.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/TlevelControllerTests/ConfirmTlevel/Then_On_Success_Redirected_To_TlevelConfirmation_Route.cs
@@ -1,14 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel;
+using Xunit;
 
 namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.TlevelControllerTests.ConfirmTlevel
 {
     public class Then_On_Success_Redirected_To_TlevelConfirmation_Route : When_ConfirmTlevel_Action_Is_Called
     {
+        private readonly int pathwayId = 99;
+
         public override void Given()
         {
-            // TODO:
+
+            InputModel = new VerifyTlevelViewModel { PathwayStatusId = (int)TlevelReviewStatus.AwaitingConfirmation, PathwayId = pathwayId };
+            TlevelLoader.ConfirmTlevelAsync(InputModel).Returns(true);
+        }
+
+        [Fact]
+        public void Then_ModelState_Valid_Redirected_To_TlevelConfirmation()
+        {
+            var routeName = (Result.Result as RedirectToRouteResult).RouteName;
+            routeName.Should().Be(RouteConstants.TlevelConfirmation);
         }
     }
 }
