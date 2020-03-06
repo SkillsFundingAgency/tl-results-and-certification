453bcf9c95e5a92abf2aae839d20374f4969ca76
Ravi Bobbili <ravi.bob@outlook.com>
2020-03-06 13:04:41 +0000

    TLRC-319 - Select a T Level to review (More than 1) -> Unit tests are added.

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/TlevelControllerTests/SelectToReviewPost/Then_On_ModelState_Invalid_GetTlevelsToReviewByUkprnAsync_Is_Called.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.SelectToReview;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.TlevelControllerTests.SelectToReviewPost
+{
+    public class Then_On_ModelState_Invalid_GetTlevelsToReviewByUkprnAsync_Is_Called : When_SelecctToReview_Get_Action_Is_Called
+    {
+        private SelectToReviewPageViewModel mockresult;
+        private readonly int selectedPathwayId = 11;
+
+        public override void Given()
+        {
+            InputModel = new SelectToReviewPageViewModel { SelectedPathwayId = selectedPathwayId };
+            Controller.ModelState.AddModelError("SelectedPathwayId", "Please select a T level.");
+
+            mockresult = new SelectToReviewPageViewModel
+            {
+                SelectedPathwayId = selectedPathwayId,
+                TlevelsToReview = new List<TlevelToReviewViewModel> { }
+            };
+
+            TlevelLoader.GetTlevelsToReviewByUkprnAsync(Arg.Any<long>())
+                .Returns(mockresult);
+        }
+
+        [Fact]
+        public void Then_GetTlevelsToReviewByUkprnAsync_Is_Called()
+        {
+            TlevelLoader.Received().GetTlevelsToReviewByUkprnAsync(ukprn);
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/TlevelControllerTests/SelectToReviewPost/Then_Redirected_To_TlevelVerify_Route.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.SelectToReview;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.TlevelControllerTests.SelectToReviewPost
+{
+    public class Then_Redirected_To_TlevelVerify_Route : When_SelecctToReview_Get_Action_Is_Called
+    {
+        private int selectedPathwayId = 11;
+        public override void Given()
+        {
+            InputModel = new SelectToReviewPageViewModel { SelectedPathwayId = selectedPathwayId };
+        }
+
+        [Fact]
+        public void Then_Redirected_To_Tlevel_Verify_Route()
+        {
+            var route = (Result.Result as RedirectToRouteResult);
+            route.Should().NotBeNull();
+
+            var actualRouteName = route.RouteName;
+            var routeParam = route.RouteValues["id"];
+
+            actualRouteName.Should().Be(RouteConstants.VerifyTlevel);
+            routeParam.Should().NotBeNull();
+            routeParam.ToString().Should().Be(selectedPathwayId.ToString());
+        }
+    }
+}
