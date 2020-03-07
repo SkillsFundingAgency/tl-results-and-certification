1e5cdd11706631602b7c95e61bff893cc6509a83
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-03-07 11:24:54 +0000

    TLRC - 390 - Service Name link on Access Denied Error Page  - TLRC - 430 - Dev - Added Web Unit Tests for Service Access Denied

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/DashboardControllerTests/Index/Then_Dashboard_View_IsCalled.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Extensions;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
+using System.Security.Claims;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.DashboardControllerTests.Index
+{
+    public class Then_Dashboard_View_IsCalled : When_Index_Action_Called
+    {
+        public override void Given()
+        {
+            HttpContextAccessor.HttpContext.Returns(new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+                {
+                    new Claim(CustomClaimTypes.HasAccessToService, "true")
+                }))
+            });
+
+            Controller = new DashboardController()
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = HttpContextAccessor.HttpContext
+                }
+            };
+        }
+
+        [Fact]
+        public void Then_Dashboard_View_Page_IsCalled()
+        {
+            Result.Should().NotBeNull();
+            Result.Should().BeOfType<ViewResult>();
+            var viewResult = Result as ViewResult;
+            viewResult.Should().NotBeNull();
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/DashboardControllerTests/Index/Then_Redirected_To_ServiceAccessDenied_IsCalled.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Extensions;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
+using System.Security.Claims;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.DashboardControllerTests.Index
+{
+    public class Then_Redirected_To_ServiceAccessDenied_IsCalled : When_Index_Action_Called
+    {
+        public override void Given()
+        {
+            HttpContextAccessor.HttpContext.Returns(new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+                {
+                    new Claim(CustomClaimTypes.HasAccessToService, "false")
+                }))
+            });
+
+            Controller = new DashboardController()
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = HttpContextAccessor.HttpContext
+                }
+            };
+        }
+
+        [Fact]
+        public void Then_Redirected_To_Route_ServiceAccessDenied()
+        {
+            var routeName = (Result as RedirectToRouteResult).RouteName;
+            routeName.Should().Be(RouteConstants.ServiceAccessDenied);
+        }
+    }
+}
