1d1f98cdf141c8441cbee4dcc03b674f02e0d12e
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-03-12 10:02:13 +0000

    TLRC - 321 - Create 'T Level details queried' confirmation page  - TLRC - 462 - Dev - Added Web Unit Test for ReportIssueAsync in Loader

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/TlevelLoaderTests/ReportIssueAsync/Then_ApiResponse_False_Is_Returned.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using Sfa.Tl.ResultsAndCertification.Web.Loader;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.TlevelLoaderTests.ReportIssueAsync
+{
+    public class Then_ApiResponse_False_Is_Returned : When_Called_Method_ReportIssueAsync
+    {
+        public override void Given()
+        {
+            PathwayId = 99;
+            ExpectedResult = false;
+
+            TlevelQueryViewModel = new TlevelQueryViewModel { PathwayId = PathwayId, TqAwardingOrganisationId = PathwayId };
+            VerifyTlevelDetails = new VerifyTlevelDetails { Id = PathwayId, TqAwardingOrganisationId = PathwayId, PathwayStatusId = StatusId };
+
+            Mapper.Map<VerifyTlevelDetails>(TlevelQueryViewModel).Returns(VerifyTlevelDetails);
+            InternalApiClient.VerifyTlevelAsync(VerifyTlevelDetails).Returns(ExpectedResult);
+
+            Loader = new TlevelLoader(InternalApiClient, Mapper);
+        }
+
+        [Fact]
+        public void Then_ApiResponse_Is_False()
+        {
+            ActualResult.Should().BeFalse();
+        }
+    }
+}
