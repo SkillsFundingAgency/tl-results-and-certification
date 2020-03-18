c3cd1938ddb21465c543a5a395614603c116a3c8
Ravi Bobbili <ravi.bob@outlook.com>
2020-03-18 11:12:36 +0000

    TLRC-320 - Report Tlevel Query -> TLRC-495: Validation for 10,000 character limit added.

--- src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/TlevelQueryViewModel.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/TlevelQueryViewModel.cs
@@ -18,6 +18,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel
         public IEnumerable<string> Specialisms { get; set; }
 
         [Required(ErrorMessageResourceType = typeof(ErrorResource.Query), ErrorMessageResourceName = "Query_Required_Validation_Message")]
+        [StringLength(10000, ErrorMessageResourceType = typeof(ErrorResource.Query), ErrorMessageResourceName = "Query_CharLimitExceeded_Validation_Message")]
         public string Query { get; set; }
     }
 }
