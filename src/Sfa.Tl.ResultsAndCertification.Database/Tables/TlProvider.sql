a228a2d9642564f31ffe686e535cddfaa20901c7
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-03-18 14:53:25 +0000

    TLRC - 393 - Create: 'Find a Provider' page (no Provider set up) - TLRC - 500 - Dev - Database changes and Seed Eligible Tlevel Providers

--- src/Sfa.Tl.ResultsAndCertification.Database/Tables/TlProvider.sql
+++ src/Sfa.Tl.ResultsAndCertification.Database/Tables/TlProvider.sql
@@ -1,14 +1,12 @@
 ﻿CREATE TABLE [dbo].[TlProvider]
 (
 	[Id] INT IDENTITY(1,1) NOT NULL,
-	[TlPathwayId] INT NOT NULL,
     [UkPrn] BIGINT NOT NULL, 
-    [Name] NVARCHAR(100) NOT NULL, 
-    [DisplayName] NVARCHAR(100) NULL, 	
+    [Name] NVARCHAR(256) NOT NULL, 
+    [DisplayName] NVARCHAR(256) NULL, 	
     [CreatedOn] DATETIME2 NOT NULL DEFAULT getutcdate(), 
     [CreatedBy] NVARCHAR(50) NULL, 
     [ModifiedOn] DATETIME2 NULL, 
     [ModifiedBy] NVARCHAR(50) NULL,
 	CONSTRAINT [PK_TlProvider] PRIMARY KEY ([Id]),
-	CONSTRAINT [FK_TlProvider_TlPathway] FOREIGN KEY ([TlPathwayId]) REFERENCES [TlPathway]([Id])
 )
