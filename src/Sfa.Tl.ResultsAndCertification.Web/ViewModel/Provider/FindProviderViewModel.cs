c7154d155566eb82280b3ba7154ecbd23ab8a570
Ravi Bobbili <ravi.bob@outlook.com>
2020-03-22 21:10:36 +0000

    TLRC-393: Find Providers Page -> TLRC-510 Content changes are completed.

--- src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/Provider/FindProviderViewModel.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/Provider/FindProviderViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using ErrorResource = Sfa.Tl.ResultsAndCertification.Web.Content.Provider;
 
 namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.Provider
 {
     public class FindProviderViewModel
     {
-        [Required(ErrorMessage = "Enter a provider’s name")] //TODO: ContentMessage
-        [StringLength(400, ErrorMessage = "The character limit exceeded")]
+        [Required(ErrorMessageResourceType = typeof(ErrorResource.FindProvider), ErrorMessageResourceName = "ProviderName_Required_Validation_Message")]
+        [StringLength(400, ErrorMessageResourceType = typeof(ErrorResource.FindProvider), ErrorMessageResourceName = "ProviderName_Char_Limit_Exceed_Validation_Message")]
         public string Search { get; set; }
     }
 }
