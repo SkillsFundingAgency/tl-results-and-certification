9e1252e29be716b7fd4d7db55375b8f5532b2194
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-03-23 13:20:20 +0000

    TLRC - 423 - Implement Email to R&C Mailbox when user raises a query on T Level details - TLRC - 515 - User Story 423 - There is a space between the ':' after Route in the email - Revmove captialisation for title

--- src/Sfa.Tl.ResultsAndCertification.Application/Services/AwardingOrganisationService.cs
+++ src/Sfa.Tl.ResultsAndCertification.Application/Services/AwardingOrganisationService.cs
@@ -88,7 +88,7 @@ namespace Sfa.Tl.ResultsAndCertification.Application.Services
         {
             var tokens = new Dictionary<string, dynamic>
                 {
-                    { "tlevel_name", $"{tqAwardingOrganisation.TlRoute.Name}: {tqAwardingOrganisation.TlPathway.Name}".ToUpperInvariant() },
+                    { "tlevel_name", $"{tqAwardingOrganisation.TlRoute.Name}: {tqAwardingOrganisation.TlPathway.Name}" },
                     { "user_comments", model.Query },
                     { "sender_name", model.ModifiedBy },
                     { "sender_email_address", model.QueriedUserEmail }
