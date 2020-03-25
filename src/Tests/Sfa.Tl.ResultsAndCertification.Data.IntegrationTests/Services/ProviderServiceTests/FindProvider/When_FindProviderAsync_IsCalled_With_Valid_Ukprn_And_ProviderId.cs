758bb6d4854a94415e031133351b41630cf665eb
Ravi Bobbili <ravi.bob@outlook.com>
2020-03-25 16:21:22 +0000

    TLRC-393: Find Providers Page -> TLRC-506 - Api Client unit test are added.

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Data.IntegrationTests/Services/ProviderServiceTests/FindProvider/When_FindProviderAsync_IsCalled_With_Valid_Ukprn_And_ProviderId.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Xunit;
+using FluentAssertions;
+using NSubstitute;
+using Microsoft.Extensions.Logging;
+using Sfa.Tl.ResultsAndCertification.Application.Services;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Data.Repositories;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using System.Collections.Generic;
+
+namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Services.ProviderServiceTests.FindProvider
+{
+    public class When_FindProviderAsync_IsCalled_With_Valid_Ukprn_And_ProviderId : ProviderServiceBaseTest
+    {
+        private IEnumerable<ProviderMetadata> _result;
+        private string _providerName = "Lordswood Sixthform College";
+        private bool _isExactMatch = true;
+
+        public override void Given()
+        {
+            SeedTestData();
+
+            ProviderRepositoryLogger = Substitute.For<ILogger<ProviderRepository>>();
+            ProviderRepository = new ProviderRepository(ProviderRepositoryLogger, DbContext);
+            TlProviderRepositoryLogger = Substitute.For<ILogger<GenericRepository<TlProvider>>>();
+            TlproviderRepository = new GenericRepository<TlProvider>(TlProviderRepositoryLogger, DbContext);
+            ProviderService = new ProviderService(ProviderRepository, TlproviderRepository, ProviderMapper, Logger);
+        }
+
+        public override void When()
+        {
+            _result = ProviderService.FindProviderAsync(_providerName, _isExactMatch).Result;
+        }
+
+        [Fact]
+        public void Then_Expected_Results_Is_Returned()
+        {
+        }
+    }
+}
