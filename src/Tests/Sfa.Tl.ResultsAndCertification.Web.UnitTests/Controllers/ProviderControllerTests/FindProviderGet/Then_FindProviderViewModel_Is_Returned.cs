8a173fc01b958c8bdca57fce83e9d843775398d9
Ravi Bobbili <ravi.bob@outlook.com>
2020-03-26 21:47:17 +0000

    TLRC-450 - Link in Find provider TLRC-555 - Unit tests are added.

--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderControllerTests/FindProviderGet/Then_FindProviderViewModel_Is_Returned.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderControllerTests/FindProviderGet/Then_FindProviderViewModel_Is_Returned.cs
@@ -1,5 +1,9 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Extensions;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
 using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Provider;
 using Xunit;
 
@@ -7,12 +11,28 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderContr
 {
     public class Then_FindProviderViewModel_Is_Returned : When_FindProviderAsync_Post_Action_Is_Called
     {
-        public override void Given() { }
+        public override void Given()
+        {
+            var httpContext = new ClaimsIdentityBuilder<ProviderController>(Controller)
+               .Add(CustomClaimTypes.Ukprn, Ukprn.ToString())
+               .Build()
+               .HttpContext;
 
-        [Fact(Skip = "TODO asper latest story")]
+            HttpContextAccessor.HttpContext.Returns(httpContext);
+
+            ProviderLoader.IsAnyProviderSetupCompletedAsync(Ukprn).Returns(true);
+        }
+
+        [Fact]
+        public void Then_IsAnyProviderSetupCompletedAsync_Method_Is_Called()
+        {
+            ProviderLoader.Received(1).IsAnyProviderSetupCompletedAsync(Ukprn);
+        }
+
+        [Fact]
         public void Then_Expeced_Results_Returned()
         {
-            Result.Should().BeOfType(typeof(ViewResult));
+            Result.Result.Should().BeOfType(typeof(ViewResult));
             
             var viewResult = Result.Result as ViewResult;
             viewResult.Model.Should().BeOfType(typeof(FindProviderViewModel));
@@ -21,6 +41,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderContr
             model.Should().NotBeNull();
             model.Search.Should().BeNull();
             model.SelectedProviderId.Should().Be(0);
+            model.ShowViewProvidersLink.Should().BeTrue();
         }
     }
 }
