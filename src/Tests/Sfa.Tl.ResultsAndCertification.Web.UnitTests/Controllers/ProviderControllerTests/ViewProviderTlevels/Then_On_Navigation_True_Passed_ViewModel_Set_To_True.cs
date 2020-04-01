3f61993643aecb1c7948601f2f74a6e417252a46
Ravi Bobbili <ravi.bob@outlook.com>
2020-04-01 18:08:43 +0100

    TLRC-452 - View Provider T Levels: TLRC-576 - Unit test of Web application

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderControllerTests/ViewProviderTlevels/Then_On_Navigation_True_Passed_ViewModel_Set_To_True.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Provider.ViewProviderTlevels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderControllerTests.ViewProviderTlevels
+{
+    public class Then_On_Navigation_True_Passed_ViewModel_Set_To_True : When_ViewProviderTlevelsAsync_Is_Called
+    {
+        private ProviderViewModel mockViewmodel;
+
+        public override void Given()
+        {
+            navigation = true;
+
+            mockViewmodel = new ProviderViewModel()
+            {
+                Tlevels = new List<TlevelViewModel>()
+                {
+                    new TlevelViewModel { TlevelTitle = "Arts" }
+                },
+            };
+
+            ProviderLoader.GetViewProviderTlevelViewModelAsync(Arg.Any<long>(), providerId)
+                .Returns(mockViewmodel);
+        }
+
+        [Fact]
+        public void Then_Expected_ViewModel_Results_Returned()
+        {
+            var actualResult = Result.Result;
+            actualResult.Should().NotBeNull();
+            actualResult.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = actualResult as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(ProviderViewModel));
+
+            var resultModel = viewResult.Model as ProviderViewModel;
+            resultModel.Should().NotBeNull();
+            resultModel.IsNavigatedFromFindProvider.Should().BeFalse();
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderControllerTests/ViewProviderTlevels/When_ViewProviderTlevelsAsync_Is_Called.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
+using Sfa.Tl.ResultsAndCertification.Web.Loader.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Provider.ViewProviderTlevels;
+using System.Threading.Tasks;
+
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderControllerTests.ViewProviderTlevels
+{
+    public abstract class When_ViewProviderTlevelsAsync_Is_Called : BaseTest<ProviderController>
+    {
+        // DI Mock objects
+        protected IProviderLoader ProviderLoader;
+        protected ILogger<ProviderController> Logger;
+
+        // input, output and other mock for tes
+        protected ProviderController Controller;
+        protected Task<IActionResult> Result;
+        protected IHttpContextAccessor HttpContextAccessor;
+        protected ProviderViewModel ViewModel;
+
+        // controller params
+        protected int providerId = 24;
+        protected bool navigation = false;
+
+        public override void Setup()
+        {
+            HttpContextAccessor = Substitute.For<IHttpContextAccessor>();
+            ProviderLoader = Substitute.For<IProviderLoader>();
+            Logger = Substitute.For<ILogger<ProviderController>>();
+            Controller = new ProviderController(ProviderLoader, Logger);
+        }
+
+        public override void When()
+        {
+            Result = Controller.ViewProviderTlevelsAsync(providerId, navigation);
+        }
+    }
+}
