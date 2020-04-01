c2746a770b43d4be3c39a20839d5b5e185fbe175
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-04-01 10:55:30 +0100

    TLRC - 451 - Create 'Your providers' page - TLRC - 567 - Dev - Added Unit Test for Api Client Enpoint GetTqAoProviderDetailsAsync method

--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/ProviderLoaderTests/AddProviderTlevelsAsync/Then_Mapper_Returns_Expected_Results.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/ProviderLoaderTests/AddProviderTlevelsAsync/Then_Mapper_Returns_Expected_Results.cs
@@ -1,63 +1,13 @@
-﻿using AutoMapper;
-using FluentAssertions;
-using Microsoft.AspNetCore.Http;
-using NSubstitute;
-using Sfa.Tl.ResultsAndCertification.Common.Enum;
+﻿using FluentAssertions;
 using Sfa.Tl.ResultsAndCertification.Models.Contracts;
-using Sfa.Tl.ResultsAndCertification.Web.Loader;
-using Sfa.Tl.ResultsAndCertification.Web.Mapper;
-using Sfa.Tl.ResultsAndCertification.Web.Mapper.Resolver;
-using Sfa.Tl.ResultsAndCertification.Web.ViewModel;
 using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Provider.SelectProviderTlevels;
 using System.Collections.Generic;
-using System.Security.Claims;
 using Xunit;
-
 
 namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.ProviderLoaderTests.AddProviderTlevelsAsync
 {
     public class Then_Mapper_Returns_Expected_Results : When_Called_Method_AddProviderTlevelsAsync
-    {
-        //private IHttpContextAccessor _httpContextAccessor;
-        
-
-        //public override void Given()
-        //{
-        //    //_httpContextAccessor = Substitute.For<IHttpContextAccessor>();
-        //    //_httpContextAccessor.HttpContext.Returns(new DefaultHttpContext
-        //    //{
-        //    //    User = new ClaimsPrincipal(new ClaimsIdentity(new[]
-        //    //    {
-        //    //        new Claim(ClaimTypes.GivenName, _givename),
-        //    //        new Claim(ClaimTypes.Surname, _surname),
-        //    //        new Claim(ClaimTypes.Email, _email)
-        //    //    }))
-        //    //});
-
-        //    //CreateMapper();
-
-        //    //ProviderTlevelDetails = new List<ProviderTlevelDetails>
-        //    //        {
-        //    //            new ProviderTlevelDetails { TqAwardingOrganisationId = 1, ProviderId = 1, PathwayId = 1, CreatedBy = "test user" },
-        //    //            new ProviderTlevelDetails { TqAwardingOrganisationId = 2, ProviderId = 1, PathwayId = 2, CreatedBy = "test user" }
-        //    //        };
-
-        //    //ProviderTlevelsViewModel = new ProviderTlevelsViewModel
-        //    //{
-        //    //    ProviderId = 1,
-        //    //    DisplayName = "Test1",
-        //    //    Ukprn = 1234589,
-        //    //    Tlevels = new List<ProviderTlevelDetailsViewModel>
-        //    //        {
-        //    //            new ProviderTlevelDetailsViewModel { TqAwardingOrganisationId = 1, ProviderId = 1, PathwayId = 1, IsSelected = true },
-        //    //            new ProviderTlevelDetailsViewModel { TqAwardingOrganisationId = 2, ProviderId = 1, PathwayId = 2, IsSelected = true }
-        //    //        }
-        //    //};
-        //    ExpectedResult = true;
-        //    InternalApiClient.AddProviderTlevelsAsync(ProviderTlevelDetails).Returns(true);
-        //    Loader = new ProviderLoader(InternalApiClient, Mapper);
-        //}
-
+    {    
         [Fact]
         public void Then_Mapper_Has_Expected_Results()
         {
