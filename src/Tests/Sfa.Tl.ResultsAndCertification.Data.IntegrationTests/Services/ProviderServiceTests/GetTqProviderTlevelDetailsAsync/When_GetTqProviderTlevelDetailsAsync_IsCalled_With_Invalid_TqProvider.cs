5f2352ca2c19352eb6243a8b78d6e6aa17602613
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-04-04 20:12:19 +0100

    TLRC - 453 - Create Are you sure you want to remove this T Level?' page - TLRC - 622 - Dev - Added Integration Test for GetTqProviderTlevelDetailsAsync

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Data.IntegrationTests/Services/ProviderServiceTests/GetTqProviderTlevelDetailsAsync/When_GetTqProviderTlevelDetailsAsync_IsCalled_With_Invalid_TqProvider.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Application.Services;
+using Sfa.Tl.ResultsAndCertification.Data.Repositories;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.DataProvider;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Services.ProviderServiceTests.GetTqProviderTlevelDetailsAsync
+{
+    public class When_GetTqProviderTlevelDetailsAsync_IsCalled_With_Invalid_TqProvider : ProviderServiceBaseTest
+    {
+        private ProviderTlevelDetails _result;
+
+        public override void Given()
+        {
+            SeedTestData();
+            CreateMapper();
+            ProviderRepositoryLogger = Substitute.For<ILogger<ProviderRepository>>();
+            ProviderRepository = new ProviderRepository(ProviderRepositoryLogger, DbContext);
+            TlProviderRepositoryLogger = Substitute.For<ILogger<GenericRepository<TlProvider>>>();
+            TlproviderRepository = new GenericRepository<TlProvider>(TlProviderRepositoryLogger, DbContext);
+            ProviderService = new ProviderService(ProviderRepository, TlproviderRepository, ProviderMapper, Logger);
+        }
+
+        public override void When()
+        {
+            _result = ProviderService.GetTqProviderTlevelDetailsAsync(TlAwardingOrganisation.UkPrn, 000).Result;
+        }
+
+        [Fact]
+        public void Then_No_Results_Is_Returned()
+        {
+            _result.Should().BeNull();
+        }
+
+        protected override void SeedProviderData()
+        {
+            TlProvider = ProviderDataProvider.CreateTlProvider(DbContext);
+            TqProvider = ProviderDataProvider.CreateTqProvider(DbContext, TqAwardingOrganisation, TlProvider);
+        }
+    }
+}
