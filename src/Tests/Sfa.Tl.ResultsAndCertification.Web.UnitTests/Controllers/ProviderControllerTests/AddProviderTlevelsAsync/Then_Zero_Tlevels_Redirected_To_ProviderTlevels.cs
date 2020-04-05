55f4b855bbb216662c5bbb3e9611d78f2bbda19e
Ravi Bobbili <ravi.bob@outlook.com>
2020-04-05 19:12:48 +0100

    TLRC-511: Add T levels - TLRC-633 - Unitest for the controller are added.

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderControllerTests/AddProviderTlevelsAsync/Then_Zero_Tlevels_Redirected_To_ProviderTlevels.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Provider.SelectProviderTlevels;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderControllerTests.AddProviderTlevelsAsync
+{
+    public class Then_Zero_Tlevels_Redirected_To_ProviderTlevels : When_AddProviderTlevelsAsync_Get_Action_Is_Called
+    {
+        private ProviderTlevelsViewModel mockresult;
+        public override void Given()
+        {
+            mockresult = new ProviderTlevelsViewModel
+            {
+                ProviderId = 1,
+                DisplayName = "Test",
+                Ukprn = 10000111,
+                Tlevels = new List<ProviderTlevelViewModel>()
+            };
+
+            ProviderLoader.GetSelectProviderTlevelsAsync(Arg.Any<long>(), Arg.Any<int>()).Returns(mockresult);
+        }
+
+        [Fact]
+        public void Then_Zero_Tlevels_Redirected_To_ProviderTlevels_Route()
+        {
+            var route = (Result.Result as RedirectToRouteResult);
+            route.RouteName.Should().Be(RouteConstants.ProviderTlevels);
+
+            route.RouteValues["providerId"].Should().Be(ProviderId);
+            route.RouteValues["navigation"].Should().Be(true);
+        }
+    }
+}
