9e2b2607c90936e3b3024f1b10f6f168ee75de2f
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-04-07 10:20:21 +0100

    TLRC - 470 - Create 'T Level removed successfully' page - TLRC - 609 - Dev - Added Unit Test for RemoveProviderTlevelAsync For Loader

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/ProviderLoaderTests/RemoveTqProviderTlevelAsync/Then_ApiResponse_False_Is_Returned.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Web.Loader;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.ProviderLoaderTests.RemoveTqProviderTlevelAsync
+{
+    public class Then_ApiResponse_False_Is_Returned : When_RemoveProviderTlevelAsync_Is_Called
+    {
+        public override void Given()
+        {
+            ApiClientResponse = false;
+            InternalApiClient.RemoveTqProviderTlevelAsync(Ukprn, TqProviderId).Returns(ApiClientResponse);
+            Loader = new ProviderLoader(InternalApiClient, Mapper);
+        }
+
+        [Fact]
+        public void Then_ApiResponse_Is_False()
+        {
+            ActualResult.Should().BeFalse();
+        }
+    }
+}
