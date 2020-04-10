dee2017ce0c670d8a78593df8beb2bbfc42cfb62
Ravi Bobbili <ravi.bob@outlook.com>
2020-04-10 17:18:00 +0100

    TLRC-513- User guide html -> TLRC682 links are integrated.

--- src/Sfa.Tl.ResultsAndCertification.Web/Controllers/HelpController.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Controllers/HelpController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Configuration;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Help;
 
 namespace Sfa.Tl.ResultsAndCertification.Web.Controllers
 {
     [AllowAnonymous]
     public class HelpController : Controller
     {
+        private readonly ResultsAndCertificationConfiguration _configuration;
+
+        public HelpController(ResultsAndCertificationConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet]
         [Route("cookies", Name = RouteConstants.Cookies)]
         public IActionResult Cookies()
@@ -31,7 +40,8 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Controllers
         [Route("user-guide", Name = RouteConstants.UserGuide)]
         public IActionResult UserGuide()
         {
-            return View();
+            var viewModel = new UserGuideViewModel { TechnicalSupportEmailAddress = _configuration.TechnicalSupportEmailAddress };
+            return View(viewModel);
         }
     }
 }
---
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/Help/UserGuideViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.Help
+{
+    public class UserGuideViewModel
+    {
+        public string TechnicalSupportEmailAddress { get; set; }
+    }
+}
