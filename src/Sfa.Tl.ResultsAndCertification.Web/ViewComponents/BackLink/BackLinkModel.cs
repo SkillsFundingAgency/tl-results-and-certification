ebc0e5082daea4759831d7fc87d918a6f45e8b00
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-05-01 11:09:47 +0100

    TLRC - 421 - Back Functionality - Back Link on Transition pages (review journey) - TLRC - 776 - Dev - Implemented BackLink View Component

---
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewComponents/BackLink/BackLinkModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.ViewComponents.BackLink
+{
+    public class BackLinkModel
+    {
+        public string RouteName { get; set; }
+        public Dictionary<string, string> RouteAttributes { get; set; }
+    }
+}
