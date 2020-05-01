abaa4643addabf31abcf298cd6566ae3b0af6815
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-05-01 18:06:54 +0100

    Added dataprotection for api due to The antiforgery token could not be decrypted exception while api is returned to web

--- src/Sfa.Tl.ResultsAndCertification.InternalApi/Startup.cs
+++ src/Sfa.Tl.ResultsAndCertification.InternalApi/Startup.cs
@@ -69,6 +69,8 @@ namespace Sfa.Tl.ResultsAndCertification.InternalApi
                 });
                 services.AddApiAuthentication(ResultsAndCertificationConfiguration).AddApiAuthorization();
             }
+
+            services.AddApiDataProtection(ResultsAndCertificationConfiguration, _env);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
