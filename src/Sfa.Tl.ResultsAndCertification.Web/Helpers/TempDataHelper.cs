8a9508d7854895d3458d5e0b625d2dc2dd6dba4d
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-05-04 18:35:58 +0100

    TLRC - 471 - Back functionality - Back Link on Transitional pages (Provider) - TLRC - 784 - Dev - Check null for tempdata

--- src/Sfa.Tl.ResultsAndCertification.Web/Helpers/TempDataHelper.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Helpers/TempDataHelper.cs
@@ -7,11 +7,15 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Helpers
     {
         public static void Set<T>(this ITempDataDictionary tempData, string key, T value)
         {
-            tempData[key] = JsonConvert.SerializeObject(value);
+            if (tempData != null)
+            {
+                tempData[key] = JsonConvert.SerializeObject(value);
+            }
         }
 
         public static T Get<T>(this ITempDataDictionary tempData, string key)
         {
+            if (tempData == null) return default;
             tempData.TryGetValue(key, out object returnValue);
             return returnValue == null ? default : JsonConvert.DeserializeObject<T>(returnValue.ToString());
         }
