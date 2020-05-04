7338f6d02587ca85e26812d22b7ece880ca1f59a
Ravi Bobbili <ravi.bob@outlook.com>
2020-05-04 12:50:42 +0100

    TLRC-421 Back link functionality: TLRC-764, TLRC-765: Model created in the ViewModel rather cshtml.

--- src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/TlevelQueryViewModel.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/TlevelQueryViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewComponents.BackLink;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using ErrorResource = Sfa.Tl.ResultsAndCertification.Web.Content.Tlevel;
 
@@ -21,5 +23,25 @@ namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel
         [Required(ErrorMessageResourceType = typeof(ErrorResource.Query), ErrorMessageResourceName = "Query_Required_Validation_Message")]
         [StringLength(10000, ErrorMessageResourceType = typeof(ErrorResource.Query), ErrorMessageResourceName = "Query_CharLimitExceeded_Validation_Message")]
         public string Query { get; set; }
+
+        public BackLinkModel BackLink
+        {
+            get
+            {
+                if (IsBackToVerifyPage)
+                    return new BackLinkModel
+                    {
+                        RouteName = RouteConstants.VerifyTlevel,
+                        RouteAttributes = new Dictionary<string, string> { { "id", PathwayId.ToString() }, { "isback", "true" } }
+                    };
+
+                else
+                    return new BackLinkModel
+                    {
+                        RouteName = RouteConstants.TlevelDetails,
+                        RouteAttributes = new Dictionary<string, string> { { "id", PathwayId.ToString() } }
+                    };
+            }
+        }
     }
 }
