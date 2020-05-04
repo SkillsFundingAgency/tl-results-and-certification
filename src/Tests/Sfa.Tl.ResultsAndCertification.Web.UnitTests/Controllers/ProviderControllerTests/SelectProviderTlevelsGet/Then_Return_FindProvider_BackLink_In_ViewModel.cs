c99791fcece798891718904099a91a80bf7a8fcd
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-05-04 19:14:54 +0100

    TLRC - 471 - Back functionality - Back Link on Transitional pages (Provider) - TLRC - 791 - Dev - Added Unit Test for Select Provider Tlevels Back Link

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderControllerTests/SelectProviderTlevelsGet/Then_Return_FindProvider_BackLink_In_ViewModel.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Provider.SelectProviderTlevels;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderControllerTests.SelectProviderTlevelsGet
+{
+    public class Then_Return_FindProvider_BackLink_In_ViewModel : When_SelectProviderTlevelsAsync_Get_Action_Is_Called
+    {
+        private ProviderTlevelsViewModel mockresult;
+
+        public override void Given()
+        {
+            mockresult = new ProviderTlevelsViewModel
+            {
+                ProviderId = 1,
+                DisplayName = "Test",
+                Ukprn = 10000111,
+                IsAddTlevel = false,
+                Tlevels = new List<ProviderTlevelViewModel>
+                {
+                    new ProviderTlevelViewModel { TqAwardingOrganisationId = 1, TlProviderId = 1, TlevelTitle = "Route1: Pathway1" },
+                    new ProviderTlevelViewModel { TqAwardingOrganisationId = 1, TlProviderId = 1, TlevelTitle = "Route1: Pathway1" }
+                }
+            };
+
+            ProviderLoader.GetSelectProviderTlevelsAsync(Arg.Any<long>(), Arg.Any<int>()).Returns(mockresult);
+        }
+
+        [Fact]
+        public void Then_GetSelectProviderTlevelsAsync_Is_Called()
+        {
+            ProviderLoader.Received().GetSelectProviderTlevelsAsync(Ukprn, ProviderId);
+        }
+
+        [Fact]
+        public void Then_Returns_FindProvider_BackLink()
+        {
+            var viewResult = Result.Result as ViewResult;
+            var model = viewResult.Model as ProviderTlevelsViewModel;
+
+            model.Should().NotBeNull();
+            model.BackLink.RouteName.Should().Be(RouteConstants.FindProvider);
+        }
+    }
+}
