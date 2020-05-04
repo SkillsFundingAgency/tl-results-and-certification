c4c56befd6b21a73ddef80375eb0c346c46f7afa
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-05-04 15:00:50 +0100

    TLRC - 471 - Back functionality - Back Link on Transitional pages (Provider) - TLRC - 782 - Dev - Implemented TempData Helper

---
+++ src/Sfa.Tl.ResultsAndCertification.Web/Helpers/TempDataHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.Helpers
+{
+    public static class TempDataHelper
+    {
+        public static void Set<T>(this ITempDataDictionary tempData, string key, T value)
+        {
+            tempData[key] = JsonConvert.SerializeObject(value);
+        }
+
+        public static T Get<T>(this ITempDataDictionary tempData, string key)
+        {
+            tempData.TryGetValue(key, out object returnValue);
+            return returnValue == null ? default : JsonConvert.DeserializeObject<T>(returnValue.ToString());
+        }
+    }
+}
