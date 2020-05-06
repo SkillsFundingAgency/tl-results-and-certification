25e6bc3ca828df774a950840718e6757a475ddf2
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-05-06 12:04:48 +0100

    TLRC - 551 - Adding Breadcrumbs on static pages (Providers Journey) - TLRC - 803 - Dev - Implemented ViewComponent for BreadCrumbs

---
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewComponents/Breadcrumb/Breadcrumb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.ViewComponents.Breadcrumb
+{
+    public class Breadcrumb : ViewComponent
+    {
+        public IViewComponentResult Invoke(BreadcrumbModel model)
+        {
+            return View("~/ViewComponents/Breadcrumb/Default.cshtml", model);
+        }
+    }
+}
---
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewComponents/Breadcrumb/BreadcrumbItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.ViewComponents.Breadcrumb
+{
+    public class BreadcrumbItem
+    {
+        public BreadcrumbItem()
+        {
+            RouteAttributes = new Dictionary<string, string>();
+        }
+
+        public bool IsCurrentPage { get; set; }
+        public string DisplayName { get; set; }
+        public string RouteName { get; set; }
+        public Dictionary<string, string> RouteAttributes { get; set; }
+    }
+}
