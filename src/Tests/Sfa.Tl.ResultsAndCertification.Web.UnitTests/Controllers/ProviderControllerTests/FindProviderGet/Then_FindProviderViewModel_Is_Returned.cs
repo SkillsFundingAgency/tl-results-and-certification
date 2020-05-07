9ba3108ffe87c5c8935eb5a3e37b3eff18803232
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-05-07 14:25:59 +0100

    TLRC - 551 - Adding Breadcrumbs on static pages (Providers Journey) - TLRC - 822 - Dev - Implemented Unit test for Find a provider Breadcrumb

--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderControllerTests/FindProviderGet/Then_FindProviderViewModel_Is_Returned.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderControllerTests/FindProviderGet/Then_FindProviderViewModel_Is_Returned.cs
@@ -2,10 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using Sfa.Tl.ResultsAndCertification.Common.Extensions;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
 using Sfa.Tl.ResultsAndCertification.Tests.Common.Helpers;
 using Sfa.Tl.ResultsAndCertification.Web.Controllers;
 using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Provider;
 using Xunit;
+using BreadcrumbContent = Sfa.Tl.ResultsAndCertification.Web.Content.ViewComponents.Breadcrumb;
 
 namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderControllerTests.FindProviderGet
 {
@@ -30,7 +32,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderContr
         }
 
         [Fact]
-        public void Then_Expeced_Results_Returned()
+        public void Then_Expected_Results_Returned()
         {
             Result.Result.Should().BeOfType(typeof(ViewResult));
             
@@ -42,6 +44,26 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderContr
             model.Search.Should().BeNull();
             model.SelectedProviderId.Should().Be(0);
             model.ShowViewProvidersLink.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Then_Expected_Breadcrumb_Returned()
+        {
+            Result.Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result.Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(FindProviderViewModel));
+
+            var model = viewResult.Model as FindProviderViewModel;
+            model.Should().NotBeNull();
+
+            model.Breadcrumb.Should().NotBeNull();
+            model.Breadcrumb.BreadcrumbItems.Should().NotBeNull();
+            model.Breadcrumb.BreadcrumbItems.Count.Should().Be(2);
+
+            model.Breadcrumb.BreadcrumbItems[0].RouteName.Should().Be(RouteConstants.Dashboard);
+            model.Breadcrumb.BreadcrumbItems[0].DisplayName.Should().Be(BreadcrumbContent.Home);
+            model.Breadcrumb.BreadcrumbItems[1].DisplayName.Should().Be(BreadcrumbContent.Provider_Find_Provider);
         }
     }
 }
