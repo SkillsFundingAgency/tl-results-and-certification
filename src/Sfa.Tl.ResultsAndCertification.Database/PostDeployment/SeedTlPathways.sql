d4245827981ba13f371d3ce07760e71b4c5daf8b
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-05-14 14:44:58 +0100

    TLRC - 789 - Update T Level titles - TLRC - 869 - Dev - Updated TLevel Titles for wave 2

--- src/Sfa.Tl.ResultsAndCertification.Database/PostDeployment/SeedTlPathways.sql
+++ src/Sfa.Tl.ResultsAndCertification.Database/PostDeployment/SeedTlPathways.sql
@@ -7,15 +7,15 @@ SET IDENTITY_INSERT [dbo].[TlPathway] ON
 MERGE INTO [dbo].[TlPathway] AS Target 
 USING (VALUES 
   (1, N'10123456', N'T Level in Design, Surveying and Planning for Construction', N'Design, Surveying and Planning', 1),
-  (2, N'10223456', N'Construction: Onsite Construction', N'Onsite Construction', 1),
-  (3, N'10323456', N'Construction: Building Services Engineering', N'Building Services Engineering', 1),
+  (2, N'10223456', N'T Level in Onsite Construction', N'Onsite Construction', 1),
+  (3, N'10323456', N'T Level in Building Services Engineering for Construction', N'Building Services Engineering', 1),
   (4, N'10423456', N'T Level in Education and Childcare', N'Education and Childcare', 2),
   (5, N'10523456', N'T Level in Digital Production, Design and Development', N'Digital Production, Design and Development', 3),
-  (6, N'10623456', N'Digital: Digital Support Services', N'Digital Support Services', 3),
-  (7, N'10723456', N'Digital: Digital Business Services', N'Digital Business Services', 3),
-  (8, N'10823456', N'Health and Science: Health', N'Health', 4),
-  (9, N'10923456', N'Health and Science: Healthcare Science', N'Healthcare Science', 4),
-  (10, N'11023456', N'Health and Science: Science', N'Science', 4)
+  (6, N'10623456', N'T Level in Digital Support and Services', N'Digital Support Services', 3),
+  (7, N'10723456', N'T Level in Digital Business Services', N'Digital Business Services', 3),
+  (8, N'10823456', N'T Level in Health', N'Health', 4),
+  (9, N'10923456', N'T Level in Healthcare Science', N'Healthcare Science', 4),
+  (10, N'11023456', N'T Level in Science', N'Science', 4)
   )
   AS Source ([Id], [LarId], [TlevelTitle], [Name], [TlRouteId]) 
 ON Target.[Id] = Source.[Id] 
