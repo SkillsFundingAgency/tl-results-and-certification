db163bbd873d0864da78dc59dbc3023032d0c89a
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-05-18 09:43:48 +0100

    TLRC - 827 - User landing on Provider's T Levels page from Find a Provider Page (when T Levels have already been added) - TLRC - 878 - Dev - Added Unit Test for Api Client HasAnyTlevelSetupForProviderAsync method

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Api.Client.UnitTests/Clients/ResultsAndCertificationInternalApiClientTest/When_HasAnyTlevelSetupForProviderAsync_Is_Called.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Api.Client.Clients;
+using Sfa.Tl.ResultsAndCertification.Api.Client.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Configuration;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Api.Client.UnitTests.Clients.ResultsAndCertificationInternalApiClientTest
+{
+    public class When_HasAnyTlevelSetupForProviderAsync_Is_Called : BaseTest<ResultsAndCertificationInternalApiClient>
+    {
+        // Dependencies
+        private ITokenServiceClient _tokenServiceClient;
+        private ResultsAndCertificationConfiguration _configuration;
+        private ResultsAndCertificationInternalApiClient _apiClient;
+        private Task<bool> _result;
+        private int _tlProviderId = 1;
+        private bool _mockHttpResult;
+
+        // Method Parameters
+        private readonly long _ukprn = 12345678;
+
+        public override void Setup()
+        {
+            _tokenServiceClient = Substitute.For<ITokenServiceClient>();
+            _configuration = new ResultsAndCertificationConfiguration
+            {
+                ResultsAndCertificationInternalApiSettings = new ResultsAndCertificationInternalApiSettings { Uri = "http://provider.api.com" }
+            };
+
+            _mockHttpResult = true;
+        }
+
+        public override void Given()
+        {
+            HttpClient = new HttpClient(new MockHttpMessageHandler<bool>(_mockHttpResult, string.Format(ApiConstants.HasAnyTlevelSetupForProviderAsyncUri, _ukprn, _tlProviderId), HttpStatusCode.OK));
+            _apiClient = new ResultsAndCertificationInternalApiClient(HttpClient, _tokenServiceClient, _configuration);
+        }
+
+        public override void When()
+        {
+            _result = _apiClient.HasAnyTlevelSetupForProviderAsync(_ukprn, _tlProviderId);
+        }
+
+        [Fact]
+        public void Then_True_Is_Returned()
+        {
+            _result.Result.Should().BeTrue();
+        }
+    }
+}
