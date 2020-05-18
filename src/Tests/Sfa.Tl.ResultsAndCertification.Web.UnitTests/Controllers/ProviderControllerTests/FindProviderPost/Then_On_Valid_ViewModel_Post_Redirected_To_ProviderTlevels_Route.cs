222ca506257ccf7c5c9b0627e5c722508a2f5f60
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-05-18 13:26:05 +0100

    TLRC - 827 - User landing on Provider's T Levels page from Find a Provider Page (when T Levels have already been added) - TLRC - 876 - Dev - Added Unit Test for FindProviderPost method

--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderControllerTests/FindProviderPost/Then_On_Valid_ViewModel_Post_Redirected_To_ProviderTlevels_Route.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderControllerTests/FindProviderPost/Then_On_Valid_ViewModel_Post_Redirected_To_ProviderTlevels_Route.cs
@@ -6,18 +6,18 @@ using Xunit;
 
 namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderControllerTests.FindProviderPost
 {
-    public class Then_On_Valid_ViewModel_Post_Redirected_To_SelectToProviderTlevels_Route : When_FindProviderAsync_Post_Action_Is_Called
+    public class Then_On_Valid_ViewModel_Post_Redirected_To_ProviderTlevels_Route : When_FindProviderAsync_Post_Action_Is_Called
     {
         public override void Given()
         {
-            ProviderLoader.HasAnyTlevelSetupForProviderAsync(Ukprn, SelectedProviderId).Returns(false);
+            ProviderLoader.HasAnyTlevelSetupForProviderAsync(Ukprn, SelectedProviderId).Returns(true);
         }
 
         [Fact]
-        public void Then_Redirected_ToSelectProviderTLevels()
+        public void Then_ModelState_Valid_Redirected_To_ProviderTlevels()
         {
             var routeName = (Result.Result as RedirectToRouteResult).RouteName;
-            routeName.Should().Be(RouteConstants.SelectProviderTlevels);
+            routeName.Should().Be(RouteConstants.ProviderTlevels);
         }
     }
 }
