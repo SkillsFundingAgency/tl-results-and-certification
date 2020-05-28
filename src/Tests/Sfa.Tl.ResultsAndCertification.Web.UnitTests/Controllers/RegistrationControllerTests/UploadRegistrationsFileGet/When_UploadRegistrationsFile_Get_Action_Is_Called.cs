e6435b1e7df5ce53624989bb0bd7c8de803237ec
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-05-29 00:24:54 +0100

    TLRC - 794 - Create 'Upload registrations file' page (Bulk Upload) - TLRC - 956 - Dev - Added Web UnitTests for UploadRegistrationsFileGet

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/RegistrationControllerTests/UploadRegistrationsFileGet/When_UploadRegistrationsFile_Get_Action_Is_Called.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
+using Sfa.Tl.ResultsAndCertification.Web.Loader.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.RegistrationControllerTests.UploadRegistrationsFileGet
+{
+    public abstract class When_UploadRegistrationsFile_Get_Action_Is_Called : BaseTest<RegistrationController>
+    {
+        protected IRegistrationLoader RegistrationLoader;
+        protected ILogger<RegistrationController> Logger;
+        protected RegistrationController Controller;
+        protected UploadRegistrationsFileViewModel ViewModel;
+        public IActionResult Result { get; private set; }
+
+        public override void Setup()
+        {
+            RegistrationLoader = Substitute.For<IRegistrationLoader>();
+            Logger = Substitute.For<ILogger<RegistrationController>>();
+            Controller = new RegistrationController(RegistrationLoader, Logger);
+            ViewModel = new UploadRegistrationsFileViewModel();
+        }
+
+        public override void When()
+        {
+            Result = Controller.UploadRegistrationsFile();
+        }
+    }
+}
