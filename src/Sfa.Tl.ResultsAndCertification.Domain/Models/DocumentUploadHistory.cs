a251553b0a7d249b4505c51aa3fceefe665c645b
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-06-14 22:21:18 +0100

    TLRC - 895 - Implement Error Reporting for Failed Uploads (Stage 2 Validations) - TLRC - 1041 - Dev - Implemented DocumentUploadHistory Table and entities

---
+++ src/Sfa.Tl.ResultsAndCertification.Domain/Models/DocumentUploadHistory.cs
@@ -0,0 +1,13 @@
+﻿namespace Sfa.Tl.ResultsAndCertification.Domain.Models
+{
+    public partial class DocumentUploadHistory : BaseEntity
+    {
+        public int TlAwardingOrganisationId { get; set; }
+        public string BlobFileName { get; set; }
+        public int DocumentType { get; set; }
+        public int FileType { get; set; }
+        public int Status { get; set; }
+
+        public virtual TlAwardingOrganisation TlAwardingOrganisation { get; set; }
+    }
+}
