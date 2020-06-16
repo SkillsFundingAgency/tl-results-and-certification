75fe4a5b19846c8ae789b17e65075e0ef2d481ec
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-06-16 16:31:40 +0100

    TLRC - 895 - Implement Error Reporting for Failed Uploads (Stage 2 Validations) - TLRC - 1049 - Dev -  Added Unit Test for Registration Loader - ProcessBulkRegistrationsAsync method

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/RegistrationLoaderTests/ProcessBulkRegistrationsAsync/Then_Expected_ApiResponse_Is_Returned.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.RegistrationLoaderTests.ProcessBulkRegistrationsAsync
+{
+    public class Then_Expected_ApiResponse_Is_Returned : When_ProcessBulkRegistrationsAsync_Is_Called
+    {
+        [Fact]
+        public void Then_Expected_UploadRegistrationResponse_Are_Returned()
+        {
+            ActualResult.Should().NotBeNull();
+
+            ActualResult.IsSuccess.Should().Be(UploadRegistrationsResponseViewModel.IsSuccess);
+            ActualResult.ErrorFileSize.Should().Be(UploadRegistrationsResponseViewModel.ErrorFileSize);
+            ActualResult.BlobUniqueReference.Should().Be(UploadRegistrationsResponseViewModel.BlobUniqueReference);
+        }
+    }
+}
