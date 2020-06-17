03868c99b6583dae6760babb9b102128ade66533
Ravi Bobbili <ravi.bob@outlook.com>
2020-06-18 00:08:30 +0100

    TLRC-895: Stage 2 validations, TLRC-1023 CsvHelper Service unit test added.

---
+++ src/Sfa.Tl.ResultsAndCertification.Common.Services.UnitTests/CsvHelper/Service/CsvHelperServiceTests/Then_Invalid_Header_Return_IsDirty_Validation_Error.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Common.Services.CsvHelper.Helpers.Constants;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Common.Services.UnitTests.CsvHelper.Service.CsvHelperServiceTests
+{
+    public class Then_Invalid_Header_Return_IsDirty_Validation_Error : When_ReadAndParseFileAsync_Is_Called
+    {
+        public override void Given()
+        {
+            InputStream = new MemoryStream(Encoding.ASCII.GetBytes("Test File"));
+        }
+
+        [Fact]
+        public void Then_Returns_Header_Not_Found_Error()
+        {
+            Response.Result.IsDirty.Should().BeFalse();
+            Response.Result.Rows.Count().Should().Be(0);
+            Response.Result.ErrorMessage.Should().Be(ValidationMessages.FileHeaderNotFound);
+        }
+    }
+}
