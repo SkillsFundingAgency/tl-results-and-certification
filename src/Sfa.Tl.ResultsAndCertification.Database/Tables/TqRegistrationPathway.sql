2959812cd041d21f9af6adc8d56297e2a7cde68e
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-06-23 22:25:44 +0100

    TLRC - 999 - Implement Error Reporting for Failed Validations (Stage 4 Validations)  - TLRC - 1071 - Dev - Create Database Tables related to Registrations

---
+++ src/Sfa.Tl.ResultsAndCertification.Database/Tables/TqRegistrationPathway.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[TqRegistrationPathway]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL, 
+	[TqRegistrationProfileId] INT NOT NULL,
+	[TqProviderId] INT NOT NULL,
+	[AcademicYear] INT NOT NULL,
+	[StartDate] DATETIME NOT NULL,	
+	[EndDate] DATETIME NULL,
+	[CourseStatus] INT NOT NULL,
+	[IsBulkUpload] BIT NOT NULL DEFAULT 0,
+	[CreatedOn] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [CreatedBy] NVARCHAR(50) NULL, 
+    [ModifiedOn] DATETIME2 NULL, 
+    [ModifiedBy] NVARCHAR(50) NULL,
+	CONSTRAINT [PK_TqRegistrationPathway] PRIMARY KEY ([Id]),
+	CONSTRAINT [FK_TqRegistrationPathway_TqRegistrationProfile] FOREIGN KEY ([TqRegistrationProfileId]) REFERENCES [TqRegistrationProfile]([Id]),
+	CONSTRAINT [FK_TqRegistrationPathway_TqProvider] FOREIGN KEY ([TqProviderId]) REFERENCES [TqProvider]([Id]),
+	INDEX IX_TqRegistrationPathway_RegistrationProfileId NONCLUSTERED (TqRegistrationProfileId)
+)
