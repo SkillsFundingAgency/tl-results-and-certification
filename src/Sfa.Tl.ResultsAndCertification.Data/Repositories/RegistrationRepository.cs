d617a3cb34921aeddbecd11675088bab172fd113
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-06-25 21:38:19 +0100

    TLRC - 949 - Successful Validations, Overwriting Data and Database Save  - TLRC - 1084 - Dev - Implemented Bulk Upload Functionality in Registration Repository

--- src/Sfa.Tl.ResultsAndCertification.Data/Repositories/RegistrationRepository.cs
+++ src/Sfa.Tl.ResultsAndCertification.Data/Repositories/RegistrationRepository.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EFCore.BulkExtensions;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Sfa.Tl.ResultsAndCertification.Data.Interfaces;
 using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,8 +12,12 @@ namespace Sfa.Tl.ResultsAndCertification.Data.Repositories
 {
     public class RegistrationRepository : GenericRepository<TqRegistrationProfile>, IRegistrationRepository
     {
-        public RegistrationRepository(ILogger<RegistrationRepository> logger, ResultsAndCertificationDbContext dbContext) : base(logger, dbContext) { }
-
+        private ILogger<RegistrationRepository> _logger;
+        public RegistrationRepository(ILogger<RegistrationRepository> logger, ResultsAndCertificationDbContext dbContext) : base(logger, dbContext) 
+        {
+            _logger = logger;
+        }
+        
         public async Task<IList<TqRegistrationProfile>> GetRegistrationProfilesAsync(IList<TqRegistrationProfile> registrations)
         {
             var ulns = new HashSet<long>();
@@ -24,6 +30,71 @@ namespace Sfa.Tl.ResultsAndCertification.Data.Repositories
                     .ThenInclude(x => x.TqProvider)
                         .ThenInclude(x => x.TqAwardingOrganisation)
                 .ToListAsync();
+        }
+
+        public async Task<bool> BulkInsertOrUpdateTqRegistrations(List<TqRegistrationProfile> profileEntities, List<TqRegistrationPathway> pathwayEntities, List<TqRegistrationSpecialism> specialismEntities)
+        {
+            var result = false;
+            if ((profileEntities != null && profileEntities.Count > 0) || (pathwayEntities != null && pathwayEntities.Count > 0) || (specialismEntities != null && specialismEntities.Count > 0))
+            {
+                var strategy = _dbContext.Database.CreateExecutionStrategy();
+                await strategy.ExecuteAsync(async () =>
+                {
+                    using (var transaction = await _dbContext.Database.BeginTransactionAsync())
+                    {
+                        try
+                        {
+                            var bulkConfig = new BulkConfig() { UseTempDB = true, PreserveInsertOrder = true, SetOutputIdentity = true, BatchSize = 4000 };
+                            var pathwayRegistrations = pathwayEntities ?? new List<TqRegistrationPathway>();
+
+                            if (profileEntities != null && profileEntities.Count > 0)
+                            {
+                                await _dbContext.BulkInsertOrUpdateAsync(profileEntities, bulkConfig);
+
+                                profileEntities.ToList().ForEach(profile =>
+                                {
+                                    profile.TqRegistrationPathways.ToList().ForEach(pathway =>
+                                    {
+                                        pathway.TqRegistrationProfileId = profile.Id;
+                                        pathwayRegistrations.Add(pathway);
+                                    });
+                                });
+                            }
+
+                            var specialismRegistrations = specialismEntities ?? new List<TqRegistrationSpecialism>();
+
+                            if (pathwayRegistrations.Count > 0)
+                            {
+                                await _dbContext.BulkInsertOrUpdateAsync(pathwayRegistrations, bulkConfig);
+
+                                pathwayRegistrations.ForEach(pathway =>
+                                {
+                                    pathway.TqRegistrationSpecialisms.ToList().ForEach(specialism =>
+                                    {
+                                        specialism.TqRegistrationPathwayId = pathway.Id;
+                                        specialismRegistrations.Add(specialism);
+                                    });
+                                });
+                            }
+
+                            if (specialismRegistrations.Count > 0)
+                            {
+                                await _dbContext.BulkInsertOrUpdateAsync(specialismRegistrations, bulkConfig => { bulkConfig.UseTempDB = true; bulkConfig.BatchSize = 4000; });
+                            }
+
+                            transaction.Commit();
+                            result = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.LogError(ex.Message, ex.InnerException);
+                            transaction.Rollback();
+                            throw;
+                        }
+                    }
+                });
+            }
+            return result;
         }
     }
 }
