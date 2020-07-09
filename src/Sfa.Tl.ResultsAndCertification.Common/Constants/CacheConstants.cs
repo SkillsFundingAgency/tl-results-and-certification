eb8785591cf2c66d4155cdb33ad21395f030beed
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-07-09 12:08:56 +0100

    TLRC - 1066 - Technical Story - Redis Cache - TLRC - 1136  - Dev - Implemented Redis cache - TLRC - 1137  - Dev - Implemented Generic methods for Cache - TLRC - 1142  - Dev - Integrated Redis cache with current solution

---
+++ src/Sfa.Tl.ResultsAndCertification.Common/Constants/CacheConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sfa.Tl.ResultsAndCertification.Common.Constants
+{
+    public static class CacheConstants
+    {
+        public static TimeSpan SmallCacheTime = new TimeSpan(0, 0, 30, 0);
+        public static TimeSpan MediumCacheTime = new TimeSpan(0, 0, 60, 0);
+        public static TimeSpan LargeCacheTime = new TimeSpan(0, 0, 20, 0);
+
+        public const string RegistrationCacheKey = "Registration";
+    }
+}
---
+++ src/Sfa.Tl.ResultsAndCertification.Common/Helpers/CacheKeyHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Sfa.Tl.ResultsAndCertification.Common.Helpers
+{
+    public class CacheKeyHelper
+    {
+        public static string GetCacheKey(string userId, string key)
+        {
+            return $"UserId:{userId}:{key}";
+        }
+    }
+}
