533a2f0222bcc7d3273be34afb9e2a8a8913fef0
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-07-13 16:16:28 +0100

    TLRC - 796 - Create 'Add a new registration' page - Select the provider Single Upload - TLRC - 1166  - Dev - Implemented Backlink

--- src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/Registration/Manual/SelectProviderViewModel.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/Registration/Manual/SelectProviderViewModel.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
-using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Provider;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewComponents.BackLink;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using ErrorResource = Sfa.Tl.ResultsAndCertification.Web.Content.Registration;
 
 namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual
@@ -12,15 +12,17 @@ namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual
         [Required(ErrorMessageResourceType = typeof(ErrorResource.SelectProvider), ErrorMessageResourceName = "Validation_Select_Provider_Required")]
         public string SelectedProviderId { get; set; }
 
-        //public IList<ProviderDetailsViewModel> RegistrationProviders { get; set; }
-
         public IList<SelectListItem> ProvidersSelectList { get; set; }
 
-        //public IList<SelectListItem> ProvidersSelectList => RegistrationProviders?.Select(p =>
-        //    new SelectListItem
-        //    {
-        //        Value = p.ProviderId.ToString(),
-        //        Text = p.DisplayNameWithUkprn
-        //    }).ToList();
+        public BackLinkModel BackLink
+        {
+            get
+            {
+                return new BackLinkModel
+                {
+                    RouteName = RouteConstants.AddRegistrationDateofBirth,
+                };
+            }
+        }
     }
 }
