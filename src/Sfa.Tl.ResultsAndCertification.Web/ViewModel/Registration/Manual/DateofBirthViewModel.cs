541e89cac031dd079212499a116401670fb36742
Ravi Bobbili <ravi.bob@outlook.com>
2020-07-14 10:10:54 +0100

    TLRC-939: New Reg-DateOfBirth - TLRC1177: Navigation links are integrated.

--- src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/Registration/Manual/DateofBirthViewModel.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/Registration/Manual/DateofBirthViewModel.cs
@@ -1,9 +1,29 @@
-﻿using System;
+﻿using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewComponents.BackLink;
+using System;
 
 namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual
 {
     public class DateofBirthViewModel
     {
+        public string Day { get; set; }
+
+        public string Month { get; set; }
+        
+        public string Year { get; set; }
+
+        //public DateTime DateofBirth { get { return string.Concat(Day, Month, Year).ParseStringToDateTime(); } }
         public DateTime DateofBirth { get; set; }
+
+        public BackLinkModel BackLink
+        {
+            get
+            {
+                return new BackLinkModel
+                {
+                    RouteName = RouteConstants.AddRegistrationLearnersName
+                };
+            }
+        }
     }
 }
