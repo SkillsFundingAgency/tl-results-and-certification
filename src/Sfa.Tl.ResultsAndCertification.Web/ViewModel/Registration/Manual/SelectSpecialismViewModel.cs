465dd3aa1b59d6c1c468a1524bb40dd7ff6e50a1
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-07-15 12:24:31 +0100

    TLRC - 799 - Create 'Add a new registration' page - Specialism Single Upload - TLRC - 1215 - Dev - Created models for Specialisms

---
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/Registration/Manual/SelectSpecialismViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewComponents.BackLink;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using ErrorResource = Sfa.Tl.ResultsAndCertification.Web.Content.Registration;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual
+{
+    public class SelectSpecialismViewModel
+    {
+        [Required(ErrorMessageResourceType = typeof(ErrorResource.SelectSpecialism), ErrorMessageResourceName = "Validation_Select_Specialism_Required_Message")]
+        public bool? HasSpecialismSelected => (Specialisms.Any(x => x.IsSelected) == true) ? true : (bool?)null;
+
+        public List<SpecialismDetailsViewModel> Specialisms { get; set; }
+
+        public BackLinkModel BackLink
+        {
+            get
+            {
+                return new BackLinkModel
+                {
+                    RouteName = RouteConstants.AddRegistrationSpecialismQuestion
+                };
+            }
+        }
+    }
+}
---
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/SpecialismDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel
+{
+    public class SpecialismDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
