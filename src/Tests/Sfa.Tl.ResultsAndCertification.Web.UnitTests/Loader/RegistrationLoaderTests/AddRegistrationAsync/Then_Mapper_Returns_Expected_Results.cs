69999dfef770d3f54cb0b52388e2dc320fba6bf9
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-07-23 17:35:07 +0100

    TLRC - 911 - Create Confirmation Page for Successful Registration (Manual Upload) - TLRC - 1243 - Dev - Added Web LoaderUnitTests for AddRegistrationAsync

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/RegistrationLoaderTests/AddRegistrationAsync/Then_Mapper_Returns_Expected_Results.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Common.Extensions;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using System.Linq;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.RegistrationLoaderTests.AddRegistrationAsync
+{
+    public class Then_Mapper_Returns_Expected_Results : When_AddRegistrationAsync_Is_Called
+    {
+        [Fact]
+        public void Then_Mapper_Has_Expected_Results()
+        {
+            var result = Mapper.Map<RegistrationRequest>(RegistrationViewModel, opt => opt.Items["aoUkprn"] = AoUkprn);
+
+            result.Should().NotBeNull();
+
+            result.AoUkprn.Should().Be(AoUkprn);
+            result.Uln.Should().Be(RegistrationViewModel.Uln.Uln.ToLong());
+            result.FirstName.Should().Be(RegistrationViewModel.LearnersName.Firstname);
+            result.LastName.Should().Be(RegistrationViewModel.LearnersName.Lastname);
+            result.DateOfBirth.Should().Be($"{RegistrationViewModel.DateofBirth.Day}/{RegistrationViewModel.DateofBirth.Month}/{RegistrationViewModel.DateofBirth.Year}".ToDateTime());
+            result.ProviderUkprn.Should().Be(RegistrationViewModel.SelectProvider.SelectedProviderUkprn.ToLong());
+            result.CoreCode.Should().Be(RegistrationViewModel.SelectCore.SelectedCoreCode);
+            result.SpecialismCodes.Should().BeEquivalentTo(RegistrationViewModel.SelectSpecialism.PathwaySpecialisms.Specialisms.Where(x => x.IsSelected).Select(s => s.Code));
+            result.CreatedBy.Should().Be($"{Givenname} {Surname}");
+        }
+    }
+}
