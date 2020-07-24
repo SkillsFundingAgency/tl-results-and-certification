fbf761e6ab6b2e4cd2c736ac9cdd6b1969f3b879
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-07-24 19:15:39 +0100

    TLRC - 772 - Create 'Search for a registration' page - TLRC - 1259 - Dev - Added Web Controller Tests for SearchRegistrationNotFound method

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/RegistrationControllerTests/SearchRegistrationNotFound/Then_Expected_Results_Are_Returned.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.RegistrationControllerTests.SearchRegistrationNotFound
+{
+    public class Then_Expected_Results_Are_Returned : When_SearchRegistrationNotFound_Is_Called
+    {
+        public override void Given()
+        {
+            TempData.Set(Constants.SearchRegistrationUlnNotFound, new UlnNotFoundViewModel { Uln = Uln });
+        }
+
+        [Fact]
+        public void Then_Expected_ViewModel_Results_Are_Returned()
+        {
+            var viewResult = Result as ViewResult;
+            var model = viewResult.Model as UlnNotFoundViewModel;
+
+            model.Should().NotBeNull();
+            model.Uln.Should().Be(Uln);
+
+            model.BackLink.Should().NotBeNull();
+            model.BackLink.RouteName.Should().Be(RouteConstants.SearchRegistration);
+            model.BackLink.RouteAttributes.Should().BeEmpty();
+        }
+    }
+}
