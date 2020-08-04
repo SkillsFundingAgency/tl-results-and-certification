15c605b7c2094a3df8050eab81b9757a536d84db
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-08-04 17:31:01 +0100

    TLRC - 1064 - Seed Core & Specialism Codes - TLRC - 1321 - Dev - Seeded Core and Specialism codes

--- src/Sfa.Tl.ResultsAndCertification.Database/PostDeployment/SeedTlPathways.sql
+++ src/Sfa.Tl.ResultsAndCertification.Database/PostDeployment/SeedTlPathways.sql
@@ -6,11 +6,11 @@ SET IDENTITY_INSERT [dbo].[TlPathway] ON
 
 MERGE INTO [dbo].[TlPathway] AS Target 
 USING (VALUES 
-  (1, N'10123456', N'T Level in Design, Surveying and Planning for Construction', N'Design, Surveying and Planning', 1),
+  (1, N'60358300', N'T Level in Design, Surveying and Planning for Construction', N'Design, Surveying and Planning', 1),
   (2, N'10223456', N'T Level in Onsite Construction', N'Onsite Construction', 1),
   (3, N'10323456', N'T Level in Building Services Engineering for Construction', N'Building Services Engineering', 1),
-  (4, N'10423456', N'T Level in Education and Childcare', N'Education and Childcare', 2),
-  (5, N'10523456', N'T Level in Digital Production, Design and Development', N'Digital Production, Design and Development', 3),
+  (4, N'60358294', N'T Level in Education and Childcare', N'Education and Childcare', 2),
+  (5, N'60358324', N'T Level in Digital Production, Design and Development', N'Digital Production, Design and Development', 3),
   (6, N'10623456', N'T Level in Digital Support and Services', N'Digital Support Services', 3),
   (7, N'10723456', N'T Level in Digital Business Services', N'Digital Business Services', 3),
   (8, N'10823456', N'T Level in Health', N'Health', 4),
