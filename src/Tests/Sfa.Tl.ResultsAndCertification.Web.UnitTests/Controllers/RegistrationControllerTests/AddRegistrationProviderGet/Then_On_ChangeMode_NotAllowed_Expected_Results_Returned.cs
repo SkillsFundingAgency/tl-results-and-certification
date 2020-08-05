362e7514e09ff292c0cc2d33078f8d07bd0ad404
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-08-05 18:58:59 +0100

    TLRC - 1111 - Change Provider on Check and submit page - TLRC - 1369 - Dev - Added Unit Test for AddRegistrationProviderAsync when changemode is selected

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/RegistrationControllerTests/AddRegistrationProviderGet/Then_On_ChangeMode_NotAllowed_Expected_Results_Returned.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.RegistrationControllerTests.AddRegistrationProviderGet
+{
+    public class Then_On_ChangeMode_NotAllowed_Expected_Results_Returned : When_AddRegistrationProviderAsync_Get_Action_Is_Called
+    {
+        private RegistrationViewModel cacheResult;
+        private UlnViewModel _ulnViewModel;
+        private LearnersNameViewModel _learnersNameViewModel;
+        private DateofBirthViewModel _dateofBirthViewModel;
+        private SelectProviderViewModel _selectProviderViewModel;
+
+        public override void Given()
+        {
+            IsChangeMode = true;
+            _ulnViewModel = new UlnViewModel { Uln = "1234567890" };
+            _learnersNameViewModel = new LearnersNameViewModel { Firstname = "First", Lastname = "Last" };
+            _dateofBirthViewModel = new DateofBirthViewModel { Day = "01", Month = "01", Year = "2020" };
+            _selectProviderViewModel = new SelectProviderViewModel { SelectedProviderUkprn = "98765432", SelectedProviderDisplayName = "Barnsley College (98765432)", ProvidersSelectList = new List<SelectListItem> { new SelectListItem { Text = "Barnsley College (98765432)", Value = "98765432" } } };
+
+            cacheResult = new RegistrationViewModel
+            {
+                Uln = _ulnViewModel,
+                LearnersName = _learnersNameViewModel,
+                DateofBirth = _dateofBirthViewModel,
+                SelectProvider = _selectProviderViewModel
+            };
+
+            RegistrationLoader.GetRegisteredTqAoProviderDetailsAsync(Ukprn).Returns(_selectProviderViewModel);
+            CacheService.GetAsync<RegistrationViewModel>(CacheKey).Returns(cacheResult);
+        }
+
+        [Fact]
+        public void Then_Expected_Results_Are_Returned()
+        {
+            Result.Should().NotBeNull();
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(SelectProviderViewModel));
+
+            var model = viewResult.Model as SelectProviderViewModel;
+            model.Should().NotBeNull();
+            model.ProvidersSelectList.Should().NotBeNull();
+            model.ProvidersSelectList.Count.Should().Be(_selectProviderViewModel.ProvidersSelectList.Count);
+            model.IsChangeMode.Should().BeFalse();
+
+            model.BackLink.Should().NotBeNull();
+            model.BackLink.RouteName.Should().Be(RouteConstants.AddRegistrationDateofBirth);
+        }
+    }
+}
