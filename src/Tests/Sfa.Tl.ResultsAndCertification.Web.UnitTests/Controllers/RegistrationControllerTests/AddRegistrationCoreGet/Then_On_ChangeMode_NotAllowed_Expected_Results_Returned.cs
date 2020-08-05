fd51b3010642f47ad887cf684a0c1d8b8f6d7e56
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-08-05 19:43:33 +0100

    TLRC - 1112 - Change Core on Check and submit page - TLRC - 1377 - Dev - Added Unit Test for AddRegistrationCoreAsync when changemode is selected

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/RegistrationControllerTests/AddRegistrationCoreGet/Then_On_ChangeMode_NotAllowed_Expected_Results_Returned.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.RegistrationControllerTests.AddRegistrationCoreGet
+{
+    public class Then_On_ChangeMode_NotAllowed_Expected_Results_Returned : When_AddRegistrationCoreAsync_Action_Is_Called
+    {
+        private RegistrationViewModel cacheResult;
+        private UlnViewModel _ulnViewModel;
+        private LearnersNameViewModel _learnersNameViewModel;
+        private DateofBirthViewModel _dateofBirthViewModel;
+        private SelectProviderViewModel _selectProviderViewModel;
+        private SelectCoreViewModel _selectCoreViewModel;
+        private long _providerUkprn = 987654321;
+
+        public override void Given()
+        {
+            IsChangeMode = true;
+            _ulnViewModel = new UlnViewModel { Uln = "1234567890" };
+            _learnersNameViewModel = new LearnersNameViewModel { Firstname = "First", Lastname = "Last" };
+            _dateofBirthViewModel = new DateofBirthViewModel { Day = "01", Month = "01", Year = "2020" };
+            _selectProviderViewModel = new SelectProviderViewModel { SelectedProviderUkprn = _providerUkprn.ToString(), SelectedProviderDisplayName = "Barnsley College (98765432)", ProvidersSelectList = new List<SelectListItem> { new SelectListItem { Text = "Barnsley College (98765432)", Value = _providerUkprn.ToString() } } };
+            _selectCoreViewModel = new SelectCoreViewModel { SelectedCoreCode = "123", CoreSelectList = new List<SelectListItem> { new SelectListItem { Text = "Education", Value = "123" } } };
+
+            cacheResult = new RegistrationViewModel
+            {
+                Uln = _ulnViewModel,
+                LearnersName = _learnersNameViewModel,
+                DateofBirth = _dateofBirthViewModel,
+                SelectProvider = _selectProviderViewModel,
+                SelectCore = _selectCoreViewModel
+            };
+
+            RegistrationLoader.GetRegisteredProviderPathwayDetailsAsync(Ukprn, _providerUkprn).Returns(_selectCoreViewModel);
+            CacheService.GetAsync<RegistrationViewModel>(CacheKey).Returns(cacheResult);
+        }
+
+        [Fact]
+        public void Then_Expected_Results_Are_Returned()
+        {
+            Result.Should().NotBeNull();
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(SelectCoreViewModel));
+
+            var model = viewResult.Model as SelectCoreViewModel;
+            model.Should().NotBeNull();
+
+            model.SelectedCoreCode.Should().Be(_selectCoreViewModel.SelectedCoreCode);
+            model.CoreSelectList.Should().NotBeNull();
+            model.CoreSelectList.Count.Should().Be(_selectCoreViewModel.CoreSelectList.Count);
+            model.IsChangeMode.Should().BeFalse();
+
+            model.BackLink.Should().NotBeNull();
+            model.BackLink.RouteName.Should().Be(RouteConstants.AddRegistrationProvider);
+        }
+    }
+}
