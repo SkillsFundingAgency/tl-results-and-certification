fba90d8e6c78b1e1eab8f5b08654fb2945bc7549
Ravi Bobbili <ravi.bob@outlook.com>
2020-08-18 16:29:24 +0100

    TLRC-1313 - Cookie preference page: TLRC-1446 - Cookies.resx file renamed to cookieDetails.resx and refactored associate changes.

--- src/Sfa.Tl.ResultsAndCertification.Web/Content/Help/CookieDetails.Designer.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Content/Help/CookieDetails.Designer.cs
@@ -22,14 +22,14 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Content.Help {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Cookies {
+    public class CookieDetails {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Cookies() {
+        internal CookieDetails() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Content.Help {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sfa.Tl.ResultsAndCertification.Web.Content.Help.Cookies", typeof(Cookies).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sfa.Tl.ResultsAndCertification.Web.Content.Help.CookieDetails", typeof(CookieDetails).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
