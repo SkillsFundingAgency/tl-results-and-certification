fc9c3cd209fb248ea8f5cefc57e4e882ecadb680
Ravi Bobbili <ravi.bob@outlook.com>
2020-08-19 09:14:21 +0100

    TLRC-1313 - Cookie preference page: TLRC-1446 - cookies.js refactored if condition.

--- src/Sfa.Tl.ResultsAndCertification.Web/Frontend/src/javascripts/cookies.js
+++ src/Sfa.Tl.ResultsAndCertification.Web/Frontend/src/javascripts/cookies.js
@@ -2,17 +2,16 @@
 
     var cookieName = 'analytics_consent';
     var options = $('input:radio[name=cookie-answer]');
-    var defaultOptionValue = $('#do-not-use-cookie');
     var saveButton = $('#SaveChanges');
     var confirmation = $('#ConfirmationBox');
+    var selectedOption = false;
 
     // Get cookie value and initialize radio option
     var consent = GOVUK.getCookie(cookieName);
-    if (consent === null || consent != 'true') {
-        defaultOptionValue.attr('checked', true);
-    } else {
-        options.filter('[value=' + consent + ']').attr('checked', true);
+    if (consent != null && consent === 'true') {
+        selectedOption = true;
     }
+    options.filter('[value=' + selectedOption + ']').attr('checked', true);
 
     saveButton.click(function () {
         var checkedValue = options.filter(':checked').val();
