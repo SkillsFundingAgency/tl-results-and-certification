390b1e9eea9387b7b9a5a354df73e0ad7c61ec1c
Ravi Bobbili <ravi.bob@outlook.com>
2020-09-08 21:22:15 +0100

    TLRC-985: Change Name confirmation  - TLRC-1562 Unit tests are added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/ProcessProfileNameChangeTests/TestSetup.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Api.Client.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Common.Services.BlobStorage.Interface;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using Sfa.Tl.ResultsAndCertification.Web.Loader;
+using Sfa.Tl.ResultsAndCertification.Web.Mapper;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.ProcessProfileNameChangeTests
+{
+    public abstract class TestSetup : BaseTest<RegistrationLoader>
+    {
+        protected readonly long AoUkprn = 12345678;
+
+        protected IResultsAndCertificationInternalApiClient InternalApiClient;
+        protected IMapper Mapper;
+        protected ILogger<RegistrationLoader> Logger;
+        public IBlobStorageService BlobStorageService { get; private set; }
+
+        protected RegistrationLoader Loader;
+        protected ChangeLearnersNameViewModel ActualResult;
+        protected 
+
+        public override void Setup()
+        {
+            Logger = Substitute.For<ILogger<RegistrationLoader>>();
+            BlobStorageService = Substitute.For<IBlobStorageService>();
+            InternalApiClient = Substitute.For<IResultsAndCertificationInternalApiClient>();
+
+            var mapperConfig = new MapperConfiguration(c => c.AddMaps(typeof(RegistrationMapper).Assembly));
+            Mapper = new AutoMapper.Mapper(mapperConfig);
+
+            Loader = new RegistrationLoader(Mapper, Logger, InternalApiClient, BlobStorageService);
+        }
+
+        public override void When()
+        {
+            ActualResult = Loader.ProcessProfileNameChangeAsync(AoUkprn, ViewModel).Result;
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/ProcessProfileNameChangeTests/When_Name_Unchanged.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.ProcessProfileNameChangeTests
+{
+    class When_Name_Unchanged
+    {
+    }
+}
