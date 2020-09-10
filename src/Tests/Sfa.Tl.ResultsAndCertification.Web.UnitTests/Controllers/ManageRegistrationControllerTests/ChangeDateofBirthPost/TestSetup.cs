5a8bf0489fff2f25ece7c521db0864c40103c122
Ravi Bobbili <ravi.bob@outlook.com>
2020-09-10 13:02:27 +0100

    TLRC-980 DateofBirth change. TLRC-1578 Unit tests added for Controller.

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ManageRegistrationControllerTests/ChangeDateofBirthPost/TestSetup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Constants;
+using Sfa.Tl.ResultsAndCertification.Common.Extensions;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Common.Services.Cache;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
+using Sfa.Tl.ResultsAndCertification.Web.Loader.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using System;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ManageRegistrationControllerTests.ChangeDateofBirthPost
+{
+    public abstract class TestSetup : BaseTest<ManageRegistrationController>
+    {
+        protected int AoUkprn;
+        protected Guid UserId;
+        protected string CacheKey;
+        protected IRegistrationLoader RegistrationLoader;
+        protected ICacheService CacheService;
+        protected ILogger<ManageRegistrationController> Logger;
+        protected ManageRegistrationController Controller;
+        protected ChangeDateofBirthViewModel ViewModel;
+        protected IHttpContextAccessor HttpContextAccessor;
+        public IActionResult Result { get; set; }
+        protected ManageRegistrationResponse MockResult;
+
+        public override void Setup()
+        {
+            HttpContextAccessor = Substitute.For<IHttpContextAccessor>();
+            RegistrationLoader = Substitute.For<IRegistrationLoader>();
+            CacheService = Substitute.For<ICacheService>();
+            Logger = Substitute.For<ILogger<ManageRegistrationController>>();
+            Controller = new ManageRegistrationController(RegistrationLoader, CacheService, Logger);
+
+            AoUkprn = 1234567890;
+            var httpContext = new ClaimsIdentityBuilder<ManageRegistrationController>(Controller)
+               .Add(CustomClaimTypes.Ukprn, AoUkprn.ToString())
+               .Add(CustomClaimTypes.UserId, Guid.NewGuid().ToString())
+               .Build()
+               .HttpContext;
+
+            HttpContextAccessor.HttpContext.Returns(httpContext);
+            CacheKey = CacheKeyHelper.GetCacheKey(httpContext.User.GetUserId(), CacheConstants.RegistrationCacheKey);
+
+            ViewModel = new ChangeDateofBirthViewModel { Day = "1", Month = "2", Year = "2000" };
+            MockResult = new ManageRegistrationResponse();
+        }
+
+        public override void When()
+        {
+            Result = Controller.ChangeDateofBirthAsync(ViewModel).Result;
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ManageRegistrationControllerTests/ChangeDateofBirthPost/When_ModelState_Invalid.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using Xunit;
+using DateContent = Sfa.Tl.ResultsAndCertification.Web.Content.Helpers.Date;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ManageRegistrationControllerTests.ChangeDateofBirthPost
+{
+    public class When_ModelState_Invalid : TestSetup
+    {
+        public override void Given()
+        {
+            ViewModel.Day = string.Empty;
+            Controller.ModelState.AddModelError("Day", DateContent.Validation_Message_Day_Required);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_ViewModel()
+        {
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(ChangeDateofBirthViewModel));
+
+            Controller.ViewData.ModelState.Should().ContainSingle();
+            Controller.ViewData.ModelState.ContainsKey(nameof(ChangeDateofBirthViewModel.Day)).Should().BeTrue();
+
+            var modelState = Controller.ViewData.ModelState[nameof(ChangeDateofBirthViewModel.Day)];
+            modelState.Errors[0].ErrorMessage.Should().Be(DateContent.Validation_Message_Day_Required);
+        }
+    }
+}
