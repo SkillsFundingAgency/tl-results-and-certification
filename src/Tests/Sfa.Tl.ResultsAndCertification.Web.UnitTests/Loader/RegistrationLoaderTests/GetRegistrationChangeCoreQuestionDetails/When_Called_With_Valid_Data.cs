7cbd488d5db6c92e1f1251cb5b453f51b42785c6
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-09-16 18:52:17 +0100

    TLRC - 1540 - Create 'Do you want to change core' page - TLRC - 1651 - Dev - Added Web Loader Unit Tests for GetRegistrationChangeCoreQuestionDetailsAsync

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/RegistrationLoaderTests/GetRegistrationChangeCoreQuestionDetails/When_Called_With_Valid_Data.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.RegistrationLoaderTests.GetRegistrationChangeCoreQuestionDetails
+{
+    public class When_Called_With_Valid_Data : TestSetup
+    {
+        public override void Given()
+        {
+            expectedApiResult = new RegistrationDetails
+            {
+                ProfileId = 1,
+                Uln = 1234567890,
+                Name = "Test",
+                DateofBirth = DateTime.UtcNow,
+                ProviderDisplayName = "Test Provider (1234567)",
+                PathwayDisplayName = "Pathway (7654321)",
+                SpecialismsDisplayName = new List<string> { "Specialism1 (2345678)", "Specialism2 (555678)" },
+                AcademicYear = 2020,
+                Status = RegistrationPathwayStatus.Active
+            };
+
+            InternalApiClient.GetRegistrationDetailsByProfileIdAsync(AoUkprn, ProfileId).Returns(expectedApiResult);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            ActualResult.Should().NotBeNull();
+            ActualResult.ProfileId.Should().Be(expectedApiResult.ProfileId);
+            ActualResult.ProviderDisplayName.Should().Be(expectedApiResult.ProviderDisplayName);
+        }
+    }
+}
