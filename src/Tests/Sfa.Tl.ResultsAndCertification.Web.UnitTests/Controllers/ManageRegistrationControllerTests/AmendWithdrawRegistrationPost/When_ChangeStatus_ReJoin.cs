994dbeccc720291f49120e87fd21632043b6ae20
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-09-22 08:18:58 +0100

    TLRC - 1476 - Create Withdrawn Options page for Withdrawn Record - TLRC - 1722 - Dev - Added Web Unit Tests for AmendWithdrawRegistrationAsyncGet and AmendWithdrawRegistrationAsyncPost

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ManageRegistrationControllerTests/AmendWithdrawRegistrationPost/When_ChangeStatus_ReJoin.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ManageRegistrationControllerTests.AmendWithdrawRegistrationPost
+{
+    public class When_ChangeStatus_ReJoin : TestSetup
+    {
+        public override void Given()
+        {
+            ViewModel.ProfileId = ProfileId;
+            ViewModel.ChangeStatus = RegistrationChangeStatus.ReJoin;
+        }
+
+        [Fact]
+        public void Then_Redirected_To_WithdrawRegistration()
+        {
+            var route = (Result as RedirectToRouteResult);
+            var routeName = route.RouteName;
+            routeName.Should().Be(RouteConstants.ReJoinRegistration);
+            route.RouteValues[Constants.ProfileId].Should().Be(ProfileId);
+        }
+    }
+}
