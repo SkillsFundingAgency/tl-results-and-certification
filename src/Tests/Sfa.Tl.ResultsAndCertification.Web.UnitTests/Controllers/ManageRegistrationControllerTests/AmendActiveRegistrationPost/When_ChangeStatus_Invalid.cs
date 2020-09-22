3043535113f904a665ef0cfd1fcb6c43a75eacb8
Ravi Bobbili <ravi.bob@outlook.com>
2020-09-22 09:30:27 +0100

    TLRC-1533 Cancel registration. TLRC-1711 - Code review comments incorporated.

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ManageRegistrationControllerTests/AmendActiveRegistrationPost/When_ChangeStatus_Invalid.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ManageRegistrationControllerTests.AmendActiveRegistrationPost
+{
+    public class When_ChangeStatus_Invalid : TestSetup
+    {
+        public override void Given()
+        {
+            ViewModel.ChangeStatus = RegistrationChangeStatus.ReJoin;
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            Result.Should().NotBeNull();
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(AmendActiveRegistrationViewModel));
+
+            var model = viewResult.Model as AmendActiveRegistrationViewModel;
+            model.Should().NotBeNull();
+
+            model.ProfileId.Should().Be(ViewModel.ProfileId);
+            model.ChangeStatus.Should().Be(ViewModel.ChangeStatus);
+
+            var backLink = model.BackLink;
+            backLink.RouteName.Should().Be(RouteConstants.RegistrationDetails);
+            backLink.RouteAttributes.Count.Should().Be(1);
+            backLink.RouteAttributes.TryGetValue(Constants.ProfileId, out string routeValue);
+            routeValue.Should().Be(ViewModel.ProfileId.ToString());
+        }
+    }
+}
