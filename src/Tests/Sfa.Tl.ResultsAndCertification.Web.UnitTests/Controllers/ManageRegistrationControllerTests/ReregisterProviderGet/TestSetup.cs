cf696c4497cfb15d6651ce4a606690d0eedff33b
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-09-24 09:22:53 +0100

    TLRC - 1488 - Create Select Provider (Re-Register) - TLRC - 1771 - Dev - Added Web Unit Tests for ReregisterProviderAsyncGet and ReregisterProviderAsyncPost methods

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ManageRegistrationControllerTests/ReregisterProviderGet/TestSetup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Constants;
+using Sfa.Tl.ResultsAndCertification.Common.Extensions;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Common.Services.Cache;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
+using Sfa.Tl.ResultsAndCertification.Web.Loader.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ManageRegistrationControllerTests.ReregisterProviderGet
+{
+    public abstract class TestSetup : BaseTest<ManageRegistrationController>
+    {
+        protected int AoUkprn;
+        protected int ProfileId;
+        protected Guid UserId;
+        protected string CacheKey;
+        protected IRegistrationLoader RegistrationLoader;
+        protected ICacheService CacheService;
+        protected ILogger<ManageRegistrationController> Logger;
+        protected ManageRegistrationController Controller;
+        protected IHttpContextAccessor HttpContextAccessor;
+        public IActionResult Result { get; private set; }
+
+        public override void Setup()
+        {
+            HttpContextAccessor = Substitute.For<IHttpContextAccessor>();
+            RegistrationLoader = Substitute.For<IRegistrationLoader>();
+            CacheService = Substitute.For<ICacheService>();
+            Logger = Substitute.For<ILogger<ManageRegistrationController>>();
+            Controller = new ManageRegistrationController(RegistrationLoader, CacheService, Logger);
+
+            ProfileId = 1;
+            AoUkprn = 1234567890;
+            var httpContext = new ClaimsIdentityBuilder<ManageRegistrationController>(Controller)
+               .Add(CustomClaimTypes.Ukprn, AoUkprn.ToString())
+               .Add(CustomClaimTypes.UserId, Guid.NewGuid().ToString())
+               .Build()
+               .HttpContext;
+
+            HttpContextAccessor.HttpContext.Returns(httpContext);
+            CacheKey = CacheKeyHelper.GetCacheKey(httpContext.User.GetUserId(), CacheConstants.ReregisterCacheKey);
+        }
+
+        public async override Task When()
+        {
+            Result = await Controller.ReregisterProviderAsync(ProfileId);
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ManageRegistrationControllerTests/ReregisterProviderPost/When_ModelState_Invalid.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using System.Collections.Generic;
+using Xunit;
+using SelectProviderContent = Sfa.Tl.ResultsAndCertification.Web.Content.Registration.SelectProvider;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ManageRegistrationControllerTests.ReregisterProviderPost
+{
+    public class When_ModelState_Invalid : TestSetup
+    {
+        private SelectProviderViewModel _selectProviderViewModel;
+
+        public override void Given()
+        {
+            _selectProviderViewModel = new SelectProviderViewModel { ProvidersSelectList = new List<SelectListItem> { new SelectListItem { Text = "Hello", Value = "1" } } };
+
+            RegistrationLoader.GetRegisteredTqAoProviderDetailsAsync(AoUkprn).Returns(_selectProviderViewModel);
+
+            Controller.ModelState.AddModelError(nameof(ReregisterProviderViewModel.SelectedProviderUkprn), SelectProviderContent.Validation_Select_Provider_Required);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_ViewModel()
+        {
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(ReregisterProviderViewModel));
+
+            var model = viewResult.Model as ReregisterProviderViewModel;
+            model.Should().NotBeNull();
+            model.ProvidersSelectList.Should().NotBeNull();
+            model.ProvidersSelectList.Count.Should().Be(_selectProviderViewModel.ProvidersSelectList.Count);
+
+            Controller.ViewData.ModelState.ContainsKey(nameof(ReregisterProviderViewModel.SelectedProviderUkprn)).Should().BeTrue();
+
+            var modelState = Controller.ViewData.ModelState[nameof(ReregisterProviderViewModel.SelectedProviderUkprn)];
+            modelState.Errors[0].ErrorMessage.Should().Be(SelectProviderContent.Validation_Select_Provider_Required);
+        }
+    }
+}
