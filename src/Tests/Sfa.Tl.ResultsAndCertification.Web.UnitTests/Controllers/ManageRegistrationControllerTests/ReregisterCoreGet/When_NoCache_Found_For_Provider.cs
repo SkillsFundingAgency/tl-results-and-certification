595fc0b25c7dc25de7995a856167cb5dfbc23e06
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-09-30 12:27:18 +0100

    TLRC - 1489 - Create Select Core Page (Re-Register) - TLRC - 1808 - Dev - Added Web Unit Tests for ReregisterCoreGet

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ManageRegistrationControllerTests/ReregisterCoreGet/When_NoCache_Found_For_Provider.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ManageRegistrationControllerTests.ReregisterCoreGet
+{
+    public class When_NoCache_Found_For_Provider : TestSetup
+    {
+        public override void Given()
+        {
+            CacheService.GetAsync<ReregisterViewModel>(CacheKey).Returns(new ReregisterViewModel());
+        }
+
+        [Fact]
+        public void Then_Redirected_To_PageNotFound()
+        {
+            var actualRouteName = (Result as RedirectToRouteResult).RouteName;
+            actualRouteName.Should().Be(RouteConstants.PageNotFound);
+        }
+    }
+}
