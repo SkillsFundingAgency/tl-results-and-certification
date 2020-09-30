592313d6c9611dc407c1cb27a23ba11f5037040b
Ravi Bobbili <ravi.bob@outlook.com>
2020-09-30 17:48:28 +0100

    TLRC-1490 - Reregister spl quest. TLRC-1816 Unit tests are added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ManageRegistrationControllerTests/ReregisterSpecialismQuestionGet/When_Cache_Found.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ManageRegistrationControllerTests.ReregisterSpecialismQuestionGet
+{
+    public class When_Cache_Found : TestSetup
+    {
+        private ReregisterViewModel cacheResult;
+        private RegistrationDetailsViewModel mockresult = null;
+        private readonly RegistrationPathwayStatus _registrationPathwayStatus = RegistrationPathwayStatus.Withdrawn;
+
+        public override void Given()
+        {
+            mockresult = new RegistrationDetailsViewModel
+            {
+                ProfileId = 1,
+                Status = _registrationPathwayStatus
+            };
+
+            cacheResult = new ReregisterViewModel { ReregisterCore = new ReregisterCoreViewModel() };
+            CacheService.GetAsync<ReregisterViewModel>(CacheKey).Returns(cacheResult);
+            RegistrationLoader.GetRegistrationDetailsAsync(AoUkprn, ProfileId, _registrationPathwayStatus).Returns(mockresult);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Called()
+        {
+            RegistrationLoader.Received(1).GetRegistrationDetailsAsync(AoUkprn, ProfileId, _registrationPathwayStatus);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            Result.Should().NotBeNull();
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(ReregisterSpecialismQuestionViewModel));
+
+            var model = viewResult.Model as ReregisterSpecialismQuestionViewModel;
+            model.Should().NotBeNull();
+
+            model.ProfileId.Should().Be(mockresult.ProfileId);
+            
+            model.BackLink.Should().NotBeNull();
+            var backLink = model.BackLink;
+            backLink.RouteName.Should().Be(RouteConstants.ReregisterCore);
+            backLink.RouteAttributes.Count.Should().Be(1);
+            backLink.RouteAttributes.TryGetValue(Constants.ProfileId, out string routeValue);
+            routeValue.Should().Be(mockresult.ProfileId.ToString());
+        }
+    }
+}
