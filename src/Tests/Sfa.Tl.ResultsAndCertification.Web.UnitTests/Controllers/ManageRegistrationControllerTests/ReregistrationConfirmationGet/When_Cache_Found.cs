775fe74bdceb36771083aebdbf023d2d5d817b5a
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-10-07 12:35:33 +0100

    TLRC - 1494 - Create Re-Register Success Page - TLRC - 1890 - Dev - Added Web Unit tests for ReregistrationConfirmationAsync method

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ManageRegistrationControllerTests/ReregistrationConfirmationGet/When_Cache_Found.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ManageRegistrationControllerTests.ReregistrationConfirmationGet
+{
+    public class When_Cache_Found : TestSetup
+    {
+        public override void Given()
+        {
+            CacheService.GetAndRemoveAsync<ReregistrationResponse>(Arg.Any<string>()).Returns(MockResult);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Result()
+        {
+            Result.Should().NotBeNull();
+            var viewResult = Result as ViewResult;
+            viewResult.Should().NotBeNull();
+
+            var actualModel = viewResult.Model as ReregistrationResponse;
+            actualModel.Uln.Should().Be(MockResult.Uln);
+            actualModel.ProfileId.Should().Be(MockResult.ProfileId);
+        }
+    }
+}
