ab5b8722b4d51102bcf4f1ee9904640a456ebfab
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-10-07 17:58:33 +0100

    TLRC - 1494 - Create Re-Register Success Page - TLRC - 1891 - Dev - Added Web Loader Unit tests for ReregistrationAsync method

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/RegistrationLoaderTests/Reregistration/When_NoData_Found.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using Sfa.Tl.ResultsAndCertification.Web.Loader;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.RegistrationLoaderTests.Reregistration
+{
+    public class When_NoData_Found : TestSetup
+    {
+        readonly RegistrationDetails mockRegDetails = null;
+        private string _coreCode;
+        public override void Given()
+        {
+            _coreCode = "10000111";
+            ViewModel = new ReregisterViewModel
+            {
+                ReregisterProvider = new ReregisterProviderViewModel { ProfileId = ProfileId },
+                ReregisterCore = new ReregisterCoreViewModel { ProfileId = ProfileId, SelectedCoreCode = _coreCode }
+            };
+            InternalApiClient.GetRegistrationDetailsAsync(AoUkprn, ProfileId, RegistrationPathwayStatus.Withdrawn).Returns(mockRegDetails);
+            Loader = new RegistrationLoader(Mapper, Logger, InternalApiClient, BlobStorageService);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Called()
+        {
+            InternalApiClient.Received(1).GetRegistrationDetailsAsync(AoUkprn, ProfileId, RegistrationPathwayStatus.Withdrawn);
+        }
+
+        [Fact]
+        public void Then_Returns_Null()
+        {
+            ActualResult.Should().BeNull();
+        }
+    }
+}
