adcb912cc6b34cd381585a5f127f3986017c7788
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-10-07 18:13:41 +0100

    TLRC - 1494 - Create Re-Register Success Page - TLRC - 1892 - Dev - Added Api Client Unit tests for ReregistrationAsync method

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Api.Client.UnitTests/Clients/ResultsAndCertificationInternalApiClientTest/When_Reregistration_Called.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Api.Client.Clients;
+using Sfa.Tl.ResultsAndCertification.Api.Client.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Configuration;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Api.Client.UnitTests.Clients.ResultsAndCertificationInternalApiClientTest
+{
+    public class When_Reregistration_Called : BaseTest<ResultsAndCertificationInternalApiClient>
+    {
+        private bool _result;
+        protected bool _mockHttpResult;
+        private ITokenServiceClient _tokenServiceClient;
+        private ResultsAndCertificationConfiguration _configuration;
+        private ResultsAndCertificationInternalApiClient _apiClient;
+        private ReregistrationRequest _model;
+
+        public override void Setup()
+        {
+            _tokenServiceClient = Substitute.For<ITokenServiceClient>();
+
+            _configuration = new ResultsAndCertificationConfiguration
+            {
+                ResultsAndCertificationInternalApiSettings = new ResultsAndCertificationInternalApiSettings { Uri = "http://tlevel.api.com" }
+            };
+
+            _mockHttpResult = true;
+            _model = new ReregistrationRequest
+            {
+                AoUkprn = 1234567890,
+                ProviderUkprn = 98765432,
+                CoreCode = "7654321",
+                SpecialismCodes = new List<string> { "23456789", "7654321" },
+                PerformedBy = "Test User"
+            };
+        }
+
+        public override void Given()
+        {
+            HttpClient = new HttpClient(new MockHttpMessageHandler<bool>(_mockHttpResult, ApiConstants.ReregistrationUri, HttpStatusCode.OK, JsonConvert.SerializeObject(_model)));
+            _apiClient = new ResultsAndCertificationInternalApiClient(HttpClient, _tokenServiceClient, _configuration);
+        }
+
+        public async override Task When()
+        {
+            _result = await _apiClient.ReregistrationAsync(_model);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            _result.Should().BeTrue();
+        }
+    }
+}
