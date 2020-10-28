f3c94229b15f3d1621506a60edd0081b3ce38962
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-10-28 17:29:26 +0000

    TLRC - 1865 - Create Upload assessment entry file page & Stage 1 Validations - TLRC - 1988 - Dev - Added UploadAssessmentsFileGet Unit Tests

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/AssessmentControllerTests/UploadAssessmentsFileGet/TestSetup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Assessment;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.AssessmentControllerTests.UploadAssessmentsFileGet
+{
+    public abstract class TestSetup : BaseTest<RegistrationController>
+    {
+        protected AssessmentController Controller;
+        protected UploadAssessmentsRequestViewModel ViewModel;
+        public IActionResult Result { get; private set; }
+
+        public override void Setup()
+        {
+            Controller = new AssessmentController();
+            ViewModel = new UploadAssessmentsRequestViewModel();
+        }
+
+        public override Task When()
+        {
+            Result = Controller.UploadAssessmentsFile();
+            return Task.CompletedTask;
+        }
+    }
+}
