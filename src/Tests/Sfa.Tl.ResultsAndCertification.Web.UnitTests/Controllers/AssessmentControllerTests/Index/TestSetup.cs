5c9553944af663c8d4f067b656fe32f954616a7c
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-10-28 13:02:11 +0000

    TLRC - 1863 - Create Assessment entries dashboard page - TLRC - 1980 - Dev - Added Unit Tests for Assessment Dashboard page

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/AssessmentControllerTests/Index/TestSetup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.AssessmentControllerTests.Index
+{
+    public abstract class TestSetup : BaseTest<AssessmentController>
+    {
+        protected AssessmentController Controller;
+        public IActionResult Result { get; private set; }
+
+        public override void Setup()
+        {
+            Controller = new AssessmentController();
+        }
+
+        public override Task When()
+        {
+            Result = Controller.Index();
+            return Task.CompletedTask;
+        }
+    }
+}
