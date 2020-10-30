b1cada592ff250371100a3cc86d256b2298694fd
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-10-30 12:50:10 +0000

    TLRC - 1917 - Create Assessment Entries Upload Success Page - TLRC - 2003 - Dev - Implemented BulkInsertOrUpdateAssessments in AssessmentRepository

--- src/Sfa.Tl.ResultsAndCertification.Data/Interfaces/IAssessmentRepository.cs
+++ src/Sfa.Tl.ResultsAndCertification.Data/Interfaces/IAssessmentRepository.cs
@@ -1,8 +1,11 @@
 ﻿using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Sfa.Tl.ResultsAndCertification.Data.Interfaces
 {
     public interface IAssessmentRepository : IRepository<TqPathwayAssessment>
     {
+        Task<bool> BulkInsertOrUpdateAssessments(List<TqPathwayAssessment> pathwayAssessmentEntities, List<TqSpecialismAssessment> specialismAssessmentEntities);
     }
 }
