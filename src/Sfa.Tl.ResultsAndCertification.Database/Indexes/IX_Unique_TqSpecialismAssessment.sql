b93f0a5f0423b71b51ecbbc8c79240a293d3976d
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-11-04 10:42:27 +0000

    TLRC - 1917 - Create Assessment Entries Upload Success Page - TLRC - 2030 - Dev - Added index to Assessment Tables

---
+++ src/Sfa.Tl.ResultsAndCertification.Database/Indexes/IX_Unique_TqSpecialismAssessment.sql
@@ -0,0 +1,5 @@
+﻿CREATE UNIQUE NONCLUSTERED INDEX [IX_Unique_TqSpecialismAssessment] ON TqSpecialismAssessment 
+(
+    TqRegistrationSpecialismId, AssessmentSeriesId
+)
+WHERE ([IsOptedin] = 1 AND [EndDate] IS NULL)
