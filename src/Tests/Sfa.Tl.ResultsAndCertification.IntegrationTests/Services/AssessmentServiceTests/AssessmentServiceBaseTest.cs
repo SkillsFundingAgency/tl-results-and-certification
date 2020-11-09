4856f3e4596dfdafbf372a687b25d82be82a8b72
Ravi Bobbili <ravi.bob@outlook.com>
2020-11-09 11:59:24 +0000

    TLRC-1916 Stage 3 validaitons - Integration test added

--- src/Tests/Sfa.Tl.ResultsAndCertification.IntegrationTests/Services/AssessmentServiceTests/AssessmentServiceBaseTest.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.IntegrationTests/Services/AssessmentServiceTests/AssessmentServiceBaseTest.cs
@@ -2,10 +2,12 @@
 using Microsoft.Extensions.Logging;
 using Sfa.Tl.ResultsAndCertification.Application.Mappers;
 using Sfa.Tl.ResultsAndCertification.Application.Services;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
 using Sfa.Tl.ResultsAndCertification.Data.Interfaces;
 using Sfa.Tl.ResultsAndCertification.Data.Repositories;
 using Sfa.Tl.ResultsAndCertification.Domain.Models;
 using Sfa.Tl.ResultsAndCertification.Models.Configuration;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders;
 using Sfa.Tl.ResultsAndCertification.Tests.Common.DataProvider;
 using Sfa.Tl.ResultsAndCertification.Tests.Common.Enum;
 using System.Collections.Generic;
@@ -52,6 +54,39 @@ namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Services.AssessmentSer
             TqProvider = ProviderDataProvider.CreateTqProvider(DbContext, TqAwardingOrganisation, TlProvider);
             AssessmentSeries = AssessmentSeriesDataProvider.CreateAssessmentSeriesList(DbContext, null, true);
             DbContext.SaveChangesAsync();
+        }
+        //public List<TqRegistrationProfile> SeedRegistrationsData1(List<long> ulns, TqProvider tqProvider = null)
+        //{
+        //    var profiles = new List<TqRegistrationProfile>();
+
+        //    foreach (var uln in ulns)
+        //    {
+        //        profiles.Add(SeedRegistrationData(uln, tqProvider));
+        //    }
+        //    return profiles;
+        //}
+
+        public List<TqRegistrationProfile> SeedRegistrationsData(Dictionary<long, RegistrationPathwayStatus> ulns, TqProvider tqProvider = null)
+        {
+            var profiles = new List<TqRegistrationProfile>();
+
+            foreach (var uln in ulns)
+            {
+                profiles.Add(SeedRegistrationData(uln.Key, uln.Value, tqProvider));
+            }
+            return profiles;
+        }
+
+        public TqRegistrationProfile SeedRegistrationData(long uln, RegistrationPathwayStatus status = RegistrationPathwayStatus.Active, TqProvider tqProvider = null)
+        {
+            var profile = new TqRegistrationProfileBuilder().BuildList().FirstOrDefault(p => p.UniqueLearnerNumber == uln);
+            var tqRegistrationProfile = RegistrationsDataProvider.CreateTqRegistrationProfile(DbContext, profile);
+            var tqRegistrationPathway = RegistrationsDataProvider.CreateTqRegistrationPathway(DbContext, tqRegistrationProfile, tqProvider ?? TqProviders.First());
+            tqRegistrationPathway.Status = status;
+            var tqRegistrationSpecialism = RegistrationsDataProvider.CreateTqRegistrationSpecialism(DbContext, tqRegistrationPathway, Specialism);
+
+            DbContext.SaveChanges();
+            return profile;
         }
     }
 }
