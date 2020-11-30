088c43b440b78c3c1f91c0f0a5a0e7ffa5296339
Ravi Bobbili <ravi.bob@outlook.com>
2020-11-30 13:42:31 +0000

    TLRC-2139 - Add Core assessment entry - Api integration tests are added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.IntegrationTests/Services/AssessmentServiceTests/When_GetAvailableAssessmentSeriesAsync_IsCalled.cs
@@ -0,0 +1,103 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Sfa.Tl.ResultsAndCertification.Application.Services;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Data.Repositories;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.Enum;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Services.AssessmentServiceTests
+{
+    public class When_GetAvailableAssessmentSeriesAsync_IsCalled : AssessmentServiceBaseTest
+    {
+        private Dictionary<long, RegistrationPathwayStatus> _ulns;
+        private AvailableAssessmentSeries _actualResult;
+        private List<TqRegistrationProfile> _registrations;
+
+        public override void Given()
+        {
+            // Create mapper
+            CreateMapper();
+
+            // Parameters
+            _ulns = new Dictionary<long, RegistrationPathwayStatus> { { 1111111111, RegistrationPathwayStatus.Active } };
+
+            // Registrations seed
+            SeedTestData(EnumAwardingOrganisation.Pearson, true);
+            _registrations = SeedRegistrationsData(_ulns, TqProvider);
+
+            AssessmentRepositoryLogger = new Logger<AssessmentRepository>(new NullLoggerFactory());
+            AssessmentSeriesRepositoryLogger = new Logger<GenericRepository<AssessmentSeries>>(new NullLoggerFactory());
+            AssessmentRepository = new AssessmentRepository(AssessmentRepositoryLogger, DbContext);
+            AssessmentSeriesRepository = new GenericRepository<AssessmentSeries>(AssessmentSeriesRepositoryLogger, DbContext);
+            AssessmentService = new AssessmentService(AssessmentRepository, AssessmentSeriesRepository, AssessmentMapper, AssessmentRepositoryLogger);
+        }
+
+        public override Task When()
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task WhenAsync(long aoUkprn, int profileId, AssessmentEntryType assessmentEntryType)
+        {
+            if (_actualResult != null)
+                return;
+
+            _actualResult = await AssessmentService.GetAvailableAssessmentSeriesAsync(aoUkprn, profileId, assessmentEntryType);
+        }
+
+        [Theory()]
+        [MemberData(nameof(Data))]
+        public async Task Then_Expected_Results_Are_Returned(RequestParameter request, AvailableAssessmentSeries expectedResult)
+        {
+            await WhenAsync(request.AoUkprn, request.ProfileId, request.AssessmentEntryType);
+
+            if (_actualResult == null)
+            {
+                expectedResult.Should().BeNull();
+                return;
+            }
+
+            // Assert
+            _actualResult.ProfileId.Should().Be(expectedResult.ProfileId);
+            _actualResult.AssessmentSeriesId.Should().Be(expectedResult.AssessmentSeriesId);
+            _actualResult.AssessmentSeriesName.Should().Be(expectedResult.AssessmentSeriesName);
+        }
+
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                return new[]
+                {
+                    // core assessment window opend
+                    new object[]
+                    { new RequestParameter { AoUkprn = 10011881, ProfileId = 1, AssessmentEntryType = AssessmentEntryType.Core },
+                      new AvailableAssessmentSeries { ProfileId = 1, AssessmentSeriesId = 1, AssessmentSeriesName = "Summer 2021" } },
+
+                    // specialism assessment window not opened
+                    new object[]
+                    { new RequestParameter { AoUkprn = 10011881, ProfileId = 1, AssessmentEntryType = AssessmentEntryType.Specialism },
+                      null },
+
+                    // invalid profil id
+                    new object[]
+                    { new RequestParameter { AoUkprn = 10011881, ProfileId = 2, AssessmentEntryType = AssessmentEntryType.Core },
+                      null }
+                };
+            }
+        }
+    }
+
+    public class RequestParameter
+    {
+        public long AoUkprn { get; set; }
+        public int ProfileId { get; set; }
+        public AssessmentEntryType AssessmentEntryType { get; set; }
+    }
+}
--- src/Tests/Sfa.Tl.ResultsAndCertification.Tests.Common/DataBuilders/AssessmentSeriesBuilder.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Tests.Common/DataBuilders/AssessmentSeriesBuilder.cs
@@ -7,13 +7,15 @@ namespace Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders
 {
     public class AssessmentSeriesBuilder
     {
+        private readonly DateTime CurrentDate = DateTime.Now.Date;
+
         public AssessmentSeries Build() => new AssessmentSeries
         {
             Name = "Summer 2021",
             Description = "Summer 2021",
             Year = 2021,
-            StartDate = DateTime.Now,
-            EndDate = DateTime.Now.AddDays(90),
+            StartDate = CurrentDate.AddDays(-1),
+            EndDate = CurrentDate.AddMonths(3),
             CreatedBy = Constants.CreatedByUser,
             CreatedOn = Constants.CreatedOn,
             ModifiedBy = Constants.ModifiedByUser,
@@ -27,8 +29,8 @@ namespace Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders
                 Name = "Summer 2021",
                 Description = "Summer 2021",
                 Year = 2021,
-                StartDate = DateTime.Now,
-                EndDate = DateTime.Now.AddDays(90),
+                StartDate = CurrentDate.AddDays(-1),
+                EndDate = CurrentDate.AddMonths(3),
                 CreatedBy = Constants.CreatedByUser,
                 CreatedOn = Constants.CreatedOn,
                 ModifiedBy = Constants.ModifiedByUser,
@@ -39,8 +41,8 @@ namespace Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders
                 Name = "Autumn 2021",
                 Description = "Autumn 2021",
                 Year = 2021,
-                StartDate = DateTime.Now,
-                EndDate = DateTime.Now.AddDays(90),
+                StartDate = CurrentDate.AddMonths(3).AddDays(1),
+                EndDate = CurrentDate.AddMonths(6),
                 CreatedBy = Constants.CreatedByUser,
                 CreatedOn = Constants.CreatedOn,
                 ModifiedBy = Constants.ModifiedByUser,
@@ -51,8 +53,8 @@ namespace Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders
                 Name = "Summer 2022",
                 Description = "Summer 2022",
                 Year = 2022,
-                StartDate = DateTime.Now,
-                EndDate = DateTime.Now.AddDays(90),
+                StartDate = CurrentDate.AddMonths(6).AddDays(1),
+                EndDate = CurrentDate.AddMonths(9),
                 CreatedBy = Constants.CreatedByUser,
                 CreatedOn = Constants.CreatedOn,
                 ModifiedBy = Constants.ModifiedByUser,
@@ -63,8 +65,8 @@ namespace Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders
                 Name = "Autumn 2022",
                 Description = "Autumn 2022",
                 Year = 2022,
-                StartDate = DateTime.Now,
-                EndDate = DateTime.Now.AddDays(90),
+                StartDate = CurrentDate.AddMonths(9).AddDays(1),
+                EndDate = CurrentDate.AddMonths(12),
                 CreatedBy = Constants.CreatedByUser,
                 CreatedOn = Constants.CreatedOn,
                 ModifiedBy = Constants.ModifiedByUser,
