669d7e99ac0f52feb605ffb510367893826d9e6e
Ravi Bobbili <ravi.bob@outlook.com>
2020-12-01 16:49:25 +0000

    TLRC-2139 - Add Core assessment entry - Web Unit tests are added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/AssessmentControllerTests/AddCoreAssessmentSeriesPost/When_IsOpted_IsFalse.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Assessment.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.AssessmentControllerTests.AddCoreAssessmentSeriesPost
+{
+    public class When_IsOpted_IsFalse : TestSetup
+    {
+        public override void Given()
+        {
+            ViewModel = new AddAssessmentSeriesViewModel 
+            {
+                ProfileId = 1,
+                AssessmentSeriesId = 11,
+                AssessmentSeriesName = "Summer 2021",
+                IsOpted = false
+            };
+        }
+
+        [Fact]
+        public void Then_Redirected_To_AssessmentDetails()
+        {
+            var route = (Result as RedirectToRouteResult);
+            route.RouteName.Should().Be(RouteConstants.AssessmentDetails);
+            route.RouteValues[Constants.ProfileId].Should().Be(ViewModel.ProfileId);
+        }
+    }
+}
