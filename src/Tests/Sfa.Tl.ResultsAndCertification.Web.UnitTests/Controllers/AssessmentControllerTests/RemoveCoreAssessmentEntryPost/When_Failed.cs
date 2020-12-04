cac021df94602cec3ff6111e3f8718ee6a96725d
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-12-04 08:45:00 +0000

    TLRC - 2142 - Create 'Assessment entry removed successfully' page - TLRC - 2255 - Dev - Added Web UnitTests

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/AssessmentControllerTests/RemoveCoreAssessmentEntryPost/When_Failed.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Assessment.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.AssessmentControllerTests.RemoveCoreAssessmentEntryPost
+{
+    public class When_Failed : TestSetup
+    {
+        public override void Given()
+        {
+            ViewModel = new AssessmentEntryDetailsViewModel
+            {
+                ProfileId = ProfileId,
+                AssessmentId = 5,
+                AssessmentEntryType = Common.Enum.AssessmentEntryType.Core,
+                CanRemoveAssessmentEntry = true
+            };
+
+            AssessmentLoader.RemoveAssessmentEntryAsync(AoUkprn, ViewModel).Returns(false);
+        }
+
+        [Fact]
+        public void Then_Redirected_To_Error()
+        {
+            var routeName = (Result as RedirectToRouteResult).RouteName;
+            var routeValue = (Result as RedirectToRouteResult).RouteValues["StatusCode"];
+            routeName.Should().Be(RouteConstants.Error);
+            routeValue.Should().Be(500);
+        }
+    }
+}
