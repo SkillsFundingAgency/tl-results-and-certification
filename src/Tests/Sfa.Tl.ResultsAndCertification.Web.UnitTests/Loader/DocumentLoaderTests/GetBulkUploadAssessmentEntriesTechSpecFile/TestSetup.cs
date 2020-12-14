98016bbfa346d21be8d07d72fb7b6306155e380e
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-12-14 13:20:06 +0000

    TLRC - 2279 - Create Single Page for Technical Specifications - TLRC - 2324 - Dev - Added Document Loader Unit Testing for Tlevel data format and rules guide

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/DocumentLoaderTests/GetBulkUploadAssessmentEntriesTechSpecFile/TestSetup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Services.BlobStorage.Interface;
+using Sfa.Tl.ResultsAndCertification.Models.BlobStorage;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using Sfa.Tl.ResultsAndCertification.Web.Loader;
+using Sfa.Tl.ResultsAndCertification.Web.Loader.Interfaces;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentResource = Sfa.Tl.ResultsAndCertification.Web.Content.Document;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.DocumentLoaderTests.GetBulkUploadAssessmentEntriesTechSpecFile
+{
+    public abstract class TestSetup : BaseTest<DocumentLoader>
+    {
+        protected ILogger<DocumentLoader> Logger;
+        protected IDocumentLoader Loader;
+        protected IBlobStorageService BlobStorageService;
+        protected Stream ActualResult;
+        protected string FileName;
+
+        public override void Setup()
+        {
+            Logger = Substitute.For<ILogger<DocumentLoader>>();
+            BlobStorageService = Substitute.For<IBlobStorageService>();
+        }
+
+        public override void Given()
+        {
+            FileName = DocumentResource.RegistrationDataFormatAndRulesGuide.Registrations_Data_Format_And_Rules_Guide_File_Name_Text;
+            BlobStorageService.DownloadFileAsync(Arg.Any<BlobStorageData>()).Returns(new MemoryStream(Encoding.ASCII.GetBytes("Test file for registration tech spec")));
+            Loader = new DocumentLoader(Logger, BlobStorageService);
+        }
+
+        public async override Task When()
+        {
+            ActualResult = await Loader.GetBulkUploadRegistrationsTechSpecFileAsync(FileName);
+        }
+    }
+}
