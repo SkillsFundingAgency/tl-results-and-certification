ceac460304fbf66da702e0019e9ea64dd35e33c6
Ravi Bobbili <ravi.bob@outlook.com>
2021-01-06 11:29:31 +0000

    TLRC-2159  Results Dashboard. TLRC-2391 Unit tests are added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ResultControllerTests/Index/TestSetup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ResultControllerTests.Index
+{
+    public abstract class TestSetup : BaseTest<ResultController>
+    {
+        protected ResultController Controller;
+        public IActionResult Result { get; private set; }
+
+        public override void Setup()
+        {
+            Controller = new ResultController();
+        }
+
+        public override Task When()
+        {
+            Result = Controller.Index();
+            return Task.CompletedTask;
+        }
+    }
+}
