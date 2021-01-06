2f764de458864c33ca52c01db20a771b214b26ff
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-01-06 14:32:45 +0000

    TLRC - 2180 - Create Upload Results file page  & Stage 1 Validations - TLRC - 2385 - Dev - Added Web Unit Tests for UploadAssessmentsFileGet

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ResultControllerTests/UploadResultsFileGet/When_FileSize_Error.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Result;
+using Xunit;
+using UploadContent = Sfa.Tl.ResultsAndCertification.Web.Content.Assessment.Upload;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ResultControllerTests.UploadResultsFileGet
+{
+    public class When_FileSize_Error : TestSetup
+    {
+        public override void Given()
+        {
+            RequestErrorTypeId = (int)RequestErrorType.FileSize;
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(UploadResultsRequestViewModel));
+
+            Controller.ViewData.ModelState.Should().ContainSingle();
+            Controller.ViewData.ModelState.ContainsKey(nameof(UploadResultsRequestViewModel.File)).Should().BeTrue();
+
+            var modelState = Controller.ViewData.ModelState[nameof(UploadResultsRequestViewModel.File)];
+            modelState.Errors[0].ErrorMessage.Should().Be(string.Format(UploadContent.File_Size_Too_Large_Validation_Message, Constants.MaxFileSizeInMb));
+        }
+    }
+}
