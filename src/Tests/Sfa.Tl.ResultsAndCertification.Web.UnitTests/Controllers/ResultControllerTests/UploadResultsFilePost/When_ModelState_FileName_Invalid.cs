79be3e047aa588c72472b5a38678f5345ce2d4d1
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-01-07 10:56:10 +0000

    TLRC - 2180 - Create Upload Results file page  & Stage 1 Validations - TLRC - 2385 - Dev - Added Web Unit Tests for UploadAssessmentsFilePost

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ResultControllerTests/UploadResultsFilePost/When_ModelState_FileName_Invalid.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Result;
+using System;
+using System.Linq;
+using Xunit;
+using UploadContent = Sfa.Tl.ResultsAndCertification.Web.Content.Result.Upload;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ResultControllerTests.UploadResultsFilePost
+{
+    public class When_ModelState_FileName_Invalid : TestSetup
+    {
+        public override void Given()
+        {
+            var _random = new Random();
+            var randomString = new String(Enumerable.Range(0, 300).Select(n => (Char)(_random.Next(97, 122))).ToArray());
+            var filename = $"{randomString}.csv";
+            FormFile = Substitute.For<IFormFile>();
+            FormFile.FileName.Returns(filename);
+            Controller.ModelState.AddModelError("File", UploadContent.File_Name_Length_Validation_Message);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(UploadResultsRequestViewModel));
+
+            Controller.ViewData.ModelState.Should().ContainSingle();
+            Controller.ViewData.ModelState.ContainsKey(nameof(UploadResultsRequestViewModel.File)).Should().BeTrue();
+
+            var modelState = Controller.ViewData.ModelState[nameof(UploadResultsRequestViewModel.File)];
+            modelState.Errors[0].ErrorMessage.Should().Be(UploadContent.File_Name_Length_Validation_Message);
+        }
+    }
+}
--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ResultControllerTests/UploadResultsFilePost/When_ModelState_FileSize_Invalid.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ResultControllerTests/UploadResultsFilePost/When_ModelState_FileSize_Invalid.cs
@@ -2,11 +2,11 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
-using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Assessment;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Result;
 using Xunit;
-using UploadContent = Sfa.Tl.ResultsAndCertification.Web.Content.Assessment.Upload;
+using UploadContent = Sfa.Tl.ResultsAndCertification.Web.Content.Result.Upload;
 
-namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.AssessmentControllerTests.UploadAssessmentsFilePost
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ResultControllerTests.UploadResultsFilePost
 {
     public class When_ModelState_FileSize_Invalid : TestSetup
     {
@@ -22,12 +22,12 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.AssessmentCon
             Result.Should().BeOfType(typeof(ViewResult));
 
             var viewResult = Result as ViewResult;
-            viewResult.Model.Should().BeOfType(typeof(UploadAssessmentsRequestViewModel));
+            viewResult.Model.Should().BeOfType(typeof(UploadResultsRequestViewModel));
 
             Controller.ViewData.ModelState.Should().ContainSingle();
-            Controller.ViewData.ModelState.ContainsKey(nameof(UploadAssessmentsRequestViewModel.File)).Should().BeTrue();
+            Controller.ViewData.ModelState.ContainsKey(nameof(UploadResultsRequestViewModel.File)).Should().BeTrue();
 
-            var modelState = Controller.ViewData.ModelState[nameof(UploadAssessmentsRequestViewModel.File)];
+            var modelState = Controller.ViewData.ModelState[nameof(UploadResultsRequestViewModel.File)];
             modelState.Errors[0].ErrorMessage.Should().Be(string.Format(UploadContent.File_Size_Too_Large_Validation_Message, 5));
         }
     }
