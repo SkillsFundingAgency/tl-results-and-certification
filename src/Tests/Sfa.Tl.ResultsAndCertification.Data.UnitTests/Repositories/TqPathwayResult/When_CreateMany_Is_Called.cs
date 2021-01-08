597f9dc4ae2d4e7c44f4fea10066ce11a5da2652
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-01-08 13:55:22 +0000

    TLRC - 2372 - Technical Story - Database design for results - TLRC - 2408 - Dev - Added unit tests for TqPathwayResult

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Data.UnitTests/Repositories/TqPathwayResult/When_CreateMany_Is_Called.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Data.UnitTests.Repositories.TqPathwayResult
+{
+    public class When_CreateMany_Is_Called : BaseTest<Domain.Models.TqPathwayResult>
+    {
+        private int _result;
+        private IList<Domain.Models.TqPathwayResult> _data;
+
+        public override void Given()
+        {
+            _data = new TqPathwayResultBuilder().BuildList();
+        }
+
+        public async override Task When()
+        {
+            _result = await Repository.CreateManyAsync(_data);
+        }
+
+        [Fact]
+        public void Then_Records_Should_Have_Been_Created()
+        {
+            var result = Repository.GetManyAsync();
+            result.Count().Should().Be(_data.Count);
+        }
+    }
+}
