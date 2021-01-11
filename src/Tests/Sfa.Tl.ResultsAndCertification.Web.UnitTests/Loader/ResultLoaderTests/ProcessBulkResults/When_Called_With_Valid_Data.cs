46395479bca71d5f163670acd9dde135d7022e87
Ravi Bobbili <ravi.bob@outlook.com>
2021-01-11 13:50:47 +0000

    TLRC-2263 Results Stage 2 validations - TLRC-2396 Loader unit tests added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/ResultLoaderTests/ProcessBulkResults/When_Called_With_Valid_Data.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Models.BlobStorage;
+using Sfa.Tl.ResultsAndCertification.Models.BulkProcess;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using Sfa.Tl.ResultsAndCertification.Web.Loader;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Result;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.ResultLoaderTests.ProcessBulkResults
+{
+    public class When_Called_With_Valid_Data : TestSetup
+    {
+        public override void Given()
+        {
+            BulkResultResponse = new BulkResultResponse
+            {
+                IsSuccess = true,
+                Stats = new BulkUploadStats
+                {
+                    TotalRecordsCount = 10
+                }
+            };
+
+            UploadResultsRequestViewModel = new UploadResultsRequestViewModel { AoUkprn = Ukprn, File = FormFile };
+
+            UploadResultsResponseViewModel = new UploadResultsResponseViewModel
+            {
+                IsSuccess = true,
+                Stats = new BulkUploadStatsViewModel
+                {
+                    TotalRecordsCount = 10
+                }
+            };
+
+            Mapper.Map<BulkProcessRequest>(UploadResultsRequestViewModel).Returns(BulkResultRequest);
+            Mapper.Map<UploadResultsResponseViewModel>(BulkResultResponse).Returns(UploadResultsResponseViewModel);
+            InternalApiClient.ProcessBulkResultsAsync(BulkResultRequest).Returns(BulkResultResponse);
+            Loader = new ResultLoader(Mapper, Logger, InternalApiClient, BlobStorageService);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Called()
+        {
+            InternalApiClient.Received(1).ProcessBulkResultsAsync(BulkResultRequest);
+            BlobStorageService.Received(1).UploadFileAsync(Arg.Any<BlobStorageData>());
+            Mapper.Received().Map<BulkProcessRequest>(UploadResultsRequestViewModel);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            ActualResult.Should().NotBeNull();
+
+            ActualResult.IsSuccess.Should().Be(UploadResultsResponseViewModel.IsSuccess);
+            ActualResult.Stats.Should().NotBeNull();
+
+            ActualResult.Stats.TotalRecordsCount.Should().Be(UploadResultsResponseViewModel.Stats.TotalRecordsCount);
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/ResultLoaderTests/ProcessBulkResults/When_Mapper_Called_With_Stats.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Models.BulkProcess;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using Sfa.Tl.ResultsAndCertification.Web.Loader;
+using Sfa.Tl.ResultsAndCertification.Web.Mapper;
+using Sfa.Tl.ResultsAndCertification.Web.Mapper.Resolver;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Result;
+using System;
+using System.Security.Claims;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.ResultLoaderTests.ProcessBulkResults
+{
+    public class When_Mapper_Called_With_Stats : TestSetup
+    {
+        private readonly string _givename = "test";
+        private readonly string _surname = "user";
+        private readonly string _email = "test.user@test.com";
+
+        public override void Given()
+        {
+            HttpContextAccessor = Substitute.For<IHttpContextAccessor>();
+            HttpContextAccessor.HttpContext.Returns(new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.GivenName, _givename),
+                    new Claim(ClaimTypes.Surname, _surname),
+                    new Claim(ClaimTypes.Email, _email)
+                }))
+            });
+
+            CreateMapper();
+
+            BulkResultRequest = new BulkProcessRequest { AoUkprn = Ukprn };
+
+            BulkResultResponse = new BulkResultResponse
+            {
+                IsSuccess = true,
+                Stats = new BulkUploadStats
+                {
+                    TotalRecordsCount = 10
+                }
+            };
+
+            UploadResultsRequestViewModel = new UploadResultsRequestViewModel { AoUkprn = Ukprn, File = FormFile };
+            InternalApiClient.ProcessBulkResultsAsync(BulkResultRequest).Returns(BulkResultResponse);
+            Loader = new ResultLoader(Mapper, Logger, InternalApiClient, BlobStorageService);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            var bulkResultRequestMapperResult = Mapper.Map<BulkProcessRequest>(UploadResultsRequestViewModel);
+
+            bulkResultRequestMapperResult.AoUkprn.Should().Be(UploadResultsRequestViewModel.AoUkprn);
+            bulkResultRequestMapperResult.BlobFileName.Should().NotBeNullOrEmpty();
+            bulkResultRequestMapperResult.BlobUniqueReference.Should().NotBeEmpty();
+            bulkResultRequestMapperResult.DocumentType.Should().Be(DocumentType.Results);
+            bulkResultRequestMapperResult.FileType.Should().Be(FileType.Csv);
+            bulkResultRequestMapperResult.PerformedBy.Should().Be($"{_givename} {_surname}");
+
+            var uploadResultsResponseMapperResult = Mapper.Map<UploadResultsResponseViewModel>(BulkResultResponse);
+
+            uploadResultsResponseMapperResult.IsSuccess.Should().Be(BulkResultResponse.IsSuccess);
+            uploadResultsResponseMapperResult.Stats.Should().NotBeNull();
+            uploadResultsResponseMapperResult.Stats.TotalRecordsCount.Should().Be(BulkResultResponse.Stats.TotalRecordsCount);
+        }
+
+        protected void CreateMapper()
+        {
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddMaps(typeof(ResultMapper).Assembly);
+                c.ConstructServicesUsing(type =>
+                            type.Name.Contains("UserNameResolver") ?
+                                new UserNameResolver<UploadResultsRequestViewModel, BulkProcessRequest>(HttpContextAccessor) :
+                                type.Name.Contains("UserEmailResolver") ? (object)new UserEmailResolver<UploadResultsRequestViewModel, BulkProcessRequest>(HttpContextAccessor) :
+                                null);
+            });
+            Mapper = new AutoMapper.Mapper(mapperConfig);
+        }
+    }
+}
