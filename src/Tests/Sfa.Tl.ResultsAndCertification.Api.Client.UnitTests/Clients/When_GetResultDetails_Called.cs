6d2658c17b0dddc4a83374b6baa83d0a8c5bda76
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-01-11 17:26:40 +0000

    TLRC - 2309 - Create ULN cannot be found page (results) - TLRC - 2441 - Dev - Added Api Client Unit Tests for GetResultDetailsAsync method

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Api.Client.UnitTests/Clients/When_GetResultDetails_Called.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Api.Client.Clients;
+using Sfa.Tl.ResultsAndCertification.Api.Client.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Configuration;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Api.Client.UnitTests.Clients
+{
+    public class When_GetResultDetails_Called : BaseTest<ResultsAndCertificationInternalApiClient>
+    {
+        private readonly long _ukprn = 12345678;
+        private readonly int _profileId = 1;
+        private readonly RegistrationPathwayStatus _registrationPathwayStatus = RegistrationPathwayStatus.Active;
+        protected AssessmentDetails _mockHttpResult;
+
+        private ITokenServiceClient _tokenServiceClient;
+        private ResultsAndCertificationConfiguration _configuration;
+        private ResultsAndCertificationInternalApiClient _apiClient;
+        private ResultDetails _result;
+
+        public override void Setup()
+        {
+            _tokenServiceClient = Substitute.For<ITokenServiceClient>();
+
+            _configuration = new ResultsAndCertificationConfiguration
+            {
+                ResultsAndCertificationInternalApiSettings = new ResultsAndCertificationInternalApiSettings { Uri = "http://tlevel.api.com" }
+            };
+
+            _mockHttpResult = new AssessmentDetails
+            {
+                ProfileId = 1,
+                Uln = 1234567890,
+                Firstname = "John",
+                Lastname = "Smith",
+                ProviderUkprn = 1234567,
+                ProviderName = "Test Provider",                
+                Status = RegistrationPathwayStatus.Active
+            };
+        }
+
+        public override void Given()
+        {
+            HttpClient = new HttpClient(new MockHttpMessageHandler<AssessmentDetails>(_mockHttpResult, string.Format(ApiConstants.GetResultDetailsUri, _ukprn, _profileId, (int)_registrationPathwayStatus), HttpStatusCode.OK));
+            _apiClient = new ResultsAndCertificationInternalApiClient(HttpClient, _tokenServiceClient, _configuration);
+        }
+
+        public async override Task When()
+        {
+            _result = await _apiClient.GetResultDetailsAsync(_ukprn, _profileId, _registrationPathwayStatus);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            _result.Should().NotBeNull();
+            _result.Uln.Should().Be(_mockHttpResult.Uln);
+            _result.Firstname.Should().Be(_mockHttpResult.Firstname);
+            _result.Lastname.Should().Be(_mockHttpResult.Lastname);
+            _result.ProviderUkprn.Should().Be(_mockHttpResult.ProviderUkprn);
+            _result.ProviderName.Should().Be(_mockHttpResult.ProviderName);            
+            _result.Status.Should().Be(_mockHttpResult.Status);
+        }
+    }
+}
