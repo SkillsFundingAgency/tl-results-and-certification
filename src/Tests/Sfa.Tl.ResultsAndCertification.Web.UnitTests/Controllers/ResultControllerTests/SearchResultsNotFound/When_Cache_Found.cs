57636729a20fbaca1d507ab4aa0c0e890e0e8056
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-01-11 12:15:29 +0000

    TLRC - 2309 - Create ULN cannot be found page (results) - TLRC - 2426 - Dev - Added Web Unit Tests for UlnNotFound

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ResultControllerTests/SearchResultsNotFound/When_Cache_Found.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Result.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ResultControllerTests.SearchResultsNotFound
+{
+    public class When_Cache_Found : TestSetup
+    {
+        public override void Given()
+        {
+            CacheService.GetAndRemoveAsync<UlnResultsNotFoundViewModel>(Arg.Any<string>())
+                .Returns(new UlnResultsNotFoundViewModel { Uln = Uln });
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            var viewResult = Result as ViewResult;
+            var model = viewResult.Model as UlnResultsNotFoundViewModel;
+
+            model.Should().NotBeNull();
+            model.Uln.Should().Be(Uln);
+
+            model.BackLink.Should().NotBeNull();
+            model.BackLink.RouteName.Should().Be(RouteConstants.SearchResults);
+            model.BackLink.RouteAttributes.Should().BeEmpty();
+        }
+    }
+}
