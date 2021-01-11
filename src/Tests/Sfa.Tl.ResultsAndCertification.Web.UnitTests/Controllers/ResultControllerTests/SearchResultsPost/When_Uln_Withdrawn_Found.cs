d3b615ffd6b54302adf7cf41139d075de8307152
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-01-11 15:11:28 +0000

    TLRC - 2309 - Create ULN cannot be found page (results) - TLRC - 2432 - Dev - Added Web Unit Tests for ResultWithdrawnDetailsAsync

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ResultControllerTests/SearchResultsPost/When_Uln_Withdrawn_Found.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Extensions;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Result.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ResultControllerTests.SearchResultsPost
+{
+    public class When_Uln_Withdrawn_Found : TestSetup
+    {
+        public override void Given()
+        {
+            SearchResultsViewModel = new SearchResultsViewModel { SearchUln = SearchUln };
+            var mockResult = new UlnResultsNotFoundViewModel { IsAllowed = true, IsWithdrawn = true, Uln = SearchUln };
+            ResultLoader.FindUlnResultsAsync(AoUkprn, SearchUln.ToLong()).Returns(mockResult);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Called()
+        {
+            ResultLoader.Received(1).FindUlnResultsAsync(AoUkprn, SearchUln.ToLong());
+        }
+
+        [Fact]
+        public void Then_Redirected_To_ResultWithdrawnDetails()
+        {
+            var routeName = (Result as RedirectToRouteResult).RouteName;
+            routeName.Should().Be(RouteConstants.ResultWithdrawnDetails);
+        }
+    }
+}
