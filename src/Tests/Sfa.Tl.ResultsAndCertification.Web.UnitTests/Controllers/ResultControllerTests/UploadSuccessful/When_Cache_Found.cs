7a7b63f07f9682ce9b95913f2093b353a1620c63
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-01-20 14:03:00 +0000

    TLRC - 2265 - Create Results Upload Success Page - TLRC - 2480 - Dev - Added Web Unit Tests for UploadSuccessful

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ResultControllerTests/UploadSuccessful/When_Cache_Found.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Result;
+using System;
+using Xunit;
+using ResultContent = Sfa.Tl.ResultsAndCertification.Web.Content.Result;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ResultControllerTests.UploadSuccessful
+{
+    public class When_Cache_Found : TestSetup
+    {
+        public override void Given()
+        {
+            BlobUniqueReference = Guid.NewGuid();
+            UploadSuccessfulViewModel = new UploadSuccessfulViewModel { Stats = new ViewModel.BulkUploadStatsViewModel { TotalRecordsCount = 10 } };
+            CacheService.GetAndRemoveAsync<UploadSuccessfulViewModel>(CacheKey).Returns(UploadSuccessfulViewModel);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            var viewResult = Result as ViewResult;
+            var model = viewResult.Model as UploadSuccessfulViewModel;
+
+            model.Should().NotBeNull();
+
+            model.Stats.Should().NotBeNull();
+            model.Stats.TotalRecordsCount.Should().Be(UploadSuccessfulViewModel.Stats.TotalRecordsCount);
+            model.SuccessfulResultText.Should().Be(string.Format(ResultContent.UploadSuccessful.Successfully_Sent_Total_Results_Text, UploadSuccessfulViewModel.Stats.TotalRecordsCount));
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ResultControllerTests/UploadSuccessful/When_Called_With_One_Result.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Result;
+using System;
+using Xunit;
+using ResultContent = Sfa.Tl.ResultsAndCertification.Web.Content.Result;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ResultControllerTests.UploadSuccessful
+{
+    public class When_Called_With_One_Result : TestSetup
+    {
+        public override void Given()
+        {
+            BlobUniqueReference = Guid.NewGuid();
+            UploadSuccessfulViewModel = new UploadSuccessfulViewModel { Stats = new ViewModel.BulkUploadStatsViewModel { TotalRecordsCount = 1 } };
+            CacheService.GetAndRemoveAsync<UploadSuccessfulViewModel>(CacheKey).Returns(UploadSuccessfulViewModel);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            var viewResult = Result as ViewResult;
+            var model = viewResult.Model as UploadSuccessfulViewModel;
+
+            model.Should().NotBeNull();
+
+            model.Stats.Should().NotBeNull();
+            model.Stats.TotalRecordsCount.Should().Be(UploadSuccessfulViewModel.Stats.TotalRecordsCount);
+            model.SuccessfulResultText.Should().Be(ResultContent.UploadSuccessful.Successfully_Sent_One_Result_Text);
+        }
+    }
+}
