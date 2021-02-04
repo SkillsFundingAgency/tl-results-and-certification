af5280f70e08cf81473126eaed61984fe5bac383
Ravi Bobbili <ravi.bob@outlook.com>
2021-02-04 15:11:48 +0000

    TLRC-2371 Change core result: Unit test added for ChangeCoreResultPost

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ResultControllerTests/ChangeCoreResultPost/When_IsCoreResultChanged_IsNull.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Result.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ResultControllerTests.ChangeCoreResultPost
+{
+    public class When_IsCoreResultChanged_IsNull : TestSetup
+    {
+        private readonly bool? mockResult = null;
+        public override void Given()
+        {
+           ResultLoader.IsCoreResultChanged(AoUkprn, Arg.Any<ManageCoreResultViewModel>()).Returns(mockResult);
+        }
+
+        [Fact]
+        public void Then_Redirected_To_PageNotFound()
+        {
+            var actualRouteName = (Result as RedirectToRouteResult).RouteName;
+            actualRouteName.Should().Be(RouteConstants.PageNotFound);
+        }
+    }
+}
