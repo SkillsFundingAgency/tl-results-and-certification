6eefb62412093eba89b26461cc919b01916048f8
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-02-21 13:40:14 +0000

    TLRC - 2639 - Technical Story - Database Design for Functions Logging Activity - TLRC - 2713 - Dev - Created domain entity for FunctionLog

---
+++ src/Sfa.Tl.ResultsAndCertification.Domain/Models/FunctionLog.cs
@@ -0,0 +1,16 @@
+﻿using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sfa.Tl.ResultsAndCertification.Domain.Models
+{
+    public partial class FunctionLog : BaseEntity
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public FunctionStatus Status { get; set; }
+        public string Message { get; set; }
+    }
+}
