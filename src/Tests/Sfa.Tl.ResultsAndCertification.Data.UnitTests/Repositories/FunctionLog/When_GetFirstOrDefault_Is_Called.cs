7ed6815afa07647456d48f6c4bad52ba390afa6e
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-02-21 14:03:22 +0000

    TLRC - 2639 - Technical Story - Database Design for Functions Logging Activity - TLRC - 2714 - Dev - Added FunctionLog Data unit tests

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Data.UnitTests/Repositories/FunctionLog/When_GetFirstOrDefault_Is_Called.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.Helpers;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Data.UnitTests.Repositories.FunctionLog
+{
+    public class When_GetFirstOrDefault_Is_Called : BaseTest<Domain.Models.FunctionLog>
+    {
+        private Domain.Models.FunctionLog _result;
+        private Domain.Models.FunctionLog _data;
+
+        public override void Given()
+        {
+            _data = new FunctionLogBuilder().Build();
+            DbContext.AddRange(_data);
+            DbContext.SaveChanges();
+        }
+
+        public async override Task When()
+        {
+            _result = await Repository.GetFirstOrDefaultAsync(x => x.Id == 1);
+        }
+
+        [Fact]
+        public void Then_Fields_Are_As_Expected()
+        {
+            _data.Should().NotBeNull();
+            _result.Should().NotBeNull();
+            _result.Id.Should().Be(1);
+            _result.Name.Should().Be(_data.Name);
+            _result.StartDate.Should().Be(_data.StartDate);
+            _result.EndDate.Should().Be(_data.EndDate);
+            _result.Status.Should().Be(_data.Status);
+            _result.Message.Should().Be(_data.Message);
+            _result.CreatedBy.Should().BeEquivalentTo(Constants.CreatedByUser);
+            _result.CreatedOn.Should().Be(Constants.CreatedOn);
+            _result.ModifiedBy.Should().BeEquivalentTo(Constants.ModifiedByUser);
+            _result.ModifiedOn.Should().Be(Constants.ModifiedOn);
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Tests.Common/DataBuilders/FunctionLogBuilder.cs
@@ -0,0 +1,78 @@
+﻿using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.Helpers;
+using System;
+using System.Collections.Generic;
+
+namespace Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders
+{
+    public class FunctionLogBuilder
+    {
+        private readonly DateTime CurrentDate = DateTime.Now.Date;
+
+        public FunctionLog Build() => new FunctionLog
+        {
+            Name = "LearningEvents",
+            StartDate = CurrentDate.AddDays(1),
+            EndDate = CurrentDate.AddDays(1).AddMinutes(10),
+            Status = FunctionStatus.Processed,
+            Message = "Processed Successfully",
+            CreatedBy = Constants.CreatedByUser,
+            CreatedOn = Constants.CreatedOn,
+            ModifiedBy = Constants.ModifiedByUser,
+            ModifiedOn = Constants.ModifiedOn
+        };
+
+        public IList<FunctionLog> BuildList() => new List<FunctionLog>
+        {
+            new FunctionLog
+            {
+                Name = "LearningEvents",
+                StartDate = CurrentDate.AddDays(1),
+                EndDate = CurrentDate.AddDays(1).AddMinutes(10),
+                Status = FunctionStatus.Processed,
+                Message = "Processed Successfully", 
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            },
+            new FunctionLog
+            {
+                Name = "VerifyLearner",
+                StartDate = CurrentDate.AddDays(1),
+                EndDate = CurrentDate.AddDays(1).AddMinutes(10),
+                Status = FunctionStatus.Processing,
+                Message = "Started Processing",
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            },
+            new FunctionLog
+            {
+                Name = "CalculateResults",
+                StartDate = CurrentDate.AddDays(1),
+                EndDate = CurrentDate.AddDays(1).AddMinutes(10),
+                Status = FunctionStatus.Processed,
+                Message = "Processed Successfully",
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            },
+            new FunctionLog
+            {
+                Name = "Test",
+                StartDate = CurrentDate.AddDays(1),
+                EndDate = CurrentDate.AddDays(1).AddMinutes(10),
+                Status = FunctionStatus.Failed,
+                Message = "Job Failed",
+                CreatedBy = Constants.CreatedByUser,
+                CreatedOn = Constants.CreatedOn,
+                ModifiedBy = Constants.ModifiedByUser,
+                ModifiedOn = Constants.ModifiedOn
+            }
+        };
+    }
+}
