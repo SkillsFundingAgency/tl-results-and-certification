e539713a649f82223e0fa1adcb565991e26b739d
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-03-05 11:45:06 +0000

    TLRC - 2774 - Technical Story: Create Database tables for Industry Placement - TLRC - 2807 - Dev - Created Industry Placement Table - TLRC - 2810 - Dev - Created domain model entity

---
+++ src/Sfa.Tl.ResultsAndCertification.Common/Enum/IndustryPlacementStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Sfa.Tl.ResultsAndCertification.Common.Enum
+{
+    public enum IndustryPlacementStatus
+    {
+        NotSpecified = 0,
+        Completed = 1,
+        CompletedWithSpecialConsideration = 2,
+        NotCompleted = 3
+    }
+}
