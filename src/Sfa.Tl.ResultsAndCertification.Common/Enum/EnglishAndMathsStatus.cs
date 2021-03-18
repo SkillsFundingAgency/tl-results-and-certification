8c3f62094db23d194afa4d52501a66307f3eb7ba
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-03-18 09:12:47 +0000

    TLRC - 2799 - Check and Submit page (no LRS data) TLRC - 2890 - Dev - Viewmodel changes - Created EnglishAndMathsStatus enum and EnglishAndMathsQuestionViewmodel

---
+++ src/Sfa.Tl.ResultsAndCertification.Common/Enum/EnglishAndMathsStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sfa.Tl.ResultsAndCertification.Common.Enum
+{
+    public enum EnglishAndMathsStatus
+    {
+        [Display(Name = "Not Specified")]
+        NotSpecified = 0,
+        [Display(Name = "Achieved minimum standard")]
+        Achieved = 1,
+        [Display(Name = "Achieved minimum standard with SEND adjustments")]
+        AchievedWithSend = 2,
+        [Display(Name = "Not achieved minimum standard")]
+        NotAchieved = 3
+    }
+}
---
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/TrainingProvider/Manual/EnglishAndMathsQuestionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewComponents.BackLink;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.TrainingProvider.Manual
+{
+    public class EnglishAndMathsQuestionViewModel
+    {
+        public EnglishAndMathsStatus? EnglishAndMathsStatus { get; set; }
+
+        public string LearnerName { get; set; }
+
+        public virtual BackLinkModel BackLink => new BackLinkModel
+        {
+            RouteName = RouteConstants.EnterUniqueLearnerNumber
+        };
+    }
+}
