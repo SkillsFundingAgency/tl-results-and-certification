15d8d6b24d87bdbc7a7cd3945597db14e2c50ba4
Ravi Bobbili <ravi.bob@outlook.com>
2021-03-18 15:45:14 +0000

    TLRC-2694 Has English and Maths achieved question: Unit tests are added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/TrainingProviderControllerTests/AddEnglishAndMathsQuestionGet/When_Cache_IsFound.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.TrainingProvider;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.TrainingProvider;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.TrainingProvider.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.TrainingProviderControllerTests.AddEnglishAndMathsQuestionGet
+{
+    public class When_Cache_IsFound : TestSetup
+    {
+        private AddLearnerRecordViewModel cacheResult;
+        private EnterUlnViewModel ulnViewModel;
+        private FindLearnerRecord learnerRecord;
+        private EnglishAndMathsQuestionViewModel englishAndMathsQuestionViewModel;
+
+        public override void Given()
+        {
+            learnerRecord = new FindLearnerRecord { Uln = 1234567890, Name = "Test Name", IsLearnerRegistered = true };
+            ulnViewModel = new EnterUlnViewModel { EnterUln = "1234567890" };
+            englishAndMathsQuestionViewModel = new EnglishAndMathsQuestionViewModel { LearnerName = "Test Name", EnglishAndMathsStatus = EnglishAndMathsStatus.Achieved };
+
+            cacheResult = new AddLearnerRecordViewModel
+            {
+                LearnerRecord = learnerRecord,
+                Uln = ulnViewModel,
+                EnglishAndMathsQuestion = englishAndMathsQuestionViewModel,
+            };
+
+            CacheService.GetAsync<AddLearnerRecordViewModel>(CacheKey).Returns(cacheResult);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Called()
+        {
+            CacheService.Received(1).GetAsync<AddLearnerRecordViewModel>(CacheKey);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            Result.Should().NotBeNull();
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(EnglishAndMathsQuestionViewModel));
+
+            var model = viewResult.Model as EnglishAndMathsQuestionViewModel;
+            model.Should().NotBeNull();
+            model.EnglishAndMathsStatus.Should().Be(englishAndMathsQuestionViewModel.EnglishAndMathsStatus);
+            model.LearnerName.Should().Be(learnerRecord.Name);
+            model.BackLink.Should().NotBeNull();
+            model.BackLink.RouteName.Should().Be(RouteConstants.EnterUniqueLearnerNumber);
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/TrainingProviderControllerTests/AddEnglishAndMathsQuestionGet/When_Cache_NotFound.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.TrainingProvider;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.TrainingProvider;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.TrainingProvider.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.TrainingProviderControllerTests.AddEnglishAndMathsQuestionGet
+{
+    public class When_Cache_NotFound : TestSetup
+    {
+        private AddLearnerRecordViewModel cacheResult;
+        private EnterUlnViewModel ulnViewModel;
+        private FindLearnerRecord learnerRecord;
+
+        public override void Given()
+        {
+            learnerRecord = new FindLearnerRecord { Uln = 1234567890, Name = "Test Name", IsLearnerRegistered = true };
+            ulnViewModel = new EnterUlnViewModel { EnterUln = "1234567890" };
+            cacheResult = new AddLearnerRecordViewModel
+            {
+                LearnerRecord = learnerRecord,
+                Uln = ulnViewModel,
+            };
+
+            CacheService.GetAsync<AddLearnerRecordViewModel>(CacheKey).Returns(cacheResult);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Called()
+        {
+            CacheService.Received(1).GetAsync<AddLearnerRecordViewModel>(CacheKey);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            Result.Should().NotBeNull();
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(EnglishAndMathsQuestionViewModel));
+
+            var model = viewResult.Model as EnglishAndMathsQuestionViewModel;
+            model.Should().NotBeNull();
+            model.EnglishAndMathsStatus.Should().BeNull();
+            model.LearnerName.Should().Be(learnerRecord.Name);
+            model.BackLink.Should().NotBeNull();
+            model.BackLink.RouteName.Should().Be(RouteConstants.EnterUniqueLearnerNumber);
+        }
+    }
+}
