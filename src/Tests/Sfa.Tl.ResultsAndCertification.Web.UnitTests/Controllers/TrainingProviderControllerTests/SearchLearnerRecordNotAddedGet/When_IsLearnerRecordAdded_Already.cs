6b784adb3850e422e957eb4f81c6de182a9af7ba
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-03-31 09:42:09 +0100

    TLRC - 2696 - ULN has not been added page  TLRC - 2978 - Dev -  Added Unit Tests for SearchLearnerRecordNotAddedAsync

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/TrainingProviderControllerTests/SearchLearnerRecordNotAddedGet/When_IsLearnerRecordAdded_Already.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.TrainingProvider.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.TrainingProviderControllerTests.SearchLearnerRecordNotAddedGet
+{
+    public class When_IsLearnerRecordAdded_Already : TestSetup
+    {
+        private readonly long uln = 1234567890;
+        private SearchLearnerRecordViewModel cacheModel = null;
+
+        public override void Given()
+        {
+            cacheModel = new SearchLearnerRecordViewModel { SearchUln = uln.ToString(), IsLearnerRecordAdded = true };
+            CacheService.GetAsync<SearchLearnerRecordViewModel>(CacheKey).Returns(cacheModel);
+        }
+
+        [Fact]
+        public void Then_Redirected_To_PageNotFound()
+        {
+            var routeName = (Result as RedirectToRouteResult).RouteName;
+            routeName.Should().Be(RouteConstants.PageNotFound);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_AreCalled()
+        {
+            CacheService.Received(1).GetAsync<SearchLearnerRecordViewModel>(CacheKey);
+        }
+    }
+}
