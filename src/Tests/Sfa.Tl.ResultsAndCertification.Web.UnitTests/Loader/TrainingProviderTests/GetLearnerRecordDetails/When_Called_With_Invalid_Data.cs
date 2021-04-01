7b4802a5315a777359f6f0412ec76403837476f7
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-04-01 12:23:40 +0100

    TLRC - 2838 - Learner T Level record page (LRS Data)  TLRC - 2995 - Dev - Added Web Loader Unit Tests for GetLearnerRecordDetails method

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/TrainingProviderTests/GetLearnerRecordDetails/When_Called_With_Invalid_Data.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.TrainingProviderTests.GetLearnerRecordDetails
+{
+    public class When_Called_With_Invalid_Data : TestSetup
+    {
+        private Models.Contracts.TrainingProvider.LearnerRecordDetails _expectedApiResult;        
+
+        public override void Given()
+        {
+            _expectedApiResult = null;
+            InternalApiClient.GetLearnerRecordDetailsAsync(Arg.Any<long>(), Arg.Any<int>()).Returns(_expectedApiResult);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_AreCalled()
+        {
+            InternalApiClient.Received(1).GetLearnerRecordDetailsAsync(Arg.Any<long>(), Arg.Any<int>());
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            ActualResult.Should().BeNull();
+        }
+    }
+}
