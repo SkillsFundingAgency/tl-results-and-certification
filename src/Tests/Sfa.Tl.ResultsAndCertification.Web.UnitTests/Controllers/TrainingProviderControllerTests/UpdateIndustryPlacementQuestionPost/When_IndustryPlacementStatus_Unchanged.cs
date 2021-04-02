c4102fff12c830990392e571ecfdbce87a792608
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-04-02 15:06:04 +0100

    TLRC - 2842 - Update Industry Placement page  TLRC - 3017 - Dev - Added Web Unit Test for UpdateIndustryPlacementQuestion Post method

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/TrainingProviderControllerTests/UpdateIndustryPlacementQuestionPost/When_IndustryPlacementStatus_Unchanged.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.TrainingProvider;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.TrainingProvider.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.TrainingProviderControllerTests.UpdateIndustryPlacementQuestionPost
+{
+    public class When_IndustryPlacementStatus_Unchanged : TestSetup
+    {
+        private UpdateLearnerRecordResponse _updateLearnerRecordResponse;
+        public override void Given()
+        {
+            _updateLearnerRecordResponse = new UpdateLearnerRecordResponse
+            {
+                ProfileId = 1,
+                IsModified = false
+            };
+
+            UpdateIndustryPlacementQuestionViewModel = new UpdateIndustryPlacementQuestionViewModel 
+            { 
+                ProfileId = 1,
+                IndustryPlacementStatus = IndustryPlacementStatus.Completed
+            };
+
+            TrainingProviderLoader.ProcessIndustryPlacementQuestionChangeAsync(ProviderUkprn, UpdateIndustryPlacementQuestionViewModel).Returns(_updateLearnerRecordResponse);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Called()
+        {
+            TrainingProviderLoader.Received(1).ProcessIndustryPlacementQuestionChangeAsync(ProviderUkprn, UpdateIndustryPlacementQuestionViewModel);
+        }
+
+        [Fact]
+        public void Then_Redirected_To_LearnerRecordDetails()
+        {
+            var routeName = (Result as RedirectToRouteResult).RouteName;
+            routeName.Should().Be(RouteConstants.LearnerRecordDetails);
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/TrainingProviderControllerTests/UpdateIndustryPlacementQuestionPost/When_NoData_Found.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.TrainingProvider;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.TrainingProvider.Manual;
+using Xunit;
+
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.TrainingProviderControllerTests.UpdateIndustryPlacementQuestionPost
+{
+    public class When_NoData_Found : TestSetup
+    {
+        private UpdateLearnerRecordResponse _updateLearnerRecordResponse;
+        public override void Given()
+        {
+            _updateLearnerRecordResponse = null;
+
+            UpdateIndustryPlacementQuestionViewModel = new UpdateIndustryPlacementQuestionViewModel
+            {
+                ProfileId = 1,
+                IndustryPlacementStatus = IndustryPlacementStatus.Completed
+            };
+
+            TrainingProviderLoader.ProcessIndustryPlacementQuestionChangeAsync(ProviderUkprn, UpdateIndustryPlacementQuestionViewModel).Returns(_updateLearnerRecordResponse);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Called()
+        {
+            TrainingProviderLoader.Received(1).ProcessIndustryPlacementQuestionChangeAsync(ProviderUkprn, UpdateIndustryPlacementQuestionViewModel);
+        }
+
+        [Fact]
+        public void Then_Redirected_To_ProblemWithService()
+        {
+            var routeName = (Result as RedirectToRouteResult).RouteName;
+            routeName.Should().Be(RouteConstants.ProblemWithService);
+        }
+    }
+}
