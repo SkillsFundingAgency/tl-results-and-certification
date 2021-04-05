72c57553eea48a9c228c626486af6acc3e087ac3
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-04-05 09:02:23 +0100

    TLRC - 2843 - IP status updated confirmation page  TLRC - 3031 - Dev - Added Web Loader Unit Tests for ProcessIndustryPlacementQuestionUpdateAsync method

--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/TrainingProviderTests/ProcessIndustryPlacementQuestionUpdate/TestSetup.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/TrainingProviderTests/ProcessIndustryPlacementQuestionUpdate/TestSetup.cs
@@ -1,6 +1,10 @@
-﻿using Sfa.Tl.ResultsAndCertification.Models.Contracts.TrainingProvider;
+﻿using AutoMapper;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.TrainingProvider;
+using Sfa.Tl.ResultsAndCertification.Web.Mapper;
+using Sfa.Tl.ResultsAndCertification.Web.Mapper.Resolver;
 using Sfa.Tl.ResultsAndCertification.Web.ViewModel.TrainingProvider.Manual;
 using System.Threading.Tasks;
+
 
 namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.TrainingProviderTests.ProcessIndustryPlacementQuestionUpdate
 {
@@ -14,6 +18,18 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.TrainingProviderTe
         public async override Task When()
         {
             ActualResult = await Loader.ProcessIndustryPlacementQuestionUpdateAsync(ProviderUkprn, ViewModel);
+        }
+
+        public void CreateMapper()
+        {
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddMaps(typeof(TrainingProviderMapper).Assembly);
+                c.ConstructServicesUsing(type =>
+                            type.Name.Contains("UserNameResolver") ?
+                                new UserNameResolver<UpdateIndustryPlacementQuestionViewModel, UpdateLearnerRecordRequest>(HttpContextAccessor) : null);
+            });
+            Mapper = new AutoMapper.Mapper(mapperConfig);
         }
     }
 }
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/TrainingProviderTests/ProcessIndustryPlacementQuestionUpdate/When_Failed.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.TrainingProvider;
+using Sfa.Tl.ResultsAndCertification.Web.Loader;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.TrainingProvider.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.TrainingProviderTests.ProcessIndustryPlacementQuestionUpdate
+{
+    public class When_Failed : TestSetup
+    {
+        private LearnerRecordDetails _expectedApiResult;
+
+        public override void Given()
+        {
+            CreateMapper();
+            ProviderUkprn = 987654321;
+
+            _expectedApiResult = new LearnerRecordDetails { ProfileId = 1, Uln = 1234567890, Name = "Test User", IsLearnerRecordAdded = true, IndustryPlacementStatus = IndustryPlacementStatus.Completed };
+            ViewModel = new UpdateIndustryPlacementQuestionViewModel { ProfileId = 1, RegistrationPathwayId = 1, IndustryPlacementId = 0, IndustryPlacementStatus = IndustryPlacementStatus.NotCompleted };
+
+            InternalApiClient.GetLearnerRecordDetailsAsync(Arg.Any<long>(), Arg.Any<int>(), Arg.Any<int>()).Returns(_expectedApiResult);
+            InternalApiClient.UpdateLearnerRecordAsync(Arg.Is<UpdateLearnerRecordRequest>
+                (x => x.ProfileId == ViewModel.ProfileId &&
+                x.Ukprn == ProviderUkprn &&
+                x.RegistrationPathwayId == ViewModel.RegistrationPathwayId &&
+                x.IndustryPlacementId == ViewModel.IndustryPlacementId &&
+                x.IndustryPlacementStatus == ViewModel.IndustryPlacementStatus &&
+                x.PerformedBy == $"{Givenname} {Surname}"))
+                .Returns(false);
+
+            Loader = new TrainingProviderLoader(InternalApiClient, Mapper);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_AreCalled()
+        {
+            InternalApiClient.Received(1).GetLearnerRecordDetailsAsync(Arg.Any<long>(), Arg.Any<int>(), Arg.Any<int>());
+            InternalApiClient.Received(1).UpdateLearnerRecordAsync(Arg.Is<UpdateLearnerRecordRequest>
+                (x => x.ProfileId == ViewModel.ProfileId &&
+                x.Ukprn == ProviderUkprn &&
+                x.RegistrationPathwayId == ViewModel.RegistrationPathwayId &&
+                x.IndustryPlacementId == ViewModel.IndustryPlacementId &&
+                x.IndustryPlacementStatus == ViewModel.IndustryPlacementStatus &&
+                x.PerformedBy == $"{Givenname} {Surname}"));
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            ActualResult.Should().NotBeNull();
+            ActualResult.IsModified.Should().BeTrue();
+            ActualResult.IsSuccess.Should().BeFalse();
+
+            ActualResult.ProfileId.Should().Be(_expectedApiResult.ProfileId);
+            ActualResult.Uln.Should().Be(_expectedApiResult.Uln);
+            ActualResult.Name.Should().Be(_expectedApiResult.Name);
+        }
+    }
+}
