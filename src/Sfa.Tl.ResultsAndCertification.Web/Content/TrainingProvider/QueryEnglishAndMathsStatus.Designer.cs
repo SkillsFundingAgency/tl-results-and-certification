70279db8f1480f86a9e605cfb295036e82a75650
Ravi Bobbili <ravi.bob@outlook.com>
2021-04-07 09:41:17 +0100

    TLRC-2839 Query English & Maths status page - Content filename has been updated.

--- src/Sfa.Tl.ResultsAndCertification.Web/Content/TrainingProvider/QueryEnglishAndMathsStatus.Designer.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Content/TrainingProvider/QueryEnglishAndMathsStatus.Designer.cs
@@ -22,14 +22,14 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Content.TrainingProvider {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class QueryEnglishAndMathsAchievement {
+    public class QueryEnglishAndMathsStatus {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal QueryEnglishAndMathsAchievement() {
+        internal QueryEnglishAndMathsStatus() {
         }
         
         /// <summary>
@@ -39,8 +39,8 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Content.TrainingProvider {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sfa.Tl.ResultsAndCertification.Web.Content.TrainingProvider.QueryEnglishAndMathsA" +
-                            "chievement", typeof(QueryEnglishAndMathsAchievement).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Sfa.Tl.ResultsAndCertification.Web.Content.TrainingProvider.QueryEnglishAndMathsS" +
+                            "tatus", typeof(QueryEnglishAndMathsStatus).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
