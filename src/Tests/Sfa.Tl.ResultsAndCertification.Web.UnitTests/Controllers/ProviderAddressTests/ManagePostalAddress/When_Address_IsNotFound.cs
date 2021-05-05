7768ac2a55eb53f4aefce9ab8ce5498e3d349f43
Ravi Bobbili <ravi.bob@outlook.com>
2021-05-05 11:12:26 +0100

    TLRC-3140 Manage Postal Address: Unit tests are added.

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderAddressTests/ManagePostalAddress/When_Address_IsNotFound.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.TrainingProvider.Manual;
+using System.Linq;
+using Xunit;
+using BreadcrumbContent = Sfa.Tl.ResultsAndCertification.Web.Content.ViewComponents.Breadcrumb;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderAddressTests.ManagePostalAddress
+{
+    public class When_Address_IsNotFound : TestSetup
+    {
+        public override void Given() { }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            Result.Should().NotBeNull();
+            (Result as ViewResult).Model.Should().NotBeNull();
+
+            var model = (Result as ViewResult).Model as ManagePostalAddressViewModel;
+
+            model.Should().NotBeNull();
+            model.Breadcrumb.Should().NotBeNull();
+            model.Breadcrumb.BreadcrumbItems.Should().NotBeNull();
+            model.Breadcrumb.BreadcrumbItems.Count().Should().Be(2);
+            model.Breadcrumb.BreadcrumbItems[0].RouteName.Should().Be(RouteConstants.Home);
+            model.Breadcrumb.BreadcrumbItems[0].DisplayName.Should().Be(BreadcrumbContent.Home);
+            model.Breadcrumb.BreadcrumbItems[1].DisplayName.Should().Be(BreadcrumbContent.Manage_Postal_Address);
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderAddressTests/ProviderAddressControllerTestBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
+using System;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderAddressTests
+{
+    public abstract class ProviderAddressControllerTestBase : BaseTest<ProviderAddressController>
+    {
+        // Dependencies
+        protected ProviderAddressController Controller;
+
+        // HttpContext
+        protected int ProviderUkprn;
+        protected Guid UserId;
+        protected IHttpContextAccessor HttpContextAccessor;
+        protected string CacheKey;
+
+        public override void Setup()
+        {
+            Controller = new ProviderAddressController();
+            ProviderUkprn = 1234567890;
+        }
+    }
+}
