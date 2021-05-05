a854a39479f99714a2eb0fcf28506ec72a4da93d
Ravi Bobbili <ravi.bob@outlook.com>
2021-05-05 12:10:19 +0100

    TLRC-3167 Provider Postal Address Search: Unit tests are added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderAddressTests/AddAddressPostcode/TestSetup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderAddressTests.AddAddressPostcode
+{
+    public abstract class TestSetup : ProviderAddressControllerTestBase
+    {
+        public IActionResult Result { get; private set; }
+
+        public async override Task When()
+        {
+            Result = await Controller.AddAddressPostcodeAsync();
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderAddressTests/AddAddressPostcode/When_Action_IsCalled.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.ProviderAddress;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderAddressTests.AddAddressPostcode
+{
+    public class When_Action_IsCalled : TestSetup
+    {
+        public override void Given() { }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            Result.Should().NotBeNull();
+            (Result as ViewResult).Model.Should().NotBeNull();
+
+            var model = (Result as ViewResult).Model as AddAddressPostcodeViewModel;
+
+            model.Should().NotBeNull();
+            model.BackLink.RouteName.Should().Be(RouteConstants.ManagePostalAddress);
+        }
+    }
+}
