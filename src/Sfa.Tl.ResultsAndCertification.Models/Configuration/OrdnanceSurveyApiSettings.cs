349cd36fa9efaece3352eb9581a2d859594e9e88
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-05-06 12:54:02 +0100

    TLRC - 3187 - Provider Postal Address - Add address - postcode search- select address - TLRC - 3262 - Dev - Configuration for Postcode Api

---
+++ src/Sfa.Tl.ResultsAndCertification.Models/Configuration/OrdnanceSurveyApiSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Sfa.Tl.ResultsAndCertification.Models.Configuration
+{
+    public class OrdnanceSurveyApiSettings
+    {
+        public string PlacesApiBaseUri { get; set; }
+        public string PlacesApiKey { get; set; }
+    }
+}
