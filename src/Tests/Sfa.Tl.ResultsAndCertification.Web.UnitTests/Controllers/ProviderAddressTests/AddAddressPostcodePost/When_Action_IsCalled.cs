4742a41d562e197cadce2a261175ee271dd53299
Ravi Bobbili <ravi.bob@outlook.com>
2021-05-11 15:20:24 +0100

    TLRC-3170 Check and Submit Address: Unit tests added for AddAddressPostcodeAsync

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderAddressTests/AddAddressPostcodePost/When_Action_IsCalled.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.ProviderAddress;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderAddressTests.AddAddressPostcodePost
+{
+    public class When_Action_IsCalled : TestSetup
+    {
+        public override void Given()
+        {
+            ViewModel = new AddAddressPostcodeViewModel { Postcode = "AB1 2CD" };
+        }
+
+        [Fact]
+        public void Then_Redirected_To_PageNotFound()
+        {
+            var routeName = (Result as RedirectToRouteResult).RouteName;
+            routeName.Should().Be(RouteConstants.AddAddressSelect);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Called()
+        {
+            CacheService.Received(1).SetAsync(CacheKey,
+                Arg.Is<AddAddressViewModel>(x =>
+                x.AddAddressPostcode != null &&
+                x.AddAddressPostcode.Postcode == ViewModel.Postcode &&
+                x.AddAddressPostcode.BackLink != null &&
+                x.AddAddressPostcode.BackLink.RouteName == RouteConstants.ManagePostalAddress));
+        }
+    }
+}
