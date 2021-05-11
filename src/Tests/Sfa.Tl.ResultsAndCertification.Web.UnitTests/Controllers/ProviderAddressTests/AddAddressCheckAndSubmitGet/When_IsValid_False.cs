c18bfc96f9770cfd55afa482d68828e828965e9d
Ravi Bobbili <ravi.bob@outlook.com>
2021-05-11 12:09:12 +0100

    TLRC-3170 Check and Submit Address: Unit tests added for AddAddressCheckAndSubmitAsync

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ProviderAddressTests/AddAddressCheckAndSubmitGet/When_IsValid_False.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.ProviderAddress;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ProviderAddressTests.AddAddressCheckAndSubmitGet
+{
+    public class When_IsValid_False : TestSetup
+    {
+        private AddAddressViewModel _cacheResult;
+
+        public override void Given()
+        {
+            _cacheResult = new AddAddressViewModel
+            {
+                AddAddressPostcode = null,
+                AddAddressSelect = new AddAddressSelectViewModel(),
+                AddAddressManual = null,
+            };
+
+            CacheService.GetAsync<AddAddressViewModel>(CacheKey).Returns(_cacheResult);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Called()
+        {
+            CacheService.Received(1).GetAsync<AddAddressViewModel>(CacheKey);
+        }
+
+        [Fact]
+        public void Then_Redirected_To_PageNotFound()
+        {
+            var routeName = (Result as RedirectToRouteResult).RouteName;
+            routeName.Should().Be(RouteConstants.PageNotFound);
+        }
+    }
+}
