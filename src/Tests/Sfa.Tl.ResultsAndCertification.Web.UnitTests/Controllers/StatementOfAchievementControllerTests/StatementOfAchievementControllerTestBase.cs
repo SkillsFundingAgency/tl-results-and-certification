d2b0614d10447ec5b81008b0e17e3a65e4d59d54
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-05-19 12:42:33 +0100

    TLRC - 3103 - Statement of Achievement - 'Statements of achievement are not yet available' - TLRC - 3368 - Dev - Added Web Unit tests for StatementsOfAchievementNotAvailable

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/StatementOfAchievementControllerTests/StatementOfAchievementControllerTestBase.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Extensions;
+using Sfa.Tl.ResultsAndCertification.Common.Services.Cache;
+using Sfa.Tl.ResultsAndCertification.Models.Configuration;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
+using Sfa.Tl.ResultsAndCertification.Web.Loader.Interfaces;
+using System;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.StatementOfAchievementControllerTests
+{
+    public abstract class StatementOfAchievementControllerTestBase : BaseTest<StatementOfAchievementController>
+    {
+        // Dependencies
+        protected IProviderAddressLoader ProviderAddressLoader;
+        protected ICacheService CacheService;
+        protected ILogger<StatementOfAchievementController> Logger;
+        protected StatementOfAchievementController Controller;
+
+        // HttpContext
+        protected int ProviderUkprn;
+        protected Guid UserId;
+        protected IHttpContextAccessor HttpContextAccessor;
+        protected ResultsAndCertificationConfiguration ResultsAndCertificationConfiguration;
+
+        public override void Setup()
+        {
+            ResultsAndCertificationConfiguration = new ResultsAndCertificationConfiguration { SoaAvailableDate = "10/08/2021".ToDateTime() };
+            CacheService = Substitute.For<ICacheService>();
+            Logger = Substitute.For<ILogger<StatementOfAchievementController>>();
+            Controller = new StatementOfAchievementController(ResultsAndCertificationConfiguration, Logger);
+            
+            ProviderUkprn = 1234567890;
+            var httpContext = new ClaimsIdentityBuilder<StatementOfAchievementController>(Controller)
+               .Add(CustomClaimTypes.Ukprn, ProviderUkprn.ToString())
+               .Add(CustomClaimTypes.UserId, Guid.NewGuid().ToString())
+               .Build()
+               .HttpContext;
+
+            HttpContextAccessor = Substitute.For<IHttpContextAccessor>();
+            HttpContextAccessor.HttpContext.Returns(httpContext);
+        }
+    }
+}
