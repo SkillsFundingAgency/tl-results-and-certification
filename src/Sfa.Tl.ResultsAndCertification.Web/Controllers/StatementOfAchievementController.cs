fad9ad4f0c4e10c41a49c0e83aac88cc083c1294
Ravi Bobbili <ravi.bob@outlook.com>
2021-05-19 12:22:04 +0100

    TLRC-3104 SOA Missing Postal Address: Controller action and view and model are implemented.

--- src/Sfa.Tl.ResultsAndCertification.Web/Controllers/StatementOfAchievementController.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Controllers/StatementOfAchievementController.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Logging;
 using Sfa.Tl.ResultsAndCertification.Common.Extensions;
 using Sfa.Tl.ResultsAndCertification.Common.Helpers;
 using Sfa.Tl.ResultsAndCertification.Models.Configuration;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.ProviderAddress;
+using Sfa.Tl.ResultsAndCertification.Web.Loader.Interfaces;
 using Sfa.Tl.ResultsAndCertification.Web.ViewModel.StatementOfAchievement;
 using System.Threading.Tasks;
 
@@ -12,11 +14,13 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Controllers
     [Authorize(Policy = RolesExtensions.RequireLearnerRecordsEditorAccess)]
     public class StatementOfAchievementController : Controller
     {
+        private readonly IProviderAddressLoader _providerAddress;
         ResultsAndCertificationConfiguration _configuration;
         private readonly ILogger _logger;
 
-        public StatementOfAchievementController(ResultsAndCertificationConfiguration configuration, ILogger<StatementOfAchievementController> logger)
+        public StatementOfAchievementController(IProviderAddressLoader providerAddress, ResultsAndCertificationConfiguration configuration, ILogger<StatementOfAchievementController> logger)
         {
+            _providerAddress = providerAddress;
             _configuration = configuration;
             _logger = logger;
         }
@@ -32,14 +36,25 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Controllers
         [Route("request-statement-of-achievement", Name = RouteConstants.RequestStatementOfAchievement)]
         public async Task<IActionResult> RequestStatementOfAchievementAsync()
         {
-            await Task.CompletedTask;
+            var address = await _providerAddress.GetAddressAsync<Address>(User.GetUkPrn());
+            if (address == null)
+                return RedirectToRoute(RouteConstants.PostalAddressMissing);
+
             var viewModel = new RequestStatementOfAchievementViewModel { SoaAvailableDate = _configuration.SoaAvailableDate };
 
-            if(!viewModel.IsSoaAvailable)
+            if (!viewModel.IsSoaAvailable)
             {
                 return RedirectToRoute(RouteConstants.StatementsOfAchievementNotAvailable);
             }
             return View();
+        }
+
+        [HttpGet]
+        [Route("postal-address-missing", Name = RouteConstants.PostalAddressMissing)]
+        public IActionResult PostalAddressMissingAsync()
+        {
+            var viewModel = new PostalAddressMissingViewModel();
+            return View(viewModel);
         }
     }
 }
