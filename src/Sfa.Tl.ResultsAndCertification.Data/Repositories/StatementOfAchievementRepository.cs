c7bed414eabfec29af015c702004b9da9a08da21
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-05-22 16:18:48 +0100

    TLRC - 3111 - Statement of Achievement - ULN4 - Not withdrawn by AO - TLRC - 3419 - Dev - Implemented FindSoaLearnerRecord in StatementOfAchievementRepository

--- src/Sfa.Tl.ResultsAndCertification.Data/Repositories/StatementOfAchievementRepository.cs
+++ src/Sfa.Tl.ResultsAndCertification.Data/Repositories/StatementOfAchievementRepository.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
 using Sfa.Tl.ResultsAndCertification.Data.Interfaces;
 using Sfa.Tl.ResultsAndCertification.Models.Contracts.StatementOfAchievement;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Sfa.Tl.ResultsAndCertification.Data.Repositories
@@ -18,8 +22,26 @@ namespace Sfa.Tl.ResultsAndCertification.Data.Repositories
 
         public async Task<FindSoaLearnerRecord> FindSoaLearnerRecordAsync(long providerUkprn, long uln)
         {
-            await Task.CompletedTask;
-            return new FindSoaLearnerRecord();
+            var soaLearnerRecord = await (from tqPathway in _dbContext.TqRegistrationPathway
+                                          join tqProfile in _dbContext.TqRegistrationProfile on tqPathway.TqRegistrationProfileId equals tqProfile.Id
+                                          join tqProvider in _dbContext.TqProvider on tqPathway.TqProviderId equals tqProvider.Id
+                                          join tlProvider in _dbContext.TlProvider on tqProvider.TlProviderId equals tlProvider.Id
+                                          join tqAo in _dbContext.TqAwardingOrganisation on tqProvider.TqAwardingOrganisationId equals tqAo.Id
+                                          join tlPathway in _dbContext.TlPathway on tqAo.TlPathwayId equals tlPathway.Id
+                                          orderby tqPathway.CreatedOn descending
+                                          where tqProfile.UniqueLearnerNumber == uln && tlProvider.UkPrn == providerUkprn
+                                          select new FindSoaLearnerRecord
+                                          {
+                                              ProfileId = tqProfile.Id,
+                                              Uln = tqProfile.UniqueLearnerNumber,
+                                              LearnerName = tqProfile.Firstname + " " + tqProfile.Lastname,
+                                              DateofBirth = tqProfile.DateofBirth,
+                                              ProviderName = tlProvider.Name + " (" + tlProvider.UkPrn + ")",
+                                              TlevelTitle = tlPathway.TlevelTitle,
+                                              Status = tqPathway.Status
+                                          })
+                                .FirstOrDefaultAsync();
+            return soaLearnerRecord;
         }
     }
 }
