334c98ec0d3f2530cdf71589c52b37fb54daf3bf
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-05-23 16:05:05 +0100

    TLRC - 3111 - Statement of Achievement - ULN4 - Not withdrawn by AO - TLRC - 3413 - Dev - Added Integration Tests for FindSoaLearnerRecord in StatementOfAchievementRepository

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.IntegrationTests/Repositories/StatementOfAchievementRepositoryTests/StatementOfAchievementRepositoryBaseTest.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Extensions.Logging;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Data.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Data.Repositories;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.DataBuilders;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.DataProvider;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Repositories.StatementOfAchievementRepositoryTests
+{
+    public abstract class StatementOfAchievementRepositoryBaseTest : BaseTest<QualificationAchieved>
+    {
+        // Seed objects.
+        protected TlAwardingOrganisation TlAwardingOrganisation;
+        protected TlRoute Route;
+        protected TlPathway Pathway;
+        protected TlSpecialism Specialism;
+        protected TqAwardingOrganisation TqAwardingOrganisation;
+        protected IEnumerable<TlProvider> TlProviders;
+        protected TlProvider TlProvider;
+        protected TqProvider TqProvider;
+        protected IList<TlLookup> TlLookup;
+        protected IList<TqProvider> TqProviders;
+
+        // Dependencies.
+        protected ILogger<StatementOfAchievementRepository> StatementOfAchievementRepositoryLogger;
+        protected IStatementOfAchievementRepository StatementOfAchievementRepository;
+
+        protected virtual void SeedTestData(EnumAwardingOrganisation awardingOrganisation = EnumAwardingOrganisation.Pearson, bool seedMultipleProviders = false)
+        {
+            TlAwardingOrganisation = TlevelDataProvider.CreateTlAwardingOrganisation(DbContext, awardingOrganisation);
+            Route = TlevelDataProvider.CreateTlRoute(DbContext, awardingOrganisation);
+            Pathway = TlevelDataProvider.CreateTlPathway(DbContext, awardingOrganisation, Route);
+            Specialism = TlevelDataProvider.CreateTlSpecialisms(DbContext, awardingOrganisation, Pathway).First();
+            TqAwardingOrganisation = TlevelDataProvider.CreateTqAwardingOrganisation(DbContext, Pathway, TlAwardingOrganisation);
+            TlProviders = ProviderDataProvider.CreateTlProviders(DbContext);
+            TlProvider = ProviderDataProvider.CreateTlProvider(DbContext);
+            TqProvider = ProviderDataProvider.CreateTqProvider(DbContext, TqAwardingOrganisation, TlProviders.First());
+            TlLookup = TlLookupDataProvider.CreateTlLookupList(DbContext, null, true);
+
+            DbContext.SaveChangesAsync();
+        }
+
+        public TqRegistrationProfile SeedRegistrationDataByStatus(long uln, RegistrationPathwayStatus status = RegistrationPathwayStatus.Active, TqProvider tqProvider = null)
+        {
+            var profile = new TqRegistrationProfileBuilder().BuildList().FirstOrDefault(p => p.UniqueLearnerNumber == uln);
+            var tqRegistrationProfile = RegistrationsDataProvider.CreateTqRegistrationProfile(DbContext, profile);
+            var tqRegistrationPathway = RegistrationsDataProvider.CreateTqRegistrationPathway(DbContext, tqRegistrationProfile, tqProvider ?? TqProviders.First());
+            tqRegistrationPathway.Status = status;
+
+            var tqRegistrationSpecialism = RegistrationsDataProvider.CreateTqRegistrationSpecialism(DbContext, tqRegistrationPathway, Specialism);
+
+            if (status == RegistrationPathwayStatus.Withdrawn)
+            {
+                tqRegistrationPathway.EndDate = DateTime.UtcNow.AddDays(-1);
+                tqRegistrationSpecialism.IsOptedin = true;
+                tqRegistrationSpecialism.EndDate = DateTime.UtcNow.AddDays(-1);
+            }
+
+            DbContext.SaveChanges();
+            return profile;
+        }
+
+        public void TransferRegistration(TqRegistrationProfile profile, Provider transferTo)
+        {
+            var toProvider = DbContext.TlProvider.FirstOrDefault(x => x.UkPrn == (long)transferTo);
+            var transferToTqProvider = ProviderDataProvider.CreateTqProvider(DbContext, TqAwardingOrganisation, toProvider, true);
+
+            foreach (var pathway in profile.TqRegistrationPathways)
+            {
+                pathway.Status = RegistrationPathwayStatus.Transferred;
+                pathway.EndDate = DateTime.UtcNow;
+
+                foreach (var specialism in pathway.TqRegistrationSpecialisms)
+                {
+                    specialism.IsOptedin = true;
+                    specialism.EndDate = DateTime.UtcNow;
+                }
+            }
+
+            var tqRegistrationPathway = RegistrationsDataProvider.CreateTqRegistrationPathway(DbContext, profile, transferToTqProvider);
+            var tqRegistrationSpecialism = RegistrationsDataProvider.CreateTqRegistrationSpecialism(DbContext, tqRegistrationPathway, Specialism);
+            DbContext.SaveChanges();
+        }
+    }
+
+    public enum Provider
+    {
+        BarsleyCollege = 10000536,
+        WalsallCollege = 10007315
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.IntegrationTests/Repositories/StatementOfAchievementRepositoryTests/When_FindSoaLearnerRecordAsync_IsCalled.cs
@@ -0,0 +1,101 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Data.Repositories;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.StatementOfAchievement;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.Enum;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Repositories.StatementOfAchievementRepositoryTests
+{
+    public class When_FindSoaLearnerRecordAsync_IsCalled : StatementOfAchievementRepositoryBaseTest
+    {
+        private Dictionary<long, RegistrationPathwayStatus> _ulns;
+        private IList<TqRegistrationProfile> _profiles;
+        private FindSoaLearnerRecord _actualResult;
+
+        public override void Given()
+        {
+            _profiles = new List<TqRegistrationProfile>();
+            _ulns = new Dictionary<long, RegistrationPathwayStatus>
+            {
+                { 1111111111, RegistrationPathwayStatus.Active },
+                { 1111111112, RegistrationPathwayStatus.Withdrawn },
+                { 1111111113, RegistrationPathwayStatus.Active }
+            };
+
+            SeedTestData(EnumAwardingOrganisation.Pearson, true);
+
+            foreach (var uln in _ulns)
+            {
+                _profiles.Add(SeedRegistrationDataByStatus(uln.Key, uln.Value, TqProvider));
+            }
+
+            TransferRegistration(_profiles.FirstOrDefault(p => p.UniqueLearnerNumber == 1111111113), Provider.WalsallCollege);
+
+            DbContext.SaveChanges();
+
+            // Test class.
+            StatementOfAchievementRepository = new StatementOfAchievementRepository(DbContext, StatementOfAchievementRepositoryLogger);
+        }
+
+        public override Task When()
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task WhenAsync(long providerUkprn, long uln)
+        {
+            if (_actualResult != null)
+                return;
+
+            _actualResult = await StatementOfAchievementRepository.FindSoaLearnerRecordAsync(providerUkprn, uln);
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public async Task Then_Returns_Expected_Results(long uln, Provider provider, RegistrationPathwayStatus expectedStatus, bool hasResult)
+        {
+            await WhenAsync((long)provider, uln);
+
+            if (hasResult == false)
+            {
+                _actualResult.Should().BeNull();
+                return;
+            }
+
+            var expectedProvider = TlProviders.FirstOrDefault(p => p.UkPrn == (long)provider);
+            var expectedProviderName = expectedProvider != null ? $"{expectedProvider.Name} ({expectedProvider.UkPrn})" : null;
+            var expectedTlevelTitle = Pathway.TlevelTitle;
+            var expectedProfile = _profiles.FirstOrDefault(p => p.UniqueLearnerNumber == uln);
+
+            expectedProfile.Should().NotBeNull();
+            _actualResult.Should().NotBeNull();
+            _actualResult.Uln.Should().Be(uln);
+            _actualResult.LearnerName.Should().Be($"{expectedProfile.Firstname} {expectedProfile.Lastname}");
+            _actualResult.DateofBirth.Should().Be(expectedProfile.DateofBirth);
+            _actualResult.ProviderName.Should().Be(expectedProviderName);
+            _actualResult.TlevelTitle.Should().Be(expectedTlevelTitle);
+            _actualResult.Status.Should().Be(expectedStatus);
+        }
+
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { 9999999999, Provider.WalsallCollege, null, false }, // Invalid Uln
+                    new object[] { 1111111111, Provider.BarsleyCollege, RegistrationPathwayStatus.Active, true }, // Active
+                    new object[] { 1111111111, Provider.WalsallCollege, null, false }, // Uln not from WalsallCollege
+                    new object[] { 1111111112, Provider.BarsleyCollege, RegistrationPathwayStatus.Withdrawn, true }, // Withdrawn
+                    new object[] { 1111111113, Provider.BarsleyCollege, RegistrationPathwayStatus.Transferred, true }, // Transferred
+                    new object[] { 1111111113, Provider.WalsallCollege, RegistrationPathwayStatus.Active, true } // Active
+                };
+            }
+        }
+    }
+}
