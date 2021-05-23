d494c5005260eba8f02c12b81adbbd76c97dce51
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-05-23 15:37:45 +0100

    TLRC - 3111 - Statement of Achievement - ULN4 - Not withdrawn by AO - TLRC - 3412 - Dev - Added Api client Unit Tests for FindSoaLearnerRecord

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Api.Client.UnitTests/Clients/ResultsAndCertificationInternalApiClientTest/When_FindSoaLearnerRecord_Called.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Api.Client.Clients;
+using Sfa.Tl.ResultsAndCertification.Api.Client.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Configuration;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.StatementOfAchievement;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Api.Client.UnitTests.Clients.ResultsAndCertificationInternalApiClientTest
+{
+    public class When_FindSoaLearnerRecord_Called : BaseTest<ResultsAndCertificationInternalApiClient>
+    {
+        // inputs
+        private readonly long _providerUkprn = 12345678;
+        private readonly long _uln = 987654321;
+
+        // results
+        private FindSoaLearnerRecord _actualResult;
+        private FindSoaLearnerRecord _mockApiResponse;
+
+        // dependencies
+        private ITokenServiceClient _tokenServiceClient;
+        private ResultsAndCertificationConfiguration _configuration;
+        private ResultsAndCertificationInternalApiClient _apiClient;
+
+        public override void Setup()
+        {
+            _tokenServiceClient = Substitute.For<ITokenServiceClient>();
+            _configuration = new ResultsAndCertificationConfiguration
+            {
+                ResultsAndCertificationInternalApiSettings = new ResultsAndCertificationInternalApiSettings { Uri = "http://tlevel.api.com" }
+            };
+
+            _mockApiResponse = new FindSoaLearnerRecord
+            {
+                ProfileId = 11,
+                Uln = _uln,
+                LearnerName = "Test User",
+                DateofBirth = DateTime.UtcNow.AddYears(30),
+                ProviderName = "Barnsley College (123456789)",
+                TlevelTitle = "Title",
+                Status = Common.Enum.RegistrationPathwayStatus.Active
+            };
+        }
+
+        public override void Given()
+        {
+            HttpClient = new HttpClient(new MockHttpMessageHandler<FindSoaLearnerRecord>(_mockApiResponse, string.Format(ApiConstants.FindSoaLearnerRecordUri, _providerUkprn, _uln), HttpStatusCode.OK));
+            _apiClient = new ResultsAndCertificationInternalApiClient(HttpClient, _tokenServiceClient, _configuration);
+        }
+
+        public async override Task When()
+        {
+            _actualResult = await _apiClient.FindSoaLearnerRecordAsync(_providerUkprn, _uln);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            _actualResult.Should().NotBeNull();
+            _actualResult.Uln.Should().Be(_mockApiResponse.Uln);
+            _actualResult.LearnerName.Should().Be(_mockApiResponse.LearnerName);
+            _actualResult.DateofBirth.Should().Be(_mockApiResponse.DateofBirth);
+            _actualResult.ProviderName.Should().Be(_mockApiResponse.ProviderName);
+            _actualResult.TlevelTitle.Should().Be(_mockApiResponse.TlevelTitle);
+            _actualResult.Status.Should().Be(_mockApiResponse.Status);
+        }
+    }
+}
