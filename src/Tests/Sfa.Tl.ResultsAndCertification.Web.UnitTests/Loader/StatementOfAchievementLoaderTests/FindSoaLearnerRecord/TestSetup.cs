a839d61a567cff6982805c269461fb04b7949e63
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-05-23 15:33:38 +0100

    TLRC - 3111 - Statement of Achievement - ULN4 - Not withdrawn by AO - TLRC - 3411 - Dev - Added Web loader Unit Tests for FindSoaLearnerRecord

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/StatementOfAchievementLoaderTests/FindSoaLearnerRecord/TestSetup.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.StatementOfAchievementLoaderTests.FindSoaLearnerRecord
+{
+    public abstract class TestSetup : StatementOfAchievementLoaderTestBase
+    {
+        protected long ProviderUkprn;
+        protected long Uln;
+        protected Models.Contracts.StatementOfAchievement.FindSoaLearnerRecord ActualResult { get; set; }
+
+        public async override Task When()
+        {
+            ActualResult = await Loader.FindSoaLearnerRecordAsync(ProviderUkprn, Uln);
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/StatementOfAchievementLoaderTests/FindSoaLearnerRecord/When_Called_With_Valid_Data.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using System;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.StatementOfAchievementLoaderTests.FindSoaLearnerRecord
+{
+    public class When_Called_With_Valid_Data : TestSetup
+    {
+        private Models.Contracts.StatementOfAchievement.FindSoaLearnerRecord _expectedApiResult;
+
+        public override void Given()
+        {
+            _expectedApiResult = new Models.Contracts.StatementOfAchievement.FindSoaLearnerRecord
+            {
+                ProfileId = 1,
+                Uln = 123456789,
+                LearnerName = "Test user",
+                DateofBirth = DateTime.UtcNow.AddYears(-20),
+                ProviderName = "Barsley College (54678945)",
+                TlevelTitle = "Title",
+                Status = Common.Enum.RegistrationPathwayStatus.Active
+            };
+
+            InternalApiClient.FindSoaLearnerRecordAsync(ProviderUkprn, Uln).Returns(_expectedApiResult);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            ActualResult.Should().NotBeNull();
+            ActualResult.ProfileId.Should().Be(_expectedApiResult.ProfileId);
+            ActualResult.Uln.Should().Be(_expectedApiResult.Uln);
+            ActualResult.LearnerName.Should().Be(_expectedApiResult.LearnerName);
+            ActualResult.DateofBirth.Should().Be(_expectedApiResult.DateofBirth);
+            ActualResult.ProviderName.Should().Be(_expectedApiResult.ProviderName);
+            ActualResult.TlevelTitle.Should().Be(_expectedApiResult.TlevelTitle);
+            ActualResult.Status.Should().Be(_expectedApiResult.Status);
+        }
+    }
+}
