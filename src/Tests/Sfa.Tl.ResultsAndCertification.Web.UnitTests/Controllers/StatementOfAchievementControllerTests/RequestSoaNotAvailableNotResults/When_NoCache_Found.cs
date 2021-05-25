16c3e2f272478194f68a83afd543b783bcac302d
Ravi Bobbili <ravi.bob@outlook.com>
2021-05-25 14:08:09 +0100

    TLRC-3316 SOA Minimum requirements not met - Repository amended and Web UT added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/StatementOfAchievementControllerTests/RequestSoaNotAvailableNotResults/When_NoCache_Found.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.StatementOfAchievement;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.StatementOfAchievementControllerTests.RequestSoaNotAvailableNotResults
+{
+    public class When_NoCache_Found : TestSetup
+    {
+        private readonly RequestSoaNotAvailableNoResultsViewModel _mockCache = null;
+
+        public override void Given()
+        {
+            CacheService.GetAndRemoveAsync<RequestSoaNotAvailableNoResultsViewModel>(CacheKey).Returns(_mockCache);
+        }       
+
+        [Fact]
+        public void Then_Expected_Method_IsCalled()
+        {
+            CacheService.Received(1).GetAndRemoveAsync<RequestSoaNotAvailableNoResultsViewModel>(CacheKey);
+        }
+
+        [Fact]
+        public void Then_Redirected_To_PageNotFound()
+        {
+            var routeName = (Result as RedirectToRouteResult).RouteName;
+            routeName.Should().Be(RouteConstants.PageNotFound);
+        }
+    }
+}
