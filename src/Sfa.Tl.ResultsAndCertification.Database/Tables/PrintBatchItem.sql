6269d5b4f60eaac7c5376f179c6b336806da7e4f
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-06-03 13:58:17 +0100

    TLRC - 3157 - Statement of Achievement - Request Complete - TLRC - 3496 - Dev - Database design and table creation scripts

---
+++ src/Sfa.Tl.ResultsAndCertification.Database/Tables/PrintBatchItem.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[PrintBatchItem]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[BatchId] INT NOT NULL,
+	[AddressId] INT NOT NULL,
+	[CreatedOn] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [CreatedBy] NVARCHAR(50) NULL, 
+    [ModifiedOn] DATETIME2 NULL, 
+    [ModifiedBy] NVARCHAR(50) NULL,
+	CONSTRAINT [PK_PrintBatchItem] PRIMARY KEY ([Id]),
+	CONSTRAINT [FK_PrintBatchItem_Batch] FOREIGN KEY ([BatchId]) REFERENCES [Batch]([Id]),
+	CONSTRAINT [FK_PrintBatchItem_TlProviderAddress] FOREIGN KEY ([AddressId]) REFERENCES [TlProviderAddress]([Id]),
+	INDEX IX_PrintBatchItem_BatchId NONCLUSTERED ([BatchId])
+)
---
+++ src/Sfa.Tl.ResultsAndCertification.Database/Tables/PrintCertificate.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[PrintCertificate]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+    [PrintBatchItemId] INT NOT NULL,    
+    [CertificateNumber] AS FORMAT([Id], '0000000#'),
+    [Uln] BIGINT NOT NULL,
+    [LearnerName] NVARCHAR(256) NOT NULL,
+    [TqRegistrationPathwayId] INT NOT NULL,
+    [Type] INT NOT NULL,
+    [LearningDetails] NVARCHAR(MAX) NULL,
+    [DisplaySnapshot] NVARCHAR(MAX) NULL,
+    [Status] INT NULL,
+    [Reason] NVARCHAR(256) NULL,
+    [TrackingId] NVARCHAR(100) NULL,
+    [StatusChangedOn] DATETIME2 NULL,
+    [CreatedOn] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [CreatedBy] NVARCHAR(50) NULL, 
+    [ModifiedOn] DATETIME2 NULL, 
+    [ModifiedBy] NVARCHAR(50) NULL,
+	CONSTRAINT [PK_PrintCertificate] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_PrintCertificate_PrintBatchItem] FOREIGN KEY ([PrintBatchItemId]) REFERENCES [PrintBatchItem]([Id]),
+    CONSTRAINT [FK_PrintCertificate_TqRegistrationPathway] FOREIGN KEY ([TqRegistrationPathwayId]) REFERENCES [TqRegistrationPathway]([Id]),
+	INDEX IX__PrintCertificate_PrintBatchItemId NONCLUSTERED ([PrintBatchItemId]),
+    INDEX IX__PrintCertificate_Uln_TqRegistrationPathwayId NONCLUSTERED ([Uln], [TqRegistrationPathwayId])
+)
