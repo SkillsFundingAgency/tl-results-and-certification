23bb339009af6e4f97f5d0ce70b2b1a4f76c9ec6
Ravi Bobbili <ravi.bob@outlook.com>
2021-06-04 13:11:08 +0100

    TLRC-3165 Change Component Achievements - Unit test added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/StatementOfAchievementControllerTests/ChangeComponentAchievement/When_Action_IsCalled.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.StatementOfAchievement;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.StatementOfAchievementControllerTests.ChangeComponentAchievement
+{
+    public class When_Action_IsCalled : TestSetup
+    {
+        public override void Given()
+        {
+            ProfileId = 11;
+        }
+
+        [Fact]
+        public void Then_Expected_Results_Are_Returned()
+        {
+            Result.Should().NotBeNull();
+            (Result as ViewResult).Model.Should().NotBeNull();
+
+            var model = (Result as ViewResult).Model as ChangeComponentAchievementsViewModel;
+            model.ProfileId.Should().Be(ProfileId);
+
+            // Back link
+            model.BackLink.Should().NotBeNull();
+            model.BackLink.RouteName.Should().Be(RouteConstants.RequestSoaCheckAndSubmit);
+            model.BackLink.RouteAttributes.Count.Should().Be(1);
+            model.BackLink.RouteAttributes.TryGetValue(Constants.ProfileId, out string routeValue);
+            routeValue.Should().Be(ProfileId.ToString());
+        }
+    }
+}
