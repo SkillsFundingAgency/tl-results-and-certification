0bdb683a1a251b6dd1b4df9d7efc1224b6697dc7
Ravi Bobbili <ravi.bob@outlook.com>
2021-06-09 11:44:39 +0100

    TLRC-3108 Soa Already Requested: Content files are created and configured.

--- src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/StatementOfAchievement/RequestSoaSubmittedAlreadyViewModel.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/StatementOfAchievement/RequestSoaSubmittedAlreadyViewModel.cs
@@ -7,9 +7,8 @@ using Sfa.Tl.ResultsAndCertification.Web.ViewModel.ProviderAddress;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using RequestSoaCheckAndSubmitContent = Sfa.Tl.ResultsAndCertification.Web.Content.StatementOfAchievement.RequestSoaCheckAndSubmit;
+using SoaRequestedAlreadyContent = Sfa.Tl.ResultsAndCertification.Web.Content.StatementOfAchievement.RequestSoaSubmittedAlready;
 using BreadcrumbContent = Sfa.Tl.ResultsAndCertification.Web.Content.ViewComponents.Breadcrumb;
-// TODO: new content file. 
 
 namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.StatementOfAchievement
 {
@@ -24,80 +23,80 @@ namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.StatementOfAchievement
         public SummaryItemModel SummaryUln => new SummaryItemModel
         {
             Id = "uln",
-            Title = RequestSoaCheckAndSubmitContent.Title_Uln_Text,
+            Title = SoaRequestedAlreadyContent.Title_Uln_Text,
             Value = SnapshotDetails.Uln.ToString()
         };
 
         public SummaryItemModel SummaryLearnerName => new SummaryItemModel
         {
             Id = "learnername",
-            Title = RequestSoaCheckAndSubmitContent.Title_Name_Text,
+            Title = SoaRequestedAlreadyContent.Title_Name_Text,
             Value = SnapshotDetails.Name
         };
 
         public SummaryItemModel SummaryDateofBirth => new SummaryItemModel
         {
             Id = "dateofbirth",
-            Title = RequestSoaCheckAndSubmitContent.Title_DateofBirth_Text,
+            Title = SoaRequestedAlreadyContent.Title_DateofBirth_Text,
             Value = SnapshotDetails.Dateofbirth.ToDobFormat()
         };
 
         public SummaryItemModel SummaryProvider => new SummaryItemModel
         {
             Id = "providername",
-            Title = RequestSoaCheckAndSubmitContent.Title_Provider_Text,
+            Title = SoaRequestedAlreadyContent.Title_Provider_Text,
             Value = SnapshotDetails.ProviderName
         };
 
         public SummaryItemModel SummaryTlevelTitle => new SummaryItemModel
         {
             Id = "tleveltitle",
-            Title = RequestSoaCheckAndSubmitContent.Title_Tlevel_Title_Text,
+            Title = SoaRequestedAlreadyContent.Title_Tlevel_Title_Text,
             Value = SnapshotDetails.TlevelTitle
         };
 
         public SummaryItemModel SummaryCoreCode => new SummaryItemModel
         {
             Id = "corecode",
-            Title = RequestSoaCheckAndSubmitContent.Title_Core_Code_Text,
-            Value = string.Format(RequestSoaCheckAndSubmitContent.Core_Code_Value, SnapshotDetails.Core, SnapshotDetails.CoreGrade),
+            Title = SoaRequestedAlreadyContent.Title_Core_Code_Text,
+            Value = string.Format(SoaRequestedAlreadyContent.Core_Code_Value, SnapshotDetails.Core, SnapshotDetails.CoreGrade),
             IsRawHtml = true
         };
 
         public SummaryItemModel SummarySpecialismCode => new SummaryItemModel
         {
             Id = "specialismcode",
-            Title = RequestSoaCheckAndSubmitContent.Title_Occupational_Specialism_Text,
-            Value = string.Format(RequestSoaCheckAndSubmitContent.Occupational_Specialism_Value, SnapshotDetails.Specialism, SnapshotDetails.SpecialismGrade),
+            Title = SoaRequestedAlreadyContent.Title_Occupational_Specialism_Text,
+            Value = string.Format(SoaRequestedAlreadyContent.Occupational_Specialism_Value, SnapshotDetails.Specialism, SnapshotDetails.SpecialismGrade),
             IsRawHtml = true
         };
 
         public SummaryItemModel SummaryEnglishAndMaths => new SummaryItemModel
         {
             Id = "englishandmaths",
-            Title = RequestSoaCheckAndSubmitContent.Title_English_And_Maths_Text,
+            Title = SoaRequestedAlreadyContent.Title_English_And_Maths_Text,
             Value = SnapshotDetails.EnglishAndMaths
         };
 
         public SummaryItemModel SummaryIndustryPlacement => new SummaryItemModel
         {
             Id = "industryplacement",
-            Title = RequestSoaCheckAndSubmitContent.Title_Industry_Placement_Text,
+            Title = SoaRequestedAlreadyContent.Title_Industry_Placement_Text,
             Value = SnapshotDetails.IndustryPlacement
         };
 
         public SummaryItemModel SummaryDepartment => new SummaryItemModel
         {
             Id = "department",
-            Title = RequestSoaCheckAndSubmitContent.Title_Department_Text,
+            Title = SoaRequestedAlreadyContent.Title_Department_Text,
             Value = SnapshotDetails.ProviderAddress?.DepartmentName
         };
 
         public SummaryItemModel SummaryAddress => new SummaryItemModel
         {
             Id = "address",
-            Title = RequestSoaCheckAndSubmitContent.Title_Organisation_Address_Text,
-            Value = string.Format(RequestSoaCheckAndSubmitContent.Organisation_Address_Value, FormatedAddress),
+            Title = SoaRequestedAlreadyContent.Title_Organisation_Address_Text,
+            Value = string.Format(SoaRequestedAlreadyContent.Organisation_Address_Value, FormatedAddress),
             IsRawHtml = true
         };
 
@@ -133,7 +132,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.StatementOfAchievement
             get
             {
                 var addressLines = new List<string> { SnapshotDetails.ProviderAddress?.OrganisationName, SnapshotDetails.ProviderAddress?.AddressLine1, SnapshotDetails.ProviderAddress?.AddressLine2, SnapshotDetails.ProviderAddress?.Town, SnapshotDetails.ProviderAddress?.Postcode };
-                return string.Join(RequestSoaCheckAndSubmitContent.Html_Line_Break, addressLines.Where(x => !string.IsNullOrWhiteSpace(x)));
+                return string.Join(SoaRequestedAlreadyContent.Html_Line_Break, addressLines.Where(x => !string.IsNullOrWhiteSpace(x)));
             }
         }
     }
