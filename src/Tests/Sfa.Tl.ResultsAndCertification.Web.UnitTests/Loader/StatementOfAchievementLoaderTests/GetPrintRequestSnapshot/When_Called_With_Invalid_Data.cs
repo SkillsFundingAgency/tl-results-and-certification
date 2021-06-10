10b5a0d9766ddc6410228d976b1cf75cda5743d6
Ravi Bobbili <ravi.bob@outlook.com>
2021-06-10 10:20:21 +0100

    TLRC-3108 Soa Already Requested: Unit tests added for Api client.

--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/StatementOfAchievementLoaderTests/GetPrintRequestSnapshot/When_Called_With_Invalid_Data.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/StatementOfAchievementLoaderTests/GetPrintRequestSnapshot/When_Called_With_Invalid_Data.cs
@@ -7,16 +7,16 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.StatementOfAchieve
 {
     public class When_Called_With_Invalid_Data : TestSetup
     {
-        private SoaLearnerRecordDetails expectedApiResult = null;
+        private PrintRequestSnapshot _expectedApiResult = null;
 
         public override void Given()
         {
-            expectedApiResult = null;
-            InternalApiClient.GetSoaLearnerRecordDetailsAsync(Arg.Any<long>(), Arg.Any<int>()).Returns(expectedApiResult);
+            _expectedApiResult = null;
+            InternalApiClient.GetPrintRequestSnapshotAsync(Arg.Any<long>(), Arg.Any<int>(), Arg.Any<int>()).Returns(_expectedApiResult);
         }
 
         [Fact]
-        public void Then_Returns_Expected_Results()
+        public void Then_Returns_Null()
         {
             ActualResult.Should().BeNull();
         }
