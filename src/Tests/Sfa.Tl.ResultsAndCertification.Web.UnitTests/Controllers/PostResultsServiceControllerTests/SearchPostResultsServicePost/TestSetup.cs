d9f4804fd35477f76f9377779b83fb15aea5daba
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-06-17 13:10:20 +0100

    TLRC - 3474 - Post Results Services - Search for a learner page - TLRC - 3569 - Dev - Added UnitTests

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/PostResultsServiceControllerTests/SearchPostResultsServicePost/TestSetup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.PostResultsService;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.PostResultsServiceControllerTests.SearchPostResultsServicePost
+{
+    public abstract class TestSetup : PostResultsServiceControllerTestBase
+    {
+        public SearchPostResultsServiceViewModel ViewModel { get; set; }
+        public IActionResult Result { get; private set; }
+
+        public async override Task When()
+        {
+            Result = await Controller.SearchPostResultsServiceAsync(ViewModel);
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/PostResultsServiceControllerTests/SearchPostResultsServicePost/When_Uln_IsInvalid.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.PostResultsService;
+using Xunit;
+using SearchPostResultsServiceContent = Sfa.Tl.ResultsAndCertification.Web.Content.PostResultsService.SearchPostResultsService;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.PostResultsServiceControllerTests.SearchPostResultsServicePost
+{
+    public class When_Uln_IsInvalid : TestSetup
+    {
+        public override void Given()
+        {
+            ViewModel = new SearchPostResultsServiceViewModel { SearchUln = "xyz" };
+            Controller.ModelState.AddModelError("SearchUln", SearchPostResultsServiceContent.Uln_Not_Valid_Validation_Message);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_ValidationErrorMessage()
+        {
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(SearchPostResultsServiceViewModel));
+
+            Controller.ViewData.ModelState.ContainsKey(nameof(SearchPostResultsServiceViewModel.SearchUln)).Should().BeTrue();
+            var modelState = Controller.ViewData.ModelState[nameof(SearchPostResultsServiceViewModel.SearchUln)];
+            modelState.Errors.Count.Should().Be(1);
+            modelState.Errors[0].ErrorMessage.Should().Be(SearchPostResultsServiceContent.Uln_Not_Valid_Validation_Message);
+        }
+    }
+}
