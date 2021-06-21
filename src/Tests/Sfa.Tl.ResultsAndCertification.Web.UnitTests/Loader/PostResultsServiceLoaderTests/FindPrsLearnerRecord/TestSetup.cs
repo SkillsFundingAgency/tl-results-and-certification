35a22bb3939d6f408fb1fc2b572a08c6ce9f54b9
Ravi Bobbili <ravi.bob@outlook.com>
2021-06-21 11:12:22 +0100

    TLRC-3475 PRS Uln not found: Loader unit tests are being added.

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/PostResultsServiceLoaderTests/FindPrsLearnerRecord/TestSetup.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.PostResultsServiceLoaderTests.FindPrsLearnerRecord
+{
+    public abstract class TestSetup : PostResultsServiceLoaderTestBase
+    {
+        protected long AoUkprn;
+        protected long Uln;
+        protected Models.Contracts.PostResultsService.FindPrsLearnerRecord ActualResult { get; set; }
+
+        public async override Task When()
+        {
+            ActualResult = await Loader.FindPrsLearnerRecordAsync(AoUkprn, Uln);
+        }
+    }
+}
