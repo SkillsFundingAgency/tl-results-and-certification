7ad1e8771020f3a02e918a3252d3a46ee4af0be6
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-06-25 11:02:32 +0100

    TLRC - 3478 - Post Results Services - Core component being appealed - TLRC - 3613 - Dev - Added Web Unit Tests for PrsAppealCoreGrade Post

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/PostResultsServiceControllerTests/PrsAppealCoreGradePost/When_ModelState_Invalid.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.PostResultsService;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.PostResultsServiceControllerTests.PrsAppealCoreGradePost
+{
+    public class When_ModelState_Invalid : TestSetup
+    {
+        public override void Given()
+        {
+            ViewModel = new AppealCoreGradeViewModel { ProfileId = 1, AppealGrade = null };
+            Controller.ModelState.AddModelError("AppealGrade", Content.PostResultsService.AppealCoreGrade.Validation_Message);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(AppealCoreGradeViewModel));
+
+            Controller.ViewData.ModelState.Should().ContainSingle();
+            Controller.ViewData.ModelState.ContainsKey(nameof(AppealCoreGradeViewModel.AppealGrade)).Should().BeTrue();
+
+            var modelState = Controller.ViewData.ModelState[nameof(AppealCoreGradeViewModel.AppealGrade)];
+            modelState.Errors[0].ErrorMessage.Should().Be(Content.PostResultsService.AppealCoreGrade.Validation_Message);
+        }
+    }
+}
