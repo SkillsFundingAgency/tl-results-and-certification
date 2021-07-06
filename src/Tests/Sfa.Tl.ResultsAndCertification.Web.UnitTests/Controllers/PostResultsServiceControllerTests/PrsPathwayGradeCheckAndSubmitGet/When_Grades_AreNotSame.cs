524b86bfcd68ddef393bf650add7d6f4178eb680
Ravi Bobbili <ravi.bob@outlook.com>
2021-07-06 16:58:52 +0100

    TLRC-3630 Prs Check and Submit: Unit tests are amended and Check And Submit view content used from rex file.

--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/PostResultsServiceControllerTests/PrsPathwayGradeCheckAndSubmitGet/When_Grades_AreNotSame.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/PostResultsServiceControllerTests/PrsPathwayGradeCheckAndSubmitGet/When_Grades_AreNotSame.cs
@@ -1,9 +1,12 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Extensions;
 using Sfa.Tl.ResultsAndCertification.Common.Helpers;
 using Sfa.Tl.ResultsAndCertification.Web.ViewModel.PostResultsService;
+using System;
 using Xunit;
+using LearnerDetailsContent = Sfa.Tl.ResultsAndCertification.Web.Content.PostResultsService.PrsPathwayGradeCheckAndSubmit;
 
 namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.PostResultsServiceControllerTests.PrsPathwayGradeCheckAndSubmitGet
 {
@@ -18,9 +21,21 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.PostResultsSe
 
             _mockCache = new PrsPathwayGradeCheckAndSubmitViewModel
             {
+                Uln = 1234567890,
+                Firstname = "John",
+                Lastname = "Smith",
+                DateofBirth = DateTime.Today.AddYears(-20),
+                TlevelTitle = "Tlevel in Education",
+                PathwayTitle = "Educateion (1234455)",
+                ProviderName = "Barsley College",
+                ProviderUkprn = 87654321,
                 NewGrade = newGrade,
                 OldGrade = previousGrade,
-                IsGradeChanged = true,
+                IsGradeChanged = false,
+
+                ProfileId = 1,
+                AssessmentId = 2,
+                ResultId = 3,
             };
             CacheService.GetAsync<PrsPathwayGradeCheckAndSubmitViewModel>(CacheKey).Returns(_mockCache);
         }
@@ -32,7 +47,48 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.PostResultsSe
             var model = viewResult.Model as PrsPathwayGradeCheckAndSubmitViewModel;
 
             model.Should().NotBeNull();
+            model.Firstname.Should().Be(_mockCache.Firstname);
+            model.Lastname.Should().Be(_mockCache.Lastname);
+            model.LearnerName.Should().Be($"{_mockCache.Firstname} {_mockCache.Lastname}");
+            model.ProviderName.Should().Be(_mockCache.ProviderName);
+            model.ProviderUkprn.Should().Be(_mockCache.ProviderUkprn);
+            model.ProviderDisplayName.Should().Be($"{_mockCache.ProviderName}<br/>({_mockCache.ProviderUkprn})");
+            model.TlevelTitle.Should().Be(_mockCache.TlevelTitle);
+            model.NewGrade.Should().Be(_mockCache.NewGrade);
+            model.OldGrade.Should().Be(_mockCache.OldGrade);
+
+            model.ProfileId.Should().Be(_mockCache.ProfileId);
+            model.AssessmentId.Should().Be(_mockCache.AssessmentId);
+            model.ResultId.Should().Be(_mockCache.ResultId);
             model.IsGradeChanged.Should().BeTrue();
+
+            // Uln
+            model.SummaryUln.Title.Should().Be(LearnerDetailsContent.Title_Uln_Text);
+            model.SummaryUln.Value.Should().Be(_mockCache.Uln.ToString());
+
+            // LearnerName
+            model.SummaryLearnerName.Title.Should().Be(LearnerDetailsContent.Title_Name_Text);
+            model.SummaryLearnerName.Value.Should().Be(_mockCache.LearnerName);
+
+            // DateofBirth
+            model.SummaryDateofBirth.Title.Should().Be(LearnerDetailsContent.Title_DateofBirth_Text);
+            model.SummaryDateofBirth.Value.Should().Be(_mockCache.DateofBirth.ToDobFormat());
+
+            // ProviderName
+            model.SummaryProvider.Title.Should().Be(LearnerDetailsContent.Title_Provider_Text);
+            model.SummaryProvider.Value.Should().Be(_mockCache.ProviderDisplayName);
+
+            // TLevelTitle
+            model.SummaryTlevelTitle.Title.Should().Be(LearnerDetailsContent.Title_TLevel_Text);
+            model.SummaryTlevelTitle.Value.Should().Be(_mockCache.TlevelTitle);
+
+            // Old Grade
+            model.SummaryOldGrade.Title.Should().Be(LearnerDetailsContent.Title_Old_Grade);
+            model.SummaryOldGrade.Value.Should().Be(_mockCache.OldGrade);
+
+            // New Grade
+            model.SummaryNewGrade.Title.Should().Be(LearnerDetailsContent.Title_New_Grade);
+            model.SummaryNewGrade.Value.Should().Be(_mockCache.NewGrade);
 
             // Backlink
             model.BackLink.Should().NotBeNull();
