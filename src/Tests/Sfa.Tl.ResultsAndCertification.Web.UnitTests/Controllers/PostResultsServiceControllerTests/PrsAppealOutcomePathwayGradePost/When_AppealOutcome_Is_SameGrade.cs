567e8d7588ce04497d73f02da192cba1baecbdd8
Ravi Bobbili <ravi.bob@outlook.com>
2021-07-06 13:46:49 +0100

    TLRC-3630 Prs Check and Submit: Loader Unit tests are added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/PostResultsServiceControllerTests/PrsAppealOutcomePathwayGradePost/When_AppealOutcome_Is_SameGrade.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.PostResultsService;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.PostResultsServiceControllerTests.PrsAppealOutcomePathwayGradePost
+{
+    public class When_AppealOutcome_Is_SameGrade : TestSetup
+    {
+        private PrsPathwayGradeCheckAndSubmitViewModel _checkAndSubmitViewModel;
+
+        public override void Given()
+        {
+            ViewModel = new AppealOutcomePathwayGradeViewModel { AppealOutcome = AppealOutcomeType.SameGrade };
+
+            _checkAndSubmitViewModel = new PrsPathwayGradeCheckAndSubmitViewModel { OldGrade = "B" };
+            Loader.GetPrsLearnerDetailsAsync<PrsPathwayGradeCheckAndSubmitViewModel>(AoUkprn, ProfileId, AssessmentId).Returns(_checkAndSubmitViewModel);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_AreCalled()
+        {
+            Loader.Received(1).GetPrsLearnerDetailsAsync<PrsPathwayGradeCheckAndSubmitViewModel>(AoUkprn, ProfileId, AssessmentId);
+            CacheService.Received(1).SetAsync(CacheKey, Arg.Is<PrsPathwayGradeCheckAndSubmitViewModel>(x => x.OldGrade == x.NewGrade && x.IsGradeChanged == false));
+        }
+
+        [Fact]
+        public void Then_Redirected_To_PrsPathwayGradeCheckAndSubmit()
+        {
+            var routeName = (Result as RedirectToRouteResult).RouteName;
+            routeName.Should().Be(RouteConstants.PrsPathwayGradeCheckAndSubmit);
+        }
+    }
+}
