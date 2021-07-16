d28540e2649e89db398ff4046dac74aa1978e31c
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-07-16 16:33:04 +0100

    TLRC - 3743 - Post Results Services - Cancel button on check & submit - TLRC - 3761 - Dev - Added Web unit tests

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/PostResultsServiceControllerTests/PrsCancelAppealUpdatePost/When_CancelRequest_IsFalse.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.PostResultsService;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.PostResultsServiceControllerTests.PrsCancelAppealUpdatePost
+{
+    public class When_CancelRequest_IsFalse : TestSetup
+    {
+        public override void Given()
+        {
+            ViewModel = new PrsCancelAppealUpdateViewModel { ProfileId = 1, AssessmentId = 10, CancelRequest = false };
+        }
+
+        [Fact]
+        public void Then_Redirected_To_AddLearnerRecordCheckAndSubmit()
+        {
+            var route = Result as RedirectToRouteResult;
+            route.RouteName.Should().Be(RouteConstants.AddLearnerRecordCheckAndSubmit);
+            route.RouteValues.Should().BeNull();
+        }
+    }
+}
