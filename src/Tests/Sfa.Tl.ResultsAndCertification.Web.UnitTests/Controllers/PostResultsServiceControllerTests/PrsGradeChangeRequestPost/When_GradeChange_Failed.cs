17e0155a8ef9e0982f909fa179030b7305b2fa86
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-08-03 15:49:00 +0100

    TLRC - 3860 - Post Results Services - Request grade change complete - TLRC - 3966 - Dev - Added Unit Tests

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/PostResultsServiceControllerTests/PrsGradeChangeRequestPost/When_GradeChange_Failed.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.PostResultsService;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.PostResultsServiceControllerTests.PrsGradeChangeRequestPost
+{
+    public class When_GradeChange_Failed : TestSetup
+    {
+        private readonly bool _gradeChangeRequestResponse = false;
+        private PrsGradeChangeRequestViewModel _mockGradeChangeRequestViewModel;
+
+        public override void Given()
+        {
+            ViewModel = new PrsGradeChangeRequestViewModel
+            {
+                ProfileId = 1,
+                AssessmentId = 2,
+                ResultId = 3,
+                ChangeRequestData = "Change grade"
+            };
+
+            _mockGradeChangeRequestViewModel = new PrsGradeChangeRequestViewModel
+            {
+                ProfileId = ViewModel.ProfileId,
+                AssessmentId = ViewModel.AssessmentId,
+                ResultId = 10,
+                Status = RegistrationPathwayStatus.Active,
+                PathwayPrsStatus = PrsStatus.Final
+            };
+
+            Loader.GetPrsLearnerDetailsAsync<PrsGradeChangeRequestViewModel>(AoUkprn, ViewModel.ProfileId, ViewModel.AssessmentId).Returns(_mockGradeChangeRequestViewModel);
+            Loader.PrsGradeChangeRequestAsync(ViewModel).Returns(_gradeChangeRequestResponse);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_AreCalled()
+        {
+            Loader.Received(1).GetPrsLearnerDetailsAsync<PrsGradeChangeRequestViewModel>(AoUkprn, ViewModel.ProfileId, ViewModel.AssessmentId);
+            Loader.Received(1).PrsGradeChangeRequestAsync(ViewModel);
+            CacheService.DidNotReceive().SetAsync(CacheKey, Arg.Any<PrsGradeChangeRequestConfirmationViewModel>(), Arg.Any<CacheExpiryTime>());            
+        }
+
+        [Fact]
+        public void Then_Redirected_To_ProblemWithService()
+        {
+            var routeName = (Result as RedirectToRouteResult).RouteName;           
+            routeName.Should().Be(RouteConstants.ProblemWithService);
+        }
+    }
+}
