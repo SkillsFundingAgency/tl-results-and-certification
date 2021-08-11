2cb3f92999c5e829d35c08d8132118c84332c862
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-08-11 17:28:11 +0100

    TLRC - 3996 - Post Results Services - Exceptions - Start page - TLRC - 4021 - Dev - Added Unit Tests

--- src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/PostResultsService/AppealGradeAfterDeadlineViewModel.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/PostResultsService/AppealGradeAfterDeadlineViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
 using Sfa.Tl.ResultsAndCertification.Web.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewComponents.BackLink;
 using System;
+using System.Collections.Generic;
 
 namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.PostResultsService
 {
@@ -10,9 +13,15 @@ namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.PostResultsService
         public int AssessmentId { get; set; }
         public int ResultId { get; set; }
         public DateTime AppealEndDate { get; set; }
-        public PrsStatus? PathwayPrsStatus { get; set; }        
+        public PrsStatus? PathwayPrsStatus { get; set; }
         public RegistrationPathwayStatus Status { get; set; }
         public bool IsValid { get { return Status == RegistrationPathwayStatus.Active && IsAppealAllowedAfterDeadline; } }
         private bool IsAppealAllowedAfterDeadline { get { return !PathwayPrsStatus.HasValue && !CommonHelper.IsAppealsAllowed(AppealEndDate); } }
+
+        public virtual BackLinkModel BackLink => new BackLinkModel
+        {
+            RouteName = RouteConstants.PrsLearnerDetails,
+            RouteAttributes = new Dictionary<string, string> { { Constants.ProfileId, ProfileId.ToString() }, { Constants.AssessmentId, AssessmentId.ToString() } }
+        };
     }
 }
