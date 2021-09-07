eae65c60f3f5f8429ce7c0aeafb7a533e415dc1a
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-09-07 18:01:57 +0100

    TLRC - 4169 - Transfer core & specialism codes into dataseed - TLRC - 4246 - Dev - Seed Core and Specialisms

--- src/Sfa.Tl.ResultsAndCertification.Database/PostDeployment/SeedTlPathways.sql
+++ src/Sfa.Tl.ResultsAndCertification.Database/PostDeployment/SeedTlPathways.sql
@@ -7,15 +7,15 @@ SET IDENTITY_INSERT [dbo].[TlPathway] ON
 MERGE INTO [dbo].[TlPathway] AS Target 
 USING (VALUES 
   (1, N'60358300', N'T Level in Design, Surveying and Planning for Construction', N'Design, Surveying and Planning', 1),
-  (2, N'10223456', N'T Level in Onsite Construction', N'Onsite Construction', 1),
-  (3, N'10323456', N'T Level in Building Services Engineering for Construction', N'Building Services Engineering', 1),
+  (2, N'60369176', N'T Level in Onsite Construction', N'Onsite Construction', 1),
+  (3, N'60369115', N'T Level in Building Services Engineering for Construction', N'Building Services Engineering', 1),
   (4, N'60358294', N'T Level in Education and Childcare', N'Education and Childcare', 2),
   (5, N'60358324', N'T Level in Digital Production, Design and Development', N'Digital Production, Design and Development', 3),
-  (6, N'10623456', N'T Level in Digital Support and Services', N'Digital Support Services', 3),
-  (7, N'10723456', N'T Level in Digital Business Services', N'Digital Business Services', 3),
-  (8, N'10823456', N'T Level in Health', N'Health', 4),
-  (9, N'10923456', N'T Level in Healthcare Science', N'Healthcare Science', 4),
-  (10, N'11023456', N'T Level in Science', N'Science', 4)
+  (6, N'60369012', N'T Level in Digital Support and Services', N'Digital Support Services', 3),
+  (7, N'60369024', N'T Level in Digital Business Services', N'Digital Business Services', 3),
+  (8, N'6037066X', N'T Level in Health', N'Health', 4),
+  (9, N'6037083X', N'T Level in Healthcare Science', N'Healthcare Science', 4),
+  (10, N'60369899', N'T Level in Science', N'Science', 4)
   )
   AS Source ([Id], [LarId], [TlevelTitle], [Name], [TlRouteId]) 
 ON Target.[Id] = Source.[Id] 
