2a993c582ad6e91cf935895f6edd530ded896be6
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-09-07 09:02:17 +0100

    TLRC - 4173 - Journey Improvements - Bulk upload - Results upload error - TLRC - 4255 - Dev - Implemented content and layout changes

--- src/Sfa.Tl.ResultsAndCertification.Web/Content/Result/UploadUnsuccessful.Designer.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Content/Result/UploadUnsuccessful.Designer.cs
@@ -61,43 +61,70 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Content.Result {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Back to results.
+        ///   Looks up a localized string similar to Back to home.
         /// </summary>
-        public static string Back_To_Results_Button_Text {
+        public static string Back_To_Home_Button {
             get {
-                return ResourceManager.GetString("Back_To_Results_Button_Text", resourceCulture);
+                return ResourceManager.GetString("Back_To_Home_Button", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Download error report.
+        ///   Looks up a localized string similar to Download.
         /// </summary>
-        public static string Download_Error_Report_Text {
+        public static string Download_Text {
             get {
-                return ResourceManager.GetString("Download_Error_Report_Text", resourceCulture);
+                return ResourceManager.GetString("Download_Text", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to If you leave this page you will not be able to access this report..
+        ///   Looks up a localized string similar to results error report.
         /// </summary>
-        public static string Errors_Not_Accessible_On_Page_Exit_Text {
+        public static string Error_Report {
             get {
-                return ResourceManager.GetString("Errors_Not_Accessible_On_Page_Exit_Text", resourceCulture);
+                return ResourceManager.GetString("Error_Report", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Error report.
+        /// </summary>
+        public static string Error_Report_Heading {
+            get {
+                return ResourceManager.GetString("Error_Report_Heading", resourceCulture);
             }
         }
         
         /// <summary>
         ///   Looks up a localized string similar to KB.
         /// </summary>
-        public static string File_Size_Kb_Text {
+        public static string File_Size_Kb {
             get {
-                return ResourceManager.GetString("File_Size_Kb_Text", resourceCulture);
+                return ResourceManager.GetString("File_Size_Kb", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Results upload unsuccessful page.
+        ///   Looks up a localized string similar to If you leave this page you will not be able to come back later to access the error report..
+        /// </summary>
+        public static string Inset_Para_If_You_Leave_This_Page_You_Will_Not_Able_To_Come_Back {
+            get {
+                return ResourceManager.GetString("Inset_Para_If_You_Leave_This_Page_You_Will_Not_Able_To_Come_Back", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Your file contains formatting errors. Please check and correct the errors before uploading it again..
+        /// </summary>
+        public static string Inset_Para_Your_File_Contains_Formatting_Errors {
+            get {
+                return ResourceManager.GetString("Inset_Para_Your_File_Contains_Formatting_Errors", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Results upload error.
         /// </summary>
         public static string Page_Title {
             get {
@@ -106,16 +133,34 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Content.Result {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Results_error_report.csv.
+        ///   Looks up a localized string similar to The error report will tell you what went wrong. Please download this file immediately..
         /// </summary>
-        public static string Result_Error_Report_File_Name_Text {
+        public static string Para_The_Error_Report_Will_Tell_You_What_Went_Wrong {
             get {
-                return ResourceManager.GetString("Result_Error_Report_File_Name_Text", resourceCulture);
+                return ResourceManager.GetString("Para_The_Error_Report_Will_Tell_You_What_Went_Wrong", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Results upload unsuccessful.
+        ///   Looks up a localized string similar to The error report will tell you where the errors were found:.
+        /// </summary>
+        public static string Para_The_Error_Report_Will_Tell_You_Where_The_Errors_Were_Found {
+            get {
+                return ResourceManager.GetString("Para_The_Error_Report_Will_Tell_You_Where_The_Errors_Were_Found", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to results-error-report.csv.
+        /// </summary>
+        public static string Result_Error_Report_File_Name {
+            get {
+                return ResourceManager.GetString("Result_Error_Report_File_Name", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to There is a problem.
         /// </summary>
         public static string Section_Heading {
             get {
@@ -124,20 +169,11 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Content.Result {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The file could not be uploaded because it contains errors..
+        ///   Looks up a localized string similar to Upload another file.
         /// </summary>
-        public static string Section_Info_Text {
+        public static string Upload_Another_File_Button {
             get {
-                return ResourceManager.GetString("Section_Info_Text", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Download this error report and send it to your organisation’s technical team..
-        /// </summary>
-        public static string Send_Errors_TechTeam_Para_Text {
-            get {
-                return ResourceManager.GetString("Send_Errors_TechTeam_Para_Text", resourceCulture);
+                return ResourceManager.GetString("Upload_Another_File_Button", resourceCulture);
             }
         }
     }
