df8f55622b27fef15894b02afa772cb11eb73294
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-09-27 10:14:11 +0100

    TLRC - 4321 - Tech: Registration of multiple occupational specialisms in the database (bulk upload) - TLRC - 4447 - Dev - Added Integration Tests for Get AcademicYears

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.IntegrationTests/Services/CommonServiceTests/When_GetAcademicYearsAsync_IsCalled.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Data.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Services.CommonServiceTests
+{
+    public class When_GetAcademicYearsAsync_IsCalled : CommonServiceBaseTest
+    {
+        private IEnumerable<Models.Contracts.Common.AcademicYear> _result;
+        private IEnumerable<Models.Contracts.Common.AcademicYear> _expectedResult;
+
+        public override void Given()
+        {
+            SeedAcademicYears();
+            _expectedResult = AcademicYears.Select(a => new Models.Contracts.Common.AcademicYear { Id = a.Id, Name = a.Name, Year = a.Year });
+            CommonRepository = new CommonRepository(DbContext);
+        }
+
+        public override Task When()
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task WhenAsync()
+        {
+            if (_result != null)
+                return;
+
+            _result = await CommonRepository.GetAcademicYearsAsync();
+        }
+
+        [Fact]
+        public async Task Then_Expected_Results_Are_Returned()
+        {
+            await WhenAsync();
+
+            _result.Should().NotBeNullOrEmpty();
+            _result.Count().Should().Be(_expectedResult.Count());
+            _result.Should().BeEquivalentTo(_expectedResult);
+        }
+    }
+}
