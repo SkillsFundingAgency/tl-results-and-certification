c2b2304e6f0c3a357459618da0131003f78e2f15
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-11-24 16:51:12 +0000

    TLRC - 4714 - MANUAL JOURNEY: ASSESSMENT ENTRY CHANGES - Details page - 2nd year learners and those going into 3rd-4th years - TLRC - 4880 - Dev - Added Web Loader Tests for GetAssessmentDetails

--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/AssessmentLoaderTests/GetAssessmentDetails/When_Called_With_Single_Specialism_Series_Open_For_Resit.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/AssessmentLoaderTests/GetAssessmentDetails/When_Called_With_Single_Specialism_Series_Open_For_Resit.cs
@@ -11,7 +11,7 @@ using Xunit;
 
 namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.AssessmentLoaderTests.GetAssessmentDetails
 {
-    public class When_Called_With_Valid_Data : TestSetup
+    public class When_Called_With_Single_Specialism_Series_Open_For_Resit : TestSetup
     {
         private IList<AssessmentSeriesDetails> _assessmentSeriesDetails;
 
@@ -40,29 +40,6 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.AssessmentLoaderTe
                         Name = "Provider Name",
                         DisplayName = "Provider display name",
                     },
-                    PathwayAssessments = new List<Assessment>
-                    {
-                        new Assessment
-                        {
-                            Id = 3,
-                            SeriesId = 1,
-                            SeriesName = "Summer 2021",
-                            AppealEndDate = System.DateTime.UtcNow.AddDays(10),
-                            LastUpdatedBy = "System",
-                            LastUpdatedOn = System.DateTime.UtcNow,
-                            Results = new List<Result>
-                            {
-                                new Result
-                                {
-                                    Id = 1,
-                                    Grade = "A",
-                                    PrsStatus = null,
-                                    LastUpdatedBy = "System",
-                                    LastUpdatedOn = System.DateTime.UtcNow
-                                }
-                            }
-                        }
-                    },
                     Specialisms = new List<Specialism>
                     {
                         new Specialism
@@ -76,21 +53,13 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.AssessmentLoaderTe
                                 {
                                     Id = 4,
                                     SeriesId = 2,
-                                    SeriesName = "Autumn 2021",
+                                    SeriesName = "Summer 2022",
                                     AppealEndDate = System.DateTime.UtcNow.AddDays(30),
                                     LastUpdatedBy = "System",
                                     LastUpdatedOn = System.DateTime.UtcNow,
                                     Results = new List<Result>()
                                 }
                             }
-                        }
-                    },
-                    IndustryPlacements = new List<IndustryPlacement>
-                    {
-                        new IndustryPlacement
-                        {
-                            Id = 7,
-                            Status = IndustryPlacementStatus.Completed
                         }
                     }
                 }
@@ -102,11 +71,11 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.AssessmentLoaderTe
                 {
                     Id = 1,
                     ComponentType = ComponentType.Core,
-                    Name = "Summer 2021",
-                    Description = "Summer 2021",            
-                    StartDate = System.DateTime.UtcNow.AddDays(-1),
-                    EndDate = System.DateTime.UtcNow.AddDays(10),
-                    AppealEndDate = System.DateTime.UtcNow.AddDays(20),
+                    Name = "Autumn 2021",
+                    Description = "Autumn 2021",
+                    StartDate = System.DateTime.UtcNow.AddDays(10),
+                    EndDate = System.DateTime.UtcNow.AddDays(30),
+                    AppealEndDate = System.DateTime.UtcNow.AddDays(40),
                     Year = 2021
                 },
                 new AssessmentSeriesDetails
@@ -115,10 +84,21 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.AssessmentLoaderTe
                     ComponentType = ComponentType.Specialism,
                     Name = "Summer 2022",
                     Description = "Summer 2022",
+                    StartDate = System.DateTime.UtcNow.AddDays(-10),
+                    EndDate = System.DateTime.UtcNow.AddDays(-1),
+                    AppealEndDate = System.DateTime.UtcNow.AddDays(20),
+                    Year = 2022
+                },
+                new AssessmentSeriesDetails
+                {
+                    Id = 3,
+                    ComponentType = ComponentType.Specialism,
+                    Name = "Summer 2023",
+                    Description = "Summer 2023",
                     StartDate = System.DateTime.UtcNow.AddDays(-1),
                     EndDate = System.DateTime.UtcNow.AddDays(10),
                     AppealEndDate = System.DateTime.UtcNow.AddDays(20),
-                    Year = 2022
+                    Year = 2023
                 }
             };
 
@@ -134,13 +114,9 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.AssessmentLoaderTe
         [Fact]
         public void Then_Returns_Expected_Results()
         {
-            ActualResult.Should().NotBeNull();
-
-            var coreAssessmentSeriesId = _assessmentSeriesDetails[0].Id;
             var coreAssessmentSeriesName = _assessmentSeriesDetails[0].Name;
-
-            var specialismAssessmentSeriesId = _assessmentSeriesDetails[1].Id;
-            var specialismAssessmentSeriesName = _assessmentSeriesDetails[1].Name;
+            var specialismAssessmentSeriesName = _assessmentSeriesDetails[2].Name;
+            ActualResult.Should().NotBeNull();
 
             ActualResult.ProfileId.Should().Be(expectedApiResult.ProfileId);
             ActualResult.Uln.Should().Be(expectedApiResult.Uln);
@@ -151,34 +127,15 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.AssessmentLoaderTe
             ActualResult.ProviderUkprn.Should().Be(expectedApiResult.Pathway.Provider.Ukprn);
             ActualResult.TlevelTitle.Should().Be(expectedApiResult.Pathway.Title);
             ActualResult.PathwayStatus.Should().Be(expectedApiResult.Pathway.Status);
-                        
-            ActualResult.IsCoreEntryEligible.Should().Be(expectedApiResult.Pathway.Status == RegistrationPathwayStatus.Active && coreAssessmentSeriesId > 0);
+
+            ActualResult.IsCoreEntryEligible.Should().BeFalse();
             ActualResult.PathwayId.Should().Be(expectedApiResult.Pathway.Id);
             ActualResult.PathwayDisplayName.Should().Be($"{expectedApiResult.Pathway.Name} ({expectedApiResult.Pathway.LarId})");
             ActualResult.NextAvailableCoreSeries.Should().Be(coreAssessmentSeriesName);
 
-            var expectedPathwayAssessment = expectedApiResult.Pathway.PathwayAssessments.FirstOrDefault(p => p.SeriesId == coreAssessmentSeriesId);
-
-            ActualResult.PathwayAssessment.AssessmentId.Should().Be(expectedPathwayAssessment.Id);
-            ActualResult.PathwayAssessment.SeriesId.Should().Be(expectedPathwayAssessment.SeriesId);
-            ActualResult.PathwayAssessment.SeriesName.Should().Be(expectedPathwayAssessment.SeriesName);
-            ActualResult.PathwayAssessment.LastUpdatedOn.Should().Be(expectedPathwayAssessment.LastUpdatedOn);
-            ActualResult.PathwayAssessment.LastUpdatedBy.Should().Be(expectedPathwayAssessment.LastUpdatedBy);
-
-            ActualResult.PathwayAssessment.Results.Count().Should().Be(expectedPathwayAssessment.Results.Count());
-           
-            foreach(var expectedResult in expectedPathwayAssessment.Results)
-            {
-                var actualResult = ActualResult.PathwayAssessment.Results.FirstOrDefault(r => r.Id == expectedResult.Id);
-                actualResult.Should().NotBeNull();
-
-                actualResult.Id.Should().Be(expectedResult.Id);
-                actualResult.Grade.Should().Be(expectedResult.Grade);
-                actualResult.LastUpdatedBy.Should().Be(expectedResult.LastUpdatedBy);
-                actualResult.LastUpdatedOn.Should().Be(expectedResult.LastUpdatedOn);
-            }
-
+            ActualResult.PathwayAssessment.Should().BeNull();
             ActualResult.PreviousPathwayAssessment.Should().BeNull();
+            ActualResult.SpecialismDetails.Should().NotBeNullOrEmpty();
 
             foreach (var expectedSpecialism in expectedApiResult.Pathway.Specialisms)
             {
@@ -190,47 +147,27 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.AssessmentLoaderTe
                 actualSpecialism.LarId.Should().Be(expectedSpecialism.LarId);
                 actualSpecialism.Name.Should().Be(expectedSpecialism.Name);
                 actualSpecialism.DisplayName.Should().Be($"{expectedSpecialism.Name} ({expectedSpecialism.LarId})");
-
-                foreach (var expectedSpecialismAssessment in expectedSpecialism.Assessments)
-                {
-                    var actualSpecialismAssessment = actualSpecialism.Assessments.FirstOrDefault(r => r.AssessmentId == expectedSpecialismAssessment.Id);
-                    actualSpecialismAssessment.Should().NotBeNull();
-
-                    actualSpecialismAssessment.AssessmentId.Should().Be(expectedSpecialismAssessment.Id);
-                    actualSpecialismAssessment.SeriesId.Should().Be(expectedSpecialismAssessment.SeriesId);
-                    actualSpecialismAssessment.SeriesName.Should().Be(expectedSpecialismAssessment.SeriesName);
-                    actualSpecialismAssessment.LastUpdatedOn.Should().Be(expectedSpecialismAssessment.LastUpdatedOn);
-                    actualSpecialismAssessment.LastUpdatedBy.Should().Be(expectedSpecialismAssessment.LastUpdatedBy);
-                }
+                actualSpecialism.Assessments.Should().BeNullOrEmpty();
             }
 
-            ActualResult.IsSpecialismEntryEligible.Should().Be(expectedApiResult.Pathway.Status == RegistrationPathwayStatus.Active && coreAssessmentSeriesId > 0);
-
-            var expectedHasCurrentSpecialismAssessmentEntry = expectedApiResult.Pathway.Specialisms.SelectMany(sa => sa.Assessments).Any(a => a.SeriesId == specialismAssessmentSeriesId);
-            ActualResult.HasCurrentSpecialismAssessmentEntry.Should().Be(expectedHasCurrentSpecialismAssessmentEntry);
-
-            var expectedIsResitForSpecialism = expectedApiResult.Pathway.Specialisms.SelectMany(sa => sa.Assessments).Any(a => a.SeriesId != specialismAssessmentSeriesId);
-            ActualResult.IsResitForSpecialism.Should().Be(expectedIsResitForSpecialism);
+            ActualResult.IsSpecialismEntryEligible.Should().BeTrue();
+            ActualResult.HasCurrentSpecialismAssessmentEntry.Should().BeFalse();
+            ActualResult.IsResitForSpecialism.Should().BeTrue();
             ActualResult.NextAvailableSpecialismSeries.Should().Be(specialismAssessmentSeriesName);
+            ActualResult.IsCoreResultExist.Should().BeFalse();
+            ActualResult.HasAnyOutstandingPathwayPrsActivities.Should().BeFalse();
+            ActualResult.IsIndustryPlacementExist.Should().BeFalse();
 
-            var expectedIsCoreResultExists = expectedApiResult.Pathway.PathwayAssessments.Any(a => a.Results.Any());
-            ActualResult.IsCoreResultExist.Should().Be(expectedIsCoreResultExists);
-
-            var expectedHasAnyOutstandingPathwayPrsActivities = expectedApiResult.Pathway.PathwayAssessments.Any(a => a.Results.Any(r => r.PrsStatus == PrsStatus.BeingAppealed));
-            ActualResult.HasAnyOutstandingPathwayPrsActivities.Should().Be(expectedHasAnyOutstandingPathwayPrsActivities);
-
-            var expectedIsIndustryPlacementExist = expectedApiResult.Pathway.IndustryPlacements.Any();
-            ActualResult.IsIndustryPlacementExist.Should().Be(expectedIsIndustryPlacementExist);
-
-
-            ActualResult.HasCurrentCoreAssessmentEntry.Should().Be(true);
-            ActualResult.HasResultForCurrentCoreAssessment.Should().Be(true);
-            ActualResult.HasPreviousCoreAssessment.Should().Be(false);
-            ActualResult.HasResultForPreviousCoreAssessment.Should().Be(false);
-            ActualResult.NeedCoreResultForPreviousAssessmentEntry.Should().Be(false);
-            ActualResult.DisplayMultipleSpecialismsCombined.Should().Be(false);
-            ActualResult.IsSpecialismRegistered.Should().Be(true);
+            ActualResult.HasCurrentCoreAssessmentEntry.Should().BeFalse();
+            ActualResult.HasResultForCurrentCoreAssessment.Should().BeFalse();
+            ActualResult.HasPreviousCoreAssessment.Should().BeFalse();
+            ActualResult.HasResultForPreviousCoreAssessment.Should().BeFalse();
+            ActualResult.NeedCoreResultForPreviousAssessmentEntry.Should().BeFalse();
+            ActualResult.DisplayMultipleSpecialismsCombined.Should().BeFalse();
+            ActualResult.IsSpecialismRegistered.Should().BeTrue();
             ActualResult.SpecialismDisplayName.Should().BeNull();
+            ActualResult.DisplaySpecialisms.Should().NotBeNullOrEmpty();
+            ActualResult.DisplayMultipleSpecialismsCombined.Should().BeFalse();
 
             var expectedDisplaySpecialisms = new List<SpecialismViewModel>();
             foreach (var specialism in expectedApiResult.Pathway.Specialisms)
@@ -243,9 +180,10 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.AssessmentLoaderTe
                     DisplayName = $"{specialism.Name} ({specialism.LarId})"
                 });
             }
+
             ActualResult.DisplaySpecialisms.Count().Should().Be(expectedDisplaySpecialisms.Count());
 
-            foreach(var expectedDisplaySpecialism in expectedDisplaySpecialisms)
+            foreach (var expectedDisplaySpecialism in expectedDisplaySpecialisms)
             {
                 var actualDisplaySpecialism = ActualResult.DisplaySpecialisms.FirstOrDefault(s => s.Id == expectedDisplaySpecialism.Id);
 
