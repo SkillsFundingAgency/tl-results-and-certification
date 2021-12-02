de047d1a6f7c7e434a77773e9c1b711d84a86e18
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-12-02 15:45:08 +0000

    TLRC - 4726 - MANUAL JOURNEY: ASSESSMENT ENTRY CHANGES - success banner after adding specialism assessment entry - TLRC - 4973 - Dev - Added Web Loader Tests for AddSpecialismAssessmentEntry

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/AssessmentLoaderTests/AddSpecialismAssessmentEntry/When_Called_With_Valid_Multiple_Specialism_Data.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Assessment.Manual;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.AssessmentLoaderTests.AddSpecialismAssessmentEntry
+{
+    public class When_Called_With_Valid_Multiple_Specialism_Data : TestSetup
+    {
+        private AddAssessmentEntryResponse ExpectedApiResult { get; set; }
+
+        public override void Given()
+        {
+            ViewModel = new AddSpecialismAssessmentEntryViewModel
+            {
+                ProfileId = ProfileId,
+                AssessmentSeriesId = 1,                
+                SpecialismDetails = new List<SpecialismViewModel>
+                {
+                    new SpecialismViewModel
+                    {
+                        Id = 5,
+                        LarId = "ZT2158963",
+                        Name = "Specialism Name1",
+                        DisplayName = "Specialism Name1 (ZT2158963)",
+                        Assessments = new List<SpecialismAssessmentViewModel>()
+                    },
+                    new SpecialismViewModel
+                    {
+                        Id = 6,
+                        LarId = "ZT2158999",
+                        Name = "Specialism Name2",
+                        DisplayName = "Specialism Name2 (ZT2158999)",
+                        Assessments = new List<SpecialismAssessmentViewModel>()
+                    }
+                }
+            };
+
+            ExpectedApiResult = new AddAssessmentEntryResponse { IsSuccess = true, Uln = 1234567890 };
+            InternalApiClient
+                .AddAssessmentEntryAsync(Arg.Is<AddAssessmentEntryRequest>(
+                    x => x.ProfileId == ViewModel.ProfileId &&
+                    x.AoUkprn == AoUkprn &&
+                    x.ComponentType == Common.Enum.ComponentType.Specialism &&
+                    x.AssessmentSeriesId == ViewModel.AssessmentSeriesId &&
+                    x.SpecialismIds.All(s => new List<int?> { 5, 6 }.Contains(s)) &&
+                    x.PerformedBy == $"{Givenname} {Surname}" ))
+                .Returns(ExpectedApiResult);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            ActualResult.Should().NotBeNull();
+            ActualResult.IsSuccess.Should().Be(ExpectedApiResult.IsSuccess);
+            ActualResult.Uln.Should().Be(ExpectedApiResult.Uln);
+        }
+    }
+}
