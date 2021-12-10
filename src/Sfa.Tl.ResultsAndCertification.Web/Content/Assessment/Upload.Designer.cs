3d263962f3c5c70412ca7d8422412d98f2332c89
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-12-10 14:43:04 +0000

    TLRC - 4825 - Update assessment entries data format and rules - v.1.4 - TLRC - 4826 - Dev - Changes to assessment entries format guide and changes to file size and version - TLRC - 5028 - Dev - Upload guide to relevant environments

--- src/Sfa.Tl.ResultsAndCertification.Web/Content/Assessment/Upload.Designer.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Content/Assessment/Upload.Designer.cs
@@ -61,7 +61,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Content.Assessment {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to published 17 November 2021.
+        ///   Looks up a localized string similar to published 21 December 2021.
         /// </summary>
         public static string Assessments_Published_On {
             get {
@@ -70,7 +70,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Content.Assessment {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to version 1.3.
+        ///   Looks up a localized string similar to version 1.4.
         /// </summary>
         public static string Assessments_Version {
             get {
@@ -106,7 +106,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Content.Assessment {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 283KB.
+        ///   Looks up a localized string similar to 285KB.
         /// </summary>
         public static string File_Size_Kb_Text {
             get {
