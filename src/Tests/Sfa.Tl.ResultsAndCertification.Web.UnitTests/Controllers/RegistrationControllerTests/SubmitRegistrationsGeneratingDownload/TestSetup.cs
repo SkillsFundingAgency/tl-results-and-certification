a3fc9a12fded3b03666f314443a36a4497a3985c
Ravi Bobbili <ravi.bob@outlook.com>
2021-12-22 19:38:09 +0000

    TLRC-4866 Data Export : Download Data Page - Registrations
    Unit tests added for Web Controller

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/RegistrationControllerTests/SubmitRegistrationsGeneratingDownload/TestSetup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Constants;
+using Sfa.Tl.ResultsAndCertification.Common.Extensions;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Common.Services.Cache;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
+using Sfa.Tl.ResultsAndCertification.Web.Loader.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using System;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.RegistrationControllerTests.SubmitRegistrationsGeneratingDownload
+{
+    public abstract class TestSetup : BaseTest<RegistrationController>
+    {
+        protected int AoUkprn;
+        protected string SearchUln;
+        protected Guid UserId;
+        protected string CacheKey;
+        protected IRegistrationLoader RegistrationLoader;
+        protected ICacheService CacheService;
+        protected ILogger<RegistrationController> Logger;
+        protected RegistrationController Controller;
+        protected RegistrationViewModel ViewModel;
+        protected IHttpContextAccessor HttpContextAccessor;
+        public IActionResult Result { get; private set; }
+
+        public override void Setup()
+        {
+            HttpContextAccessor = Substitute.For<IHttpContextAccessor>();
+            RegistrationLoader = Substitute.For<IRegistrationLoader>();
+            CacheService = Substitute.For<ICacheService>();
+            Logger = Substitute.For<ILogger<RegistrationController>>();
+            Controller = new RegistrationController(RegistrationLoader, CacheService, Logger);
+
+            AoUkprn = 1234567890;
+            var httpContext = new ClaimsIdentityBuilder<RegistrationController>(Controller)
+               .Add(CustomClaimTypes.Ukprn, AoUkprn.ToString())
+               .Add(CustomClaimTypes.UserId, Guid.NewGuid().ToString())
+               .Build()
+               .HttpContext;
+
+            HttpContextAccessor.HttpContext.Returns(httpContext);
+            CacheKey = CacheKeyHelper.GetCacheKey(httpContext.User.GetUserId(), CacheConstants.RegistrationCacheKey);
+
+        }
+
+        public async override Task When()
+        {
+            Result = await Controller.SubmitRegistrationsGeneratingDownloadAsync();
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/RegistrationControllerTests/SubmitRegistrationsGeneratingDownload/When_Called_With_ResponseCount_Invalid.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.DataExport;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.RegistrationControllerTests.SubmitRegistrationsGeneratingDownload
+{
+    public class When_Called_With_ResponseCount_Invalid : TestSetup
+    {
+        private IList<DataExportResponse> _dataExportResponse;
+
+        public override void Given()
+        {
+            _dataExportResponse = new List<DataExportResponse>
+            {
+                new DataExportResponse { IsDataFound = true },
+                new DataExportResponse { IsDataFound = true }
+            };
+
+            RegistrationLoader.GenerateRegistrationsExportAsync(AoUkprn, Arg.Any<string>())
+                .Returns(_dataExportResponse);
+        }
+
+        [Fact]
+        public void Then_Redirected_To_ProblemWithService()
+        {
+            var actualRouteName = (Result as RedirectToRouteResult).RouteName;
+            actualRouteName.Should().Be(RouteConstants.ProblemWithService);
+        }
+
+        [Fact]
+        public void Then_Expected_Method_Is_Called()
+        {
+            CacheService.DidNotReceive().SetAsync(
+                Arg.Any<string>(),
+                Arg.Any<RegistrationsDownloadViewModel>(),
+                Arg.Any<CacheExpiryTime>());
+        }
+    }
+}
