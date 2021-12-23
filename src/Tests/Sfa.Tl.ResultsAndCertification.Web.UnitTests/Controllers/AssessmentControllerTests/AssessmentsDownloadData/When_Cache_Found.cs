a23f0446156ff00e09f9e263e4bbfed1b2ec756b
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-12-23 14:43:52 +0000

    TLRC - 4895 - Data Export : Download Data Page - Assessments - TLRC - 5171 - Dev - Added Web Unit tests for AssessmentsDownloadData and AssessmentsDownloadDataLinks

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/AssessmentControllerTests/AssessmentsDownloadData/When_Cache_Found.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Assessment.Manual;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Common;
+using System;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.AssessmentControllerTests.AssessmentsDownloadData
+{
+    public class When_Cache_Found : TestSetup
+    {
+        public override void Given()
+        {
+            AssessmentsDownloadViewModel = new AssessmentsDownloadViewModel
+            {
+                CoreAssesmentsDownloadLinkViewModel = new DownloadLinkViewModel
+                {
+                    BlobUniqueReference = Guid.NewGuid(),
+                    FileSize = 1.7,
+                    FileType = FileType.Csv.ToString().ToUpperInvariant()
+                },
+                SpecialismAsssmentsDownloadLinkViewModel = new DownloadLinkViewModel
+                {
+                    BlobUniqueReference = Guid.NewGuid(),
+                    FileSize = 2.1,
+                    FileType = FileType.Csv.ToString().ToUpperInvariant()
+                }
+            };
+                
+            CacheService.GetAndRemoveAsync<AssessmentsDownloadViewModel>(CacheKey).Returns(AssessmentsDownloadViewModel);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            var viewResult = Result as ViewResult;
+            var model = viewResult.Model as AssessmentsDownloadViewModel;
+
+            model.Should().NotBeNull();
+            model.CoreAssesmentsDownloadLinkViewModel.BlobUniqueReference.Should().Be(AssessmentsDownloadViewModel.CoreAssesmentsDownloadLinkViewModel.BlobUniqueReference);
+            model.CoreAssesmentsDownloadLinkViewModel.FileType.Should().Be(AssessmentsDownloadViewModel.CoreAssesmentsDownloadLinkViewModel.FileType);
+            model.CoreAssesmentsDownloadLinkViewModel.FileSize.Should().Be(AssessmentsDownloadViewModel.CoreAssesmentsDownloadLinkViewModel.FileSize);
+
+            model.SpecialismAsssmentsDownloadLinkViewModel.BlobUniqueReference.Should().Be(AssessmentsDownloadViewModel.SpecialismAsssmentsDownloadLinkViewModel.BlobUniqueReference);
+            model.SpecialismAsssmentsDownloadLinkViewModel.FileType.Should().Be(AssessmentsDownloadViewModel.SpecialismAsssmentsDownloadLinkViewModel.FileType);
+            model.SpecialismAsssmentsDownloadLinkViewModel.FileSize.Should().Be(AssessmentsDownloadViewModel.SpecialismAsssmentsDownloadLinkViewModel.FileSize);
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/AssessmentControllerTests/AssessmentsDownloadDataLink/When_Called_With_Valid_Data_For_Specialism.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Extensions;
+using System;
+using System.IO;
+using System.Text;
+using Xunit;
+using AssessmentContent = Sfa.Tl.ResultsAndCertification.Web.Content.Assessment;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.AssessmentControllerTests.AssessmentsDownloadDataLink
+{
+    public class When_Called_With_Valid_Data_For_Specialism : TestSetup
+    {
+        public override void Given()
+        {
+            Id = Guid.NewGuid().ToString();
+            ComponentType = Common.Enum.ComponentType.Specialism;
+            AssessmentLoader.GetAssessmentsDataFileAsync(Ukprn, Id.ToGuid()).Returns(new MemoryStream(Encoding.ASCII.GetBytes("Test File for specialism assessment entries")));
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Called()
+        {
+            AssessmentLoader.Received(1).GetAssessmentsDataFileAsync(Ukprn, Id.ToGuid());
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            var viewResult = Result as FileStreamResult;
+            viewResult.Should().NotBeNull();
+            viewResult.FileDownloadName.Should().Be(AssessmentContent.AssessmentsDownloadData.Specialism_Assessments_Download_FileName);
+            viewResult.ContentType.Should().Be("text/csv");
+            viewResult.FileStream.Should().NotBeNull();
+        }
+    }
+}
