3fc1196c72245c0a006196100d27442f42540dba
Ravi Bobbili <ravi.bob@outlook.com>
2021-12-23 13:31:38 +0000

    TLRC-4866 Data Export : Download Data Page - Registrations
    Unit tests for Loader are added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/RegistrationLoaderTests/GetRegistrationsDataFile/When_Called_With_Invalid_Data.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Models.BlobStorage;
+using System;
+using System.IO;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.RegistrationLoaderTests.GetRegistrationsDataFile
+{
+    public class When_Called_With_Invalid_Data : TestSetup
+    {
+        public override void Given()
+        {
+            Stream stream = null;
+            BlobUniqueReference = Guid.NewGuid();
+            BlobStorageService.DownloadFileAsync(Arg.Any<BlobStorageData>()).Returns(stream);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_NotCalled()
+        {
+            BlobStorageService.Received(1).DownloadFileAsync(Arg.Any<BlobStorageData>());
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            ActualResult.Should().BeNull();
+        }
+    }
+}
