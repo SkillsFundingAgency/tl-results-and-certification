6133a598a0fcc592831dc00fee77bf29b4c4f551
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2021-12-24 09:54:54 +0000

    TLRC - 4895 - Data Export : Download Data Page - Assessments - TLRC - 5171 - Dev - Added Web Unit tests for SubmitAssessmentsGeneratingDownloadAsync method

--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/AssessmentControllerTests/AssessmentsDownloadData/When_Cache_Found.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/AssessmentControllerTests/AssessmentsDownloadData/When_Cache_Found.cs
@@ -2,10 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
 using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Assessment.Manual;
 using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Common;
 using System;
 using Xunit;
+using BreadcrumbContent = Sfa.Tl.ResultsAndCertification.Web.Content.ViewComponents.Breadcrumb;
 
 namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.AssessmentControllerTests.AssessmentsDownloadData
 {
@@ -36,16 +38,24 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.AssessmentCon
         public void Then_Returns_Expected_Results()
         {
             var viewResult = Result as ViewResult;
-            var model = viewResult.Model as AssessmentsDownloadViewModel;
+            var actualResult = viewResult.Model as AssessmentsDownloadViewModel;
 
-            model.Should().NotBeNull();
-            model.CoreAssesmentsDownloadLinkViewModel.BlobUniqueReference.Should().Be(AssessmentsDownloadViewModel.CoreAssesmentsDownloadLinkViewModel.BlobUniqueReference);
-            model.CoreAssesmentsDownloadLinkViewModel.FileType.Should().Be(AssessmentsDownloadViewModel.CoreAssesmentsDownloadLinkViewModel.FileType);
-            model.CoreAssesmentsDownloadLinkViewModel.FileSize.Should().Be(AssessmentsDownloadViewModel.CoreAssesmentsDownloadLinkViewModel.FileSize);
+            actualResult.Should().NotBeNull();
+            actualResult.CoreAssesmentsDownloadLinkViewModel.BlobUniqueReference.Should().Be(AssessmentsDownloadViewModel.CoreAssesmentsDownloadLinkViewModel.BlobUniqueReference);
+            actualResult.CoreAssesmentsDownloadLinkViewModel.FileType.Should().Be(AssessmentsDownloadViewModel.CoreAssesmentsDownloadLinkViewModel.FileType);
+            actualResult.CoreAssesmentsDownloadLinkViewModel.FileSize.Should().Be(AssessmentsDownloadViewModel.CoreAssesmentsDownloadLinkViewModel.FileSize);
 
-            model.SpecialismAsssmentsDownloadLinkViewModel.BlobUniqueReference.Should().Be(AssessmentsDownloadViewModel.SpecialismAsssmentsDownloadLinkViewModel.BlobUniqueReference);
-            model.SpecialismAsssmentsDownloadLinkViewModel.FileType.Should().Be(AssessmentsDownloadViewModel.SpecialismAsssmentsDownloadLinkViewModel.FileType);
-            model.SpecialismAsssmentsDownloadLinkViewModel.FileSize.Should().Be(AssessmentsDownloadViewModel.SpecialismAsssmentsDownloadLinkViewModel.FileSize);
+            actualResult.SpecialismAsssmentsDownloadLinkViewModel.BlobUniqueReference.Should().Be(AssessmentsDownloadViewModel.SpecialismAsssmentsDownloadLinkViewModel.BlobUniqueReference);
+            actualResult.SpecialismAsssmentsDownloadLinkViewModel.FileType.Should().Be(AssessmentsDownloadViewModel.SpecialismAsssmentsDownloadLinkViewModel.FileType);
+            actualResult.SpecialismAsssmentsDownloadLinkViewModel.FileSize.Should().Be(AssessmentsDownloadViewModel.SpecialismAsssmentsDownloadLinkViewModel.FileSize);
+
+            actualResult.Breadcrumb.Should().NotBeNull();
+            actualResult.Breadcrumb.BreadcrumbItems.Should().NotBeNull();
+            actualResult.Breadcrumb.BreadcrumbItems.Count.Should().Be(2);
+            actualResult.Breadcrumb.BreadcrumbItems[0].RouteName.Should().Be(RouteConstants.Home);
+            actualResult.Breadcrumb.BreadcrumbItems[0].DisplayName.Should().Be(BreadcrumbContent.Home);
+            actualResult.Breadcrumb.BreadcrumbItems[1].RouteName.Should().Be(RouteConstants.AssessmentDashboard);
+            actualResult.Breadcrumb.BreadcrumbItems[1].DisplayName.Should().Be(BreadcrumbContent.Assessment_Dashboard);
         }
     }
 }
