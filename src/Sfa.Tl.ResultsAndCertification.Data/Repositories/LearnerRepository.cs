13b172d7c117bc0b246ff081b9baaf2d9d9d4bfd
Ravi Bobbili <ravi.bob@outlook.com>
2022-01-24 11:31:20 +0000

    TLRC-5180 (1) Result Entry: Learner results page (Core) Learner Repository updated to sort Assessments of Core and Specialisms

--- src/Sfa.Tl.ResultsAndCertification.Data/Repositories/LearnerRepository.cs
+++ src/Sfa.Tl.ResultsAndCertification.Data/Repositories/LearnerRepository.cs
@@ -45,6 +45,13 @@ namespace Sfa.Tl.ResultsAndCertification.Data.Repositories
                                 p.Status == RegistrationPathwayStatus.Withdrawn
                            ));
 
+            // Sort core and specialism assessments.
+            regPathway.TqPathwayAssessments = regPathway.TqPathwayAssessments.OrderByDescending(x => x.AssessmentSeriesId).ThenByDescending(x => x.CreatedOn).ToList();
+            regPathway.TqRegistrationSpecialisms.ToList().ForEach(s => 
+            {
+                s.TqSpecialismAssessments = s.TqSpecialismAssessments.OrderByDescending(x => x.AssessmentSeriesId).ThenByDescending(x => x.CreatedOn).ToList();
+            });  
+
             return regPathway;
         }
     }
