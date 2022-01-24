23e987685740876669486d0c900c4983f91b8114
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-01-24 15:21:45 +0000

    TLRC - 5192 - (7) Result Entry: Specialism - adding the OS result - TLRC - 5398 - Dev - Unit tests - added unit tests for AddSpecialismResultGet and AddSpecialismResultPost methods

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ResultControllerTests/AddSpecialismResultGet/When_Called_With_Valid_Data.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Result.Manual;
+using Xunit;
+using System.Collections.Generic;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Common;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ResultControllerTests.AddSpecialismResultGet
+{
+    public class When_Called_With_Valid_Data : TestSetup
+    {
+        private ManageSpecialismResultViewModel _mockresult = null;
+        private Dictionary<string, string> _routeAttributes;
+        private List<LookupViewModel> grades;
+
+        public override void Given()
+        {
+            grades = new List<LookupViewModel> { new LookupViewModel { Id = 1, Code = "C1", Value = "V1" }, new LookupViewModel { Id = 2, Code = "C2", Value = "V2" } };
+            _mockresult = new ManageSpecialismResultViewModel
+            {
+                ProfileId = 1,
+                SpecialismName = "Specialism",
+                SpecialismDisplayName = "Specialism (7654321)",
+                AssessmentSeries = "Summer 2021",
+                AssessmentId = 11,
+                ResultId = null,
+                SelectedGradeCode = string.Empty,
+                Grades = grades
+            };
+
+            _routeAttributes = new Dictionary<string, string> { { Constants.ProfileId, ProfileId.ToString() } };
+            ResultLoader.GetManageSpecialismResultAsync(AoUkprn, ProfileId, AssessmentId, false).Returns(_mockresult);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            Result.Should().NotBeNull();
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(ManageSpecialismResultViewModel));
+
+            var model = viewResult.Model as ManageSpecialismResultViewModel;
+            model.Should().NotBeNull();
+
+            model.ProfileId.Should().Be(_mockresult.ProfileId);
+            model.SpecialismName.Should().Be(_mockresult.SpecialismName);
+            model.SpecialismDisplayName.Should().Be(_mockresult.SpecialismDisplayName);
+            model.AssessmentId.Should().Be(_mockresult.AssessmentId);
+            model.AssessmentSeries.Should().Be(_mockresult.AssessmentSeries);
+            model.ResultId.Should().Be(_mockresult.ResultId);
+            model.SelectedGradeCode.Should().Be(_mockresult.SelectedGradeCode);
+
+            // Back link
+            model.BackLink.Should().NotBeNull();
+            model.BackLink.RouteName.Should().Be(RouteConstants.ResultDetails);
+            model.BackLink.RouteAttributes.Should().BeEquivalentTo(_routeAttributes);
+        }
+    }
+}
--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ResultControllerTests/AddSpecialismResultPost/TestSetup.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ResultControllerTests/AddSpecialismResultPost/TestSetup.cs
@@ -6,27 +6,31 @@ using Sfa.Tl.ResultsAndCertification.Common.Constants;
 using Sfa.Tl.ResultsAndCertification.Common.Extensions;
 using Sfa.Tl.ResultsAndCertification.Common.Helpers;
 using Sfa.Tl.ResultsAndCertification.Common.Services.Cache;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
 using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
 using Sfa.Tl.ResultsAndCertification.Tests.Common.Helpers;
 using Sfa.Tl.ResultsAndCertification.Web.Controllers;
 using Sfa.Tl.ResultsAndCertification.Web.Loader.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Result.Manual;
 using System;
 using System.Threading.Tasks;
 
-namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ResultControllerTests.AddCoreResultGet
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ResultControllerTests.AddSpecialismResultPost
 {
     public abstract class TestSetup : BaseTest<ResultController>
     {
         protected int AoUkprn;
         protected int ProfileId;
-        protected int AssessmentId;
         protected Guid UserId;
         protected string CacheKey;
         protected IResultLoader ResultLoader;
         protected ICacheService CacheService;
         protected ILogger<ResultController> Logger;
         protected ResultController Controller;
+        protected ManageSpecialismResultViewModel ViewModel;
         protected IHttpContextAccessor HttpContextAccessor;
+        protected AddResultResponse MockResult;
+
         public IActionResult Result { get; private set; }
 
         public override void Setup()
@@ -38,8 +42,15 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ResultControl
             Controller = new ResultController(ResultLoader, CacheService, Logger);
 
             ProfileId = 1;
-            AssessmentId = 11;
             AoUkprn = 1234567890;
+
+            ViewModel = new ManageSpecialismResultViewModel
+            {
+                ProfileId = ProfileId,
+                AssessmentId = 1,
+                SelectedGradeCode = "PCG1"
+            };
+
             var httpContext = new ClaimsIdentityBuilder<ResultController>(Controller)
                .Add(CustomClaimTypes.Ukprn, AoUkprn.ToString())
                .Add(CustomClaimTypes.UserId, Guid.NewGuid().ToString())
@@ -48,11 +59,12 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ResultControl
 
             HttpContextAccessor.HttpContext.Returns(httpContext);
             CacheKey = CacheKeyHelper.GetCacheKey(httpContext.User.GetUserId(), CacheConstants.ResultCacheKey);
+            MockResult = new AddResultResponse();
         }
 
         public async override Task When()
         {
-            Result = await Controller.AddCoreResultAsync(ProfileId, AssessmentId);
+            Result = await Controller.AddSpecialismResultAsync(ViewModel);
         }
     }
 }
