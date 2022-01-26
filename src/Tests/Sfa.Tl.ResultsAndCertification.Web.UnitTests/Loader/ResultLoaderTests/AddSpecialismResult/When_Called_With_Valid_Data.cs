3b577ff1849859ec920a90a70a6499113156412e
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-01-26 09:49:15 +0000

    TLRC - 5193 - (8) Result Entry: Specialism - Result successfully added - TLRC - 5405 - Dev - Added unit tests for AddSpecialism result post method and AddSpecialismResult method in ResultLoader

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/ResultLoaderTests/AddSpecialismResult/When_Called_With_Valid_Data.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.Common;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Result.Manual;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.ResultLoaderTests.AddSpecialismResult
+{
+    public class When_Called_With_Valid_Data : TestSetup
+    {
+        private AddResultResponse ExpectedApiResult { get; set; }
+
+        public override void Given()
+        {
+            var lookupApiClientResponse = new List<LookupData> { new LookupData { Id = 1, Code = "SCG1", Value = "Merit" } };
+
+            ViewModel = new ManageSpecialismResultViewModel
+            {
+                ProfileId = ProfileId,
+                AssessmentId = 1,
+                SelectedGradeCode = "SCG1",
+                LookupId = 1
+            };
+
+            ExpectedApiResult = new AddResultResponse { IsSuccess = true, Uln = 1234567890, ProfileId = ProfileId };
+
+            InternalApiClient.GetLookupDataAsync(LookupCategory.SpecialismComponentGrade).Returns(lookupApiClientResponse);
+
+            InternalApiClient.AddResultAsync(Arg.Is<AddResultRequest>(
+                                            x => x.ProfileId == ViewModel.ProfileId &&
+                                            x.AoUkprn == AoUkprn &&
+                                            x.ComponentType == ComponentType.Specialism &&
+                                            x.LookupId == ViewModel.LookupId))
+                                        .Returns(ExpectedApiResult);
+        }
+
+        [Fact]
+        public void Then_Recieved_Call_To_GetLookupData()
+        {
+            InternalApiClient.Received(1).GetLookupDataAsync(LookupCategory.SpecialismComponentGrade);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            ActualResult.Should().NotBeNull();
+            ActualResult.IsSuccess.Should().Be(ExpectedApiResult.IsSuccess);
+            ActualResult.Uln.Should().Be(ExpectedApiResult.Uln);
+            ActualResult.ProfileId.Should().Be(ExpectedApiResult.ProfileId);
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/ResultLoaderTests/AddSpecialismResult/When_LookupData_NotFound.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.Common;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Result.Manual;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.ResultLoaderTests.AddSpecialismResult
+{
+    public class When_LookupData_NotFound : TestSetup
+    {
+        public override void Given()
+        {
+            var lookupApiClientResponse = new List<LookupData>();
+
+            ViewModel = new ManageSpecialismResultViewModel
+            {
+                ProfileId = ProfileId,
+                AssessmentId = 1,
+                SelectedGradeCode = "SCG1",
+                LookupId = 1
+            };
+
+            InternalApiClient.GetLookupDataAsync(LookupCategory.SpecialismComponentGrade).Returns(lookupApiClientResponse);            
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            ActualResult.Should().BeNull();
+        }
+    }
+}
