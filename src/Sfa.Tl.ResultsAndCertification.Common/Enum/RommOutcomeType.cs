5f0a7aaab75d8491deb75249b1f470ed908b35bc
Ravi Bobbili <ravi.bob@outlook.com>
2022-03-11 16:36:23 +0000

    TLRC - 5755 - (8.3) Post Results: Add the ROMM outcome (Core) Changed enum type

--- src/Sfa.Tl.ResultsAndCertification.Common/Enum/RommOutcomeType.cs
+++ src/Sfa.Tl.ResultsAndCertification.Common/Enum/RommOutcomeType.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sfa.Tl.ResultsAndCertification.Common.Enum
+﻿namespace Sfa.Tl.ResultsAndCertification.Common.Enum
 {
     public enum RommOutcomeType
     {
         GradeChanged = 1,
         GradeNotChanged = 2,
-        WithdrawAppeal = 3
+        Withdraw = 3
     }
 }
