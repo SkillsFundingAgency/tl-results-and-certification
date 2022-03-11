0cf28a97a280292a288a6128aa79e4e7016b5543
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-03-11 16:40:31 +0000

    TLRC - 5757 - (8.9) ROMM banner - ROMM outcome recorded - Learners detail page (Core) - TLRC - 5959 - Dev - Added Web unit tests - TLRC - 5960 - Dev - Added Web Loader unit tests

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/PostResultsServiceLoaderTests/PrsRommActivity/When_NewGrade_IsNotValid.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.Common;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.PostResultsService;
+using Sfa.Tl.ResultsAndCertification.Web.Mapper;
+using Sfa.Tl.ResultsAndCertification.Web.Mapper.Resolver;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.PostResultsService;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.PostResultsServiceLoaderTests.PrsRommActivity
+{
+    public class When_NewGrade_IsNotValid : TestSetup
+    {
+        private PrsRommCheckAndSubmitViewModel _model;
+
+        public override void Given()
+        {
+            CreateMapper();
+
+            _model = new PrsRommCheckAndSubmitViewModel
+            {
+                ProfileId = 1,
+                AssessmentId = 2,
+                ResultId = 3,
+                ComponentType = ComponentType.Core,
+                NewGrade = "K"
+            };
+
+            var lookupGrades = new List<LookupData> { new LookupData { Id = 11, Code = "A", Value = "A" } };
+            InternalApiClient.GetLookupDataAsync(LookupCategory.PathwayComponentGrade).
+                Returns(lookupGrades);
+        }
+        
+        public async override Task When()
+        {
+            ActualResult = await Loader.PrsRommActivityAsync(AoUkprn, _model);
+        }
+
+        [Fact]
+        public void Then_False_Returned()
+        {
+            ActualResult.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Are_Called()
+        {
+            InternalApiClient.Received(1).GetLookupDataAsync(LookupCategory.PathwayComponentGrade);
+            InternalApiClient.DidNotReceive().PrsActivityAsync(Arg.Any<PrsActivityRequest>());
+        }
+
+        public override void CreateMapper()
+        {
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddMaps(typeof(PostResultsServiceMapper).Assembly);
+                c.ConstructServicesUsing(type =>
+                            type.Name.Contains("UserNameResolver") ?
+                                new UserNameResolver<PrsRommCheckAndSubmitViewModel, PrsActivityRequest>(HttpContextAccessor) : null);
+            });
+            Mapper = new AutoMapper.Mapper(mapperConfig);
+        }
+    }
+}
