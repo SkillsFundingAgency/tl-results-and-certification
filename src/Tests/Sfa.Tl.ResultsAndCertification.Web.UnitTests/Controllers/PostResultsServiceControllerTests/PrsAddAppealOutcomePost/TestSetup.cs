df2d5a983740195b2ef77ec888d26886696e2ae7
ramagudimetla07 <rama.gudimetla@digital.education.gov.uk>
2022-03-30 13:16:58 +0100

    TLRC - 5893 - (10.4) Post Results APPEAL CORE: Add an appeal outcome page - Add outcome route Added web unit tests Added web loader unit tests

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/PostResultsServiceControllerTests/PrsAddAppealOutcomePost/TestSetup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.PostResultsService;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.PostResultsServiceControllerTests.PrsAddAppealOutcomePost
+{
+    public abstract class TestSetup : PostResultsServiceControllerTestBase
+    {
+        public int ProfileId { get; set; }
+        public int AssessmentId { get; set; }
+        public ComponentType ComponentType { get; set; }
+        public IActionResult Result { get; private set; }
+        public PrsAddAppealOutcomeViewModel ViewModel { get; set; }
+
+        public async override Task When()
+        {
+            Result = await Controller.PrsAddAppealOutcomeAsync(ViewModel);
+        }
+    }
+}
