33ee7676bc390879f14b1ac87ce449e483cf21a6
Ravi Bobbili <ravi.bob@outlook.com>
2022-04-22 17:49:50 +0100

    TLRC-6114 (12.8) M&E: English - success banner Loader UT added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/TrainingProviderTests/UpdateLearnerSubjectAsync/TestSetup.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.TrainingProvider;
+using Sfa.Tl.ResultsAndCertification.Web.Mapper;
+using Sfa.Tl.ResultsAndCertification.Web.Mapper.Resolver;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.TrainingProvider.Manual;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.TrainingProviderTests.UpdateLearnerSubjectAsync
+{
+    public abstract class TestSetup : TrainingProviderLoaderTestBase
+    {
+        protected long ProviderUkprn;
+        protected AddEnglishStatusViewModel AddEnglishStatusViewModel { get; set; }
+        protected bool ActualResult { get; set; }
+
+        public async override Task When()
+        {
+            ActualResult = await Loader.UpdateLearnerSubjectAsync(ProviderUkprn, AddEnglishStatusViewModel);
+        }
+
+        public void CreateMapper()
+        {
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddMaps(typeof(TrainingProviderMapper).Assembly);
+                c.ConstructServicesUsing(type =>
+                            type.Name.Contains("UserNameResolver") ?
+                                new UserNameResolver<AddEnglishStatusViewModel, UpdateLearnerSubjectRequest>(HttpContextAccessor) :
+                                type.Name.Contains("UserEmailResolver") ?
+                                (object)new UserEmailResolver<AddEnglishStatusViewModel, UpdateLearnerSubjectRequest>(HttpContextAccessor) : null);
+
+            });
+            Mapper = new AutoMapper.Mapper(mapperConfig);
+        }
+    }
+}
