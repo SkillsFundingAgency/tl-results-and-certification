bff351cc2fdaca4f4f34c6edc0cb7ff9c8e7e6fa
Ravi Bobbili <ravi.bob@outlook.com>
2022-05-04 17:08:45 +0100

    TLRC-6251 2.1.3a (10 T Levels) IP: Was [Learner] granted a temporary flexibility? Unit test for Loader

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/IndustryPlacementLoaderTests/GetTempFlexNavigation/When_Called_With_Invalid_Data.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.IndustryPlacementLoaderTests.GetTempFlexNavigation
+{
+    public class When_Called_With_Invalid_Data : TestSetup
+    {
+        public override void Given() { }
+        
+        [Fact]
+        public void Then_Expected_Results_Are_Returned()
+        {
+            ActualResult.Should().BeNull();
+        }
+    }
+}
