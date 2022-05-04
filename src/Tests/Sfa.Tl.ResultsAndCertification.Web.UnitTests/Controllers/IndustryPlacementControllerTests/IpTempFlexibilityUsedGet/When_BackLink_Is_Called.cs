30a8f871c7432df1f585b9396dd543b9c110bd33
Ravi Bobbili <ravi.bob@outlook.com>
2022-05-04 15:18:54 +0100

    TLRC-6251 2.1.3a (10 T Levels) IP: Was [Learner] granted a temporary flexibility? Unit test for Get method added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/IndustryPlacementControllerTests/IpTempFlexibilityUsedGet/When_BackLink_Is_Called.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.IndustryPlacement.Manual;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.IndustryPlacementControllerTests.IpTempFlexibilityUsedGet
+{
+    public class When_BackLink_Is_Called
+    {
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { new IpModelViewModel { IpModelUsed = new IpModelUsedViewModel { IsIpModelUsed = false } }, RouteConstants.IpModelUsed },
+                    new object[] { new IpModelViewModel { IpModelUsed = new IpModelUsedViewModel { IsIpModelUsed = true }, IpMultiEmployerUsed = new IpMultiEmployerUsedViewModel { IsMultiEmployerModelUsed = true } }, RouteConstants.IpMultiEmployerOther },
+                    new object[] { new IpModelViewModel { IpModelUsed = new IpModelUsedViewModel { IsIpModelUsed = true }, IpMultiEmployerUsed = new IpMultiEmployerUsedViewModel { IsMultiEmployerModelUsed = false } }, RouteConstants.IpMultiEmployerSelect },
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Then_Returns_Expected_Results(IpModelViewModel ipModel, string expectedRouteName)
+        {
+            var viewModel = new IpTempFlexibilityUsedViewModel();
+            viewModel.SetBackLink(ipModel);
+
+            viewModel.BackLink.Should().NotBeNull();
+            viewModel.BackLink.RouteName.Should().Be(expectedRouteName);
+            viewModel.BackLink.RouteAttributes.Count.Should().Be(0);
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/IndustryPlacementControllerTests/IpTempFlexibilityUsedGet/When_Called_With_Valid_Data_With_Cache.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.IndustryPlacement;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.IndustryPlacement.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.IndustryPlacementControllerTests.IpTempFlexibilityUsedGet
+{
+    public class When_Called_With_Valid_Data_With_Cache : TestSetup
+    {
+        private IndustryPlacementViewModel _cacheResult;
+        private IpCompletionViewModel _ipCompletionViewModel;
+        private IpTempFlexibilityUsedViewModel  _ipTempFlexibilityUsedViewModel;
+        private IpTempFlexNavigation _ipTempFlexNavigation;
+
+        public override void Given()
+        {
+            
+            // Cache object
+            _ipCompletionViewModel = new IpCompletionViewModel { ProfileId = 1, AcademicYear = 2020, LearnerName = "First Last", IndustryPlacementStatus = IndustryPlacementStatus.Completed };
+            _ipTempFlexibilityUsedViewModel = new IpTempFlexibilityUsedViewModel { LearnerName = _ipCompletionViewModel.LearnerName };
+            _cacheResult = new IndustryPlacementViewModel
+            {
+                IpCompletion = _ipCompletionViewModel,
+                IpModelViewModel = new IpModelViewModel { IpModelUsed = new IpModelUsedViewModel { IsIpModelUsed = false } },
+                TempFlexibility = new IpTempFlexibilityViewModel { IpTempFlexibilityUsed = _ipTempFlexibilityUsedViewModel } 
+            };
+            CacheService.GetAsync<IndustryPlacementViewModel>(CacheKey).Returns(_cacheResult);
+
+            // Loader navigation
+            _ipTempFlexNavigation = new IpTempFlexNavigation { AskBlendedPlacement = true, AskTempFlexibility = true };
+            IndustryPlacementLoader.GetTempFlexNavigationAsync(_cacheResult.IpCompletion.PathwayId, _cacheResult.IpCompletion.AcademicYear).Returns(_ipTempFlexNavigation);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_AreCalled()
+        {
+            IndustryPlacementLoader.Received(1).GetTempFlexNavigationAsync(_cacheResult.IpCompletion.PathwayId, _cacheResult.IpCompletion.AcademicYear);
+            IndustryPlacementLoader.DidNotReceive().TransformIpCompletionDetailsTo<IpTempFlexibilityUsedViewModel>(Arg.Any<IpCompletionViewModel>());
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            Result.Should().NotBeNull();
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(IpTempFlexibilityUsedViewModel));
+
+            var model = viewResult.Model as IpTempFlexibilityUsedViewModel;
+            model.Should().NotBeNull();
+            model.LearnerName.Should().Be(_ipTempFlexibilityUsedViewModel.LearnerName);
+            model.IsTempFlexibilityUsed.Should().BeNull();
+
+            model.BackLink.Should().NotBeNull();
+            model.BackLink.RouteName.Should().Be(RouteConstants.IpModelUsed);
+            model.BackLink.RouteAttributes.Count.Should().Be(0);
+        }
+    }
+}
