9e593cba54185c89952414415f460695f3b053ea
ramagudimetla07 <rama.gudimetla@digital.education.gov.uk>
2022-05-09 16:15:23 +0100

    TLRC - 6549 - 2.1.5b (10 T Levels) IP: Which temporary flexibilities were granted to [Learner]? Added unit tests for IpGrantedTempFlexibilitiesGet method

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/IndustryPlacementControllerTests/IpGrantedTempFlexibilityGet/When_BackLink_Is_Called.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.IndustryPlacement.Manual;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.IndustryPlacementControllerTests.IpGrantedTempFlexibilityGet
+{
+    public class When_BackLink_Is_Called
+    {
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { new IpTempFlexibilityViewModel { IpBlendedPlacementUsed = new IpBlendedPlacementUsedViewModel { IsBlendedPlacementUsed = false } }, RouteConstants.IpBlendedPlacementUsed },
+                    new object[] { new IpTempFlexibilityViewModel { IpTempFlexibilityUsed = new IpTempFlexibilityUsedViewModel { IsTempFlexibilityUsed = true } }, RouteConstants.IpTempFlexibilityUsed }
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Then_Returns_Expected_Results(IpTempFlexibilityViewModel ipTempFlexModel, string expectedRouteName)
+        {
+            var viewModel = new IpGrantedTempFlexibilityViewModel();
+            viewModel.SetBackLink(ipTempFlexModel);
+
+            viewModel.BackLink.Should().NotBeNull();
+            viewModel.BackLink.RouteName.Should().Be(expectedRouteName);
+            viewModel.BackLink.RouteAttributes.Count.Should().Be(0);
+        }
+    }
+}
