296ea7ce563ba1b5860c6ecabd942f399659b59d
ramagudimetla07 <rama.gudimetla@digital.education.gov.uk>
2022-05-09 17:35:53 +0100

    TLRC - 6549 - 2.1.5b (10 T Levels) IP: Which temporary flexibilities were granted to [Learner]? Added web loader tests for TransformIpCompletionDetailsTo method

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Loader/IndustryPlacementLoaderTests/TransformIpCompletionDetailsTo/When_Called_With_IpGrantedTempFlexibilityViewModel.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.IndustryPlacement.Manual;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Loader.IndustryPlacementLoaderTests.TransformIpCompletionDetailsTo
+{
+    public  class When_Called_With_IpGrantedTempFlexibilityViewModel : TestSetup
+    {
+        public IpGrantedTempFlexibilityViewModel ActualResult;
+
+        public override void Given()
+        {
+            IpCompletionViewModel = new IpCompletionViewModel
+            {
+                LearnerName = "First Last"
+            };
+        }
+
+        public override async Task When()
+        {
+            ActualResult = await Loader.TransformIpCompletionDetailsTo<IpGrantedTempFlexibilityViewModel>(IpCompletionViewModel);
+        }
+
+        [Fact]
+        public void Then_Expected_Results_Are_Returned()
+        {
+            ActualResult.LearnerName.Should().Be(IpCompletionViewModel.LearnerName);
+        }
+    }
+}
