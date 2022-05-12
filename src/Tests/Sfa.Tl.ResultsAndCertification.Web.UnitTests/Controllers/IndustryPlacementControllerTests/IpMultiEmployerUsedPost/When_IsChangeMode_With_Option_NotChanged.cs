84a992236455d9351bcec3a4e4627835548fe764
ramagudimetla07 <rama.gudimetla@digital.education.gov.uk>
2022-05-12 10:50:00 +0100

    TLRC-6677 3.6a IP: change link - Multiple employer model used (from changing answer from "no" to "yes" for using a model)

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/IndustryPlacementControllerTests/IpMultiEmployerUsedPost/When_IsChangeMode_With_Option_NotChanged.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.IndustryPlacement.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.IndustryPlacementControllerTests.IpMultiEmployerUsedPost
+{
+    public class When_IsChangeMode_With_Option_NotChanged : TestSetup
+    {
+        private IndustryPlacementViewModel _cacheResult;
+        private IpCompletionViewModel _ipCompletionViewModel;
+        private IpModelUsedViewModel _ipModelUsedViewModel;
+
+        public override void Given()
+        {
+            _ipCompletionViewModel = new IpCompletionViewModel { ProfileId = 1, AcademicYear = 2020, LearnerName = "First Last", IndustryPlacementStatus = Common.Enum.IndustryPlacementStatus.Completed };
+            _ipModelUsedViewModel = new IpModelUsedViewModel { IsIpModelUsed = true };
+
+            _cacheResult = new IndustryPlacementViewModel
+            {
+                IpCompletion = _ipCompletionViewModel,
+                IpModelViewModel = new IpModelViewModel { IpModelUsed = _ipModelUsedViewModel, IpMultiEmployerUsed = new IpMultiEmployerUsedViewModel { IsMultiEmployerModelUsed = true } },
+            };
+
+            ViewModel = new IpMultiEmployerUsedViewModel { IsMultiEmployerModelUsed = true, IsChangeMode = true };
+
+            CacheService.GetAsync<IndustryPlacementViewModel>(CacheKey).Returns(_cacheResult);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_AreCalled()
+        {
+            CacheService.DidNotReceive().SetAsync(CacheKey, Arg.Any<IndustryPlacementViewModel>());
+        }
+
+        [Fact]
+        public void Then_Redirected_To_IpCheckAndSubmit()
+        {
+            var routeName = (Result as RedirectToRouteResult).RouteName;
+            routeName.Should().Be(RouteConstants.IpCheckAndSubmit);
+        }
+    }
+}
