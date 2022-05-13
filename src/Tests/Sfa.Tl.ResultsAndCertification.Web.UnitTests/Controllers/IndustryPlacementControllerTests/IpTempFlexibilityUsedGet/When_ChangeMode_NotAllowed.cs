632b33a8fc5cd9a6a2a54dd62335547b4c83e21b
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-05-14 00:16:18 +0100

    TLRC - 6439 - 3.10a (10 T Levels) IP: change link - Temporary flexibilities used - TLRC - 7236 - Dev - Added Web unit tests

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/IndustryPlacementControllerTests/IpTempFlexibilityUsedGet/When_ChangeMode_NotAllowed.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.IndustryPlacement;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.IndustryPlacement.Manual;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.IndustryPlacementControllerTests.IpTempFlexibilityUsedGet
+{
+    public class When_ChangeMode_NotAllowed : TestSetup
+    {
+        private IndustryPlacementViewModel _cacheResult;
+        private IpCompletionViewModel _ipCompletionViewModel;
+        private IpTempFlexibilityUsedViewModel _ipTempFlexibilityUsedViewModel;
+        private IpTempFlexNavigation _ipTempFlexNavigation;
+
+        public override void Given()
+        {
+            IsChangeMode = true;
+            // Cache object
+            _ipCompletionViewModel = new IpCompletionViewModel { ProfileId = 1, AcademicYear = 2020, LearnerName = "First Last", IndustryPlacementStatus = IndustryPlacementStatus.Completed };
+            _ipTempFlexibilityUsedViewModel = new IpTempFlexibilityUsedViewModel { LearnerName = _ipCompletionViewModel.LearnerName, IsTempFlexibilityUsed = true };
+
+            _cacheResult = new IndustryPlacementViewModel
+            {
+                IpCompletion = _ipCompletionViewModel,
+                IpModelViewModel = new IpModelViewModel { IpModelUsed = new IpModelUsedViewModel { IsIpModelUsed = true }, IpMultiEmployerUsed = new IpMultiEmployerUsedViewModel { IsMultiEmployerModelUsed = true } },
+                TempFlexibility = new IpTempFlexibilityViewModel
+                {
+                    IpTempFlexibilityUsed = _ipTempFlexibilityUsedViewModel,
+                    IpGrantedTempFlexibility = new IpGrantedTempFlexibilityViewModel
+                    {
+                        TemporaryFlexibilities = new List<IpLookupDataViewModel>
+                        {
+                            new IpLookupDataViewModel { Id = 1, Name = "Test 1", IsSelected = true }
+                        }
+                    }
+                },
+                IsChangeModeAllowed = false
+            };
+
+            CacheService.GetAsync<IndustryPlacementViewModel>(CacheKey).Returns(_cacheResult);
+
+            // Loader navigation
+            _ipTempFlexNavigation = new IpTempFlexNavigation { AskBlendedPlacement = true, AskTempFlexibility = true };
+            IndustryPlacementLoader.GetTempFlexNavigationAsync(_cacheResult.IpCompletion.PathwayId, _cacheResult.IpCompletion.AcademicYear).Returns(_ipTempFlexNavigation);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_AreCalled()
+        {
+            IndustryPlacementLoader.Received(1).GetTempFlexNavigationAsync(_cacheResult.IpCompletion.PathwayId, _cacheResult.IpCompletion.AcademicYear);
+            IndustryPlacementLoader.DidNotReceive().TransformIpCompletionDetailsTo<IpTempFlexibilityUsedViewModel>(Arg.Any<IpCompletionViewModel>());
+            CacheService.DidNotReceive().SetAsync(CacheKey, Arg.Any<IndustryPlacementViewModel>());
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            Result.Should().NotBeNull();
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(IpTempFlexibilityUsedViewModel));
+
+            var model = viewResult.Model as IpTempFlexibilityUsedViewModel;
+            model.Should().NotBeNull();
+            model.LearnerName.Should().Be(_ipTempFlexibilityUsedViewModel.LearnerName);
+            model.IsTempFlexibilityUsed.Should().BeTrue();
+            model.IsChangeMode.Should().BeFalse();
+
+            model.BackLink.Should().NotBeNull();
+            model.BackLink.RouteName.Should().Be(RouteConstants.IpMultiEmployerOther);
+            model.BackLink.RouteAttributes.Count.Should().Be(0);
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/IndustryPlacementControllerTests/IpTempFlexibilityUsedPost/When_Yes_To_No_ChangeMode_True.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.IndustryPlacement.Manual;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.IndustryPlacementControllerTests.IpTempFlexibilityUsedPost
+{
+    public class When_Yes_To_No_ChangeMode_True : TestSetup
+    {
+        private IndustryPlacementViewModel _cacheResult;
+        private IpCompletionViewModel _ipCompletionViewModel;
+        private IpTempFlexibilityUsedViewModel _ipTempFlexibilityUsedViewModel;
+
+        public override void Given()
+        {
+            // Cache object
+            _ipCompletionViewModel = new IpCompletionViewModel { ProfileId = 1, AcademicYear = 2020, LearnerName = "First Last", IndustryPlacementStatus = IndustryPlacementStatus.Completed };
+            _ipTempFlexibilityUsedViewModel = new IpTempFlexibilityUsedViewModel { LearnerName = _ipCompletionViewModel.LearnerName, IsTempFlexibilityUsed = true, IsChangeMode = true };
+
+            _cacheResult = new IndustryPlacementViewModel
+            {
+                IpCompletion = _ipCompletionViewModel,
+                IpModelViewModel = new IpModelViewModel { IpModelUsed = new IpModelUsedViewModel { IsIpModelUsed = true }, IpMultiEmployerUsed = new IpMultiEmployerUsedViewModel { IsMultiEmployerModelUsed = true } },
+                TempFlexibility = new IpTempFlexibilityViewModel
+                {
+                    IpTempFlexibilityUsed = _ipTempFlexibilityUsedViewModel,
+                    IpGrantedTempFlexibility = new IpGrantedTempFlexibilityViewModel
+                    {
+                        TemporaryFlexibilities = new List<IpLookupDataViewModel>
+                        {
+                            new IpLookupDataViewModel { Id = 1, Name = "Test 1", IsSelected = true }
+                        }
+                    }
+                },
+                IsChangeModeAllowed = true
+            };
+
+            CacheService.GetAsync<IndustryPlacementViewModel>(CacheKey).Returns(_cacheResult);
+
+            ViewModel = new IpTempFlexibilityUsedViewModel { IsTempFlexibilityUsed = false, IsChangeMode = true };
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_AreCalled()
+        {
+            CacheService.Received(2).SetAsync(CacheKey, Arg.Any<IndustryPlacementViewModel>());
+        }
+
+        [Fact]
+        public void Then_Redirected_To_Expected_Route()
+        {
+            var route = Result as RedirectToRouteResult;
+            route.RouteName.Should().Be(RouteConstants.IpCheckAndSubmit);
+            route.RouteValues.Should().BeNullOrEmpty();
+        }
+    }
+}
