744a41934a41695528a281303af54bf369b7c8e5
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-05-15 20:38:13 +0100

    TLRC - 6562- 3.11a (10 T Levels) IP: change link - Blended placement temporary flexibility used - TLRC - 7266 - Dev - Added Web unit tests for Post method

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/IndustryPlacementControllerTests/IpBlendedPlacementUsedPost/When_No_To_Yes_For_2020_Cohort_ChangeMode_True.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.IndustryPlacement.Manual;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.IndustryPlacementControllerTests.IpBlendedPlacementUsedPost
+{
+    public class When_No_To_Yes_For_2020_Cohort_ChangeMode_True : TestSetup
+    {
+        private IndustryPlacementViewModel _cacheResult;
+        private IpCompletionViewModel _ipCompletionViewModel;
+        private IpTempFlexibilityUsedViewModel _ipTempFlexibilityUsedViewModel;
+        private IpBlendedPlacementUsedViewModel _ipBlendedPlacementUsedViewModel;
+
+        public override void Given()
+        {
+            // Cache object
+            _ipCompletionViewModel = new IpCompletionViewModel { ProfileId = 1, AcademicYear = 2020, LearnerName = "First Last", IndustryPlacementStatus = IndustryPlacementStatus.Completed };
+            _ipTempFlexibilityUsedViewModel = new IpTempFlexibilityUsedViewModel { LearnerName = _ipCompletionViewModel.LearnerName, IsTempFlexibilityUsed = true };
+            _ipBlendedPlacementUsedViewModel = new IpBlendedPlacementUsedViewModel { LearnerName = _ipCompletionViewModel.LearnerName, IsBlendedPlacementUsed = false };
+
+            _cacheResult = new IndustryPlacementViewModel
+            {
+                IpCompletion = _ipCompletionViewModel,
+                IpModelViewModel = new IpModelViewModel { IpModelUsed = new IpModelUsedViewModel { IsIpModelUsed = true }, IpMultiEmployerUsed = new IpMultiEmployerUsedViewModel { IsMultiEmployerModelUsed = true } },
+                TempFlexibility = new IpTempFlexibilityViewModel
+                {
+                    IpTempFlexibilityUsed = _ipTempFlexibilityUsedViewModel,
+                    IpGrantedTempFlexibility = new IpGrantedTempFlexibilityViewModel
+                    {
+                        TemporaryFlexibilities = new List<IpLookupDataViewModel>
+                        {
+                            new IpLookupDataViewModel
+                            {
+                                Id = 1,
+                                Name = "Test 1",
+                                IsSelected = true
+                            }
+                        }
+                    },
+                    IpBlendedPlacementUsed = _ipBlendedPlacementUsedViewModel
+                },
+                IsChangeModeAllowed = true
+            };
+
+            CacheService.GetAsync<IndustryPlacementViewModel>(CacheKey).Returns(_cacheResult);
+
+            ViewModel = new IpBlendedPlacementUsedViewModel { IsBlendedPlacementUsed = true, IsChangeMode = true };
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_AreCalled()
+        {
+            CacheService.Received(1).SetAsync(CacheKey, Arg.Any<IndustryPlacementViewModel>());
+        }
+
+        [Fact]
+        public void Then_Redirected_To_Expected_Route()
+        {
+            var route = Result as RedirectToRouteResult;
+            route.RouteName.Should().Be(RouteConstants.IpEmployerLedUsed);
+            route.RouteValues.Should().BeNullOrEmpty();
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/IndustryPlacementControllerTests/IpBlendedPlacementUsedPost/When_Yes_To_No_For_2021_Cohort_ChangeMode_True.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.IndustryPlacement.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.IndustryPlacementControllerTests.IpBlendedPlacementUsedPost
+{
+    public class When_Yes_To_No_For_2021_Cohort_ChangeMode_True : TestSetup
+    {
+        private IndustryPlacementViewModel _cacheResult;
+        private IpCompletionViewModel _ipCompletionViewModel;
+        private IpTempFlexibilityUsedViewModel _ipTempFlexibilityUsedViewModel;
+        private IpBlendedPlacementUsedViewModel _ipBlendedPlacementUsedViewModel;
+
+        public override void Given()
+        {
+            // Cache object
+            _ipCompletionViewModel = new IpCompletionViewModel { ProfileId = 1, AcademicYear = 2020, LearnerName = "First Last", IndustryPlacementStatus = IndustryPlacementStatus.Completed };
+            _ipTempFlexibilityUsedViewModel = null;
+            _ipBlendedPlacementUsedViewModel = new IpBlendedPlacementUsedViewModel { LearnerName = _ipCompletionViewModel.LearnerName, IsBlendedPlacementUsed = true };
+
+            _cacheResult = new IndustryPlacementViewModel
+            {
+                IpCompletion = _ipCompletionViewModel,
+                IpModelViewModel = new IpModelViewModel { IpModelUsed = new IpModelUsedViewModel { IsIpModelUsed = true }, IpMultiEmployerUsed = new IpMultiEmployerUsedViewModel { IsMultiEmployerModelUsed = true } },
+                TempFlexibility = new IpTempFlexibilityViewModel
+                {
+                    IpTempFlexibilityUsed = _ipTempFlexibilityUsedViewModel,                    
+                    IpBlendedPlacementUsed = _ipBlendedPlacementUsedViewModel
+                },
+                IsChangeModeAllowed = true
+            };
+
+            CacheService.GetAsync<IndustryPlacementViewModel>(CacheKey).Returns(_cacheResult);
+
+            ViewModel = new IpBlendedPlacementUsedViewModel { IsBlendedPlacementUsed = false, IsChangeMode = true };
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_AreCalled()
+        {
+            CacheService.Received(1).SetAsync(CacheKey, Arg.Any<IndustryPlacementViewModel>());
+        }
+
+        [Fact]
+        public void Then_Redirected_To_Expected_Route()
+        {
+            var route = Result as RedirectToRouteResult;
+            route.RouteName.Should().Be(RouteConstants.IpCheckAndSubmit);
+            route.RouteValues.Should().BeNullOrEmpty();
+        }
+    }
+}
