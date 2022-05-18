40778a105a89d7d53c84a5664d7d284f25e026a7
ramagudimetla07 <rama.gudimetla@digital.education.gov.uk>
2022-05-18 16:02:35 +0100

    TLRC - 6563 - 3.12 IP: change link - Employer-led activities/projects temporary flexibility used Added unit tests

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/IndustryPlacementControllerTests/IpEmployerLedUsedGet/When_Cache_Found_IsChangeMode_IsTrue.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.IndustryPlacement.Manual;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.IndustryPlacementControllerTests.IpEmployerLedUsedGet
+{
+    public class When_Cache_Found_IsChangeMode_IsTrue : TestSetup
+    {
+        private IndustryPlacementViewModel _cacheResult;
+        private IpCompletionViewModel _ipCompletionViewModel;
+        private IpModelUsedViewModel _ipModelUsedViewModel;
+        private IpTempFlexibilityUsedViewModel _ipTempFlexibilityUsedViewModel;
+        private IpBlendedPlacementUsedViewModel _ipBlendedPlacementUsedViewModel;
+        private IpEmployerLedUsedViewModel _ipEmployerLedUsedViewModel;
+
+        public override void Given()
+        {
+            IsChangeMode = true;
+
+            // Cache object
+            _ipCompletionViewModel = new IpCompletionViewModel { ProfileId = 1, AcademicYear = 2020, LearnerName = "First Last", IndustryPlacementStatus = IndustryPlacementStatus.Completed };
+            _ipModelUsedViewModel = new IpModelUsedViewModel { LearnerName = _ipCompletionViewModel.LearnerName, IsIpModelUsed = false };
+            _ipTempFlexibilityUsedViewModel = new IpTempFlexibilityUsedViewModel { LearnerName = _ipCompletionViewModel.LearnerName, IsTempFlexibilityUsed = true };
+            _ipBlendedPlacementUsedViewModel = new IpBlendedPlacementUsedViewModel { LearnerName = _ipCompletionViewModel.LearnerName, IsBlendedPlacementUsed = true };
+            _ipEmployerLedUsedViewModel = new IpEmployerLedUsedViewModel { LearnerName = _ipCompletionViewModel.LearnerName, IsEmployerLedUsed = true };
+
+            _cacheResult = new IndustryPlacementViewModel
+            {
+                IpCompletion = _ipCompletionViewModel,
+                IpModelViewModel = new IpModelViewModel { IpModelUsed = _ipModelUsedViewModel },
+                TempFlexibility = new IpTempFlexibilityViewModel { IpTempFlexibilityUsed = _ipTempFlexibilityUsedViewModel, IpBlendedPlacementUsed = _ipBlendedPlacementUsedViewModel, IpEmployerLedUsed = _ipEmployerLedUsedViewModel },
+                IsChangeModeAllowed = true
+            };
+
+            CacheService.GetAsync<IndustryPlacementViewModel>(CacheKey).Returns(_cacheResult);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_AreCalled()
+        {
+            CacheService.Received(1).GetAsync<IndustryPlacementViewModel>(CacheKey);
+            IndustryPlacementLoader.DidNotReceive().GetTemporaryFlexibilitiesAsync(_cacheResult.IpCompletion.PathwayId, _cacheResult.IpCompletion.AcademicYear, true);
+            IndustryPlacementLoader.DidNotReceive().TransformIpCompletionDetailsTo<IpEmployerLedUsedViewModel>(_cacheResult.IpCompletion);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+        {
+            Result.Should().NotBeNull();
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(IpEmployerLedUsedViewModel));
+
+            var model = viewResult.Model as IpEmployerLedUsedViewModel;
+            model.Should().NotBeNull();
+            model.LearnerName.Should().Be(_ipEmployerLedUsedViewModel.LearnerName);
+            model.IsEmployerLedUsed.Should().BeTrue();
+            model.IsChangeMode.Should().BeTrue();
+
+            model.BackLink.Should().NotBeNull();
+            model.BackLink.RouteName.Should().Be(RouteConstants.IpCheckAndSubmit);
+            model.BackLink.RouteAttributes.Count.Should().Be(0);
+        }
+    }
+}
