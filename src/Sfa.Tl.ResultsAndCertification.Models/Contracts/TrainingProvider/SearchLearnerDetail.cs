d6eb5b7170774223e9517311f5e5237312707c3c
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-05-19 09:59:02 +0100

    TLRC - 6648 - MANAGE T LEVEL JOURNEY - interim learners list - TLRC - 7328 - API - Dev - Created contracts to search learner details

---
+++ src/Sfa.Tl.ResultsAndCertification.Models/Contracts/TrainingProvider/SearchLearnerDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace Sfa.Tl.ResultsAndCertification.Models.Contracts.TrainingProvider
+{
+    public class SearchLearnerDetail
+    {
+        public long Uln { get; set; }
+        public string LearnerName { get; set; }
+        public string TlevelTitle { get; set; }
+        public int AcademicYear { get; set; }
+    }
+}
