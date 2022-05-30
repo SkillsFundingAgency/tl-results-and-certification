c4c5b83a1b7c005964d9a1abd73beb909ef133cd
ramagudimetla07 <rama.gudimetla@digital.education.gov.uk>
2022-05-30 10:13:40 +0100

    TLRC-6771 OVERALL RESULTS: content and design when no records are found Dev implentation

--- src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/TrainingProvider/Manual/SearchLearnerDetailsListViewModel.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/TrainingProvider/Manual/SearchLearnerDetailsListViewModel.cs
@@ -7,6 +7,11 @@ namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.TrainingProvider.Manual
 {
     public class SearchLearnerDetailsListViewModel
     {
+        public SearchLearnerDetailsListViewModel()
+        {
+            SearchLearnerDetailsList = new List<SearchLearnerDetailsViewModel>();
+        }
+
         public IList<SearchLearnerDetailsViewModel> SearchLearnerDetailsList;
         public int TotalRecords { get; set; }
 
