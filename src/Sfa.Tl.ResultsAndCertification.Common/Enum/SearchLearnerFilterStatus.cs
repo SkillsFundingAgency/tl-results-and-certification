1c7fcd96185aa38a88bf08af184c52b969705d0b
Ravi Bobbili <ravi.bob@outlook.com>
2022-06-10 10:47:26 +0100

    TLRC-6721 OVERALL RESULTS: T Level filter Amended the content

--- src/Sfa.Tl.ResultsAndCertification.Common/Enum/SearchLearnerFilterStatus.cs
+++ src/Sfa.Tl.ResultsAndCertification.Common/Enum/SearchLearnerFilterStatus.cs
@@ -4,16 +4,16 @@ namespace Sfa.Tl.ResultsAndCertification.Common.Enum
 {
     public enum SearchLearnerFilterStatus
     {
-        [Display(Name = "English incomplete")]
+        [Display(Name = "English level")]
         EnglishIncomplete = 1,
 
-        [Display(Name = "Maths incomplete")]
+        [Display(Name = "Maths level")]
         MathsIncomplete = 2,
 
         [Display(Name = "Industry placement")]
         IndustryPlacementIncomplete = 3,
 
-        [Display(Name = "All incompleted records")]
+        [Display(Name = "All incomplete records")]
         AllIncompletedRecords = 4
     }
 }
