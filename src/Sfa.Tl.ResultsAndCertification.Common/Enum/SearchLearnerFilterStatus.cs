d093556bc99d746116f90943e2ef214cb8e2a00a
ramagudimetla07 <rama.gudimetla@digital.education.gov.uk>
2022-06-10 09:53:05 +0100

    TLRC-6408 OVERALL RESULTS: statuses filter Service layer updated to convert enum filter status to FilterLookup object

---
+++ src/Sfa.Tl.ResultsAndCertification.Common/Enum/SearchLearnerFilterStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sfa.Tl.ResultsAndCertification.Common.Enum
+{
+    public enum SearchLearnerFilterStatus
+    {
+        [Display(Name = "English incomplete")]
+        EnglishIncomplete = 1,
+
+        [Display(Name = "Maths incomplete")]
+        MathsIncomplete = 2,
+
+        [Display(Name = "Industry placement")]
+        IndustryPlacementIncomplete = 3,
+
+        [Display(Name = "All incompleted records")]
+        AllIncompletedRecords = 4
+    }
+}
