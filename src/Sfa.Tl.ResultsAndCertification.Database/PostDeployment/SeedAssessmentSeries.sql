2c42c8ffbad670a96077fbceca0960483204f22a
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-06-23 19:17:28 +0100

    TLRC - 7398 - OVERALL RESULTS: Add additional columns & seed data in AssessmentSeries table - Fixed issues with build and sorted out nullable int and nullable date comparision

--- src/Sfa.Tl.ResultsAndCertification.Database/PostDeployment/SeedAssessmentSeries.sql
+++ src/Sfa.Tl.ResultsAndCertification.Database/PostDeployment/SeedAssessmentSeries.sql
@@ -6,15 +6,15 @@ SET IDENTITY_INSERT [dbo].[AssessmentSeries] ON
 
 MERGE INTO [dbo].[AssessmentSeries] AS Target 
 USING (VALUES 
-  (1, 1, N'Summer 2021', N'Summer 2021', 2021, N'2020-11-26', N'2021-08-10', N'2021-09-24', NULL, NULL),
-  (2, 1, N'Autumn 2021', N'Autumn 2021', 2021, N'2021-08-11', N'2022-03-07', N'2022-04-28', NULL, NULL),
-  (3, 1, N'Summer 2022', N'Summer 2022', 2022, N'2022-03-08', N'2022-08-08', N'2022-09-29', 2020, N'2022-08-17'),
-  (4, 1, N'Autumn 2022', N'Autumn 2022', 2022, N'2022-08-09', N'2023-03-06', N'2023-04-27', 2020, N'2023-03-15'),
-  (5, 1, N'Summer 2023', N'Summer 2023', 2023, N'2023-03-07', N'2023-08-07', N'2023-09-28', 2021, N'2023-08-16'),
-  (6, 1, N'Autumn 2023', N'Autumn 2023', 2023, N'2023-08-08', N'2024-03-11', N'2024-05-02', 2021, N'2024-03-20'),
-  (7, 2, N'Summer 2022', N'Summer 2022', 2022, N'2021-10-01', N'2022-08-08', N'2022-09-29', NULL, NULL),
-  (8, 2, N'Summer 2023', N'Summer 2023', 2023, N'2022-08-09', N'2023-08-07', N'2023-09-28', NULL, NULL),
-  (9, 2, N'Summer 2024', N'Summer 2024', 2024, N'2023-08-08', N'2024-08-05', N'2024-09-26', NULL, NULL)
+  (1, 1, N'Summer 2021', N'Summer 2021', 2021, N'2020-11-26', N'2021-08-10', N'2021-09-24', N'2022-10-31', NULL, NULL),
+  (2, 1, N'Autumn 2021', N'Autumn 2021', 2021, N'2021-08-11', N'2022-03-07', N'2022-04-28', N'2022-10-31', NULL, NULL),
+  (3, 1, N'Summer 2022', N'Summer 2022', 2022, N'2022-03-08', N'2022-08-08', N'2022-09-29', N'2022-10-31', 2020, N'2022-08-17'),
+  (4, 1, N'Autumn 2022', N'Autumn 2022', 2022, N'2022-08-09', N'2023-03-06', N'2023-04-27', N'2023-05-31', 2020, N'2023-03-15'),
+  (5, 1, N'Summer 2023', N'Summer 2023', 2023, N'2023-03-07', N'2023-08-07', N'2023-09-28', N'2023-10-31', 2021, N'2023-08-16'),
+  (6, 1, N'Autumn 2023', N'Autumn 2023', 2023, N'2023-08-08', N'2024-03-11', N'2024-05-02', N'2024-05-31', 2021, N'2024-03-20'),
+  (7, 2, N'Summer 2022', N'Summer 2022', 2022, N'2021-10-01', N'2022-08-08', N'2022-09-29', N'2022-10-31', NULL, NULL),
+  (8, 2, N'Summer 2023', N'Summer 2023', 2023, N'2022-08-09', N'2023-08-07', N'2023-09-28', N'2023-10-31', NULL, NULL),
+  (9, 2, N'Summer 2024', N'Summer 2024', 2024, N'2023-08-08', N'2024-08-05', N'2024-09-26', N'2024-10-31', NULL, NULL)
   )
   AS Source ([Id], [ComponentType], [Name], [Description], [Year], [StartDate], [EndDate], [RommEndDate], [AppealEndDate], [ResultCalculationYear], [ResultPublishDate]) 
 ON Target.[Id] = Source.[Id] 
@@ -27,9 +27,9 @@ WHEN MATCHED
 	 OR (Target.[StartDate] <> Source.[StartDate] COLLATE Latin1_General_CS_AS)
 	 OR (Target.[EndDate] <> Source.[EndDate] COLLATE Latin1_General_CS_AS)
 	 OR (Target.[RommEndDate] <> Source.[RommEndDate] COLLATE Latin1_General_CS_AS)
-	 OR (Target.[AppealEndDate] <> Source.[AppealEndDate] COLLATE Latin1_General_CS_AS)
-	 OR (Target.[ResultCalculationYear] <> Source.[ResultCalculationYear])
-	 OR (Target.[ResultPublishDate] <> Source.[ResultPublishDate] COLLATE Latin1_General_CS_AS)
+	 OR (Target.[AppealEndDate] <> Source.[AppealEndDate])
+	 OR (ISNULL(Target.[ResultCalculationYear],0) <> ISNULL(Source.[ResultCalculationYear], 0))
+	 OR (ISNULL(Target.[ResultPublishDate], '') <> ISNULL(Source.[ResultPublishDate], ''))
 	 )
 THEN 
 UPDATE SET 
