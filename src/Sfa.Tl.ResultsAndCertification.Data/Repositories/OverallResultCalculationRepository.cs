b72a2cd541badf79075383cee9c4b866baafc4ba
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-06-24 15:06:21 +0100

    TLRC - 7008 - OVERALL RESULTS - Calculation: Implement logic to fetch valid learners based on academic years

--- src/Sfa.Tl.ResultsAndCertification.Data/Repositories/OverallResultCalculationRepository.cs
+++ src/Sfa.Tl.ResultsAndCertification.Data/Repositories/OverallResultCalculationRepository.cs
@@ -19,25 +19,24 @@ namespace Sfa.Tl.ResultsAndCertification.Data.Repositories
 
         public async Task<IList<TqRegistrationPathway>> GetLearnersForOverallGradeCalculation(int resultCalculationYear)
         {
-            var registrationPathways = await _dbContext.TqRegistrationPathway
+            var latestRegistrations = await _dbContext.TqRegistrationPathway
                 .Include(x => x.TqRegistrationProfile)
                 .Include(x => x.IndustryPlacements)
                 .Include(x => x.TqPathwayAssessments.Where(pa => pa.IsOptedin && pa.TqRegistrationPathway.Status == RegistrationPathwayStatus.Withdrawn ? pa.EndDate != null : pa.EndDate == null))
                     .ThenInclude(x => x.TqPathwayResults.Where(pr => pr.IsOptedin && pr.TqPathwayAssessment.TqRegistrationPathway.Status == RegistrationPathwayStatus.Withdrawn ? pr.EndDate != null : pr.EndDate == null))
                 .Include(x => x.TqRegistrationSpecialisms.Where(s => s.IsOptedin && s.TqRegistrationPathway.Status == RegistrationPathwayStatus.Withdrawn ? s.EndDate != null : s.EndDate == null))
                     .ThenInclude(x => x.TqSpecialismAssessments.Where(sa => sa.IsOptedin && sa.TqRegistrationSpecialism.TqRegistrationPathway.Status == RegistrationPathwayStatus.Withdrawn ? sa.EndDate != null : sa.EndDate == null))
-                    .ThenInclude(x => x.TqSpecialismResults.Where(sr => sr.IsOptedin && sr.TqSpecialismAssessment.TqRegistrationSpecialism.TqRegistrationPathway.Status == RegistrationPathwayStatus.Withdrawn ? sr.EndDate != null : sr.EndDate == null))
-                .Include(x => x.OverallResults)
+                        .ThenInclude(x => x.TqSpecialismResults.Where(sr => sr.IsOptedin && sr.TqSpecialismAssessment.TqRegistrationSpecialism.TqRegistrationPathway.Status == RegistrationPathwayStatus.Withdrawn ? sr.EndDate != null : sr.EndDate == null))
+                .Include(x => x.OverallResults.Where(o => o.EndDate == null))
                 .Where(pw => (pw.Status == RegistrationPathwayStatus.Active || pw.Status == RegistrationPathwayStatus.Withdrawn) &&
-                             (pw.AcademicYear <= resultCalculationYear && pw.AcademicYear > resultCalculationYear - 4))
+                             pw.AcademicYear <= resultCalculationYear && pw.AcademicYear > resultCalculationYear - 4 &&
+                             (!pw.OverallResults.Any() || pw.OverallResults.Any(o => o.EndDate == null && (pw.IndustryPlacements.Any(ip => ip.CreatedOn > o.CreatedOn || ip.ModifiedOn > o.CreatedOn) ||
+                             pw.TqPathwayAssessments.SelectMany(pa => pa.TqPathwayResults).Any(pr => pr.CreatedOn > o.CreatedOn || pr.ModifiedOn > o.CreatedOn) ||
+                             pw.TqRegistrationSpecialisms.SelectMany(s => s.TqSpecialismAssessments.SelectMany(sa => sa.TqSpecialismResults)).Any(sr => sr.CreatedOn > o.CreatedOn || sr.ModifiedOn > o.CreatedOn)
+                             ))))
+                .GroupBy(x => x.TqRegistrationProfile.UniqueLearnerNumber)
+                .Select(x => x.OrderByDescending(o => o.CreatedOn).First())
                 .ToListAsync();
-
-            if (registrationPathways == null) return null;
-
-            var latestRegistrations = registrationPathways
-                    .GroupBy(x => x.TqRegistrationProfile.UniqueLearnerNumber)
-                    .Select(x => x.OrderByDescending(o => o.CreatedOn).First())
-                    .ToList();
 
             return latestRegistrations;
         }
