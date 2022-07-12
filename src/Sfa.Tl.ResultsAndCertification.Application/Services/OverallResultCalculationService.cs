be98bb4d59bb38dc0d15e4a184890e11454fed73
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-07-12 19:11:50 +0100

    TLRC - 7407 - OVERALL RESULTS - Functions : Implement function to trigger - TLRC - 7904 - Dev - Implemented function to trigger OverallResult calculation

--- src/Sfa.Tl.ResultsAndCertification.Application/Services/OverallResultCalculationService.cs
+++ src/Sfa.Tl.ResultsAndCertification.Application/Services/OverallResultCalculationService.cs
@@ -76,14 +76,10 @@ namespace Sfa.Tl.ResultsAndCertification.Application.Services
             var learners = await _overallGradeCalculationRepository.GetLearnersForOverallGradeCalculation(resultCalculationYearFrom, resultCalculationAssessment.ResultCalculationYear ?? 0);
 
             if (learners == null || !learners.Any())
-            {
-                var message = $"No learners data retrieved to process overall result calculation. Method: CalculateOverallResultsAsync()";
-                response.Add(new OverallResultResponse { IsSuccess = true, Message = message });
-            }
+                return null;
 
             var overallResultLookupData = await GetOverallResultLookupData();
             var overallGradeLookupData = await GetOverallGradeLookupData();
-
 
             var batchSize = _configuration.OverallResultBatchSettings.BatchSize <= 0 ? Constants.OverallResultDefaultBatchSize : _configuration.OverallResultBatchSettings.BatchSize;
             var batchesToProcess = (int)Math.Ceiling(learners.Count / (decimal)batchSize);
@@ -91,12 +87,9 @@ namespace Sfa.Tl.ResultsAndCertification.Application.Services
             for (var batchIndex = 0; batchIndex < batchesToProcess; batchIndex++)
             {
                 var leanersToProcess = learners.Skip(batchIndex * batchSize).Take(batchSize);
-                //tasks.Add(ProcessOverallResults(leanersToProcess, overallResultLookupData, overallGradeLookupData, resultCalculationAssessment));
-
                 response.Add(await ProcessOverallResults(leanersToProcess, overallResultLookupData, overallGradeLookupData, resultCalculationAssessment));
             }
 
-            //var batchResults = await Task.WhenAll(tasks);
             return response;
         }
 
