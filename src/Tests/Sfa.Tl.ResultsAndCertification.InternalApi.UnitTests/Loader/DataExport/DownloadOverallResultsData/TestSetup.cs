14aeefbe8459d94e82778f735983f2dcdacd1e65
Ravi Bobbili <ravi.bob@outlook.com>
2022-07-13 22:53:42 +0100

    TLRC-7392 OVERALL RESULTS: generate results file Unit tests added for the Loader methods

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.InternalApi.UnitTests/Loader/DataExport/DownloadOverallResultsData/TestSetup.cs
@@ -0,0 +1,36 @@
+﻿using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Application.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Common.Services.BlobStorage.Interface;
+using Sfa.Tl.ResultsAndCertification.InternalApi.Loader;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.DataExport;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.InternalApi.UnitTests.Loader.DataExport.DownloadOverallResultsData
+{
+    public abstract class TestSetup : BaseTest<DataExportLoader>
+    {
+        protected IDataExportService DataExportService;
+        protected IBlobStorageService BlobService;
+        protected IOverallResultCalculationService OverallResultCalculationService;
+        private DataExportLoader _loader;
+        protected DataExportResponse Response;
+
+        // params
+        protected long ProviderUkprn = 1234567891;
+        protected string RequestedBy = "Test User";
+
+        public override void Setup()
+        {
+            DataExportService = Substitute.For<IDataExportService>();
+            BlobService = Substitute.For<IBlobStorageService>();
+            OverallResultCalculationService = Substitute.For<IOverallResultCalculationService>();
+        }
+
+        public async override Task When()
+        {
+            _loader = new DataExportLoader(DataExportService, BlobService, OverallResultCalculationService);
+            Response = await _loader.DownloadOverallResultsDataAsync(ProviderUkprn, RequestedBy);
+        }
+    }
+}
