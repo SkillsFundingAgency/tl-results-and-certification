a7a8b9c5bbdae82b88d8bff9321daac2d72065b7
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-07-13 23:16:35 +0100

    TLRC - 7407 - OVERALL RESULTS - Functions : Implement function to trigger - TLRC - 7909 - Dev - Added Unit tests for OverallResultCalculationFunctionService

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/Services/OverallResultCalculationFunctionService/TestSetup.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Application.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Functions.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Models.Functions;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace Sfa.Tl.ResultsAndCertification.Functions.UnitTests.Services.OverallResultCalculationFunctionService
+{
+    public abstract class TestSetup : BaseTest<Functions.Services.LrsLearnerService>
+    {
+        protected IMapper Mapper;
+        protected ILogger<IOverallResultCalculationFunctionService> Logger;
+        protected IOverallResultCalculationService OverallResultCalculationService;
+        protected Functions.Services.OverallResultCalculationFunctionService Service;
+        protected IList<OverallResultResponse> ActualResult;
+
+        public override void Setup()
+        {
+            Logger = Substitute.For<ILogger<IOverallResultCalculationFunctionService>>();
+            OverallResultCalculationService = Substitute.For<IOverallResultCalculationService>();
+
+            Service = new Functions.Services.OverallResultCalculationFunctionService(OverallResultCalculationService, Logger);
+        }
+
+        public async override Task When()
+        {
+            ActualResult = await Service.CalculateOverallResultsAsync();
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/Services/OverallResultCalculationFunctionService/When_OverallResultRecords_IsNull.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Models.Functions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Functions.UnitTests.Services.OverallResultCalculationFunctionService
+{
+    public class When_OverallResultRecords_IsNull : TestSetup
+    {
+        private List<OverallResultResponse> _mockData;
+        private List<OverallResultResponse> _expectedResult;
+        private DateTime _runDate;
+
+        public override void Given()
+        {
+            _mockData = null;
+            _expectedResult = new List<OverallResultResponse> { new OverallResultResponse { IsSuccess = true, NewRecords = 0, TotalRecords = 0, SavedRecords = 0, UnChangedRecords = 0, UpdatedRecords = 0 } };
+
+            _runDate = DateTime.UtcNow.Date;
+            OverallResultCalculationService.CalculateOverallResultsAsync(_runDate).Returns(_mockData);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Are_Called()
+        {
+            OverallResultCalculationService.Received(1).CalculateOverallResultsAsync(Arg.Any<DateTime>());
+        }
+
+        [Fact]
+        public void Then_Expected_Response_Returned()
+        {
+            ActualResult.Should().NotBeNull();
+            ActualResult.Should().HaveCount(1);
+            ActualResult[0].IsSuccess.Should().BeTrue();
+            ActualResult[0].TotalRecords.Should().Be(_expectedResult[0].TotalRecords);
+            ActualResult[0].NewRecords.Should().Be(_expectedResult[0].TotalRecords);
+            ActualResult[0].UpdatedRecords.Should().Be(_expectedResult[0].TotalRecords);
+            ActualResult[0].UnChangedRecords.Should().Be(_expectedResult[0].TotalRecords);
+            ActualResult[0].SavedRecords.Should().Be(_expectedResult[0].TotalRecords);
+        }
+    }
+}
