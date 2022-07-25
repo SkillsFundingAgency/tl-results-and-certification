d9ab0d302129f19589082660cfb88065cc3601e9
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-07-25 13:58:40 +0100

    TLRC - 7819 - FREEZE PERIOD: create logic not to allow users to access site when freeze period is in place for 2022 - TLRC - 7991 - Dev - Configured freeze startdate and enddate - TLRC - 7992 - Dev - Implemented changes to ResultsAndCertificationConfiguration - TLRC - 7993 - Dev - Created freezeperiod filter attribute - TLRC - 7994 - Dev - Implemented logic for FreezePreriod - TLRC - 7995 - Dev - Implemented changes to config file

---
+++ src/Sfa.Tl.ResultsAndCertification.Web/Filters/FreezePeriodFilterAttribute.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Configuration;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
+using System;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.Filters
+{
+    public class FreezePeriodFilterAttribute : IAsyncActionFilter
+    {
+        private readonly ResultsAndCertificationConfiguration _configuration;
+        private readonly ILogger _logger;
+
+        public FreezePeriodFilterAttribute(ResultsAndCertificationConfiguration configuration, ILogger<FreezePeriodFilterAttribute> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            try
+            {               
+                if (context.Controller.GetType() != typeof(HelpController) && context.Controller.GetType() != typeof(ErrorController) && IsFreezePeriodActive())
+                {
+                    var routeValues = new RouteValueDictionary
+                    {
+                        { "controller", Constants.ErrorController },
+                        { "action", nameof(ErrorController.ServiceAccessDenied) }
+                    };
+                    context.Result = new RedirectToRouteResult(routeValues);
+                    await context.Result.ExecuteResultAsync(context);
+                }
+                else
+                {
+                    await next();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+            }
+        }
+
+        private bool IsFreezePeriodActive()
+        {
+            return DateTime.UtcNow >= _configuration.FreezePeriodStartDate && DateTime.UtcNow <= _configuration.FreezePeriodEndDate;
+        }
+    }
+}
