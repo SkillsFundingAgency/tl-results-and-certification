1cf402aad084d50c7baca08c2b65c099a36afac2
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-07-27 13:06:21 +0100

    TLRC - 7795 - UCAS RESULTS JOB: pipeline changes - Fixed build

--- src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/DateTimeExtensions/TestSetup.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/DateTimeExtensions/TestSetup.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using System;
 using System.Collections.Generic;
 
 namespace Sfa.Tl.ResultsAndCertification.Functions.UnitTests.DateTimeExtensions
@@ -12,13 +13,13 @@ namespace Sfa.Tl.ResultsAndCertification.Functions.UnitTests.DateTimeExtensions
                 return new[]
                 {
                     // Valid
-                    new object[] { new DateTime(2022,06,07), DayOfWeek.Wednesday, false },
-                    new object[] { new DateTime(2022, 06, 17), DayOfWeek.Wednesday, false },
-                    new object[] { new DateTime(2022, 06, 24), DayOfWeek.Wednesday, false },
-                    new object[] { new DateTime(2022, 06, 08), DayOfWeek.Wednesday, false },
-                    new object[] { new DateTime(2022, 06, 15), DayOfWeek.Wednesday, false },
-                    new object[] { new DateTime(2022, 06, 22), DayOfWeek.Wednesday, false },
-                    new object[] { new DateTime(2022, 06, 29), DayOfWeek.Wednesday, true },
+                    new object[] { new DateTime(2022,06,07), DayOfWeek.Wednesday, Months.June, false },
+                    new object[] { new DateTime(2022, 06, 17), DayOfWeek.Wednesday, Months.June, false },
+                    new object[] { new DateTime(2022, 06, 24), DayOfWeek.Wednesday, Months.June, false },
+                    new object[] { new DateTime(2022, 06, 08), DayOfWeek.Wednesday, Months.June, false },
+                    new object[] { new DateTime(2022, 06, 15), DayOfWeek.Wednesday, Months.June, false },
+                    new object[] { new DateTime(2022, 06, 22), DayOfWeek.Wednesday, Months.June, false },
+                    new object[] { new DateTime(2022, 06, 29), DayOfWeek.Wednesday, Months.June, true },
                 };
             }
         }
