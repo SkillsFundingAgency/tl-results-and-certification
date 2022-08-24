eb719050a9ccb3b6ad6200545d60897b3a5d7e3d
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-08-24 14:45:22 +0100

    TLRC - 7801 - CERTIFICATION: Changes to OverallResult table - TLRC - 8266 - Dev - Added Integration Tests

--- src/Tests/Sfa.Tl.ResultsAndCertification.IntegrationTests/Services/OverallResultCalculationServiceTests/When_CalculateOverallResults_IsCalled.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.IntegrationTests/Services/OverallResultCalculationServiceTests/When_CalculateOverallResults_IsCalled.cs
@@ -97,7 +97,7 @@ namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Services.OverallResult
             }
             OverallGradeLookupProvider.CreateOverallGradeLookupList(DbContext, _overallGradeLookup);
 
-            _printAvailableFrom = DateTime.Now.Date.AddMonths(5).AddDays(1);
+            _printAvailableFrom = DateTime.Now.Date.AddMonths(7);
 
             // Seed Overall results
             var sameResultPathwayId = _registrations.FirstOrDefault(x => x.UniqueLearnerNumber == 1111111113).TqRegistrationPathways.FirstOrDefault().Id;
