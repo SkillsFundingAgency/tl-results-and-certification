d1feac66453bbdd498a3eeb5b4bb1c032113721d
Ravi Bobbili <ravi.bob@outlook.com>
2022-09-07 12:20:37 +0100

    TLRC-7811 CERTIFICATION: 6.1 - Functions - Implement Certifcate function job Refactored CertificatePrinting functions

--- src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/CertificatePrinting/When_SubmitCertificatePrintingRequest_Is_Triggered.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/CertificatePrinting/When_SubmitCertificatePrintingRequest_Is_Triggered.cs
@@ -1,17 +1,26 @@
-﻿using NSubstitute;
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Timers;
+using Microsoft.Extensions.Logging.Abstractions;
+using NSubstitute;
 using Sfa.Tl.ResultsAndCertification.Models.Contracts;
 using Sfa.Tl.ResultsAndCertification.Models.Functions;
+using System.Threading.Tasks;
 using Xunit;
 
-namespace Sfa.Tl.ResultsAndCertification.Functions.UnitTests.CertificatePrintingRequest
+namespace Sfa.Tl.ResultsAndCertification.Functions.UnitTests.CertificatePrinting
 {
-    public class When_Timer_Function_Is_Triggered : TestSetup
+    public class When_SubmitCertificatePrintingRequest_Is_Triggered : TestSetup
     {
         public override void Given()
         {
             CommonService.CreateFunctionLog(Arg.Any<FunctionLogDetails>()).Returns(true);
             CertificatePrintingService.ProcessPrintingRequestAsync().Returns(new CertificatePrintingResponse { IsSuccess = true });
             CommonService.UpdateFunctionLog(Arg.Any<FunctionLogDetails>()).Returns(true);
+        }
+
+        public async override Task When()
+        {
+            await CertificatePrintingFunction.SubmitCertificatePrintingRequestAsync(new TimerInfo(TimerSchedule, new ScheduleStatus()), new ExecutionContext(), new NullLogger<CertificatePrintingRequest>());
         }
 
         [Fact]
