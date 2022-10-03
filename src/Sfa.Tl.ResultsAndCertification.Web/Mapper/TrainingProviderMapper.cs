a6a52d1d9d036d30c30c92f12110ed080e7600ec
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-10-03 17:01:17 +0100

    TLRC - 8110 - CERTIFICATION Replacement cert: 1 - Implement logic to fetch printing data to display in UI - removed certificate properties that are not required

--- src/Sfa.Tl.ResultsAndCertification.Web/Mapper/TrainingProviderMapper.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Mapper/TrainingProviderMapper.cs
@@ -65,10 +65,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Mapper
                .ForMember(d => d.IndustryPlacementStatus, opts => opts.MapFrom(s => s.IndustryPlacementStatus))
                .ForMember(d => d.OverallResultDetails, opts => opts.MapFrom(s => !string.IsNullOrWhiteSpace(s.OverallResultDetails) ? JsonConvert.DeserializeObject<OverallResultDetail>(s.OverallResultDetails) : null))
                .ForMember(d => d.OverallResultPublishDate, opts => opts.MapFrom(s => s.OverallResultPublishDate))
-               .ForMember(d => d.LastDocumentRequestedDate, opts => opts.MapFrom(s => s.LastDocumentRequestedDate))
-               .ForMember(d => d.PrintCertificateId, opts => opts.MapFrom(s => s.PrintCertificateId))
-               .ForMember(d => d.PrintCertificateType, opts => opts.MapFrom(s => s.PrintCertificateType))
-               .ForMember(d => d.ProviderAddress, opts => opts.MapFrom(s => s.ProviderAddress));
+               .ForMember(d => d.LastDocumentRequestedDate, opts => opts.MapFrom(s => s.LastDocumentRequestedDate));               
 
             CreateMap<Address, AddressViewModel>()
                 .ForMember(d => d.AddressId, opts => opts.MapFrom(s => s.AddressId))
