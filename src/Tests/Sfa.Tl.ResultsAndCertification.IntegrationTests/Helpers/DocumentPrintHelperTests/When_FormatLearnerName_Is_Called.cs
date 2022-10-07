20cec0f0becb6fbe839cbb01c4803270565bea22
Ravi Bobbili <ravi.bob@outlook.com>
2022-10-07 17:03:18 +0100

    TLRC-8477 Print Certificates - Learner Name format Unit tests are added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.IntegrationTests/Helpers/DocumentPrintHelperTests/When_FormatLearnerName_Is_Called.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Sfa.Tl.ResultsAndCertification.Application.Helpers;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Helpers.DocumentPrintHelperTests
+{
+    public class When_FormatLearnerName_Is_Called
+    {
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] {"John", "Smith", "John Smith"},
+                    new object[] {"JOHN", "Smith", "John Smith"},
+                    new object[] {"McDonald", "Forest", "McDonald Forest"}
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Expected_Results_Are_Returned(string fname, string lname, string expectedResult)
+        {
+            var actualResult = DocumentPrintHelper.FormatLearnerName(fname, lname);
+            actualResult.Should().Be(expectedResult);
+        }
+    }
+}
