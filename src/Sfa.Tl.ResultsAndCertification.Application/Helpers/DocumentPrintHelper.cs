785cb2ff705749c0f55f4baa6d3fc0b21e22e784
Ravi Bobbili <ravi.bob@outlook.com>
2022-10-07 16:30:16 +0100

    TLRC-8477 Print Certificates - Learner Name format DocumentPrintHelper written for the format the learner name.

---
+++ src/Sfa.Tl.ResultsAndCertification.Application/Helpers/DocumentPrintHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Linq;
+
+namespace Sfa.Tl.ResultsAndCertification.Application.Helpers
+{
+    public static class DocumentPrintHelper
+    {
+        public static string FormatLearnerName(string firstName, string lastName)
+        {
+            return $"{firstName.ApplyTitleCase()} {lastName.ApplyTitleCase()}";
+        }
+
+        /// <summary>
+        /// Convert ToTitleCase if input string is in uppercase.
+        /// </summary>
+        public static string ApplyTitleCase(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return value;
+
+            if (value.Replace(" ", string.Empty).All(c => char.IsUpper(c)))
+                return CultureInfo.InvariantCulture.TextInfo.ToTitleCase(value.ToLower());
+
+            return value;
+        }
+    }
+}
