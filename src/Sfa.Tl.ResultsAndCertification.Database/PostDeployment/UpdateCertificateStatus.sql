47ca034e5b1007b18033ae63b6891624c7632322
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2022-11-29 10:27:21 +0000

    Removed UpdateCertificateStatus script as this is not required anymore

--- src/Sfa.Tl.ResultsAndCertification.Database/PostDeployment/UpdateCertificateStatus.sql
+++
@@ -1,3 +0,0 @@
-﻿UPDATE [dbo].[OverallResult]  
-SET [CertificateStatus] = 1
-WHERE IsOptedin = 1 AND EndDate IS NULL AND CertificateStatus IS NULL AND CalculationStatus IN (1,4) -- 1 - Completed, 4 - Partially Completed
