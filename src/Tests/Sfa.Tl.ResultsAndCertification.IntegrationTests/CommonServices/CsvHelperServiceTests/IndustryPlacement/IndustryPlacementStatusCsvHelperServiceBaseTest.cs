190dc021ee8adbf86ab043c4bfe70841748aeccc
Ravi Bobbili <ravi.bob@outlook.com>
2023-02-07 11:39:40 +0000

    TLRC-9045 5.1 Bulk Upload - IP Journey Stage 2 validations Integration tests are added

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.IntegrationTests/CommonServices/CsvHelperServiceTests/IndustryPlacement/IndustryPlacementStatusCsvHelperServiceBaseTest.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Logging;
+using Sfa.Tl.ResultsAndCertification.Common.Services.CsvHelper.DataParser.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Common.Services.CsvHelper.Service;
+using Sfa.Tl.ResultsAndCertification.Common.Services.CsvHelper.Service.Interface;
+using Sfa.Tl.ResultsAndCertification.Models.BulkProcess;
+using Sfa.Tl.ResultsAndCertification.Models.IndustryPlacement.BulkProcess;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.CommonServices.CsvHelperServiceTests.IndustryPlacement
+{
+    public abstract class IndustryPlacementStatusCsvHelperServiceBaseTest : BaseTest
+    {
+        protected ILogger<CsvHelperService<IndustryPlacementCsvRecordRequest, CsvResponseModel<IndustryPlacementCsvRecordResponse>, IndustryPlacementCsvRecordResponse>> Logger;
+        protected ICsvHelperService<IndustryPlacementCsvRecordRequest, CsvResponseModel<IndustryPlacementCsvRecordResponse>, IndustryPlacementCsvRecordResponse> Service;
+        protected IValidator<IndustryPlacementCsvRecordRequest> Validator;
+        protected IDataParser<IndustryPlacementCsvRecordResponse> DataParser;
+        protected CsvResponseModel<IndustryPlacementCsvRecordResponse> ReadAndParseFileResponse;
+        protected string FilePath;
+
+        protected virtual string GetCodeBaseAbsolutePath()
+        {
+            var codeBaseUri = new Uri(Assembly.GetExecutingAssembly().Location);
+            return Uri.UnescapeDataString(codeBaseUri.AbsolutePath);
+        }
+
+        public override Task When()
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task WhenAsync()
+        {
+            ValidatorOptions.Global.DisplayNameResolver = (type, memberInfo, expression) =>
+            {
+                return memberInfo.GetCustomAttribute<System.ComponentModel.DataAnnotations.DisplayAttribute>()?.GetName();
+            };
+
+            await using (var stream = File.Open(FilePath, FileMode.Open))
+            {
+                ReadAndParseFileResponse = await Service.ReadAndParseFileAsync(new IndustryPlacementCsvRecordRequest
+                {
+                    FileStream = stream
+                });
+            }
+        }
+    }
+}
