411f8d0482d7f1277991e495cb29fc5724541e72
Mustafa JAWAD <Mustafa.JAWAD@EDUCATION.GOV.UK>
2023-04-30 17:21:35 +0100

    TLRC- Added the related reference of resx

--- src/Sfa.Tl.ResultsAndCertification.Web/Content/Home/StartPage.Designer.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Content/Home/StartPage.Designer.cs
@@ -19,7 +19,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Content.Home {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class StartPage {
@@ -493,7 +493,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Content.Home {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Start now.
+        ///   Looks up a localized string similar to Start now BUT.
         /// </summary>
         public static string Start_Button_Text {
             get {
