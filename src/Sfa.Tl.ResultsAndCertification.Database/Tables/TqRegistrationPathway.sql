209cfba01e7798c1c10a7cdc9515332c36be67aa
Mustafa JAWAD <Mustafa.JAWAD@EDUCATION.GOV.UK>
2023-05-19 14:00:07 +0100

    TLRC-9279 Added IsPendingWithdrawal column to TqRegistrationPathway table

--- src/Sfa.Tl.ResultsAndCertification.Database/Tables/TqRegistrationPathway.sql
+++ src/Sfa.Tl.ResultsAndCertification.Database/Tables/TqRegistrationPathway.sql
@@ -12,7 +12,8 @@
     [CreatedBy] NVARCHAR(50) NULL, 
     [ModifiedOn] DATETIME2 NULL, 
     [ModifiedBy] NVARCHAR(50) NULL,
-	CONSTRAINT [PK_TqRegistrationPathway] PRIMARY KEY ([Id]),
+	[IsPendingWithdrawal] BIT NULL DEFAULT 0, 
+    CONSTRAINT [PK_TqRegistrationPathway] PRIMARY KEY ([Id]),
 	CONSTRAINT [FK_TqRegistrationPathway_TqRegistrationProfile] FOREIGN KEY ([TqRegistrationProfileId]) REFERENCES [TqRegistrationProfile]([Id]),
 	CONSTRAINT [FK_TqRegistrationPathway_TqProvider] FOREIGN KEY ([TqProviderId]) REFERENCES [TqProvider]([Id]),
 	INDEX IX_TqRegistrationPathway_RegistrationProfileId NONCLUSTERED ([TqRegistrationProfileId], [TqProviderId])
