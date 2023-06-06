97878514e3b028d69ddc6259fb4f45df04eb58a8
Mustafa JAWAD <Mustafa.JAWAD@EDUCATION.GOV.UK>
2023-06-06 16:43:01 +0100

    TLRC-9592 Refactored the TlDualSpecialism

--- src/Sfa.Tl.ResultsAndCertification.Database/PostDeployment/SeedTlDualSpecialism.sql
+++ src/Sfa.Tl.ResultsAndCertification.Database/PostDeployment/SeedTlDualSpecialism.sql
@@ -6,11 +6,11 @@ SET IDENTITY_INSERT [dbo].[TlDualSpecialism] ON
 
 MERGE INTO [dbo].[TlDualSpecialism] AS Target 
 USING (VALUES 
-  (1, 3,'ZTLOS030','Plumbing and Heating Engineering', 1),
-  (2, 3,'ZTLOS031','Heating Engineering and Ventilation', 1),
-  (3, 3,'ZTLOS032','Refrigeration Engineering and Air Conditioning Engineering', 1)  
+  (1, 3,'ZTLOS030','Plumbing and Heating Engineering', 1, GETDATE(), 'System', GETDATE(), 'System'),
+  (2, 3,'ZTLOS031','Heating Engineering and Ventilation', 1, GETDATE(), 'System', GETDATE(), 'System'),
+  (3, 3,'ZTLOS032','Refrigeration Engineering and Air Conditioning Engineering', 1, GETDATE(), 'System', GETDATE(), 'System')
   )
-  AS Source ([Id], [TlPathwayId], [LarId], [Name], [IsActive]) 
+  AS Source ([Id], [TlPathwayId], [LarId], [Name], [IsActive], [CreatedOn], [CreatedBy], [ModifiedOn], [ModifiedBy]) 
 ON Target.[Id] = Source.[Id] 
 -- Update from Source when Id is Matched
 WHEN MATCHED 
