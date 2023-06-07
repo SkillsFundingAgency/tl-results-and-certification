a2458812e9bcaf7870760247f260c2b925b551b9
Mustafa JAWAD <Mustafa.JAWAD@EDUCATION.GOV.UK>
2023-06-07 08:52:33 +0100

    TLRC-9592 Removed the recent added seed files from PostDeploy.sql

--- src/Sfa.Tl.ResultsAndCertification.Database/PostDeployment/PostDeploy.sql
+++ src/Sfa.Tl.ResultsAndCertification.Database/PostDeployment/PostDeploy.sql
@@ -44,6 +44,4 @@ BEGIN
 :r ".\SeedIpTempFlexTlevelCombination.sql"
 :r ".\SeedIpTempFlexNavigation.sql"
 :r ".\SeedOverallGradeLookup.sql"
-:r ".\SeedTlDualSpecialism.sql"
-:r ".\SeedTlDualSpecialismTolSpecialism.sql"
 END
