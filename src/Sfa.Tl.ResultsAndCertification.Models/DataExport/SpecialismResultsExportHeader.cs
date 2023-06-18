fcafb2a309b40aadccc2ebc01c316bb8f3e44d94
Sajid Shafique Malik <sajid.malik@education.gov.uk>
2023-06-18 14:06:45 +0100

    Feature added to include academic year

--- src/Sfa.Tl.ResultsAndCertification.Models/DataExport/SpecialismResultsExportHeader.cs
+++ src/Sfa.Tl.ResultsAndCertification.Models/DataExport/SpecialismResultsExportHeader.cs
@@ -3,6 +3,7 @@
     public class SpecialismResultsExportHeader
     {
         public const string Uln = "ULN";
+        public const string AcademicYear = "Academic Year";
         public const string SpecialismCode = "Component code (specialism)";
         public const string SpecialismAssessmentEntry = "Assessment series (specialism)";
         public const string SpecialismGrade = "Component grade (specialism)";
