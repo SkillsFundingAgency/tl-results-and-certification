445eba3105fc19b13bfd0586345f7c39339ee118
Mustafa JAWAD <Mustafa.JAWAD@EDUCATION.GOV.UK>
2023-06-20 13:43:19 +0100

    9608 Removed unnecessary code

--- src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/Services/IndustryPlacementExtractTests/IndustryPlacementExtractFunctionTestBase.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/Services/IndustryPlacementExtractTests/IndustryPlacementExtractFunctionTestBase.cs
@@ -16,30 +16,6 @@ namespace Sfa.Tl.ResultsAndCertification.Functions.UnitTests.Services.IndustryPl
         // Actual function instance
         protected IndustryPlacementExtract IndustryPlacementExtractFunction;
 
-        //protected IIndustryPlacementService IndustryPlacementService;
-        //protected IBlobStorageService BlobStorageService;
-        //protected IUcasApiClient UcasApiClient;
-        //protected IRepository<IpLookup> IpLookupRepository;
-        //protected IRepository<TqRegistrationPathway> RegistrationPathwayRepository;
-        //protected ILogger<GenericRepository<TqRegistrationPathway>> RegistrationPathwayRepositoryLogger;
-        //protected ICommonRepository CommonRepository;
-
-        //protected ILogger<GenericRepository<IndustryPlacement>> IndustryPlacementLogger;
-        //protected IRepository<IndustryPlacement> IndustryPlacementRepository;
-        //protected ResultsAndCertificationDbContext DbContext;
-        //protected ILogger<GenericRepository<IpLookup>> IpLookupRepositoryLogger;
-
-
-        //protected ILogger<IIndustryPlacementService> Logger;
-        //protected UcasRecordEntriesSegment UcasRecordEntrySegment;
-
-        //// Actual test instance
-        //protected Application.Services.IndustryPlacementService Service;
-
-        //// Result
-        //protected FunctionResponse ActualResult;
-
-
         public override void Setup()
         {
             TimerSchedule = Substitute.For<TimerSchedule>();
@@ -48,26 +24,6 @@ namespace Sfa.Tl.ResultsAndCertification.Functions.UnitTests.Services.IndustryPl
 
             IndustryPlacementExtractFunction = new IndustryPlacementExtract(IndustryPlacementService, CommonService);
         }
-
-        //public override void Setup()
-        //{
-        //    IpLookupRepositoryLogger = new Logger<GenericRepository<IpLookup>>(new NullLoggerFactory());
-        //    IpLookupRepository = new GenericRepository<IpLookup>(IpLookupRepositoryLogger, DbContext);
-
-        //    IndustryPlacementLogger = new Logger<GenericRepository<IndustryPlacement>>(new NullLoggerFactory());
-        //    IndustryPlacementRepository = new GenericRepository<IndustryPlacement>(IndustryPlacementLogger, DbContext);
-
-        //    RegistrationPathwayRepositoryLogger = new Logger<GenericRepository<TqRegistrationPathway>>(new NullLoggerFactory());
-        //    RegistrationPathwayRepository = new GenericRepository<TqRegistrationPathway>(RegistrationPathwayRepositoryLogger, DbContext);
-
-        //    CommonRepository = new CommonRepository(DbContext);
-
-
-        //    BlobStorageService = Substitute.For<IBlobStorageService>();
-        //    Logger = Substitute.For<ILogger<IIndustryPlacementService>>();
-
-        //    //Service = new IndustryPlacementService(IpLookupRepository, IndustryPlacementRepository, 
-        //    //    RegistrationPathwayRepository, CommonRepository, BlobStorageService, mapper,  Logger);
-        //}
+       
     }
 }
