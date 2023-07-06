e03f87171959843270e22a58f88cf28e7756cb8a
Ibai Gonzalez <Ibai.GONZALEZ@EDUCATION.GOV.UK>
2023-07-06 09:45:19 +0100

    Add tests

--- src/Tests/Sfa.Tl.ResultsAndCertification.IntegrationTests/Services/OverallResultCalculationServiceTests/When_DownloadOverallResultsData_With_Dual_Speciality_IsCalled.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.IntegrationTests/Services/OverallResultCalculationServiceTests/When_DownloadOverallResultsData_With_Dual_Speciality_IsCalled.cs
@@ -18,20 +18,35 @@ namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Services.OverallResult
 {
     public class When_DownloadOverallResultsData_With_Dual_Speciality_IsCalled : OverallResultCalculationServiceBaseTest
     {
-        private const int Uln = 1111111111;
+        private const long FirstUln = 1111111111;
+        private const long SecondUln = 1111111112;
+        private const long ThirdUln = 1111111113;
+
+        private const string PlumbingAndHeatingEngineeringDualSpecialismLarId = "ZTLOS030";
+        private const string HeatingEngineeringAndVentilationDualSpecialismLarId = "ZTLOS031";
+        private const string RefrigerationAndAirConditioningEngineeringDualSpecialismLarId = "ZTLOS032";
+
+        private readonly Dictionary<long, string> _ulnDualSpecialismDict = new()
+        {
+            [FirstUln] = PlumbingAndHeatingEngineeringDualSpecialismLarId,
+            [SecondUln] = HeatingEngineeringAndVentilationDualSpecialismLarId,
+            [ThirdUln] = RefrigerationAndAirConditioningEngineeringDualSpecialismLarId
+        };
+
         private const EnumAwardingOrganisation AwardingOrganisation = EnumAwardingOrganisation.Pearson;
         private const string PreviousAssessmentName = "Previous Assessment";
 
         private readonly DateTime _publishDate = DateTime.Today.AddDays(-1);
 
-        private TqRegistrationProfile _registration;
-        private TlDualSpecialism _dualSpecialism;
+        private readonly List<TqRegistrationProfile> _tqRegistrationProfiles = new();
+        private readonly List<TqRegistrationPathway> _tqRegistrationPathways = new();
+        private TlDualSpecialism[] _dualSpecialisms;
 
         private IList<DownloadOverallResultsData> _actualResult;
 
         public override void Given()
         {
-            // Registration seed
+            // Registrations seed
             var tlPathway = TlevelDataProvider.CreateTlPathway(DbContext, AwardingOrganisation);
             var tlAwardingOrganisation = TlevelDataProvider.CreateTlAwardingOrganisation(DbContext, AwardingOrganisation);
             var tqAwardingOrganisation = TlevelDataProvider.CreateTqAwardingOrganisation(DbContext, tlPathway, tlAwardingOrganisation);
@@ -39,40 +54,64 @@ namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Services.OverallResult
             var tlProvider = ProviderDataProvider.CreateTlProvider(DbContext);
             var tqProvider = ProviderDataProvider.CreateTqProvider(DbContext, tqAwardingOrganisation, tlProvider);
 
-            var profile = new TqRegistrationProfileBuilder().Build(Uln);
-            var tqRegistrationProfile = RegistrationsDataProvider.CreateTqRegistrationProfile(DbContext, profile);
-            var tqRegistrationPathway = RegistrationsDataProvider.CreateTqRegistrationPathway(DbContext, tqRegistrationProfile, tqProvider);
+            foreach (var currentUlnDualSpecialism in _ulnDualSpecialismDict)
+            {
+                var profile = new TqRegistrationProfileBuilder().Build(currentUlnDualSpecialism.Key);
+                var tqRegistrationProfile = RegistrationsDataProvider.CreateTqRegistrationProfile(DbContext, profile);
+                var tqRegistrationPathway = RegistrationsDataProvider.CreateTqRegistrationPathway(DbContext, tqRegistrationProfile, tqProvider);
 
-            _registration = tqRegistrationProfile;
+                _tqRegistrationProfiles.Add(tqRegistrationProfile);
+                _tqRegistrationPathways.Add(tqRegistrationPathway);
+            }
 
             // Specialisms seed
             var heatingEngineeringSpecialism = CreateSpecialism(tlPathway.Id, "10202101", "Heating Engineering");
             var plumbingEngineeringSpecialism = CreateSpecialism(tlPathway.Id, "10202102", "Plumbing Engineering");
-            var specialisms = new[] { heatingEngineeringSpecialism, plumbingEngineeringSpecialism };
+            var ventilationSpecialism = CreateSpecialism(tlPathway.Id, "10202105", "Ventilation");
+            var refrigerationEngineeringSpecialism = CreateSpecialism(tlPathway.Id, "10202103", "Refrigeration Engineering");
+            var airConditioningEngineeringSpecialism = CreateSpecialism(tlPathway.Id, "10202104", "Air Conditioning Engineering");
+
+            var specialisms = new[] { heatingEngineeringSpecialism, plumbingEngineeringSpecialism, ventilationSpecialism, refrigerationEngineeringSpecialism, airConditioningEngineeringSpecialism };
             DbContext.AddRange(specialisms);
 
             // Dual specialism seed
-            _dualSpecialism = CreateDualSpecialism(tlPathway.Id, "ZTLOS030", "Plumbing and Heating Engineering");
-            DbContext.Add(_dualSpecialism);
+            var plumbingAndHeatingEngineeringDualSpecialism = CreateDualSpecialism(tlPathway.Id, PlumbingAndHeatingEngineeringDualSpecialismLarId, "Plumbing and Heating Engineering");
+            var heatingEngineeringAndVentilationDualSpecialism = CreateDualSpecialism(tlPathway.Id, HeatingEngineeringAndVentilationDualSpecialismLarId, "Heating Engineering and Ventilation");
+            var refrigerationAndAirConditioningEngineeringDualSpecialism = CreateDualSpecialism(tlPathway.Id, RefrigerationAndAirConditioningEngineeringDualSpecialismLarId, "Refrigeration Engineering and Air Conditioning Engineering");
+
+            _dualSpecialisms = new[] { plumbingAndHeatingEngineeringDualSpecialism, heatingEngineeringAndVentilationDualSpecialism, refrigerationAndAirConditioningEngineeringDualSpecialism };
+            DbContext.AddRange(_dualSpecialisms);
 
             DbContext.SaveChanges();
 
             // Dual specialism to specialism seed
-            var dualSpecialismToSpecialism = CreateDualSpecialismToSpecialism(_dualSpecialism, heatingEngineeringSpecialism, plumbingEngineeringSpecialism);
-            DbContext.AddRange(dualSpecialismToSpecialism);
+            var plumbingAndHeatingEngineeringDualSpecialismToSpecialism = CreateDualSpecialismToSpecialism(plumbingAndHeatingEngineeringDualSpecialism, heatingEngineeringSpecialism, plumbingEngineeringSpecialism);
+            var heatingEngineeringAndVentilationDualSpecialismToSpecialism = CreateDualSpecialismToSpecialism(heatingEngineeringAndVentilationDualSpecialism, heatingEngineeringSpecialism, ventilationSpecialism);
+            var refrigerationAndAirConditioningEngineeringDualSpecialismToSpecialism = CreateDualSpecialismToSpecialism(refrigerationAndAirConditioningEngineeringDualSpecialism, refrigerationEngineeringSpecialism, airConditioningEngineeringSpecialism);
+
+            DbContext.AddRange(plumbingAndHeatingEngineeringDualSpecialismToSpecialism);
+            DbContext.AddRange(heatingEngineeringAndVentilationDualSpecialismToSpecialism);
+            DbContext.AddRange(refrigerationAndAirConditioningEngineeringDualSpecialismToSpecialism);
 
             // Registration specialisms seed
-            IEnumerable<TqRegistrationSpecialism> registrationSpecialisms = specialisms.Select(s => new TqRegistrationSpecialism
-            {
-                TlSpecialismId = s.Id,
-                TqRegistrationPathwayId = tqRegistrationPathway.Id
-            });
+            TqRegistrationPathway firstRegistrationPathway = _tqRegistrationPathways.First(p => p.TqRegistrationProfile.UniqueLearnerNumber == FirstUln);
+            TqRegistrationPathway secondRegistrationPathway = _tqRegistrationPathways.First(p => p.TqRegistrationProfile.UniqueLearnerNumber == SecondUln);
+            TqRegistrationPathway thirdRegistrationPathway = _tqRegistrationPathways.First(p => p.TqRegistrationProfile.UniqueLearnerNumber == ThirdUln);
 
-            DbContext.AddRange(registrationSpecialisms);
+            IEnumerable<TqRegistrationSpecialism> firstRegistrationSpecialisms = CreateRegistrationSpecialisms(plumbingAndHeatingEngineeringDualSpecialismToSpecialism, firstRegistrationPathway);
+            IEnumerable<TqRegistrationSpecialism> secondRegistrationSpecialisms = CreateRegistrationSpecialisms(heatingEngineeringAndVentilationDualSpecialismToSpecialism, secondRegistrationPathway);
+            IEnumerable<TqRegistrationSpecialism> thirdRegistrationSpecialisms = CreateRegistrationSpecialisms(refrigerationAndAirConditioningEngineeringDualSpecialismToSpecialism, thirdRegistrationPathway);
+
+            DbContext.AddRange(firstRegistrationSpecialisms);
+            DbContext.AddRange(secondRegistrationSpecialisms);
+            DbContext.AddRange(thirdRegistrationSpecialisms);
 
             // Overall results seed
-            var overallResult = CreateOverallResult(tqRegistrationPathway.Id, "Distinction", "Merit", tlPathway, specialisms);
-            OverallResultDataProvider.CreateOverallResult(DbContext, new[] { overallResult });
+            var firstOverallResult = CreateOverallResult(firstRegistrationPathway.Id, "Distinction", "Distinction", tlPathway, new[] { heatingEngineeringSpecialism, plumbingEngineeringSpecialism });
+            var secondOverallResult = CreateOverallResult(secondRegistrationPathway.Id, "Distinction", "Merit", tlPathway, new[] { heatingEngineeringSpecialism, ventilationSpecialism });
+            var thirdOverallResult = CreateOverallResult(thirdRegistrationPathway.Id, "Pass", "Pass", tlPathway, new[] { refrigerationEngineeringSpecialism, airConditioningEngineeringSpecialism });
+
+            OverallResultDataProvider.CreateOverallResult(DbContext, new[] { firstOverallResult, secondOverallResult, thirdOverallResult });
 
             // SeedPreviousAssessment
             AssessmentSeriesDataProvider.CreateAssessmentSeries(DbContext);
@@ -99,15 +138,17 @@ namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Services.OverallResult
         }
 
         [Theory]
-        [InlineData(Provider.BarsleyCollege, Uln)]
+        [InlineData(Provider.BarsleyCollege, FirstUln)]
+        [InlineData(Provider.BarsleyCollege, SecondUln)]
+        [InlineData(Provider.BarsleyCollege, ThirdUln)]
         public async Task Then_Expected_Results_Are_Returned(Provider provider, int uln)
         {
             await WhenAsync(provider);
 
-            _actualResult.Should().HaveCount(1);
+            _actualResult.Where(x => x.Uln == uln).Should().HaveCount(1);
 
             var actualResult = _actualResult.FirstOrDefault(x => x.Uln == uln);
-            var expectedRegistration = _registration;
+            var expectedRegistration = _tqRegistrationProfiles.FirstOrDefault(x => x.UniqueLearnerNumber == uln);
 
             actualResult.Should().NotBeNull();
             actualResult.Uln.Should().Be(expectedRegistration.UniqueLearnerNumber);
@@ -123,8 +164,8 @@ namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Services.OverallResult
             actualResult.CoreComponent.Should().Be($"\"{expectedOverallResultsDetails.PathwayName}\"");
             actualResult.CoreResult.Should().Be($"{expectedOverallResultsDetails.PathwayResult}");
 
-            actualResult.SpecialismCode.Should().Be(_dualSpecialism.LarId);
-            actualResult.SpecialismComponent.Should().Be($"\"{_dualSpecialism.Name}\"");
+            actualResult.SpecialismCode.Should().Be(_ulnDualSpecialismDict[uln]);
+            actualResult.SpecialismComponent.Should().Be($"\"{_dualSpecialisms.FirstOrDefault(d => d.LarId == _ulnDualSpecialismDict[uln]).Name}\"");
             actualResult.SpecialismResult.Should().Be(expectedOverallResults.SpecialismResultAwarded);
 
             actualResult.IndustryPlacementStatus.Should().Be($"{expectedOverallResultsDetails.IndustryPlacementStatus}");
@@ -151,6 +192,15 @@ namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Services.OverallResult
             };
         }
 
+        private IEnumerable<TqRegistrationSpecialism> CreateRegistrationSpecialisms(IEnumerable<TlDualSpecialismToSpecialism> dualSpecialismToSpecialisms, TqRegistrationPathway tqRegistrationPathway)
+        {
+            return dualSpecialismToSpecialisms.Select(p => new TqRegistrationSpecialism
+            {
+                TlSpecialismId = p.TlSpecialismId,
+                TqRegistrationPathwayId = tqRegistrationPathway.Id
+            });
+        }
+
         private IEnumerable<TlDualSpecialismToSpecialism> CreateDualSpecialismToSpecialism(TlDualSpecialism dualSpecialism, TlSpecialism firstSpecialism, TlSpecialism secondSpecialism)
         {
             return new[]
@@ -169,8 +219,8 @@ namespace Sfa.Tl.ResultsAndCertification.IntegrationTests.Services.OverallResult
         }
 
         private OverallResult CreateOverallResult(
-            int registrationPathwayId, 
-            string resultAwarded, 
+            int registrationPathwayId,
+            string resultAwarded,
             string specialismResultAwarded,
             TlPathway pathway,
             IEnumerable<TlSpecialism> specialisms)
