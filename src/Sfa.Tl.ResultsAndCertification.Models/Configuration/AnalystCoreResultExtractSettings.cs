2f3d67f090bccbe9ad7c7739c0a4f0b03e82c8c0
Sajid Shafique Malik <sajid.malik@education.gov.uk>
2023-07-24 11:46:39 +0100

    Added configuration file for academic years

---
+++ src/Sfa.Tl.ResultsAndCertification.Models/Configuration/AnalystCoreResultExtractSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Sfa.Tl.ResultsAndCertification.Models.Configuration
+{
+    public class AnalystCoreResultExtractSettings
+    {
+        /// <summary>
+        /// Gets or sets the academic years to process.
+        /// </summary>
+        /// <value>
+        /// The academic years to process.
+        /// </value>
+        public int[] AcademicYearsToProcess { get; set; }
+    }
+}
