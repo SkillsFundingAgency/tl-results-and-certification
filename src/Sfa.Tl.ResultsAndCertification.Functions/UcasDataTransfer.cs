835aacaca64c12b3311788c81e3f23ff174e4920
Mustafa JAWAD <Mustafa.JAWAD@EDUCATION.GOV.UK>
2023-07-26 08:39:04 +0100

    TLRC-9765 Reverted back UCas Transfer Results back to its original

--- src/Sfa.Tl.ResultsAndCertification.Functions/UcasDataTransfer.cs
+++ src/Sfa.Tl.ResultsAndCertification.Functions/UcasDataTransfer.cs
@@ -76,9 +76,7 @@ namespace Sfa.Tl.ResultsAndCertification.Functions
 
 
             //Check if it is the second Thursday in August and run the function if it is true
-            if (_commonService.CurrentDate.Date == new DateTime(2023, 07, 25)
-                || _commonService.CurrentDate.Date == new DateTime(2023, 07, 26))
-
+            if (_commonService.CurrentDate.IsNthWeekdayOfMonth(DayOfWeek.Thursday, Months.August, 2))
             {
                 var functionLogDetails = CommonHelper.CreateFunctionLogRequest(context.FunctionName, FunctionType.UcasTransferResults);
 
