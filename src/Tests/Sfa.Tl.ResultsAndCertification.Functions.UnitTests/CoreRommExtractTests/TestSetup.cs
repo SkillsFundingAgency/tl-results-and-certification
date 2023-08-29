72a779cf26f0fafa0e5a5a65dcf35212d0d44450
Ibai Gonzalez <Ibai.GONZALEZ@EDUCATION.GOV.UK>
2023-08-29 10:45:08 +0100

    Add unit tests

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/CoreRommExtractTests/TestSetup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Timers;
+using Microsoft.Extensions.Logging.Abstractions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Application.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Common.Utils.Ranges;
+using Sfa.Tl.ResultsAndCertification.Functions.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Models.Configuration;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using System;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Functions.UnitTests.CoreRommExtractTests
+{
+    public abstract class TestSetup : BaseTest<CoreRommExtract>
+    {
+        private readonly TimerSchedule _timerSchedule = Substitute.For<TimerSchedule>();
+        protected CoreRommExtract CoreRommExtract;
+
+        protected ICoreRommExtractService CoreRommExtractService = Substitute.For<ICoreRommExtractService>();
+        protected ICommonService CommonService = Substitute.For<ICommonService>();
+
+        protected int[] AssesmentSeriesYearsToProcess = new[] { 2023 };
+
+        public override void Setup()
+        {
+            DateTime today = DateTime.UtcNow.Date;
+
+            var config = new ResultsAndCertificationConfiguration
+            {
+                CoreRommExtractSettings = new CoreRommExtractSettings
+                {
+                    AssesmentSeriesYearsToProcess = AssesmentSeriesYearsToProcess,
+                    CoreRommValidDateRanges = new[]
+                    {
+                        new DateTimeRange
+                        {
+                            From = today.AddDays(-1),
+                            To = today.AddDays(1)
+                        }
+                    }
+                }
+            };
+
+            CoreRommExtract = new CoreRommExtract(config, CoreRommExtractService, CommonService);
+        }
+
+        public override Task When()
+        {
+            return CoreRommExtract.CoreRommExtractAsync(new TimerInfo(_timerSchedule, new ScheduleStatus()), new ExecutionContext(), new NullLogger<AnalystOverallResultExtraction>());
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/CoreRommExtractTests/When_Service_Exception.cs
@@ -0,0 +1,27 @@
+﻿using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Functions.UnitTests.CoreRommExtractTests
+{
+    public class When_Service_Exception : TestSetup
+    {
+        public override void Given()
+        {
+            CommonService.CreateFunctionLog(Arg.Any<FunctionLogDetails>()).Returns(true);
+            CoreRommExtractService.ProcessCoreRommExtractAsync(AssesmentSeriesYearsToProcess).Returns(x => Task.FromException(new Exception()));
+            CommonService.UpdateFunctionLog(Arg.Any<FunctionLogDetails>()).Returns(true);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Are_Called()
+        {
+            CommonService.Received(2).CreateFunctionLog(Arg.Any<FunctionLogDetails>());
+            CoreRommExtractService.Received(1).ProcessCoreRommExtractAsync(AssesmentSeriesYearsToProcess);
+            CommonService.DidNotReceive().UpdateFunctionLog(Arg.Any<FunctionLogDetails>());
+            CommonService.Received(1).SendFunctionJobFailedNotification(Arg.Any<string>(), Arg.Any<string>());
+        }
+    }
+}
