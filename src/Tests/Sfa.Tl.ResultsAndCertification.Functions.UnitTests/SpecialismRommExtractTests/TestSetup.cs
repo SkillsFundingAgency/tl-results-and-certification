8d08f2863890be5441fa17fa7dcc3bdb313c36a6
Sajid Shafique Malik <sajid.malik@education.gov.uk>
2023-09-05 13:00:49 +0100

    Added unit tests

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/SpecialismRommExtractTests/TestSetup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Timers;
+using Microsoft.Extensions.Logging.Abstractions;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Application.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Common.Utils.Ranges;
+using Sfa.Tl.ResultsAndCertification.Functions.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Models.Configuration;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using System;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Functions.UnitTests.SpecialismRommExtractTests
+{
+    public abstract class TestSetup : BaseTest<SpecialismRommExtract>
+    {
+        private readonly TimerSchedule _timerSchedule = Substitute.For<TimerSchedule>();
+        protected SpecialismRommExtract SpecialismRommExtract;
+
+        protected ISpecialismRommExtractionService SpecialismRommExtractService = Substitute.For<ISpecialismRommExtractionService>();
+        protected ICommonService CommonService = Substitute.For<ICommonService>();
+
+        protected int[] AssessmentSeriesYearsToProcess = new[] { 2023 };
+
+        public override void Setup()
+        {
+            DateTime today = DateTime.UtcNow.Date;
+
+            var config = new ResultsAndCertificationConfiguration
+            {
+                SpecialismRommExtractSetting = new SpecialismRommExtractSetting
+                {
+                    AssesmentSeriesYearsToProcess = AssessmentSeriesYearsToProcess,
+                    SpecialismRommValidDateRanges = new[]
+                    {
+                        new DateTimeRange
+                        {
+                            From = today.AddDays(-1),
+                            To = today.AddDays(1)
+                        }
+                    }
+                }
+            };
+
+            SpecialismRommExtract = new SpecialismRommExtract(config, SpecialismRommExtractService, CommonService);
+        }
+
+        public override Task When()
+        {
+            return SpecialismRommExtract.SpecialismRommExtractAsync(new TimerInfo(_timerSchedule, new ScheduleStatus()), new ExecutionContext(), new NullLogger<AnalystOverallResultExtraction>());
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/SpecialismRommExtractTests/When_Today_Is_InvalidDate.cs
@@ -0,0 +1,47 @@
+﻿using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Utils.Ranges;
+using Sfa.Tl.ResultsAndCertification.Models.Configuration;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts;
+using System;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Functions.UnitTests.SpecialismRommExtractTests
+{
+    public class When_Today_Is_InvalidDate : TestSetup
+    {
+        public override void Setup() 
+        {
+            DateTime today = DateTime.UtcNow.Date;
+
+            var config = new ResultsAndCertificationConfiguration
+            {
+                SpecialismRommExtractSetting = new SpecialismRommExtractSetting
+                {
+                    AssesmentSeriesYearsToProcess = AssessmentSeriesYearsToProcess,
+                    SpecialismRommValidDateRanges = new[]
+                    {
+                        new DateTimeRange
+                        {
+                            From = today.AddDays(1),
+                            To = today.AddDays(10)
+                        }
+                    }
+                }
+            };
+
+            SpecialismRommExtract = new SpecialismRommExtract(config, SpecialismRommExtractService, CommonService);
+        }
+
+        public override void Given()
+        {
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_Are_Called()
+        {
+            CommonService.Received(0).CreateFunctionLog(Arg.Any<FunctionLogDetails>());
+            SpecialismRommExtractService.Received(0).ProcessSpecialismRommExtractsAsync(AssessmentSeriesYearsToProcess);
+            CommonService.Received(0).UpdateFunctionLog(Arg.Any<FunctionLogDetails>());
+        }
+    }
+}
