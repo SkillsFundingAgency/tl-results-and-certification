3eea079169d403d54924cd9f7e57fdc3f1abd719
Santhosh <111443660+Skumarhpa@users.noreply.github.com>
2023-10-27 10:44:44 +0100

    changed the content in the footer (#505)

--- src/Sfa.Tl.ResultsAndCertification.Web/Content/Layout/Footer.Designer.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Content/Layout/Footer.Designer.cs
@@ -19,7 +19,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Content.Layout {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Footer {
@@ -151,7 +151,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Content.Layout {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to opens in a new window or tab.
+        ///   Looks up a localized string similar to opens in a new tab.
         /// </summary>
         public static string Opens_In_New_Window_Or_Tab_Text {
             get {
