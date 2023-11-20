c612f01c0125054c6a53835f8aedfcba28646e63
Ibai Gonzalez <Ibai.GONZALEZ@EDUCATION.GOV.UK>
2023-11-20 16:19:36 +0000

    Remove unused namespaces

--- src/Sfa.Tl.ResultsAndCertification.Api.Client/Clients/ResultsAndCertificationInternalApiClient.cs
+++ src/Sfa.Tl.ResultsAndCertification.Api.Client/Clients/ResultsAndCertificationInternalApiClient.cs
@@ -17,7 +17,6 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
