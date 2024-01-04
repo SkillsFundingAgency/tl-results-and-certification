5fa05631928187b0a7b191d8d0e608ba81262a1f
ibai-gonzalez <131773078+ibai-gonzalez@users.noreply.github.com>
2024-01-04 17:15:58 +0000

    Fix (#566)

--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Utilities/CustomValidations/DateValidatorAttributeTests/When_Validation_Date_Greater_Than_Today.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Utilities/CustomValidations/DateValidatorAttributeTests/When_Validation_Date_Greater_Than_Today.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Xunit;
+using ErrorResource = Sfa.Tl.ResultsAndCertification.Web.Content.AdminDashboard;
 
 namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Utilities.CustomValidations.DateValidatorAttributeTests
 {
@@ -22,8 +23,10 @@ namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Utilities.CustomValidatio
         [Fact]
         public void Then_Returns_ExpectedResults()
         {
-            overAllResult.Should().BeTrue();
-            validationResults.Should().BeEmpty();
+            overAllResult.Should().BeFalse();
+            validationResults.Should().NotBeNull();
+            validationResults.Should().HaveCount(1);
+            validationResults[0].ErrorMessage.Should().Be(string.Format(ErrorResource.ReviewChangeStartYear.Validation_Date_When_Change_Requested_Future_Date_Text, model.RequiredDate));
         }
     }
 }
