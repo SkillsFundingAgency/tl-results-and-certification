db19a8faf7d5af348df2b9acfe7a065bb79ec48f
skumarhpa <skumar.hpa@gmail.com>
2024-01-12 16:27:24 +0000

    Latest Merge

--- src/Sfa.Tl.ResultsAndCertification.Web/Controllers/AdminDashboardController.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Controllers/AdminDashboardController.cs
@@ -243,5 +243,30 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Controllers
             }
             else { return RedirectToRoute(RouteConstants.ProblemWithService); }
         }
+
+        [HttpGet]
+        [Route("admin/change-industry-placement/{pathwayId}", Name = RouteConstants.AdminChangeIndustryPlacement)]
+        public async Task<IActionResult> ChangeIndustryPlacementAsync(int pathwayId)
+        {
+            var viewModel = await _loader.GetAdminLearnerRecordAsync<AdminChangeIndustryPlacementViewModel>(pathwayId);
+
+            if (viewModel == null)
+                return RedirectToRoute(RouteConstants.PageNotFound);
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route("admin/submit-change-industry-placement", Name = RouteConstants.AdminSubmitChangeIndustryPlacement)]
+        public async Task<IActionResult> ChangeIndustryPlacementAsync(AdminChangeIndustryPlacementViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return RedirectToAction(nameof(RouteConstants.AdminLearnerRecord), new { pathwayId = model.PathwayId });
+        }
+
+
     }
 }
