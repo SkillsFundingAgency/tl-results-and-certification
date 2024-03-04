ffd994672f14954650803e54f2ef95f94964dab7
Santhosh <111443660+Skumarhpa@users.noreply.github.com>
2024-03-04 09:29:24 +0000

    Implement the change assessment Result (#638)

---
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/AdminDashboard/Result/AdminChangePathwayResultViewModel.cs
@@ -0,0 +1,89 @@
+﻿using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.Content.AdminDashboard;
+using Sfa.Tl.ResultsAndCertification.Web.ViewComponents.BackLink;
+using Sfa.Tl.ResultsAndCertification.Web.ViewComponents.Summary.SummaryItem;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Common;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.AdminDashboard.Result
+{
+    public class AdminChangePathwayResultViewModel
+    {
+        public int RegistrationPathwayId { get; set; }
+
+        public int PathwayAssessmentId { get; set; }
+
+        public string PathwayName { get; set; }
+
+        #region Personal details
+
+        public string Learner { get; set; }
+
+        public long Uln { get; set; }
+
+        public string Provider { get; set; }
+
+        public string Tlevel { get; set; }
+
+        public string StartYear { get; set; }
+
+        public bool IsRemoveResult { get; set; }
+
+        public SummaryItemModel SummaryLearner
+            => CreateSummaryItemModel(AdminChangePathwayResult.Summary_Learner_Id, AdminChangePathwayResult.Summary_Learner_Text, Learner);
+
+        public SummaryItemModel SummaryUln
+            => CreateSummaryItemModel(AdminChangePathwayResult.Summary_ULN_Id, AdminChangePathwayResult.Summary_ULN_Text, Uln.ToString());
+
+        public SummaryItemModel SummaryProvider
+            => CreateSummaryItemModel(AdminChangePathwayResult.Summary_Provider_Id, AdminChangePathwayResult.Summary_Provider_Text, Provider);
+
+        public SummaryItemModel SummaryTlevel
+            => CreateSummaryItemModel(AdminChangePathwayResult.Summary_TLevel_Id, AdminChangePathwayResult.Summary_TLevel_Text, Tlevel);
+
+        public SummaryItemModel SummaryStartYear
+            => CreateSummaryItemModel(AdminChangePathwayResult.Summary_StartYear_Id, AdminChangePathwayResult.Summary_StartYear_Text, StartYear);
+
+        #endregion
+
+        #region Assessment
+
+        public string ExamPeriod { get; set; }
+
+        public string Grade { get; set; }
+
+        public SummaryItemModel SummaryExamPeriod
+           => CreateSummaryItemModel(AdminChangePathwayResult.Summary_Exam_Period_Id, AdminChangePathwayResult.Summary_Exam_Period_Text, ExamPeriod);
+
+        public SummaryItemModel SummaryGrade
+           => CreateSummaryItemModel(
+               AdminChangePathwayResult.Summary_Grade_Id,
+               AdminChangePathwayResult.Summary_Grade_Text,
+               string.IsNullOrWhiteSpace(Grade) ? AdminChangePathwayResult.No_Grade_Entered : Grade);
+
+        #endregion
+
+        [Required(ErrorMessageResourceType = typeof(AdminChangePathwayResult), ErrorMessageResourceName = "Validation_Message")]
+        public int? SelectedGradeId { get; set; }
+
+        public string SelectedGradeValue
+            => Grades?.FirstOrDefault(g => g.Id == SelectedGradeId)?.Value;
+
+        public List<LookupViewModel> Grades { get; set; }
+
+        public BackLinkModel BackLink => new()
+        {
+            RouteName = RouteConstants.AdminLearnerRecord,
+            RouteAttributes = new Dictionary<string, string> { { Constants.PathwayId, RegistrationPathwayId.ToString() } }
+        };
+
+        private static SummaryItemModel CreateSummaryItemModel(string id, string title, string value)
+            => new()
+            {
+                Id = id,
+                Title = title,
+                Value = value
+            };
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/AdminDashboardControllerTests/AdminChangeSpecialismResultPost/When_Grade_Selected.cs
@@ -0,0 +1,37 @@
+﻿using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.UnitTests.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.AdminDashboard.Result;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.AdminDashboardControllerTests.AdminChangeSpecialismResultPost
+{
+    public class When_Grade_Selected : TestSetup
+    {
+        private AdminChangeSpecialismResultViewModel _viewModel;
+
+        public override void Given()
+        {
+            _viewModel = CreateViewModel(selectedGradeId: 1);
+        }
+
+        public async override Task When()
+        {
+            Result = await Controller.AdminChangeSpecialismResultAsync(_viewModel);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_AreCalled()
+        {
+            AdminDashboardLoader.Received(1).LoadAdminChangeSpecialismResultGrades(_viewModel);
+            CacheService.Received(1).SetAsync(CacheKey, _viewModel);
+        }
+
+        [Fact]
+        public void Then_Redirected_To_ReviewChangeSpecialismResult()
+        {
+            Result.ShouldBeRedirectToActionResult(RouteConstants.AdminLearnerRecord);
+        }
+    }
+}
