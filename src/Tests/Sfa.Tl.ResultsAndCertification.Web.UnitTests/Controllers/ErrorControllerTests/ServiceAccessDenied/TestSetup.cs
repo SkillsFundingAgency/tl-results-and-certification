cd3b7d80d17a900122b93ebb34c96e594257d16f
Ibai Gonzalez <Ibai.GONZALEZ@EDUCATION.GOV.UK>
2024-03-15 15:50:52 +0000

    Fix and unit tests

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ErrorControllerTests/ServiceAccessDenied/TestSetup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Models.Configuration;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+using Sfa.Tl.ResultsAndCertification.Web.Controllers;
+using System;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ErrorControllerTests
+{
+    public abstract class TestSetup : BaseTest<ErrorController>
+    {
+        protected ErrorController Controller;
+        protected IActionResult Result;
+        protected ILogger<ErrorController> Logger;
+        protected HttpContext HttpContext;
+        protected IServiceProvider ServiceProvider;
+
+        private readonly ITempDataProvider _tempDataProvider = Substitute.For<ITempDataProvider>();
+
+        public ResultsAndCertificationConfiguration Configuration { get; set; }
+
+        public override void Setup()
+        {
+            HttpContext = Substitute.For<HttpContext>();
+            ServiceProvider = Substitute.For<IServiceProvider>();
+            ServiceProvider.GetService(typeof(IUrlHelperFactory)).Returns(new UrlHelperFactory());
+            HttpContext.RequestServices = ServiceProvider;
+            Logger = Substitute.For<ILogger<ErrorController>>();
+            Controller = new ErrorController(Configuration, Logger);
+
+            var tempDataDictionary = new TempDataDictionary(HttpContext, _tempDataProvider);
+            Controller.TempData = tempDataDictionary;
+        }
+
+        public override Task When()
+        {
+            Result = Controller.ServiceAccessDenied();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ErrorControllerTests/ServiceAccessDenied/When_User_Not_In_FreezePeriod.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using System.Linq;
+using System.Security.Claims;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ErrorControllerTests.ServiceAccessDenied
+{
+    public class When_User_Not_In_FreezePeriod : TestSetup
+    {
+        public override void Given()
+        {
+            HttpContext.User.Identity.IsAuthenticated.Returns(true);
+            HttpContext.User.Claims.Returns(Enumerable.Empty<Claim>());
+
+            Controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = HttpContext,
+                RouteData = new Microsoft.AspNetCore.Routing.RouteData()
+            };
+        }
+
+        [Fact]
+        public void Then_Redirected_To_ServiceUnavailable()
+        {
+            Result.Should().NotBeNull().And.BeOfType<ViewResult>();
+        }
+    }
+}
