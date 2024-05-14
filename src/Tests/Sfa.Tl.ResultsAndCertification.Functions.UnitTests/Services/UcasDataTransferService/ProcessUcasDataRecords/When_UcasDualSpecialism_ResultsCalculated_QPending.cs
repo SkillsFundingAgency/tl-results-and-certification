57128416d5513c1d40cb7a8ce605815de0b5fead
Ibai Gonzalez <Ibai.GONZALEZ@EDUCATION.GOV.UK>
2024-05-14 11:34:24 +0100

    Update report

--- src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/Services/UcasDataTransferService/ProcessUcasDataRecords/When_UcasDualSpecialism_ResultsCalculated_QPending.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/Services/UcasDataTransferService/ProcessUcasDataRecords/When_UcasDualSpecialism_ResultsCalculated_QPending.cs
@@ -1,107 +1,48 @@
-﻿using Sfa.Tl.ResultsAndCertification.Domain.Models;
-using Sfa.Tl.ResultsAndCertification.Common.Enum;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
 using Sfa.Tl.ResultsAndCertification.Models.Functions;
-using System.Globalization;
-using NSubstitute;
-using Sfa.Tl.ResultsAndCertification.Models.OverallResults;
+using System.Collections.Generic;
+using Xunit;
 
 namespace Sfa.Tl.ResultsAndCertification.Functions.UnitTests.Services.UcasDataTransferService.ProcessUcasDataRecords
 {
     public class When_UcasDualSpecialism_ResultsCalculated_QPending : TestBase
     {
-
-        private OverallResultDetail _mockUcasData;
         private TqRegistrationPathway _mockExpectedUcasData;
-        private OverallResultDetail _details;
-        private List<UcasDataComponent> _dataComponent;
+        private readonly List<UcasDataComponent> _dataComponent = new();
 
         public override void Given()
         {
             UcasDataType = UcasDataType.Results;
-            _mockUcasData = GetoveralldetailsMockData();
             _mockExpectedUcasData = GetExpectedPathwayMockData();
-            _details = UcasRecordResultSegment.ReplaceDualSpecialismCode(_mockUcasData);
-            _dataComponent = new List<UcasDataComponent>();
-
         }
-
-
 
         [Fact]
         public void Then_Expected_DualSpecialsim_Result_Matches()
         {
             UcasRecordResultSegment.AddSpecialismSegment(_dataComponent, _mockExpectedUcasData);
-            Assert.Equal("Q", _dataComponent.FirstOrDefault().Grade);
-        }
-
-        private OverallResultDetail GetoveralldetailsMockData()
-        {
-
-            var detail = new OverallResultDetail()
-            {
-                SpecialismDetails =
-
-                 new List<OverallSpecialismDetail>() {
-
-                     new OverallSpecialismDetail() {
-
-                        SpecialismLarId= "10202101",
-                         SpecialismName="Heating Engineering",
-                         SpecialismResult = "Pass"
-                     },
-
-                      new OverallSpecialismDetail() {
-
-                        SpecialismLarId= "10202105",
-                         SpecialismName="Ventilation",
-                         SpecialismResult = "Pass"
-                   }
-
-                  }
-            };
-
-            return detail;
-
+            Assert.Equal("Q", _dataComponent[0].Grade);
         }
 
         private TqRegistrationPathway GetExpectedPathwayMockData()
         {
-
             string overallDetails = "{\"TlevelTitle\": \"T Level in Building Services Engineering for Construction\",\"PathwayName\": \"Building Services Engineering\", \"PathwayLarId\": \"60369115\"," +
             "\"PathwayResult\": \"A*\",    \"SpecialismDetails\": [{ \"SpecialismName\": \"Heating Engineering\",\"SpecialismLarId\": \"10202101\",\"SpecialismResult\": \"Pass\"}, " +
             "{\"SpecialismName\": \"Ventilation\",\"SpecialismLarId\": \"10202105\",\"SpecialismResult\": \" Q - Pending\"}],\"IndustryPlacementStatus\": \"Completed\",\"OverallResult\": \"Distinction\"}";
-            var pathWayData = new TqRegistrationPathway()
+
+            return new TqRegistrationPathway()
             {
-
                 OverallResults = new List<OverallResult>
-                { new OverallResult()
                 {
-
-
-                    Details = overallDetails,
-                    Id = 1,
-                    IsOptedin = true,
-                     SpecialismResultAwarded ="Q - pending result"
-
-                }
+                    new()
+                    {
+                        Details = overallDetails,
+                        Id = 1,
+                        IsOptedin = true,
+                        SpecialismResultAwarded ="Q - pending result"
+                    }
                 }
             };
-
-            return pathWayData;
         }
     }
-
 }
-
-
-
-
-
-
-
--- src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/Services/UcasDataTransferService/ProcessUcasDataRecords/When_UcasDualSpecialism_ResultsNotAvailable.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Functions.UnitTests/Services/UcasDataTransferService/ProcessUcasDataRecords/When_UcasDualSpecialism_ResultsNotAvailable.cs
@@ -1,106 +1,47 @@
-﻿using Sfa.Tl.ResultsAndCertification.Domain.Models;
-using Sfa.Tl.ResultsAndCertification.Common.Enum;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
 using Sfa.Tl.ResultsAndCertification.Models.Functions;
-using System.Globalization;
-using NSubstitute;
-using Sfa.Tl.ResultsAndCertification.Models.OverallResults;
+using System.Collections.Generic;
+using Xunit;
 
 namespace Sfa.Tl.ResultsAndCertification.Functions.UnitTests.Services.UcasDataTransferService.ProcessUcasDataRecords
 {
     public class When_UcasDualSpecialism_ResultsNotAvailable : TestBase
     {
-
-        private OverallResultDetail _mockUcasData;
         private TqRegistrationPathway _mockExpectedUcasData;
-        private OverallResultDetail _details;
-        private List<UcasDataComponent> _dataComponent;
+        private readonly List<UcasDataComponent> _dataComponent = new();
 
         public override void Given()
         {
             UcasDataType = UcasDataType.Results;
-            _mockUcasData = GetoveralldetailsMockData();
             _mockExpectedUcasData = GetExpectedPathwayMockData();
-            _details = UcasRecordResultSegment.ReplaceDualSpecialismCode(_mockUcasData);
-            _dataComponent = new List<UcasDataComponent>();
-
         }
-
-       
 
         [Fact]
         public void Then_Expected_DualSpecialsim_Result_Matches()
         {
             UcasRecordResultSegment.AddSpecialismSegment(_dataComponent, _mockExpectedUcasData);
-            Assert.Empty(_dataComponent.FirstOrDefault().Grade);
-        }
-
-        private OverallResultDetail GetoveralldetailsMockData()
-        {
-
-            var detail = new OverallResultDetail()
-            {
-                SpecialismDetails =
-
-                 new List<OverallSpecialismDetail>() {
-
-                     new OverallSpecialismDetail() {
-
-                        SpecialismLarId= "10202101",
-                         SpecialismName="Heating Engineering",
-                         SpecialismResult = "Pass"
-                     },
-
-                      new OverallSpecialismDetail() {
-
-                        SpecialismLarId= "10202105",
-                         SpecialismName="Ventilation",
-                         SpecialismResult = "Pass"
-                   }
-
-                  }
-            };
-
-            return detail;
-
+            Assert.Empty(_dataComponent[0].Grade);
         }
 
         private TqRegistrationPathway GetExpectedPathwayMockData()
         {
-
             string overallDetails = "{\"TlevelTitle\": \"T Level in Building Services Engineering for Construction\",\"PathwayName\": \"Building Services Engineering\", \"PathwayLarId\": \"60369115\"," +
             "\"PathwayResult\": \"A*\",    \"SpecialismDetails\": [{ \"SpecialismName\": \"Heating Engineering\",\"SpecialismLarId\": \"10202101\",\"SpecialismResult\": \"\"}, " +
             "{\"SpecialismName\": \"Ventilation\",\"SpecialismLarId\": \"10202105\",\"SpecialismResult\": \"\"}],\"IndustryPlacementStatus\": \"Completed\",\"OverallResult\": \"Distinction\"}";
-            var pathWayData = new TqRegistrationPathway()
+
+            return new TqRegistrationPathway()
             {
-
                 OverallResults = new List<OverallResult>
-                { new OverallResult()
                 {
-
-
-                    Details = overallDetails,
-                    Id = 1,
-                    IsOptedin = true
-
-                }
+                    new()
+                    {
+                        Details = overallDetails,
+                        Id = 1,
+                        IsOptedin = true
+                    }
                 }
             };
-
-            return pathWayData;
         }
     }
-
 }
-
-
-
-
-
-
-
