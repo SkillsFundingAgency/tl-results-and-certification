54703c6dca4d2452f8ecf9e85875e5c65fab5e7f
ibai-gonzalez <131773078+ibai-gonzalez@users.noreply.github.com>
2024-05-17 13:03:31 +0100

    TL2023-381 (#720)
    * Update request
    
    * Add extra fields
    
    * Code refactor
    
    * Remove old template references

--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/AdminDashboardControllerTests/AdminReviewChangeStartYearGet/When_Called_With_Invalid_Data.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/AdminDashboardControllerTests/AdminReviewChangeStartYearGet/When_Called_With_Invalid_Data.cs
@@ -3,28 +3,20 @@ using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using Sfa.Tl.ResultsAndCertification.Common.Helpers;
 using Sfa.Tl.ResultsAndCertification.Web.ViewModel.AdminDashboard;
-using System.Threading.Tasks;
 using Xunit;
+
 namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.AdminDashboardControllerTests.AdminReviewChangeStartYearGet
 {
     public class When_Called_With_Invalid_Data: TestSetup
     {
-        public int PathwayId { get; set; }
         protected AdminChangeStartYearViewModel AdminChangeStartYearViewModel = null;
         protected ReviewChangeStartYearViewModel Mockresult = null;
-        public IActionResult Result { get; private set; }
-
 
         public override void Given()
         {
             PathwayId = 0;
             AdminDashboardLoader.GetAdminLearnerRecordAsync<ReviewChangeStartYearViewModel>(PathwayId).Returns(Mockresult);
             CacheService.GetAsync<AdminChangeStartYearViewModel>(CacheKey).Returns(AdminChangeStartYearViewModel);
-        }
-
-        public async override Task When()
-        {
-            Result = await Controller.ReviewChangeStartYearAsync(PathwayId);
         }
 
         [Fact]
