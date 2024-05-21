18380e2ab36763f898e224d183c3926aeaf62c00
Sajid Shafique Malik <sajid.malik@education.gov.uk>
2024-05-21 12:12:22 +0100

    added feature to download tempalte file

--- src/Sfa.Tl.ResultsAndCertification.Web/Controllers/DocumentController.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/Controllers/DocumentController.cs
@@ -21,7 +21,7 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Controllers
         {
             _documentLoader = documentLoader;
             _logger = logger;
-        }        
+        }
 
         [HttpGet]
         [Route("tlevels-registration-data-format-and-rules", Name = RouteConstants.DownloadRegistrationDataFormatAndRulesGuide)]
@@ -37,6 +37,25 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Controllers
 
             fileStream.Position = 0;
             return new FileStreamResult(fileStream, "text/xlsx")
+            {
+                FileDownloadName = fileName
+            };
+        }
+
+        [HttpGet]
+        [Route("tlevels-registration-data-template", Name = RouteConstants.DownloadRegistrationDataTemplate)]
+        public async Task<IActionResult> DownloadRegistrationDataTemplateAsync()
+        {
+            var fileName = DocumentResource.TlevelDataFormatAndRulesGuide.Tlevels_Registration_Data_Template_File_Name;
+            var fileStream = await _documentLoader.GetBulkUploadRegistrationsTechSpecFileAsync(fileName);
+            if (fileStream == null)
+            {
+                _logger.LogWarning(LogEvent.FileStreamNotFound, $"No FileStream found to download bulk upload registration tech spec document. Method: GetBulkUploadRegistrationsTechSpecFileAsync(FileName: {fileName})");
+                return RedirectToRoute(RouteConstants.PageNotFound);
+            }
+
+            fileStream.Position = 0;
+            return new FileStreamResult(fileStream, "text/csv")
             {
                 FileDownloadName = fileName
             };
@@ -100,21 +119,21 @@ namespace Sfa.Tl.ResultsAndCertification.Web.Controllers
             var viewModel = new TlevelDataFormatAndRulesGuideViewModel
             {
                 FileType = FileType.Xlsx.ToString().ToUpperInvariant(),
-                
+
                 RegistrationsFileSize = DocumentResource.TlevelDataFormatAndRulesGuide.Registrations_FileSize_Text,
                 RegistrationsVersion = DocumentResource.TlevelDataFormatAndRulesGuide.Registrations_Version_Text,
                 RegistrationsPublishedDate = $"{DocumentResource.TlevelDataFormatAndRulesGuide.Published_Text} {DocumentResource.TlevelDataFormatAndRulesGuide.Registrations_PublishedDate_Text}",
-                
+
                 AssessmentEntriesFileSize = DocumentResource.TlevelDataFormatAndRulesGuide.Assessment_Entries_FileSize_Text,
                 AssessmentEntriesVersion = DocumentResource.TlevelDataFormatAndRulesGuide.Assessment_Entries_Version_Text,
                 AssessmentEntriesPublishedDate = $"{DocumentResource.TlevelDataFormatAndRulesGuide.Published_Text} {DocumentResource.TlevelDataFormatAndRulesGuide.Assessment_Entries_PublishedDate_Text}",
 
                 ResultsFileSize = DocumentResource.TlevelDataFormatAndRulesGuide.Results_FileSize_Text,
                 ResultsVersion = DocumentResource.TlevelDataFormatAndRulesGuide.Results_Version_Text,
-                ResultsPublishedDate = $"{DocumentResource.TlevelDataFormatAndRulesGuide.Published_Text} {DocumentResource.TlevelDataFormatAndRulesGuide.Results_PublishedDate_Text}"               
+                ResultsPublishedDate = $"{DocumentResource.TlevelDataFormatAndRulesGuide.Published_Text} {DocumentResource.TlevelDataFormatAndRulesGuide.Results_PublishedDate_Text}"
             };
 
             return View(viewModel);
-        }        
+        }
     }
 }
