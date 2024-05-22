747049deef98b9cc80589c8f9e6d20b15d569e1a
Ibai Gonzalez <Ibai.GONZALEZ@EDUCATION.GOV.UK>
2024-05-22 16:33:23 +0100

    Wrap fields with quotes

--- src/Sfa.Tl.ResultsAndCertification.Models/CertificateTrackingExtraction/CertificateTrackingExtractionDataExportMap.cs
+++ src/Sfa.Tl.ResultsAndCertification.Models/CertificateTrackingExtraction/CertificateTrackingExtractionDataExportMap.cs
@@ -8,6 +8,14 @@ namespace Sfa.Tl.ResultsAndCertification.Models.CertificateTrackingExtraction
         public CertificateTrackingExtractionDataExportMap()
         {
             AutoMap(CultureInfo.InvariantCulture);
+
+            Map(c => c.FirstName).Convert(c => WrapWithQuotes(c.Value.FirstName));
+            Map(c => c.LastName).Convert(c => WrapWithQuotes(c.Value.LastName));
+            Map(c => c.TLevelTitle).Convert(c => WrapWithQuotes(c.Value.TLevelTitle));
+            Map(c => c.SignedForBy).Convert(c => WrapWithQuotes(c.Value.SignedForBy));
         }
+
+        private static string WrapWithQuotes(string value)
+            => $"\"{value}\"";
     }
 }
