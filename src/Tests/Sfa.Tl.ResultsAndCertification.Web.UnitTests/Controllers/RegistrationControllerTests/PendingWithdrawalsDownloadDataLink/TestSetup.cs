3a7f50bcee4dfb2083732e2a0fbbeaf41eaa9692
ibai-gonzalez <131773078+ibai-gonzalez@users.noreply.github.com>
2024-06-04 12:47:50 +0100

    TL2023-501 pending withdrawals csv download (#738)
    * Update description
    
    * Add paragraphs and update download link text
    
    * Download pending withdrawals file
    
    * Add missing test
    
    * Text change

--- src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/RegistrationControllerTests/PendingWithdrawalsDownloadDataLink/TestSetup.cs
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/RegistrationControllerTests/PendingWithdrawalsDownloadDataLink/TestSetup.cs
@@ -2,55 +2,48 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
-using Sfa.Tl.ResultsAndCertification.Common.Constants;
 using Sfa.Tl.ResultsAndCertification.Common.Extensions;
-using Sfa.Tl.ResultsAndCertification.Common.Helpers;
 using Sfa.Tl.ResultsAndCertification.Common.Services.Cache;
 using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
 using Sfa.Tl.ResultsAndCertification.Tests.Common.Helpers;
 using Sfa.Tl.ResultsAndCertification.Web.Controllers;
 using Sfa.Tl.ResultsAndCertification.Web.Loader.Interfaces;
-using System;
-using System.Security.Claims;
 using System.Threading.Tasks;
 
-namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.RegistrationControllerTests.SubmitRegistrationsGeneratingDownload
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.RegistrationControllerTests.PendingWithdrawalsDownloadDataLink
 {
     public abstract class TestSetup : BaseTest<RegistrationController>
     {
-        protected const int AoUkprn = 1234567890;
-        protected const string UserEmail = "user@email.com";
+        protected string Id;
 
-        protected string CacheKey;
+        protected const long Ukprn = 123456789;
         protected IRegistrationLoader RegistrationLoader;
-        protected ICacheService CacheService;
-        private RegistrationController _controller;
 
         protected IActionResult Result { get; private set; }
+
+        private RegistrationController _controller;
 
         public override void Setup()
         {
             RegistrationLoader = Substitute.For<IRegistrationLoader>();
-            CacheService = Substitute.For<ICacheService>();
 
-            _controller = new RegistrationController(RegistrationLoader, CacheService, Substitute.For<ILogger<RegistrationController>>());
+            _controller = new RegistrationController(
+                RegistrationLoader,
+                Substitute.For<ICacheService>(),
+                Substitute.For<ILogger<RegistrationController>>());
 
             var httpContext = new ClaimsIdentityBuilder<RegistrationController>(_controller)
-               .Add(CustomClaimTypes.Ukprn, AoUkprn.ToString())
-               .Add(CustomClaimTypes.UserId, Guid.NewGuid().ToString())
-               .Add(ClaimTypes.Email, UserEmail)
+               .Add(CustomClaimTypes.Ukprn, Ukprn.ToString())
                .Build()
                .HttpContext;
 
             IHttpContextAccessor httpContextAccessor = Substitute.For<IHttpContextAccessor>();
             httpContextAccessor.HttpContext.Returns(httpContext);
-
-            CacheKey = CacheKeyHelper.GetCacheKey(httpContext.User.GetUserId(), CacheConstants.RegistrationCacheKey);
         }
 
         public async override Task When()
         {
-            Result = await _controller.SubmitRegistrationsGeneratingDownloadAsync();
+            Result = await _controller.PendingWithdrawalsDownloadDataLinkAsync(Id);
         }
     }
 }
