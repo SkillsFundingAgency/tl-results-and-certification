f9fe802803ed1f290e4b19d04e246d0e0f8fad77
Ibai Gonzalez <Ibai.GONZALEZ@EDUCATION.GOV.UK>
2024-06-27 13:44:59 +0100

    Empty text to NULL

---
+++ src/Sfa.Tl.ResultsAndCertification.Application/Mappers/Action/AnalystOverallResultExtractionEmptyToNullTextAction.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Models.AnalystOverallResultExtraction;
+
+namespace Sfa.Tl.ResultsAndCertification.Application.Mappers.Action
+{
+    internal class AnalystOverallResultExtractionEmptyToNullTextAction : IMappingAction<TqRegistrationPathway, AnalystOverallResultExtractionData>
+    {
+        private const string Null = "NULL";
+
+        public void Process(TqRegistrationPathway source, AnalystOverallResultExtractionData destination, ResolutionContext context)
+        {
+            destination.Status = ToNullTextIfNullOrEmpty(destination.Status);
+            destination.ProviderName = ToNullTextIfNullOrEmpty(destination.ProviderName);
+            destination.LastName = ToNullTextIfNullOrEmpty(destination.LastName);
+            destination.FirstName = ToNullTextIfNullOrEmpty(destination.FirstName);
+            destination.Gender = ToNullTextIfNullOrEmpty(destination.Gender);
+            destination.TlevelTitle = ToNullTextIfNullOrEmpty(destination.TlevelTitle);
+            destination.StartYear = ToNullTextIfNullOrEmpty(destination.StartYear);
+            destination.CoreComponent = ToNullTextIfNullOrEmpty(destination.CoreComponent);
+            destination.CoreCode = ToNullTextIfNullOrEmpty(destination.CoreCode);
+            destination.CoreResult = ToNullTextIfNullOrEmpty(destination.CoreResult);
+            destination.OccupationalSpecialism = ToNullTextIfNullOrEmpty(destination.OccupationalSpecialism);
+            destination.SpecialismCode = ToNullTextIfNullOrEmpty(destination.SpecialismCode);
+            destination.SpecialismResult = ToNullTextIfNullOrEmpty(destination.SpecialismResult);
+            destination.IndustryPlacementStatus = ToNullTextIfNullOrEmpty(destination.IndustryPlacementStatus);
+            destination.OverallResult = ToNullTextIfNullOrEmpty(destination.OverallResult);
+        }
+
+        private static string ToNullTextIfNullOrEmpty(string value)
+            => string.IsNullOrWhiteSpace(value) ? Null : value;
+    }
+}
