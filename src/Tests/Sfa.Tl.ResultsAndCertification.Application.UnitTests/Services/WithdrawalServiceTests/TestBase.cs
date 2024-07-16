dd8172e9353b60e1d825ec790814995a95d0c858
Sajid Shafique Malik <sajid.malik@education.gov.uk>
2024-07-16 11:50:03 +0100

    merge Release21.6 into develop (#777)
    * Show UK time (#774)
    
    * Add DBO check (#775)
    
    * Update seed post deployment scripts (#776)

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Application.UnitTests/Services/WithdrawalServiceTests/TestBase.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Application.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Application.Mappers;
+using Sfa.Tl.ResultsAndCertification.Application.Services;
+using Sfa.Tl.ResultsAndCertification.Data.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+
+namespace Sfa.Tl.ResultsAndCertification.Application.UnitTests.Services.WithdrawalServiceTests
+{
+    public abstract class TestBase : BaseTest<WithdrawalService>
+    {
+        protected IProviderRepository ProviderRepository;
+        protected IRegistrationRepository RegistrationRepository;
+        protected IRepository<TqRegistrationPathway> TqRegistrationPathwayRepository;
+        protected ICommonService CommonService;
+
+        protected WithdrawalService WithdrawalService;
+
+        public override void Setup()
+        {
+            ProviderRepository = Substitute.For<IProviderRepository>();
+            RegistrationRepository = Substitute.For<IRegistrationRepository>();
+            TqRegistrationPathwayRepository = Substitute.For<IRepository<TqRegistrationPathway>>();
+            CommonService = Substitute.For<ICommonService>();
+
+            IMapper mapper = CreateMapper();
+            ILogger<WithdrawalService> logger = Substitute.For<ILogger<WithdrawalService>>();
+
+            WithdrawalService = new WithdrawalService(ProviderRepository, RegistrationRepository, TqRegistrationPathwayRepository, CommonService, mapper, logger);
+        }
+
+        private static AutoMapper.Mapper CreateMapper()
+        {
+            var mapperConfig = new MapperConfiguration(c => c.AddMaps(typeof(RegistrationMapper).Assembly));
+            return new AutoMapper.Mapper(mapperConfig);
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Application.UnitTests/Services/WithdrawalServiceTests/ValidateWithdrawalLearners/When_Core_Result_BeingAppealed.cs
@@ -0,0 +1,39 @@
+﻿using Sfa.Tl.ResultsAndCertification.Common.Constants;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Models.Registration;
+using Sfa.Tl.ResultsAndCertification.Models.Registration.BulkProcess;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Application.UnitTests.Services.WithdrawalServiceTests.ValidateWithdrawalLearners
+{
+    public class When_Core_Result_BeingAppealed : WithdrawalServiceBaseTest
+    {
+        private const long AoUkprn = 1;
+        private const long Uln = 1234567890;
+        private const int TqRegistrationProfileId = 125;
+
+        private readonly DateTime _dob = new(2020, 1, 1);
+
+        private IList<WithdrawalRecordResponse> _actualResult;
+
+        public override void Given()
+        {
+            TqRegistrationProfile profile = CreateTqRegistrationProfile(TqRegistrationProfileId, Uln, _dob, pathwayResultStatus: PrsStatus.BeingAppealed);
+            ConfigureRegistrationRepository(Uln, profile);
+        }
+
+        public override async Task When()
+        {
+            WithdrawalCsvRecordResponse record = CreateWithdrawalCsvRecordResponse(Uln, _dob);
+            _actualResult = await When(AoUkprn, record);
+        }
+
+        [Fact]
+        public void Then_Returns_Expected_Results()
+            => Then_Returns_Expected_ValidationErrors_Results(_actualResult, rowNum: 2, Uln, ValidationMessages.InvalidResultState);
+    }
+}
