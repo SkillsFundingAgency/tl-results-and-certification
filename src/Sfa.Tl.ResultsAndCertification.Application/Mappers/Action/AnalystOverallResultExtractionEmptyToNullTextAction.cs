2278db01d46d6f15713f1cee62840e33b302f544
Ibai Gonzalez <Ibai.GONZALEZ@EDUCATION.GOV.UK>
2024-07-30 09:57:49 +0100

    Update query

--- src/Sfa.Tl.ResultsAndCertification.Application/Mappers/Action/AnalystOverallResultExtractionEmptyToNullTextAction.cs
+++ src/Sfa.Tl.ResultsAndCertification.Application/Mappers/Action/AnalystOverallResultExtractionEmptyToNullTextAction.cs
@@ -6,28 +6,33 @@ namespace Sfa.Tl.ResultsAndCertification.Application.Mappers.Action
 {
     internal class AnalystOverallResultExtractionEmptyToNullTextAction : IMappingAction<TqRegistrationPathway, AnalystOverallResultExtractionData>
     {
+        private const char QuotationMark = '"', Space = ' ';
         private const string Null = "NULL";
 
         public void Process(TqRegistrationPathway source, AnalystOverallResultExtractionData destination, ResolutionContext context)
         {
-            destination.Status = ToNullTextIfNullOrEmpty(destination.Status);
-            destination.ProviderName = ToNullTextIfNullOrEmpty(destination.ProviderName);
-            destination.LastName = ToNullTextIfNullOrEmpty(destination.LastName);
-            destination.FirstName = ToNullTextIfNullOrEmpty(destination.FirstName);
-            destination.Gender = ToNullTextIfNullOrEmpty(destination.Gender);
-            destination.TlevelTitle = ToNullTextIfNullOrEmpty(destination.TlevelTitle);
-            destination.StartYear = ToNullTextIfNullOrEmpty(destination.StartYear);
-            destination.CoreComponent = ToNullTextIfNullOrEmpty(destination.CoreComponent);
-            destination.CoreCode = ToNullTextIfNullOrEmpty(destination.CoreCode);
-            destination.CoreResult = ToNullTextIfNullOrEmpty(destination.CoreResult);
-            destination.OccupationalSpecialism = ToNullTextIfNullOrEmpty(destination.OccupationalSpecialism);
-            destination.SpecialismCode = ToNullTextIfNullOrEmpty(destination.SpecialismCode);
-            destination.SpecialismResult = ToNullTextIfNullOrEmpty(destination.SpecialismResult);
-            destination.IndustryPlacementStatus = ToNullTextIfNullOrEmpty(destination.IndustryPlacementStatus);
-            destination.OverallResult = ToNullTextIfNullOrEmpty(destination.OverallResult);
+            destination.Status = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.Status);
+            destination.ProviderName = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.ProviderName);
+            destination.LastName = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.LastName);
+            destination.FirstName = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.FirstName);
+            destination.Gender = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.Gender);
+            destination.TlevelTitle = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.TlevelTitle);
+            destination.StartYear = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.StartYear);
+            destination.CoreComponent = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.CoreComponent);
+            destination.CoreCode = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.CoreCode);
+            destination.CoreResult = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.CoreResult);
+            destination.OccupationalSpecialism = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.OccupationalSpecialism);
+            destination.SpecialismCode = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.SpecialismCode);
+            destination.SpecialismResult = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.SpecialismResult);
+            destination.IndustryPlacementStatus = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.IndustryPlacementStatus);
+            destination.OverallResult = ToNullTextIfNullOrEmptyOrOnlyQuotes(destination.OverallResult);
         }
 
-        private static string ToNullTextIfNullOrEmpty(string value)
-            => string.IsNullOrWhiteSpace(value) ? Null : value;
+        private static string ToNullTextIfNullOrEmptyOrOnlyQuotes(string value)
+            => string.IsNullOrWhiteSpace(value)
+                ? Null
+                : string.IsNullOrEmpty(value.Trim(QuotationMark, Space))
+                    ? Null
+                    : value;
     }
 }
