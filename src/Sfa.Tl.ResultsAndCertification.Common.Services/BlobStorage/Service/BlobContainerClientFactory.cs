f1edda3d27bbd64c26a8f98c271365393fea8e16
Ibai Gonzalez <Ibai.GONZALEZ@EDUCATION.GOV.UK>
2024-10-01 09:16:40 +0100

    Code refactor

---
+++ src/Sfa.Tl.ResultsAndCertification.Common.Services/BlobStorage/Service/BlobContainerClientFactory.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Identity;
+using Azure.Storage.Blobs;
+using Sfa.Tl.ResultsAndCertification.Common.Services.BlobStorage.Interface;
+using Sfa.Tl.ResultsAndCertification.Models.Configuration;
+using System;
+
+namespace Sfa.Tl.ResultsAndCertification.Common.Services.BlobStorage.Service
+{
+    public class BlobContainerClientFactory : IBlobContainerClientFactory
+    {
+        private readonly string _blobContainerUriTemplate;
+
+        public BlobContainerClientFactory(ResultsAndCertificationConfiguration config)
+        {
+            _blobContainerUriTemplate = config.BlobContainerUriTemplate;
+        }
+
+        public BlobContainerClient Create(string containerName)
+        {
+            var blobContainerUri = new Uri(string.Format(_blobContainerUriTemplate, containerName.ToLowerInvariant()));
+
+            var blobContainerClient = new BlobContainerClient(blobContainerUri, new DefaultAzureCredential());
+            blobContainerClient.CreateIfNotExists();
+
+            return blobContainerClient;
+        }
+    }
+}
--- src/Sfa.Tl.ResultsAndCertification.Common.Services/BlobStorage/Service/BlobStorageService.cs
+++ src/Sfa.Tl.ResultsAndCertification.Common.Services/BlobStorage/Service/BlobStorageService.cs
@@ -3,34 +3,31 @@ using Azure.Storage.Blobs.Models;
 using Azure.Storage.Blobs.Specialized;
 using Sfa.Tl.ResultsAndCertification.Common.Services.BlobStorage.Interface;
 using Sfa.Tl.ResultsAndCertification.Models.BlobStorage;
-using Sfa.Tl.ResultsAndCertification.Models.Configuration;
-using Sfa.Tl.ResultsAndCertification.Common.Helpers;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-using Azure.Identity;
 
 namespace Sfa.Tl.ResultsAndCertification.Common.Services.BlobStorage.Service
 {
     public class BlobStorageService : IBlobStorageService
     {
-        private readonly ResultsAndCertificationConfiguration _configuration;
+        private readonly IBlobContainerClientFactory _blobContainerClientFactory;
 
-        public BlobStorageService(ResultsAndCertificationConfiguration configuration)
+        public BlobStorageService(IBlobContainerClientFactory blobContainerClientFactory)
         {
-            _configuration = configuration;
+            _blobContainerClientFactory = blobContainerClientFactory;
         }
 
         public async Task UploadFileAsync(BlobStorageData blobStorageData)
         {
-            var blobClient = await GetBlobClient(blobStorageData.ContainerName, blobStorageData.SourceFilePath, blobStorageData.BlobFileName);
+            var blobClient = GetBlobClient(blobStorageData.ContainerName, blobStorageData.SourceFilePath, blobStorageData.BlobFileName);
             await blobClient.UploadAsync(blobStorageData.FileStream);
         }
 
         public async Task UploadFromByteArrayAsync(BlobStorageData blobStorageData)
         {
-            var blobClient = await GetBlobClient(blobStorageData.ContainerName, blobStorageData.SourceFilePath, blobStorageData.BlobFileName);
+            var blobClient = GetBlobClient(blobStorageData.ContainerName, blobStorageData.SourceFilePath, blobStorageData.BlobFileName);
 
             var metadata = new Dictionary<string, string>
             {
@@ -44,7 +41,7 @@ namespace Sfa.Tl.ResultsAndCertification.Common.Services.BlobStorage.Service
 
         public async Task<Stream> DownloadFileAsync(BlobStorageData blobStorageData)
         {
-            var blobClient = await GetBlobClient(blobStorageData.ContainerName, blobStorageData.SourceFilePath, blobStorageData.BlobFileName);
+            var blobClient = GetBlobClient(blobStorageData.ContainerName, blobStorageData.SourceFilePath, blobStorageData.BlobFileName);
 
             if (await blobClient.ExistsAsync())
             {
@@ -60,14 +57,14 @@ namespace Sfa.Tl.ResultsAndCertification.Common.Services.BlobStorage.Service
 
         public async Task<bool> MoveFileAsync(BlobStorageData blobStorageData)
         {
-            var sourceBlobClient = await GetBlobClient(blobStorageData.ContainerName, blobStorageData.SourceFilePath, blobStorageData.BlobFileName);
+            var sourceBlobClient = GetBlobClient(blobStorageData.ContainerName, blobStorageData.SourceFilePath, blobStorageData.BlobFileName);
 
             if (await sourceBlobClient.ExistsAsync())
             {
                 var lease = sourceBlobClient.GetBlobLeaseClient();
                 await lease.AcquireAsync(TimeSpan.FromSeconds(-1));
 
-                var destinationBlobClient = await GetBlobClient(blobStorageData.ContainerName, blobStorageData.DestinationFilePath, blobStorageData.BlobFileName);
+                var destinationBlobClient = GetBlobClient(blobStorageData.ContainerName, blobStorageData.DestinationFilePath, blobStorageData.BlobFileName);
 
                 await destinationBlobClient.StartCopyFromUriAsync(sourceBlobClient.Uri);
 
@@ -83,27 +80,16 @@ namespace Sfa.Tl.ResultsAndCertification.Common.Services.BlobStorage.Service
 
         public async Task<bool> DeleteFileAsync(BlobStorageData blobStorageData)
         {
-            var blobClient = await GetBlobClient(blobStorageData.ContainerName, blobStorageData.SourceFilePath, blobStorageData.BlobFileName);
+            var blobClient = GetBlobClient(blobStorageData.ContainerName, blobStorageData.SourceFilePath, blobStorageData.BlobFileName);
             return await blobClient.DeleteIfExistsAsync();
         }
 
-        private async Task<BlobClient> GetBlobClient(string containerName, string filePath, string fileName)
+        private BlobClient GetBlobClient(string containerName, string filePath, string fileName)
         {
-            var blobContainerClient = await GetContainerAsync(containerName);
+            var blobContainerClient = _blobContainerClientFactory.Create(containerName);
             var blobFileReference = !string.IsNullOrWhiteSpace(filePath) ? $"{filePath}/{fileName}" : fileName;
             var blobClient = blobContainerClient.GetBlobClient(blobFileReference?.ToLowerInvariant());
             return blobClient;
-        }
-
-        private async Task<BlobContainerClient> GetContainerAsync(string containerName)
-        {
-            string fullContainerName = $"https://s126d01resacdevstr.blob.core.windows.net/{containerName.ToLower()}";
-            var containerUri = new Uri(fullContainerName);
-
-            var containerClient = new BlobContainerClient(containerUri, new DefaultAzureCredential());
-            await containerClient.CreateIfNotExistsAsync();
-
-            return containerClient;
         }
     }
 }
