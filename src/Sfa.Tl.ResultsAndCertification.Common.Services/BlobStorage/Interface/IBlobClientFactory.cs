548404720c78673fc17f1b01ba79c632bbe13737
Ibai Gonzalez <Ibai.GONZALEZ@EDUCATION.GOV.UK>
2024-10-16 10:17:44 +0100

    Code refactor

---
+++ src/Sfa.Tl.ResultsAndCertification.Common.Services/BlobStorage/Interface/IBlobClientFactory.cs
@@ -0,0 +1,10 @@
+﻿using Azure.Storage.Blobs;
+
+namespace Sfa.Tl.ResultsAndCertification.Common.Services.BlobStorage.Interface
+{
+    public interface IBlobClientFactory
+    {
+        BlobClient Create(string containerName, string blobName);
+        BlobClient Create(string containerName, string filePath, string fileName);
+    }
+}
--- src/Sfa.Tl.ResultsAndCertification.Common.Services/BlobStorage/Interface/IBlobContainerClientFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using Azure.Storage.Blobs;
-
-namespace Sfa.Tl.ResultsAndCertification.Common.Services.BlobStorage.Interface
-{
-    public interface IBlobContainerClientFactory
-    {
-        BlobContainerClient Create(string containerName);
-    }
-}
