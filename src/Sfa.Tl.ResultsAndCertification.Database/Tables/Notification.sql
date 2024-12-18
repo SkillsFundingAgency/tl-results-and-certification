cff19d41cf6138543f94cc07684aa195f226a2dd
Ibai Gonzalez <Ibai.GONZALEZ@EDUCATION.GOV.UK>
2024-12-18 15:58:45 +0000

    Rename

--- src/Sfa.Tl.ResultsAndCertification.Database/Tables/Notification.sql
+++ src/Sfa.Tl.ResultsAndCertification.Database/Tables/Notification.sql
@@ -10,6 +10,6 @@
 	,[ModifiedOn] [datetime2](7) NULL
 	,[ModifiedBy] [nvarchar](50) NULL
 	,CONSTRAINT [PK_Notification] PRIMARY KEY ([Id])
-	,CONSTRAINT [CHK_Target] CHECK ([Target] IN (1, 2, 3))
-	,CONSTRAINT [CHK_Start_End] CHECK ([End] >= [Start])
+	,CONSTRAINT [Notification_CHK_Target] CHECK ([Target] IN (1, 2, 3))
+	,CONSTRAINT [Notification_CHK_Start_End] CHECK ([End] >= [Start])
 	)
