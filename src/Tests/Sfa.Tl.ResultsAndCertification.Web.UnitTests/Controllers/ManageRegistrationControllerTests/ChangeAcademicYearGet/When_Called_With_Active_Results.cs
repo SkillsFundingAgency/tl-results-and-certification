dcea4ca08bd504bcd1e74eb57d304e25fb92cfcc
Sajid Shafique Malik <sajid.malik@education.gov.uk>
2025-01-09 16:55:40 +0000

    feature/tl2023 819 defer learner start year on the same course (#894)

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ManageRegistrationControllerTests/ChangeAcademicYearGet/When_Called_With_Active_Results.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.Common;
+using Sfa.Tl.ResultsAndCertification.Web.UnitTests.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ManageRegistrationControllerTests.ChangeAcademicYearGet
+{
+    public class When_Called_With_Active_Results : TestSetup
+    {
+        private IActionResult Result;
+        private ChangeAcademicYearViewModel mockresult;
+
+        public override void Given()
+        {
+            mockresult = new ChangeAcademicYearViewModel
+            {
+                ProfileId = 1,
+                Uln = 1234567890,
+                AoUkprn = AoUkprn,
+                Name = "John Smith",
+                ProviderDisplayName = "Barnsley College (10000536)",
+                PathwayDisplayName = "Digital Business Services",
+                AcademicYear = 2020,
+                AcademicYearToBe = 2021,
+                HasActiveAssessmentResults = true
+            };
+
+            RegistrationLoader.GetAcademicYearsAsync().Returns(new List<AcademicYear> {
+                new AcademicYear { Year = 2020, Name = "2020/21" }
+                , new AcademicYear { Year = 2021, Name = "2021/22" } });
+
+            RegistrationLoader.GetRegistrationProfileAsync<ChangeAcademicYearViewModel>(AoUkprn, ProfileId).Returns(mockresult);
+        }
+
+        public async override Task When()
+        {
+            Result = await Controller.ChangeAcademicYearAsync(ProfileId);
+        }
+
+        [Fact]
+        public void Then_Expected_Methods_AreCalled()
+        {
+            RegistrationLoader.GetRegistrationProfileAsync<ChangeAcademicYearViewModel>(AoUkprn, ProfileId);
+        }
+
+        [Fact]
+        public void Then_Redirected_To_PageNotFound()
+        {
+            Result.ShouldBeRedirectToRouteResult(RouteConstants.CannotChangeAcademicYear, ("profileId", ProfileId));
+        }
+    }
+}
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/ManageRegistrationControllerTests/ChangeAcademicYearPost/When_ModelState_Invalid.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.Common;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.Registration.Manual;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.ManageRegistrationControllerTests.ChangeAcademicYearPost
+{
+    public class When_ModelState_Invalid : TestSetup
+    {
+        protected ChangeAcademicYearViewModel MockResult = null;
+
+        public override void Given()
+        {
+            MockResult = new ChangeAcademicYearViewModel
+            {
+                ProfileId = ProfileId,
+                Uln = 1234567890,
+                AoUkprn = AoUkprn,
+                Name = "John Smith",
+                ProviderDisplayName = "Barnsley College (10000536)",
+                PathwayDisplayName = "T Level in Digital Support Services",
+                AcademicYear = 2022,
+                AcademicYearToBe = 2023,
+                AcademicYears = new List<AcademicYear>
+                {
+                    new() { Year = 2022 },
+                    new() { Year = 2023 }
+                }
+            };
+            Controller.ModelState.AddModelError(nameof(ChangeAcademicYearViewModel.AcademicYearChangeTo), Content.Registration.ChangeAcademicYear.Validation_Message);
+        }
+        public async override Task When()
+        {
+            Result = await Controller.ChangeAcademicYearAsync(MockResult);
+        }
+
+        [Fact]
+        void Then_Returns_Expected_Results()
+        {
+            Result.Should().BeOfType(typeof(ViewResult));
+
+            var viewResult = Result as ViewResult;
+            viewResult.Model.Should().BeOfType(typeof(ChangeAcademicYearViewModel));
+
+            var model = viewResult.Model as ChangeAcademicYearViewModel;
+
+            model.Should().NotBeNull();
+            model.ProfileId.Should().Be(MockResult.ProfileId);
+            model.Uln.Should().Be(MockResult.Uln);
+            model.Name.Should().Be(MockResult.Name);
+            model.ProviderDisplayName.Should().Be(MockResult.ProviderDisplayName);
+            model.PathwayDisplayName.Should().Be(MockResult.PathwayDisplayName);
+            model.AoUkprn.Should().Be(MockResult.AoUkprn);
+            model.AcademicYear.Should().Be(MockResult.AcademicYear);
+            model.AcademicYearToBe.Should().Be(MockResult.AcademicYearToBe);
+            Controller.ViewData.ModelState.Should().ContainSingle();
+            Controller.ViewData.ModelState.ContainsKey(nameof(ChangeAcademicYearViewModel.AcademicYearChangeTo)).Should().BeTrue();
+
+            var modelState = Controller.ViewData.ModelState[nameof(ChangeAcademicYearViewModel.AcademicYearChangeTo)];
+            modelState.Errors[0].ErrorMessage.Should().Be(Content.Registration.ChangeAcademicYear.Validation_Message);
+        }
+    }
+}
