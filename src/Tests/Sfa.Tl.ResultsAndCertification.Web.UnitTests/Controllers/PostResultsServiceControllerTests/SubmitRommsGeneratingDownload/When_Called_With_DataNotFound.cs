53f8730f8a992f533569459bf735dd08b1ab283e
Sajid Shafique Malik <sajid.malik@education.gov.uk>
2025-02-03 12:26:27 +0000

    unit tests

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/PostResultsServiceControllerTests/SubmitRommsGeneratingDownload/When_Called_With_DataNotFound.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Models.Contracts.DataExport;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.PostResultsService;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.PostResultsServiceControllerTests.SubmitRommsGeneratingDownload
+{
+    public class When_Called_With_DataNotFound : TestSetup
+    {
+        public override void Given()
+        {
+            DataExportResponse response = new()
+            {
+                IsDataFound = false
+            };
+
+            PostResultsServiceLoader.GenerateRommsDataExportAsync(AoUkprn, UserEmail).Returns(new List<DataExportResponse> { response });
+        }
+
+        [Fact]
+        public void Then_Redirected_To_PostResultsServicesNoRecordsFound()
+        {
+            var actualRouteName = (Result as RedirectToRouteResult).RouteName;
+            actualRouteName.Should().Be(RouteConstants.RommsNoRecordsFound);
+        }
+
+        [Fact]
+        public void Then_Expected_Method_Is_Called()
+        {
+            CacheService.DidNotReceive().SetAsync(
+                Arg.Any<string>(),
+                Arg.Any<RommsNoRecordsFoundViewModel>(),
+                Arg.Any<CacheExpiryTime>());
+        }
+    }
+}
