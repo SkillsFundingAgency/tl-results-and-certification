045a8dd9836f8f2a8d33bfcdf6e05bc4882763ec
Chris L <9040571+defc0nONE@users.noreply.github.com>
2025-06-11 13:04:22 +0100

    wip - red test

--- src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/AdminDashboard/LevelTwoResults/AdminChangeResultsViewModel.cs
+++ src/Sfa.Tl.ResultsAndCertification.Web/ViewModel/AdminDashboard/LevelTwoResults/AdminChangeResultsViewModel.cs
@@ -1,7 +1,78 @@
-﻿namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.AdminDashboard.LevelTwoResults
+﻿using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.Content.AdminDashboard;
+using Sfa.Tl.ResultsAndCertification.Web.ViewComponents.BackLink;
+using Sfa.Tl.ResultsAndCertification.Web.ViewComponents.Summary.SummaryItem;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.ViewModel.AdminDashboard.LevelTwoResults
 {
     public class AdminChangeResultsViewModel
     {
+        public int RegistrationPathwayId { get; set; }
+        public string LearnerName { get; set; }
+        public int Uln { get; set; }
+        public string Provider { get; set; }
+        public string TlevelName { get; set; }
+        public int AcademicYear { get; set; }
+        public string StartYear { get; set; }
+        public SubjectStatus? MathsStatus { get; set; }
+        public bool IsLearnerRegisteredFourYearsAgo => DateTime.Now.Year - AcademicYear > 4;
 
+        public SummaryItemModel SummaryLearner => new()
+        {
+            Id = "learner",
+            Title = AdminChangeLevelTwoMaths.Title_Learner_Text,
+            Value = LearnerName
+        };
+
+        public SummaryItemModel SummaryULN => new()
+        {
+            Id = "uln",
+            Title = AdminChangeLevelTwoMaths.Title_ULN_Text,
+            Value = Uln.ToString()
+        };
+
+        public SummaryItemModel SummaryProvider => new()
+        {
+            Id = "provider",
+            Title = AdminChangeLevelTwoMaths.Title_Provider_Text,
+            Value = Provider
+        };
+
+        public SummaryItemModel SummaryTlevel => new()
+        {
+            Id = "tlevel",
+            Title = AdminChangeLevelTwoMaths.Title_TLevel_Text,
+            Value = TlevelName
+        };
+
+        public SummaryItemModel SummaryAcademicYear => new()
+        {
+            Id = "academicyear",
+            Title = AdminChangeLevelTwoMaths.Title_StartYear_Text,
+            Value = StartYear
+        };
+
+        public SummaryItemModel SummaryMathsStatus => new()
+        {
+            Id = "mathsstatus",
+            Title = AdminChangeLevelTwoMaths.Title_Maths_Status,
+            Value = GetSubjectStatusDisplayText(MathsStatus)
+        };
+
+        public BackLinkModel BackLink => new()
+        {
+            RouteName = RouteConstants.AdminLearnerRecord,
+            RouteAttributes = new Dictionary<string, string> { { Constants.PathwayId, RegistrationPathwayId.ToString() } }
+        };
+
+        public string GetSubjectStatusDisplayText(SubjectStatus? mathsStatus) => mathsStatus switch
+        {
+            SubjectStatus.NotAchieved => AdminChangeLevelTwoMaths.Status_NotAchieved_Text,
+            SubjectStatus.NotAchievedByLrs => AdminChangeLevelTwoMaths.Status_NotAchievedByLrs_Text
+        };
     }
 }
---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Web.UnitTests/Controllers/AdminDashboardControllerTests/ChangeLevelTwoMathsGet/TestSetup.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Sfa.Tl.ResultsAndCertification.Common.Enum;
+using Sfa.Tl.ResultsAndCertification.Common.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.Content.AdminDashboard;
+using Sfa.Tl.ResultsAndCertification.Web.UnitTests.Helpers;
+using Sfa.Tl.ResultsAndCertification.Web.ViewModel.AdminDashboard.LevelTwoResults;
+using System.Threading.Tasks;
+
+namespace Sfa.Tl.ResultsAndCertification.Web.UnitTests.Controllers.AdminDashboardControllerTests.ChangeLevelTwoMathsGet
+{
+    public abstract class TestSetup : AdminDashboardControllerTestBase
+    {
+        protected const int RegistrationPathwayId = 999;
+        protected IActionResult Result { get; private set; }
+
+        protected static AdminChangeResultsViewModel ViewModel 
+            => new()
+            {
+                RegistrationPathwayId = RegistrationPathwayId,
+                LearnerName = "Kevin Smith",
+                Uln = 1234567890,
+                Provider = "Barnsley College (10000536)",
+                TlevelName = "Education and Early Years",
+                AcademicYear = 2020,
+                StartYear = "2021 to 2022",
+                MathsStatus = SubjectStatus.NotAchieved
+            };
+
+        public async override Task When()
+        {
+            Result = await Controller.AdminChangeIndustryPlacementAsync(RegistrationPathwayId);
+        }
+
+        protected void AssertViewResult()
+        {
+            var model = Result.ShouldBeViewResult<AdminChangeResultsViewModel>();
+            var changeResultsModel = ViewModel;
+            model.RegistrationPathwayId.Should().Be(changeResultsModel.RegistrationPathwayId);
+            model.Uln.Should().Be(changeResultsModel.Uln);
+            model.LearnerName.Should().Be(changeResultsModel.LearnerName);
+            model.Provider.Should().Be(changeResultsModel.Provider);
+            model.TlevelName.Should().Be(changeResultsModel.TlevelName);
+            model.MathsStatus.Should().Be(changeResultsModel.MathsStatus);
+
+            model.RegistrationPathwayId.Should().Be(changeResultsModel.RegistrationPathwayId);
+            model.Uln.Should().Be(changeResultsModel.Uln);
+            model.LearnerName.Should().Be(changeResultsModel.LearnerName);
+            model.Provider.Should().Be(changeResultsModel.Provider);
+            model.TlevelName.Should().Be(changeResultsModel.TlevelName);
+            model.MathsStatus.Should().Be(changeResultsModel.MathsStatus);
+
+            // Learner
+            model.SummaryLearner.Title.Should().Be(AdminChangeLevelTwoMaths.Title_Learner_Text);
+            model.SummaryLearner.Value.Should().Be(changeResultsModel.LearnerName);
+
+            model.SummaryULN.Title.Should().Be(AdminChangeLevelTwoMaths.Title_ULN_Text);
+            model.SummaryULN.Value.Should().Be(changeResultsModel.Uln.ToString());
+
+            model.SummaryProvider.Title.Should().Be(AdminChangeLevelTwoMaths.Title_Provider_Text);
+            model.SummaryProvider.Value.Should().Be(changeResultsModel.Provider);
+
+            model.SummaryTlevel.Title.Should().Be(AdminChangeLevelTwoMaths.Title_TLevel_Text);
+            model.SummaryTlevel.Value.Should().Be(changeResultsModel.TlevelName);
+
+            model.SummaryAcademicYear.Title.Should().Be(AdminChangeLevelTwoMaths.Title_StartYear_Text);
+            model.SummaryAcademicYear.Value.Should().Be(changeResultsModel.StartYear);
+
+            model.SummaryMathsStatus.Title.Should().Be(AdminChangeLevelTwoMaths.Title_Maths_Status);
+            model.SummaryMathsStatus.Value.Should().Be(changeResultsModel.GetSubjectStatusDisplayText(changeResultsModel.MathsStatus));
+
+            model.BackLink.Should().NotBeNull();
+            model.BackLink.RouteName.Should().Be(RouteConstants.AdminLearnerRecord);
+        }
+    }
+}
