8ec20dc1074b95253aef9fea96f625c7f7fd36f6
Sajid Shafique Malik <sajid.malik@education.gov.uk>
2025-06-19 15:45:26 +0100

    merge Release23.6 into develop (Release23.6) (#971)
    * update extract filename
    
    * content update
    
    * excluded withdrawn learners

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Application.UnitTests/Services/IndustryPlacementNotificationServiceTests/IndustryPlacementNotificationServiceBaseTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using Notify.Interfaces;
+using NSubstitute;
+using Sfa.Tl.ResultsAndCertification.Api.Client.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Application.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Application.Services;
+using Sfa.Tl.ResultsAndCertification.Data.Interfaces;
+using Sfa.Tl.ResultsAndCertification.Domain.Models;
+using Sfa.Tl.ResultsAndCertification.Tests.Common.BaseTest;
+
+namespace Sfa.Tl.ResultsAndCertification.Application.UnitTests.Services.IndustryPlacementNotificationServiceTests
+{
+    public abstract class IndustryPlacementNotificationServiceBaseTest : BaseTest<IndustryPlacementNotificationService>
+    {
+        protected IRepository<TqRegistrationPathway> RegistrationPathwayRepository;
+        protected IRepository<NotificationTemplate> NotificationTemplateRepository;
+        protected IRepository<TlProvider> ProviderRepository;
+        protected IDfeSignInApiClient DfeSignInApiClient;
+        protected INotificationService NotificationService;
+        protected IAsyncNotificationClient NotificationClient;
+        protected ICommonRepository CommonRepository;
+        protected IndustryPlacementNotificationService IndustryPlacementNotificationService;
+        ILogger<IndustryPlacementNotificationService> Logger;
+
+        public override void Setup()
+        {
+            RegistrationPathwayRepository = Substitute.For<IRepository<TqRegistrationPathway>>();
+            NotificationTemplateRepository = Substitute.For<IRepository<NotificationTemplate>>();
+            ProviderRepository = Substitute.For<IRepository<TlProvider>>();
+            DfeSignInApiClient = Substitute.For<IDfeSignInApiClient>();
+            NotificationService = Substitute.For<INotificationService>();
+            NotificationClient = Substitute.For<IAsyncNotificationClient>();
+            CommonRepository = Substitute.For<ICommonRepository>();
+            Logger = Substitute.For<ILogger<IndustryPlacementNotificationService>>();
+
+            IndustryPlacementNotificationService = new(
+                    RegistrationPathwayRepository,
+                    NotificationTemplateRepository,
+                    ProviderRepository,
+                    CommonRepository,
+                    DfeSignInApiClient,
+                    NotificationService,
+                    NotificationClient,
+                    Logger);
+        }
+    }
+}
