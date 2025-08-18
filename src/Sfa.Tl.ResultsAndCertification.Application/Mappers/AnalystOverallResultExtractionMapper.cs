e021b3c0f2bd8515049693bca2814691085d0a99
Chris L <9040571+defc0nONE@users.noreply.github.com>
2025-08-18 03:44:18 +0100

    Create new mapper converter for the highest attained Core assessment series

--- src/Sfa.Tl.ResultsAndCertification.Application/Mappers/AnalystOverallResultExtractionMapper.cs
+++ src/Sfa.Tl.ResultsAndCertification.Application/Mappers/AnalystOverallResultExtractionMapper.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using Sfa.Tl.ResultsAndCertification.Application.Mappers.Action;
 using Sfa.Tl.ResultsAndCertification.Application.Mappers.Converter;
+using Sfa.Tl.ResultsAndCertification.Application.Mappers.Converter.HighestAttainedCoreSeries;
 using Sfa.Tl.ResultsAndCertification.Application.Mappers.Converter.IndustryPlacement;
 using Sfa.Tl.ResultsAndCertification.Application.Mappers.Converter.PathwayResult;
 using Sfa.Tl.ResultsAndCertification.Application.Mappers.Converter.Specialism;
@@ -32,6 +33,7 @@ namespace Sfa.Tl.ResultsAndCertification.Application.Mappers
                 .ForMember(d => d.CoreComponent, opts => opts.ConvertUsing(new DoubleQuotedStringConverter(), s => s.TqProvider.TqAwardingOrganisation.TlPathway.Name))
                 .ForMember(d => d.CoreCode, opts => opts.MapFrom(s => s.TqProvider.TqAwardingOrganisation.TlPathway.LarId))
                 .ForMember(d => d.CoreResult, opts => opts.ConvertUsing(new PathwayResultStringConverter(), p => p.TqPathwayAssessments))
+                .ForMember(d => d.HighestAttainedCoreSeries, opts => opts.ConvertUsing(new HighestAttainedCoreSeriesConverter(), p => p.TqPathwayAssessments))
                 .ForMember(d => d.OccupationalSpecialism, opts => opts.ConvertUsing(new SpecialismNameConverter(), p => p.TqRegistrationSpecialisms))
                 .ForMember(d => d.SpecialismCode, opts => opts.ConvertUsing(new SpecialismCodeConverter(), p => p.TqRegistrationSpecialisms))
                 .ForMember(d => d.SpecialismResult, opts => opts.MapFrom(s => GetSpecialismResultAwarded(s)))
