c5b87c8968b3f0f7835d329acfe5e14816be3cde
Chris L <9040571+defc0nONE@users.noreply.github.com>
2025-08-18 04:41:54 +0100

    Add unit tests for highest attained specialism series

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Application.UnitTests/Mapper/Converter/HighestAttainedSpecialismSeries/HighestAttainedSpecialismSeriesConverter/When_Specialism_Assessment_Null.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Sfa.Tl.ResultsAndCertification.Application.UnitTests.Mapper.Converter.HighestAttainedSpecialismSeries.HighestAttainedSpecialismSeriesConverter
+{
+    internal class When_Specialism_Assessment_Null : TestSetup
+    {
+        public override void Given()
+        {
+            Source = null;
+        }
+
+        public void Then_Return_Empty()
+        {
+            Result.Should().BeEmpty();
+        }
+    }
+}
