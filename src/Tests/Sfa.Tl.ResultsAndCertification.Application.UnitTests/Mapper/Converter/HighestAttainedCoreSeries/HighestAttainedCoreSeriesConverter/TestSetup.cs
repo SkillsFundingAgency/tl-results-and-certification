9108f2a52585b6d6a2da0818c35608791889b275
Chris <9040571+defc0nONE@users.noreply.github.com>
2025-08-20 11:48:37 +0100

    TL2023-1056: Add highest attained Core/Specialism assessment series to overall extract (#986)
    * Create new mapper converter for the highest attained Core assessment series
    * Add new member and header to extraction to display new core assessment series
    * Add query parameters in GetRegistrationPathways to get relevant data for Core (and future Specialism) assessment series data
    * Add action to convert text to null, add unit tests
    * Begin to add functionality for highest specialism assessment series
    * Add unit tests for highest attained specialism series
    * Rename highest attained core series tests with more clairvoyant naming

---
+++ src/Tests/Sfa.Tl.ResultsAndCertification.Application.UnitTests/Mapper/Converter/HighestAttainedCoreSeries/HighestAttainedCoreSeriesConverter/TestSetup.cs
@@ -0,0 +1,9 @@
+﻿using Sfa.Tl.ResultsAndCertification.Application.UnitTests.Mapper.Converter.HighestAttainedCoreSeries;
+using CoreSeriesConverter = Sfa.Tl.ResultsAndCertification.Application.Mappers.Converter.HighestAttainedCoreSeries.HighestAttainedCoreSeriesConverter;
+
+namespace Sfa.Tl.ResultsAndCertification.Application.UnitTests.Mapper.Converter.HighestAttainedCoreSeries.HighestAttainedCoreSeriesConverter
+{
+    public abstract class TestSetup : HighestAttainedCoreSeriesBaseTest<CoreSeriesConverter, string>
+    {
+    }
+}
